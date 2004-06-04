aa405bef14469e612d692ce59052ce279523c6f1
Matthias Machacek <matthias.machacek@hispeed.ch>
2004-06-04 18:17:53 +0000

    Reverted back to the original version because multidimensional cases gave a error message.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@630 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/emClustering/multinorm.m
+++ mathfun/emClustering/multinorm.m
@@ -3,23 +3,17 @@ function y = multinorm(x,m,covar)
 % of mean m and covariance matrix covar
 % at the array of points x
 %
-
-% make sure we get a reasonable warning here if we get bad input
-if isnan(m) | isnan(covar) | isnan(x)
-    y = repmat(NaN,size(x));
-    warning('MULTINORM:nanInput','at least one of the input arguments for multinorm is NaN!');
+[dim npoints] = size(x);
+dd = det(covar+ realmin*eye(dim));
+in = inv(covar+ realmin*eye(dim));
+ff = ((2*pi)^(-dim/2))*((dd)^(-0.5));
+quadform = zeros(1,npoints);
+centered = (x-m*ones(1,npoints));
+if dim ~= 1
+   y = ff * exp(-0.5*sum(centered.*(in*centered)));
 else
-    [dim npoints] = size(x);
-    dd = det(covar+ 100*realmin*eye(dim)); % you can not invert realmin!
-    in = inv(covar+ 100*realmin*eye(dim)); % therefore multiply by 100
-    ff = ((2*pi)^(-dim/2))*((dd)^(-0.5));
-    quadform = zeros(1,npoints);
-    centered = (x-m*ones(1,npoints));
-    if dim ~= 1
-        y = ff * exp(-0.5*sum(centered.*(in*centered)));
-    else
-        y = ff * exp(-0.5*in*centered.^2 );
-    end
+   y = ff * exp(-0.5*in*centered.^2 );
 end
+
 
 
