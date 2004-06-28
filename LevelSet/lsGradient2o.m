e0eb78ab9fd98f3ecc69afd1618ab699ee44c95a
Matthias Machacek <matthias.machacek@hispeed.ch>
2004-06-28 17:19:54 +0000

    Changed the x, y directions. Now index i corresponds to the y direction.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@690 0bcb941b-74d1-0310-bb26-e6636707f451

--- LevelSet/lsGradient2o.m
+++ LevelSet/lsGradient2o.m
@@ -33,43 +33,43 @@ for i=1:i_end
    for j=1:j_end
       if i < 3
          % assume that the value at i=1 is the same as at i=2
-         Dx_minus(i,j)          =  (phi(i+1,j)-  phi(i  ,j)           )  / delta_x;
-         Dx_plus(i,j)           =  (phi(i+1,j)-  phi(i  ,j)           )  / delta_x;
-         Dx_minus_x_minus(i,j)  =  (phi(i+2,j)-2*phi(i+1,j)+phi(i,j)  )  / delta_x^2;
-         Dx_plus_x_plus(i,j)    =  (phi(i+2,j)-2*phi(i+1,j)+phi(i,j)  )  / delta_x^2;
-         Dx_plus_x_minus(i,j)   =  (phi(i+2,j)-2*phi(i+1,j)+phi(i,j)  )  / delta_x^2;         
+         Dy_minus(i,j)          =  (phi(i+1,j)-  phi(i  ,j)           )  / delta_y;
+         Dy_plus(i,j)           =  (phi(i+1,j)-  phi(i  ,j)           )  / delta_y;
+         Dy_minus_y_minus(i,j)  =  (phi(i+2,j)-2*phi(i+1,j)+phi(i,j)  )  / delta_y^2;
+         Dy_plus_y_plus(i,j)    =  (phi(i+2,j)-2*phi(i+1,j)+phi(i,j)  )  / delta_y^2;
+         Dy_plus_y_minus(i,j)   =  (phi(i+2,j)-2*phi(i+1,j)+phi(i,j)  )  / delta_y^2;         
       elseif i > i_end-3
-         Dx_minus(i,j)          =  (phi(i  ,j)-  phi(i-1,j)             )  / delta_x;
-         Dx_plus(i,j)           =  (phi(i  ,j)-  phi(i-1,j)             )  / delta_x;
-         Dx_minus_x_minus(i,j)  =  (phi(i  ,j)-2*phi(i-1,j)+phi(i-2,j)  )  / delta_x^2;
-         Dx_plus_x_plus(i,j)    =  (phi(i  ,j)-2*phi(i-1,j)+phi(i-2,j)  )  / delta_x^2;
-         Dx_plus_x_minus(i,j)   =  (phi(i  ,j)-2*phi(i-1,j)+phi(i-2,j)  )  / delta_x^2;       
+         Dy_minus(i,j)          =  (phi(i  ,j)-  phi(i-1,j)             )  / delta_y;
+         Dy_plus(i,j)           =  (phi(i  ,j)-  phi(i-1,j)             )  / delta_y;
+         Dy_minus_y_minus(i,j)  =  (phi(i  ,j)-2*phi(i-1,j)+phi(i-2,j)  )  / delta_y^2;
+         Dy_plus_y_plus(i,j)    =  (phi(i  ,j)-2*phi(i-1,j)+phi(i-2,j)  )  / delta_y^2;
+         Dy_plus_y_minus(i,j)   =  (phi(i  ,j)-2*phi(i-1,j)+phi(i-2,j)  )  / delta_y^2;       
       else
-         Dx_minus(i,j)          =  (phi(i  ,j)-  phi(i-1,j)             )  / delta_x;
-         Dx_plus(i,j)           =  (phi(i+1,j)-  phi(i  ,j)             )  / delta_x;
-         Dx_minus_x_minus(i,j)  =  (phi(i  ,j)-2*phi(i-1,j)+phi(i-2,j)  )  / delta_x^2;
-         Dx_plus_x_plus(i,j)    =  (phi(i+2,j)-2*phi(i+1,j)+phi(i  ,j)  )  / delta_x^2;
-         Dx_plus_x_minus(i,j)   =  (phi(i+1,j)-2*phi(i  ,j)+phi(i-1,j)  )  / delta_x^2;      
+         Dy_minus(i,j)          =  (phi(i  ,j)-  phi(i-1,j)             )  / delta_y;
+         Dy_plus(i,j)           =  (phi(i+1,j)-  phi(i  ,j)             )  / delta_y;
+         Dy_minus_y_minus(i,j)  =  (phi(i  ,j)-2*phi(i-1,j)+phi(i-2,j)  )  / delta_y^2;
+         Dy_plus_y_plus(i,j)    =  (phi(i+2,j)-2*phi(i+1,j)+phi(i  ,j)  )  / delta_y^2;
+         Dy_plus_y_minus(i,j)   =  (phi(i+1,j)-2*phi(i  ,j)+phi(i-1,j)  )  / delta_y^2;      
       end
       
       if j < 3
-         Dy_minus(i,j)          =  (phi(i,j+1)-  phi(i,j  )             )  / delta_y;
-         Dy_plus(i,j)           =  (phi(i,j+1)-  phi(i,j  )             )  / delta_y;
-         Dy_minus_y_minus(i,j)  =  (phi(i,j+2)-2*phi(i,j+1)+phi(i,j  )  )  / delta_y^2;
-         Dy_plus_y_plus(i,j)    =  (phi(i,j+2)-2*phi(i,j+1)+phi(i,j  )  )  / delta_y^2;
-         Dy_plus_y_minus(i,j)   =  (phi(i,j+2)-2*phi(i,j+1)+phi(i,j  )  )  / delta_y^2;        
+         Dx_minus(i,j)          =  (phi(i,j+1)-  phi(i,j  )             )  / delta_x;
+         Dx_plus(i,j)           =  (phi(i,j+1)-  phi(i,j  )             )  / delta_x;
+         Dx_minus_x_minus(i,j)  =  (phi(i,j+2)-2*phi(i,j+1)+phi(i,j  )  )  / delta_x^2;
+         Dx_plus_x_plus(i,j)    =  (phi(i,j+2)-2*phi(i,j+1)+phi(i,j  )  )  / delta_x^2;
+         Dx_plus_x_minus(i,j)   =  (phi(i,j+2)-2*phi(i,j+1)+phi(i,j  )  )  / delta_x^2;        
       elseif j > j_end-3
-         Dy_minus(i,j)          =  (phi(i,j  )-  phi(i,j-1)             )  / delta_y;
-         Dy_plus(i,j)           =  (phi(i,j  )-  phi(i,j-1)             )  / delta_y;
-         Dy_minus_y_minus(i,j)  =  (phi(i,j  )-2*phi(i,j-1)+phi(i,j-2)  )  / delta_y^2;
-         Dy_plus_y_plus(i,j)    =  (phi(i,j  )-2*phi(i,j-1)+phi(i,j-2)  )  / delta_y^2;
-         Dy_plus_y_minus(i,j)   =  (phi(i,j  )-2*phi(i,j-1)+phi(i,j-2)  )  / delta_y^2;    
+         Dx_minus(i,j)          =  (phi(i,j  )-  phi(i,j-1)             )  / delta_x;
+         Dx_plus(i,j)           =  (phi(i,j  )-  phi(i,j-1)             )  / delta_x;
+         Dx_minus_x_minus(i,j)  =  (phi(i,j  )-2*phi(i,j-1)+phi(i,j-2)  )  / delta_x^2;
+         Dx_plus_x_plus(i,j)    =  (phi(i,j  )-2*phi(i,j-1)+phi(i,j-2)  )  / delta_x^2;
+         Dx_plus_x_minus(i,j)   =  (phi(i,j  )-2*phi(i,j-1)+phi(i,j-2)  )  / delta_x^2;    
       else
-         Dy_minus(i,j)          =  (phi(i,j  )-  phi(i,j-1)             )  / delta_y;
-         Dy_plus(i,j)           =  (phi(i,j+1)-  phi(i,j  )             )  / delta_y;
-         Dy_minus_y_minus(i,j)  =  (phi(i,j  )-2*phi(i,j-1)+phi(i,j-2)  )  / delta_y^2;
-         Dy_plus_y_plus(i,j)    =  (phi(i,j+2)-2*phi(i,j+1)+phi(i,j  )  )  / delta_y^2;
-         Dy_plus_y_minus(i,j)   =  (phi(i,j+1)-2*phi(i,j  )+phi(i,j-1)  )  / delta_y^2;
+         Dx_minus(i,j)          =  (phi(i,j  )-  phi(i,j-1)             )  / delta_x;
+         Dx_plus(i,j)           =  (phi(i,j+1)-  phi(i,j  )             )  / delta_x;
+         Dx_minus_x_minus(i,j)  =  (phi(i,j  )-2*phi(i,j-1)+phi(i,j-2)  )  / delta_x^2;
+         Dx_plus_x_plus(i,j)    =  (phi(i,j+2)-2*phi(i,j+1)+phi(i,j  )  )  / delta_x^2;
+         Dx_plus_x_minus(i,j)   =  (phi(i,j+1)-2*phi(i,j  )+phi(i,j-1)  )  / delta_x^2;
       end
    end
 end
