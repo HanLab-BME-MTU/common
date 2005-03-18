067f874f650142d1100765d261e34e6e61a5be87
Jonas Dorn <jonas.dorn@gmail.com>
2005-03-18 21:44:56 +0000

    bugfix for discrete data
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@1413 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/histogram.m
+++ graphics/histogram.m
@@ -21,13 +21,26 @@ if nargin < 2 || isempty(factor)
     factor = 1;
 end
 
-% create bins with the optimal bin width
-% W = 2*(IQD)*N^(-1/3)
-interQuartileDist = diff(prctile(data,[25,75]));
-binLength = 2*interQuartileDist*length(data)^(-1/3)*factor;
+nData = length(data);
+if nData < 20
+    warning('Less than 20 data points!')
+    nBins = ceil(nData/4);
+else
 
-% number of bins: divide data range by binLength
-nBins = max(3,floor((max(data)-min(data))/binLength));
+
+    % create bins with the optimal bin width
+    % W = 2*(IQD)*N^(-1/3)
+    interQuartileDist = diff(prctile(data,[25,75]));
+    binLength = 2*interQuartileDist*length(data)^(-1/3)*factor;
+
+    % number of bins: divide data range by binLength
+    nBins = floor((max(data)-min(data))/binLength);
+
+    if ~isfinite(nBins)
+        nBins = length(unique(data));
+    end
+
+end
 
 % histogram
 if nargout > 0
