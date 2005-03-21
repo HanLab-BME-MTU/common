8dd782cb8d2da8f92119f116257704a0ddccfc8d
Jonas Dorn <jonas.dorn@gmail.com>
2005-03-21 22:52:58 +0000

    discriminationMatrix: Allows the use of different tests.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@1424 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/discriminationMatrix.m
+++ mathfun/discriminationMatrix.m
@@ -1,4 +1,4 @@
-function compMatrices = discriminationMatrix(dataStructure)
+function compMatrices = discriminationMatrix(dataStructure,testStructure)
 %DISCRIMINATIONMATRIX generates discriminationMatrices comparing means and distributions of lists of values in the input data structures
 %
 % SYNOPSIS compMatrices = discriminationMatrix(dataStructure)
@@ -6,6 +6,21 @@ function compMatrices = discriminationMatrix(dataStructure)
 % INPUT    dataStructure : n-by-1 structure containing the lists of values
 %                          that are to be compared
 %                          Fieldnames can be arbitrary
+%          testStructure : (opt) 1-by-1 structure containing information on
+%                          test to be used. Fieldnames have to be exactly
+%                          the same as in dataStructure.
+%                          The fields have to contain a vector of length
+%                          two, specifying the tests to be used. The first
+%                          entry specifies the test shown below the
+%                          diagonal, the second entry specifies the test
+%                          shown above the diagonal.
+%                          1 - t-test for means (default below diag.)
+%                          2 - Wilcoxon ranksum test for medians
+%                          10 - K-S test for distributions
+%                          11 - K-S test for distributions with subtracted
+%                               mean (default for above diag.)
+%                          12 - K-S test for distributions with subtracted
+%                               median
 %
 % OUTPUT   compMatrices  : Structure with fieldnames equal to the
 %                     fieldnames of the data structure, containing
@@ -23,10 +38,29 @@ function compMatrices = discriminationMatrix(dataStructure)
 % TEST INPUT
 %=====================
 
+defaultTest = [1 11];
+% tests:
+% If you add tests, please don't forget to reference them here AND in the
+% help AND add the entry to the testList. Don't change the default.
+% 1 - t-test for means (default below diag.)
+% 2 - Wilcoxon ranksum test for medians
+% 10 - K-S test for distributions
+% 11 - K-S test for distributions with subtracted
+%       mean (default for above diag.)
+% 12 - K-S test for distributions with subtracted
+%       median
+testList = [1 2 10 11 12];
+
 % nargin
 if nargin == 0 || isempty(dataStructure) || ~isstruct(dataStructure)
     error('Please input a non-empty data structure in DISCRIMINATIONMATRIX')
 end
+if nargin < 2 || isempty(testStructure)
+    useDefaultTest = 1;
+else
+    useDefaultTest = 0;
+end
+
 
 % length structure
 nGroups = length(dataStructure);
@@ -38,19 +72,45 @@ end
 dataNames = fieldnames(dataStructure);
 nData = length(dataNames);
 
-% loop through all the data and get means (check whether empty, too)
-groupMeans = zeros(nGroups,nData);
+% loop through all the data. Get means, medians and fill testStruct
+% (check whether empty, too)
+groupMeans = zeros(nGroups,nData,2);
 for iData = 1:nData
+
     for iGroup = 1:nGroups
         if ~isempty(dataStructure(iGroup).(dataNames{iData}))
-            
-            groupMeans(iGroup,iData) = ...
+
+            groupMeans(iGroup,iData,1) = ...
                 mean(dataStructure(iGroup).(dataNames{iData}));
+            groupMeans(iGroup,iData,2) = ...
+                median(dataStructure(iGroup).(dataNames{iData}));
+
+
         else
             error('no empty entries are allowed in the dataStructure')
         end
     end
+
+    % select tests
+    if useDefaultTest
+        testStructure.(dataNames{iData}) = defaultTest;
+    else
+        % first: make sure there is a field of the right name.
+        % Then make sure two tests are given.
+        % Finally, make sure they are on the testList
+        if isfield(testStructure,dataNames{iData})
+            if length(testStructure.(dataNames{iData}))==2 && ...
+                    all(ismember(testStructure.(dataNames{iData}),testList))
+                % all is good.
+            else
+                error('You either didn''t specify two tests, or you specified an invalid test')
+            end
+        else
+            testStructure.(dataNames{iData}) = defaultTest;
+        end
+    end
 end
+
 
 %======================
 
@@ -71,20 +131,49 @@ for iData = 1:nData
 
     for gi = 1:nGroups
         for gj = gi-1:-1:1
-            % compare mean:  ttest. Below diagonal
-            [dummy,pValue] = ...
-                ttest2(...
-                dataStructure(gi).(dataNames{iData}),...
+
+            % test 1: below diagonal
+            pValue = test(dataStructure(gi).(dataNames{iData}),...
                 dataStructure(gj).(dataNames{iData}),...
-                0.05,'both','unequal');
+                testStructure.(dataNames{iData})(1),...
+                groupMeans([gi,gj],iData,:));
             compMatrices.(dataNames{iData})(gi,gj) = pValue;
-            % compare distribution (shifted data): kolmogorov-smirnov test.
-            % Above diagonal
-            [dummy,pValue] = ...
-                kstest2(...
-                dataStructure(gi).(dataNames{iData})-groupMeans(gi,iData),...
-                dataStructure(gj).(dataNames{iData})-groupMeans(gj,iData));
+
+            % test 2: above diagonal
+            pValue = test(dataStructure(gi).(dataNames{iData}),...
+                dataStructure(gj).(dataNames{iData}),...
+                testStructure.(dataNames{iData})(1),...
+                groupMeans([gi,gj],iData,:));
             compMatrices.(dataNames{iData})(gj,gi) = pValue;
+
         end
     end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%  SUBFUNCTION TEST    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function pValue = test(data1, data2, whichTest, groupMeans)
+% groupMeans is 2-by-1-by-2; (:,:,1) is the mean, (:,:,2) is the median
+
+switch whichTest
+    case 1
+        % compare mean:  ttest. Below diagonal
+        [dummy,pValue] = ...
+            ttest2(data1, data2, 0.05,'both','unequal');
+    case 2
+        % compare median: Wilcoxon Mann Whitney ranksum test
+        pValue = ranksum(data1, data2);
+    case 10
+        % compare distributions: KS-test. No correction
+        [dummy,pValue] = ...
+            kstest2(data1, data2);
+    case 11
+        % compare distributions: KS-test. Subtract means
+        [dummy,pValue] = ...
+            kstest2(data1-groupMeans(1,1,1), data2-groupMeans(2,1,1));
+    case 12
+        % compare distributions: KS-test. Subtract medians
+        [dummy,pValue] = ...
+            kstest2(data1-groupMeans(1,1,2), data2-groupMeans(2,1,2));
 end
