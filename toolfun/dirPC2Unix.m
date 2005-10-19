40fd996e00fde389ed846f87e24e96aa8cfb6c85
lji <lji@scripps.edu>
2005-10-19 16:52:37 +0000

    feature modification.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@1748 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/dirPC2Unix.m
+++ toolfun/dirPC2Unix.m
@@ -1,29 +1,65 @@
-function outImgDir = dirPC2Unix(inImgDir,imgDrive)
+function outDir = dirPC2Unix(inDir,unixDrive)
 %dirPC2Unix: Convert image directories from PC format to Unix format.
 %
+% SYNOPSIS:
+%    outDir = dirPC2Unix(inDir,unixDrive)
+%
 % INPUT:
-%    inImgDir: Image directories in PC format. It can be a cell array.
-%    imgDrive: The disk drive name for the image directory in Unix format.
+%    inDir     : Image directories in PC format. It can be a cell array.
+%    unixDrive : The name of the mounted disk drive in Unix. It can be a cell
+%                array. In this case, the length has to match 'inDir' cell
+%                array.
 %
 % See also: dirUnix2PC.
+% Author: Lin Ji, Mar, 2005
 
-if ~strcmp(imgDrive(1),'/')
-   imgDrive = ['/' imgDrive];
-end
-
-if ~iscell(inImgDir)
-   outImgDir = {inImgDir};
+if ~iscell(inDir)
+   outDir = {inDir};
 else
-   outImgDir = inImgDir;
+   outDir = inDir;
 end
 
-for k = 1:length(outImgDir)
-   fileSepInd = findstr('\',outImgDir{k});
-   outImgDir{k}(fileSepInd) = '/';
+unixDriveList = cell(size(outDir));
 
-   outImgDir{k} = [imgDrive outImgDir{k}(fileSepInd(1):end)];
+if ~iscell(unixDrive)
+   for k = 1:length(outDir)
+      unixDriveList{k} = unixDrive;
+   end
+elseif length(unixDrive) == 1
+   for k = 1:length(outDir)
+      unixDriveList{k} = unixDrive{1};
+   end
+else
+   unixDriveList = unixDrive;
 end
 
-if ~iscell(inImgDir)
-   outImgDir = outImgDir{1};
+for k = 1:length(unixDriveList)
+   unixDriveList{k} = rmExtraFilesep(unixDriveList{k});
+
+   if ~strcmp(unixDriveList{k}(1),'/')
+      unixDriveList{k} = ['/' unixDriveList{k}];
+   end
+
+   %Remove extra '/' from the end of 'unixDrive'.
+   if strcmp(unixDriveList{k}(end),'/')
+      unixDriveList{k}(end) = '';
+   end
+end
+
+
+for k = 1:length(outDir)
+   colonInd = findstr(':',outDir{k});
+   outDir{k}(1:colonInd(1)) = '';
+
+   fileSepInd = findstr('\',outDir{k});
+   outDir{k}(fileSepInd) = '/';
+
+   outDir{k} = [unixDriveList{k} '/' outDir{k}];
+
+   %Remove extra filesep.
+   outDir{k} = rmExtraFilesep(outDir{k},'/');
+end
+
+if ~iscell(inDir)
+   outDir = outDir{1};
 end
