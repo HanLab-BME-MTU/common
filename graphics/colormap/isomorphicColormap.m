c284de7af62fec6b803c54e081382c570a261a42
Jonas Dorn <jonas.dorn@gmail.com>
2007-04-09 23:06:51 +0000

    added r/g to isomorphicColormap
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2321 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/colormap/isomorphicColormap.m
+++ graphics/colormap/isomorphicColormap.m
@@ -3,15 +3,19 @@ function colormap = isomorphicColormap(color, cLength, test)
 %
 % SYNOPSIS: colormap = isomorphicColormap(color,cLength, test)
 %
-% INPUT color (opt): 'red', 'green', 'blue', 'b/y' for r, g, b, and
-%                    blue/yellow colormaps. Default: 'green'
-%                    'bw','gw','rw' for blue, green, red going from white
-%                    to color
+% INPUT color (opt):  'red' (or 'r'), 'green' (or 'g'), 'blue' (or 'b')
+%                         for colormaps going in luminosity from
+%                         black->COLOR->white
+%                     'bw','gw','rw' for blue, green, red changing in
+%                         luminosity from b/g/r to color
+%                     'b/y' for saturation from blue->grey->yellow
+%                     'g/r' for saturation from green->grey->red
+%
 %       cLength (opt): Number of entries of the colormap. Default: 64
 %       test (opt) : if 1, colormapTest is being run with the colormap.
 %                    Default: 0
 % OUTPUT colormap: colormap (64x3 array of RGB values)
-%			
+%
 %
 % REMARKS see
 %           http://www.research.ibm.com/people/l/lloydt/color/color.htm
@@ -49,33 +53,69 @@ end
 switch color
     case {'red','r'}
         % change color, saturation a tiny bit, luminosity lots
-        cmap = hsl2rgb([[linspace(0.95,1,cLength/2)';linspace(0,0.05,cLength/2)'],...
-            linspace(0.7,0.8,cLength)',linspace(0.1,1,cLength)']);
+        hue = [linspace(0.95,1,cLength/2)';linspace(0,0.05,cLength/2)'];
+        sat = linspace(0.7,0.8,cLength)';
+        lum = linspace(0.1,1,cLength)';
+
+        cmap = hsl2rgb([hue,sat,lum]);
+
     case {'rw'}
         % same as 'r', but going from white to color
-        cmap = hsl2rgb([[linspace(0.95,1,cLength/2)';linspace(0,0.05,cLength/2)'],...
-            repmat(1,cLength,1),linspace(1,0.5,cLength)']);
+        hue = [linspace(0.95,1,cLength/2)';linspace(0,0.05,cLength/2)'];
+        sat = repmat(1,cLength,1);
+        lum = linspace(1,0.5,cLength)';
+
+        cmap = hsl2rgb([hue,sat,lum]);
+
     case {'green','g'}
         % change color, saturation a tiny bit, luminosity lots
-        cmap = hsl2rgb([linspace(0.3,0.4,cLength)',linspace(0.7,0.8,cLength)',...
-            linspace(0.1,1,cLength)']);
-        case {'gw'}
+        hue = linspace(0.3,0.4,cLength)';
+        sat = linspace(0.7,0.8,cLength)';
+        lum = linspace(0.1,1,cLength)';
+
+        cmap = hsl2rgb([hue,sat,lum]);
+
+    case {'gw'}
         % same as 'g', but going from white to color
-        cmap = hsl2rgb([linspace(0.3,0.4,cLength)',...
-            repmat(1,cLength,1),linspace(1,0.5,cLength)']);
+        hue = linspace(0.3,0.4,cLength)';
+        sat = repmat(1,cLength,1);
+        lum = linspace(1,0.5,cLength)';
+
+        cmap = hsl2rgb([hue,sat,lum]);
+
     case {'blue','b'}
         % change color, saturation a tiny bit, luminosity lots
-        cmap = hsl2rgb([linspace(0.5,0.6,cLength)',linspace(0.7,0.8,cLength)',...
-            linspace(0.1,1,cLength)']);
-        case {'bw'}
+        hue = linspace(0.5,0.6,cLength)';
+        sat = linspace(0.7,0.8,cLength)';
+        lum = linspace(0.1,1,cLength)';
+
+        cmap = hsl2rgb([hue,sat,lum]);
+
+    case {'bw'}
         % same as 'g', but going from white to color
-        cmap = hsl2rgb([linspace(0.5,0.6,cLength)',...
-            repmat(1,cLength,1),linspace(1,0.5,cLength)']);
+        hue = linspace(0.5,0.6,cLength)';
+        sat = repmat(1,cLength,1);
+        lum = linspace(1,0.5,cLength)';
+
+        cmap = hsl2rgb([hue,sat,lum]);
+
     case 'b/y'
         % color: blue->yellow, saturation 1->0->1, luminosity 0.5 (=max
         % color)
-        cmap = hsl2rgb([repeatEntries([0.66;0.16],cLength/2),[linspace(0.8,0,cLength/2)';...
-        linspace(0,0.8,cLength/2)'],ones(cLength,1)*0.50]);
+        hue = repeatEntries([0.66;0.16],cLength/2);
+        sat = [linspace(1,0,cLength/2)';linspace(0,1,cLength/2)'];
+        lum = ones(cLength,1)*0.50;
+
+        cmap = hsl2rgb([hue,sat,lum]);
+
+    case 'g/r'
+        % color: green->red, saturation 1->0->1, luminosity 0.5 (=max
+        % color)
+        hue = repeatEntries([0.33;1],cLength/2);
+        sat = [linspace(0.8,0,cLength/2)';linspace(0,0.8,cLength/2)'];
+        lum = ones(cLength,1)*0.50;
+
+        cmap = hsl2rgb([hue,sat,lum]);
     otherwise
         error('color %s not implemented yet',color)
 end
