30fc7cd0b5a70def2ad10f9c2296c4962c852c6f
Khuloud Jaqaman <khuloud_jaqaman@hms.harvard.edu>
2007-08-02 19:52:42 +0000

    -modified convStruct2MatNoMS to convert feature indices in addition to positions and amplitude.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2452 0bcb941b-74d1-0310-bb26-e6636707f451

--- trackWithGapClosing/postprocessing/convStruct2MatNoMS.m
+++ trackWithGapClosing/postprocessing/convStruct2MatNoMS.m
@@ -1,4 +1,4 @@
-function trackedFeatureInfo = convStruct2MatNoMS(tracksFinal)
+function [trackedFeatureInfo,trackedFeatureIndx] = convStruct2MatNoMS(tracksFinal)
 
 %get number of tracks
 numTracks = length(tracksFinal);
@@ -8,7 +8,10 @@ tmp = vertcat(tracksFinal.seqOfEvents);
 numTimePoints = max(tmp(:,1));
 
 %reserve memory for matrix of tracks
-trackedFeatureInfo = NaN*ones(numTracks,8*numTimePoints);
+trackedFeatureInfo = NaN(numTracks,8*numTimePoints);
+
+%reserve memory for matrix of feature indices
+trackedFeatureIndx = zeros(numTracks,numTimePoints);
 
 %put tracks in matrix
 for iTrack = 1 : numTracks
@@ -16,4 +19,6 @@ for iTrack = 1 : numTracks
     endTime   = tracksFinal(iTrack).seqOfEvents(end,1);
     trackedFeatureInfo(iTrack,8*(startTime-1)+1:8*endTime) = ...
         tracksFinal(iTrack).tracksCoordAmpCG;
+    trackedFeatureIndx(iTrack,startTime:endTime) = ...
+        tracksFinal(iTrack).tracksFeatIndxCG;
 end
