42afdb3378900849a95db7c4420c5b6b8a3d1ed3
Jonas Dorn <jonas.dorn@gmail.com>
2007-11-06 21:44:23 +0000

    LAP: some more memory improvements
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2560 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/linearAssignment/lap.m
+++ mathfun/linearAssignment/lap.m
@@ -3,7 +3,7 @@ function [x, y] = lap(cc, NONLINK_MARKER, extendedTesting, augmentCC, noLinkCost
 %
 % A linear assignment tries to establish links between points in two sets.
 % One point in set A can only link to one point in set B and vice versa.
-% The cost associated with the link from element i of A to element j of B 
+% The cost associated with the link from element i of A to element j of B
 % is given by cc(i,j).
 %
 % SYNOPSIS [x, y] = lap(cc, NONLINK_MARKER, extendedTesting, augmentCC, noLinkCost)
@@ -28,9 +28,9 @@ function [x, y] = lap(cc, NONLINK_MARKER, extendedTesting, augmentCC, noLinkCost
 %             matrix input)
 %
 % NONLINK_MARKER : value to indicate that two points cannot be linked.
-%             Default: -1. NaN is not allowed here. 
+%             Default: -1. NaN is not allowed here.
 %
-% extendedTesting (used to be optional, now input has no effect and 
+% extendedTesting (used to be optional, now input has no effect and
 %                  extendedTesting will always be performed)
 %                LAP will always make sure that:
 %                  - There cannot be NaNs in cc
@@ -49,7 +49,7 @@ function [x, y] = lap(cc, NONLINK_MARKER, extendedTesting, augmentCC, noLinkCost
 %            Any x > m or y > n indicates that this point is not linked.
 %
 % EXAMPLE
-% 
+%
 % % Make a set of points p1 and slightly shift the points in p2
 % p1 = rand(5,2)*10;
 % p2 = p1+rand(5,2);
@@ -69,7 +69,7 @@ function [x, y] = lap(cc, NONLINK_MARKER, extendedTesting, augmentCC, noLinkCost
 % [links12, links21] = lap(dm,-1,0,1)
 % % links12 should now look something like
 % % links12 =
-% % 
+% %
 % %            4
 % %            3
 % %            7
@@ -124,17 +124,17 @@ end
 scc = size(cc);
 % check size only if no augmentation
 if ~augmentCC
-    
+
     if scc(1) ~= scc(2) || length(scc) > 2
         error('cost must be a 2D square matrix!')
     end
-    
+
 elseif length(scc) > 2
-    
+
     error('cost must be a 2D matrix!')
-    
+
 else
-    
+
     % if we're augmenting, sparse matrices are produced. This will be
     % problematic with cost 0
     if any(cc(:)==0) && ~issparse(cc)
@@ -145,12 +145,14 @@ else
         end
         cc(validCC) = cc(validCC) + 10;
     end
-    
+
 end
 
-if nargin < 3 || isempty(extendedTesting)
-    extendedTesting = 1;
-end
+% extended testing has been made mandatory
+%
+% if nargin < 3 || isempty(extendedTesting)
+%     extendedTesting = true;
+% end
 
 if nargin < 5 || isempty(noLinkCost)
     noLinkCost = max(max(cc)) + 1;
@@ -175,63 +177,65 @@ if augmentCC
 
     % expand the m-by-n cost matrix to an (m+n)-by-(n+m) matrix, adding
     % diagonals with noLinkCost
-    
+
     % in the lower right corner, we want the lowest cost of all - take
     % minimum cost, subtract 5 and make sure it's not 0.
     % NONLINK_MARKER is not a problem because we make the matrix sparse
     % before augmenting.
-    minCost = min(min(cc(cc~=NONLINK_MARKER)))-5;
-    if minCost == 0 
-        minCost = -2;
-    end
-    
+    %
+    % not needed anymore since we use cc' as costLR
+%     minCost = min(min(cc(cc~=NONLINK_MARKER)))-5;
+%     if minCost == 0
+%         minCost = -2;
+%     end
+
 
     % check if sparse
     if issparse(cc)
-        
+
         % mmDiag = spdiags(noLinkCost * ones(scc(1),1), 0, scc(1), scc(1));
         % nnDiag = spdiags(noLinkCost * ones(scc(2),1), 0, scc(2), scc(2));
         % nmMat  = sparse(ones(scc(2), scc(1)));
         % cc = [cc, mmDiag; nnDiag, nmMat];
-        
+
         % use transposed costmatrix for LR - otherwise, nonLinkCost has no
         % effect
         %[rowIdx, colIdx] = find(cc);
         %costLR = sparse(colIdx,rowIdx,minCost*ones(length(colIdx),1),scc(2),scc(1));
         costLR = cc';
-        
+
         cc = [cc, spdiags(noLinkCost * ones(scc(1),1), 0, scc(1), scc(1));...
             spdiags(noLinkCost * ones(scc(2),1), 0, scc(2), scc(2)),...
             costLR];
-        
+
     else
-        
+
         % mmDiag = diag(noLinkCost * ones(scc(1),1));
         % nnDiag = diag(noLinkCost * ones(scc(2),1));
         % nmMat  = sparse(ones(scc(2), scc(1)));
         % cc = [cc, mmDiag; nnDiag, nmMat];
-        
+
         % use transposed costmatrix for LR - otherwise, nonLinkCost has no
         % effect
         %[rowIdx, colIdx] = find(cc ~= NONLINK_MARKER);
         %costLR = sparse(colIdx,rowIdx,minCost*ones(length(colIdx),1),scc(2),scc(1));
-        
+
         % make cc sparse. Take NLM in cc into account!
         cc(cc==NONLINK_MARKER) = 0;
         cc=sparse(cc);
         costLR = cc';
-        
+
         cc = [cc, diag(noLinkCost * ones(scc(1),1)); ...
             diag(noLinkCost * ones(scc(2),1)), ...
             costLR];
-        
+
     end
 
     % remember that the size of the matrix has increased!
     scc = [sum(scc), sum(scc)];
-    
+
     clear costLR colIdx rowIdx
-    
+
 end
 
 
@@ -255,24 +259,24 @@ else
     val = cc(cc ~= NONLINK_MARKER);
 end
 
-% test that all cols and all rows are filled, and that there are no nans
-if issparse(cc)
-    if (~all(sum(cc~=0,1)) || ~all(sum(cc~=0,2)))
-        error('LAP:BadCostMatrix',...
-            'Rows and columns of the cost matrix must allow at least one possible link!')
-    end
-else
-    if (~all(sum(cc~=NONLINK_MARKER,1)) || ~all(sum(cc~=NONLINK_MARKER,2)))
-        error('LAP:BadCostMatrix',...
-            'Rows and columns of the cost matrix must allow at least one possible link!')
-    end
-end
 
-if any(~isfinite(nonzeros(cc)))
-    error('LAP:NanCostMatrix','Cost matrix cannot contain NaNs or Inf!')
-end
+    % test that all cols and all rows are filled, and that there are no nans
+    if issparse(cc)
+        if (~all(sum(cc,1)) || ~all(sum(cc,2)))
+            error('LAP:BadCostMatrix',...
+                'Rows and columns of the cost matrix must allow at least one possible link!')
+        end
+    else
+        if (~all(sum(cc~=NONLINK_MARKER,1)) || ~all(sum(cc~=NONLINK_MARKER,2)))
+            error('LAP:BadCostMatrix',...
+                'Rows and columns of the cost matrix must allow at least one possible link!')
+        end
+    end
 
-    
+    if any(~isfinite(val))
+        error('LAP:NanCostMatrix','Cost matrix cannot contain NaNs or Inf!')
+    end
+
 
 %clear cc to save memory
 clear cc;
@@ -282,7 +286,7 @@ clear cc;
 % compactCC = [0; val];
 % % write kk, pad a zero
 % kk = [0; rowIdx];
-% 
+%
 % % colIdx is already sorted, so we can find out the number of entries per
 % % column via diff. Wherever there is a jump in the column, the deltaIdx
 % % will be 1, and its rowIdx will equal fst
@@ -290,8 +294,17 @@ clear cc;
 % % add 0 and length+1
 % fst = [0;find(deltaIdx);length(val)+1];
 
-%pad value vector with a zero --Khuloud
-val = [0; val];
+
+% save some more memory: re-write values into row/col-idx. Do in the order
+% row, col, val, because int32 needs less memory, and col shrinks a lot
+% (but requires diff).
+% Also, type the variables here already. Val should be a double, anyway,
+% but better make sure, as the mex function is not forgiving.
+rowIdx = int32([0;rowIdx]);
+colIdx = int32([0;find(diff([0;colIdx]));length(val)]);
+val = double([0; val]);
+
+
 
 %==================================
 
@@ -303,8 +316,14 @@ val = [0; val];
 % %I have re-written the following line to save memory --Khuloud
 % [x, y, u, v] = mexLap(double(scc(1)), int32(length(compactCC)), double(compactCC), int32(kk), int32(fst));
 
-[x, y, u, v] = mexLap(double(scc(1)), int32(length(val)), double(val), ...
-    int32([0; rowIdx]), int32([0;find(diff([0;colIdx]));length(val)]));
+% [x, y, u, v] = mexLap(double(scc(1)), int32(length(val)), double(val), ...
+%     int32([0; rowIdx]), int32([0;find(diff([0;colIdx]));length(val)]));
+
+% I saved some more memory --Jonas
+% The function wants to give four outputs. u and v seem to be the costs
+% associated with links in x and y, respectively.
+[x, y, u, v] = ...
+    mexLap(double(scc(1)), int32(length(val)), val, rowIdx, colIdx); %#ok<NASGU>
 
 %==================================
 
