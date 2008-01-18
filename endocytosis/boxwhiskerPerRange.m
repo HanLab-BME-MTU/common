dc24d48b935f354275ab272dd8a5cc2414b2a341
Dinah Loerke <dinah.loerke@du.edu>
2008-01-18 02:06:40 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2656 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ endocytosis/boxwhiskerPerRange.m
@@ -0,0 +1,32 @@
+function [percrange] = boxwhiskerPerRange(tau, k,perc)
+% calculate the range of the box-and-whisker plot for a function of
+% time constant tau which contains a given percentage of the data
+% k indicates if the function is an exponential or a rayleigh
+
+
+if tau==0
+    
+    percrange = 0;
+    
+else
+    
+    if k==1
+        a = 1;
+        b = -perc*exp(1);
+        c = 1;
+        quadsol = ( -b + sqrt( b^2+4 ) )/2;
+        factor = log ( quadsol );
+        percrange = tau* factor;
+    elseif k==2
+        rvec = [0:0.1:round(3*tau)];
+        ivec =  exp(-((tau-rvec).^2)/tau^2) - exp(-((tau+rvec).^2)/tau^2) ;
+        rpos = min(find(ivec>=perc));
+        percrange = rvec(rpos);
+    end
+
+end % of if
+
+
+
+end % of function
+
