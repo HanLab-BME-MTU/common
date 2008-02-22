f232dd0f99903f7f8dff756e1842d9e111465770
Dinah Loerke <dinah.loerke@du.edu>
2008-02-22 23:35:00 +0000

    update to plot cross-correlation and handel single population entry
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2715 0bcb941b-74d1-0310-bb26-e6636707f451

--- endocytosis/pitClusterRipleyCross_project.m
+++ endocytosis/pitClusterRipleyCross_project.m
@@ -4,20 +4,30 @@ function [allResults]=pitClusterRipleyCross_project(data, restrict, udist, scale
 % movie, then averaging over all movies
 % SYNOPSIS: [allResults]=pitClusterRipleyCross_project(data, udist, restrict)
 %
-% INPUT:   data         =   data structure, this should contain the fields
-%                           .source
-%                           which is the location where the TrackInfo is 
-%                           located, from which we can calculate the 
-%                           pertinent information like
-%                           .lftInfo.Mat_status
-%                           .lftInfo.Mat_xcoord
-%                           .lftInfo.Mat_ycoord
-%                           .lftInfo.Mat_disapp
-%           restrict    =   restriction vector can have variable length:
-%                           [stat da minfr minlft maxlft ...
-%                               minint maxint minmot maxmot]
-%           udist       =   distance vector for Ripley function
-%           scale (opt) =   pixel sclaing, e.g. 0.067 if 1 pixel = 67nm
+% INPUT:   data     =   data structure pointing to all the
+%                       image/detection/tracking data for a given condition;
+%                       for e.g. 10 cell movies, the structure should have 10
+%                       entries; the data structure can be created with the 
+%                       function loadConditionData, and it needs to contain
+%                       at least the field .source, which is the (path) 
+%                       location of the TrackInfo matrix, from which we can
+%                       calculate the pertinent lifetime information like
+%                       .lftInfo.Mat_status
+%                       .lftInfo.Mat_xcoord
+%                       .lftInfo.Mat_ycoord
+%                       .lftInfo.Mat_disapp
+%           restrict  = restriction vector can have variable length;
+%                       minimum length is five, where the entries are
+%                       [stat da minfr minlft maxlft]
+%                       optionally, the length can be extended to nine,
+%                       where the additional entries are 
+%                       [... minint maxint minmot maxmot], see below  
+%                       NOTE: For calculating the cross-correlation of 
+%                       two populations, the vector needs to have two rows,
+%                       where each row constitutes the restriction for one
+%                       population                       
+%           udist   =   distance vector for Ripley function, e.g. [1:20]
+%           scale   =   distance scaling (optional), e.g. 0.067 (um/pix)
 %
 % OUTPUT:   allResults      = structure containing the fields:
 %           .restriction    restrict vector for later reference
@@ -32,8 +42,50 @@ function [allResults]=pitClusterRipleyCross_project(data, restrict, udist, scale
 % written by Dinah Loerke
 % last modified January 28th, 2008
 % last modified Feb 13th, 2008
+%
 
 
+%% EXPLANATION of restriction values:
+% rest = [stat da minfr minlft maxlft minint maxint minmot maxmot]
+%                     
+% stat  =   object status, possible values are 1, 2, 3
+%           1: object appears and disappears in the movie
+%           2: object is present for entire length of the movie
+%           3: object is present in either first or last frame of the movie
+% da    =   appearance/disappearance status, possible values are 1,0,-1
+%           1: marks first frame
+%           0: marks middle frame
+%           -1: marks last frame
+% minfr =   minimum lifetime/length in FRAMES - e.g. 4, to exclude tracking
+%           artifacts of false detection positives
+% minlft =  minimum lifetime in SECONDS - e.g. 60 to select for productive
+%           clathrin-coated pits
+% maxlft =  maximum lifetime in SECONDS - e.g. 25 to select for abortive
+%           clathrin-coated pits
+%
+% OPTIONAL:
+%
+% minint =  minimum normalized intensity (ranging from 0 to 1)
+% maxint =  maximum normalized intensity (ranging from 0 to 1)
+% minmot =  minimum normalized motility (ranging from 0 to 1)
+% maxmot =  maximum normalized motility (ranging from 0 to 1)
+%
+% These latter criteria allow you to select e.g. the brightest 10% of the
+% population, or the faster 50%.
+% 
+% EXAMPLE:  to select the positions where productive pits appear
+%           rest1 = [1 1 4 60 300]
+%           to select the positions where abortive pits are located in each
+%           frame
+%           rest2 = [1 0 4 8 25]
+%
+% EXAMPLE of function call:
+% [res] = pitClusterRipleyCross_project(dataClathrinLatr,...
+%         [[1 1 4 60 300];[1 1 4 8 25]], [1:20], 0.67);
+
+
+
+%%
 % number of restriction 'sub-populations'
 [ccx,ccy] = size(restrict);
 % total number of self- and cross-correlations
@@ -202,7 +254,10 @@ allResults.comb = ccComb;
 
  
 areaplot1 = [ ((lrAV(:,1)-lrSEM(:,1))') ; 2*lrSEM(:,1)' ];
-areaplot2 = [ ((lrAV(:,2)-lrSEM(:,2))') ; 2*lrSEM(:,2)' ];
+if ccx>1
+    areaplot2 = [ ((lrAV(:,2)-lrSEM(:,2))') ; 2*lrSEM(:,2)' ];
+    areaplot3 = [ ((lrAV(:,3)-lrSEM(:,3))') ; 2*lrSEM(:,3)' ];
+end
 
 pdist = udist;
 if nargin>3
@@ -212,9 +267,13 @@ end
 figure
 hold on;
 area(pdist,areaplot1');
-area(pdist,areaplot2');
 plot(pdist,lrAV(:,1),'g-');
-plot(pdist,lrAV(:,2),'r-');
+if ccx>1
+    area(pdist,areaplot2');
+    plot(pdist,lrAV(:,2),'r-');
+    area(pdist,areaplot3');
+    plot(pdist,lrAV(:,3),'c-');
+end
 
 %%=========================================================================
 %           convert allLR to local densities
@@ -222,12 +281,13 @@ plot(pdist,lrAV(:,2),'r-');
 
 
 allDen = allLR;
+[dlx,dly,dlz] = size(allLR);
 
 carea = udist.^2;
 careadiff = carea; careadiff(2:length(careadiff)) = diff(carea);
-amat = repmat(careadiff',1,3);
+amat = repmat(careadiff',1,dly);
 
-dmat = repmat(udist',1,3);
+dmat = repmat(udist',1,dly);
 
 for i=1:length(data)
     currLR = allLR(:,:,i);
@@ -252,14 +312,21 @@ allResults.NDsem = ndenSEM;
 
 
 areaplot1d = [ ((ndenAV(:,1)-ndenSEM(:,1))') ; 2*ndenSEM(:,1)' ];
-areaplot2d = [ ((ndenAV(:,2)-ndenSEM(:,2))') ; 2*ndenSEM(:,2)' ];
+if ccx>1
+    areaplot2d = [ ((ndenAV(:,2)-ndenSEM(:,2))') ; 2*ndenSEM(:,2)' ];
+    areaplot3d = [ ((ndenAV(:,3)-ndenSEM(:,3))') ; 2*ndenSEM(:,3)' ];
+end
 
 figure
 hold on;
 area(pdist,areaplot1d');
-area(pdist,areaplot2d');
 plot(pdist,ndenAV(:,1),'g-');
-plot(pdist,ndenAV(:,2),'r-');
+if ccx>1
+    area(pdist,areaplot2d');
+    plot(pdist,ndenAV(:,2),'r-');
+    area(pdist,areaplot3d');
+    plot(pdist,ndenAV(:,3),'c-');
+end
 
 
 end % of function
