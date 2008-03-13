e0565f6e9237fb93e7cf9239e7777084467d7d49
Dinah Loerke <dinah.loerke@du.edu>
2008-03-13 17:48:29 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2746 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ endocytosis/lifetimeCompactFitData.m
@@ -0,0 +1,51 @@
+function [compactRes, data] = lifetimeCompactFitData(data, restrict, shape);
+% perform multiple-population fit on lifetime data, including cell-to-cell
+% varaiations in all parameters
+% INPUT:    data    = experiment data, has to contain fields
+%           restrict    = time restriction in seconds, e.g. 300
+%           shape   = shape for populations, e.g. [2 2 1], where 1
+%                   indicates an exponential distribution, and 2 a Rayleigh
+%                   distribution
+% OUTPUT:   compactRes    = compact results
+%           data (optional) = with added field .lftHist if not already
+%           present
+
+
+[data] = fillStructLifetimeHist(data);
+
+% k= number of movies/cells
+k = length(data);
+
+% combine lifetime histograms of the slow and fast movies separately
+res     = stageFitLifetimesPlat(data);
+res_E2E = stageFitLifetimes_E2E(data);
+mer     = mergeFastSlowHistogramsPlat(res, restrict, shape);
+mer_E2E = mergeFastSlowHistogramsPlat_E2E(res_E2E, restrict, shape);
+
+% n = number of trajectories
+n = mer.numcells;
+
+% fit results
+resmat1 = mer.compactFitRes;
+
+% determine cell-to-cell error with jackknife
+for s = 1:length(mer_E2E)
+    E2Emat(:,:,s) = mer_E2E(s).compactFitRes;
+end
+[mx,my,mz] = size(E2Emat);
+for inx=1:mx
+    for iny=1:my
+        E2Eval(inx,iny) = jackknifeError(E2Emat(inx,iny,:));
+    end
+end
+
+% enter output results
+compactRes.numcells = k;
+compactRes.numtraj = n;
+compactRes.contr = resmat1(:,1);
+compactRes.contrError = round(10*E2Eval(:,1))/10;
+compactRes.tau = resmat1(:,2);
+compactRes.tauError = round(10*E2Eval(:,2))/10;
+compactRes.tau50 = resmat1(:,3);
+
+end
