44ca211362b9baf7655b6ee4cd3412c2cb075663
Dinah Loerke <dinah.loerke@du.edu>
2008-05-22 23:07:32 +0000

    updated version to accomodate smaller values of tau
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2825 0bcb941b-74d1-0310-bb26-e6636707f451

--- endocytosis/boxwhiskerPerRange.m
+++ endocytosis/boxwhiskerPerRange.m
@@ -18,7 +18,15 @@ else
         factor = log ( quadsol );
         percrange = tau* factor;
     elseif k==2
-        rvec = [0:0.1:round(3*tau)];
+        if tau>=1
+            rvec = [0:0.1:round(3*tau)];
+        % if tau<1, use smaller increment
+        else
+            % exponent
+            ep = floor(log10(tau))-1;
+            incr = 10^ep;
+            rvec = [0:incr:3*tau];
+        end
         ivec =  exp(-((tau-rvec).^2)/tau^2) - exp(-((tau+rvec).^2)/tau^2) ;
         rpos = min(find(ivec>=perc));
         percrange = rvec(rpos);
