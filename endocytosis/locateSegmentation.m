92f252adcbb2ecf14570a444e76f4972f516d837
Dinah Loerke <dinah.loerke@du.edu>
2008-07-23 19:22:11 +0000

    updated and extended version, for outside segmentation (e.g. cell edge)
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2885 0bcb941b-74d1-0310-bb26-e6636707f451

--- endocytosis/locateSegmentation.m
+++ endocytosis/locateSegmentation.m
@@ -1,13 +1,25 @@
-function [data] = locateSegmentation(data);
+function [data] = locateSegmentation(data, outvar);
 % locate segmentation data in the specified paths, and store the location 
 % as fields in the data structure
 %
 % INPUT     data    :    experiment structure, which has to contain a field
 %                       .source
 %                        which is the path to the data location
+%           outvar (optional) : if outvar is set to 1 (default is zero),
+%                        then the located segmentation data is considered
+%                        the segmentation for the OUTSIDE (e.g. cell
+%                        outline), as opposed to the INSIDE (e.g. adhesive
+%                        pattern outline). If this is the case, then the
+%                        results are written to a different field name (see
+%                        below)
 % OUTPUT    two new fields are added to (or overwritten in) data, which are
 %                   data.segmentDataFilePath
 %                   data.segmentDataFileName
+%           IF the value of input variable outvar==1, then the fields are
+%           called
+%                   data.segmentDataFilePathOUT
+%                   data.segmentDataFileNameOUT
+%           to avoid overwriting the pattern segmentation path
 %
 % NOTE: If the folder containing the segmentation data has a standardized
 % name (e.g. if it's always called 'SegmentImages' or something along these
@@ -16,8 +28,16 @@ function [data] = locateSegmentation(data);
 % just the source path - this would mean less user clicking
 %
 % Dinah Loerke, last modified April 20, 2008
+% Dinah Loerke, updated July 23, 2008
 
 od = cd;
+
+ovariable = 0;
+if nargin>1
+    if outvar==1
+        ovariable = 1;
+    end
+end
 
 % loop over all entries in the structure to enter the image data necessary
 % for the detection input
@@ -29,8 +49,13 @@ for i=1:length(data)
     
     [oriImageName, oriImagePath] = uigetfile('.tif',['Select first segmentation image']); 
     
-    data(i).segmentDataFileName = oriImageName;
-    data(i).segmentDataFilePath = oriImagePath;
+    if ovariable == 0;
+        data(i).segmentDataFileName = oriImageName;
+        data(i).segmentDataFilePath = oriImagePath;
+    else
+        data(i).segmentDataFileNameOUT = oriImageName;
+        data(i).segmentDataFilePathOUT = oriImagePath;
+    end
         
     cd(od);
 end
