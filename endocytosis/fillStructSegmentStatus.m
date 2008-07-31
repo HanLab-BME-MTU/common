c873ae8a15cd6985e7e3c025742f48d7028fcd03
Dinah Loerke <dinah.loerke@du.edu>
2008-07-31 00:25:33 +0000

    first version
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2896 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ endocytosis/fillStructSegmentStatus.m
@@ -0,0 +1,75 @@
+function [data] = fillStructSegmentStatus(data);
+% fill experiment structure with vectors designating the segmentation
+% status of each trajectory (inside vs. outside pattern) 
+% based on the image segmentation in the specified folders
+% 
+% SYNOPSIS [data] = fillStructSegmentStatus(data);
+%
+% INPUT     data: experiment structure, which has to contain the fields
+%                .source
+%                .movieLength
+%                .segmentDataFileName 
+%                .segmentDataFilePath 
+%
+% OUTPUT    creates new fields in the data structure called
+%                .segmentStatus   
+%           which is a vector containing the segmentation status for every
+%           objects in the trackInfo; 
+%           status = 1 means inside the segmented pattern
+%           status = 0 means outside the segmented pattern
+%           
+%
+% Dinah Loerke, modified July 30, 2008
+
+
+% number of entries in data structure
+lens = length(data);
+
+od = cd;
+
+for i=1:lens
+    
+    fprintf('movie #%02d',i);
+    
+    % current path
+    path = data(i).source;
+    
+    % number of frames for this exp
+    lenf = data(i).movieLength;
+    
+    
+    % load segmentation image from specified location
+    SegmFileName = data(i).segmentDataFileName;
+    SegmFilePath = data(i).segmentDataFilePath;
+
+    cd(SegmFilePath);
+    SegmentMask = imread(SegmFileName);    
+    if ~islogical(SegmentMask)
+        SegmentMask = logical(SegmentMask/max(SegmentMask(:)));
+    end
+    
+        
+    % load lifetime info data file
+    lftpath = [path,'/LifetimeInfo'];
+    cd(lftpath);
+    lftname = 'lftInfo.mat';
+    loadfile = load(lftname);
+    cd(od);
+    
+    lftInfo = loadfile.lftInfo;
+
+    % calculate segmentation status (1=Inside, 0=oustide segmented region)
+    [segmentStatusVector] = calcIORegionLfthistSimple(lftInfo, SegmentMask);
+    
+    
+    % lifetime histograms
+    data(i).segmentStatus = segmentStatusVector';
+    
+        
+    fprintf('\b\b\b\b\b\b\b\b\b');       
+
+end % of for
+
+fprintf('\n'); 
+
+end % of function
