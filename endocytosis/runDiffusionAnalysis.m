0bd1f9384375f3ba346a66ea81116459a2408541
Daniel Nunez <daniel_nunez@hms.harvard.edu>
2008-07-31 23:06:56 +0000

    made restriction vector an input to runDiffusionAnalysis.m and added further comments to function
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@2900 0bcb941b-74d1-0310-bb26-e6636707f451

--- endocytosis/runDiffusionAnalysis.m
+++ endocytosis/runDiffusionAnalysis.m
@@ -1,17 +1,20 @@
-function [] = runDiffusionAnalysis();
+function [] = runDiffusionAnalysis(rest);
 
-% runDiffusion analyses diffusion of pits for a given condition
-%
-% SYNOPSIS [experiment] = runTracking()
+% runDiffusion analyses diffusion of tracks for a given condition that fit 
+% the characteristics imposed by the restriction vector
 %
 % INPUT
+%           restrict  = restriction vector can have variable length;
+%                       minimum length is five, where the entries are
+%                       [stat da minfr minlft maxlft]          
 %
 % OUTPUT
 %
 % REMARKS   The function does not overwrite previous results; it uses
 %           secureSave. It makes a new folder called diffusionAnalysis
 %           under movie folder. It uses the function
-%           trackDiffusionAnalysis1 to do the diffusion analysis.
+%           trackDiffusionAnalysis1 to do the diffusion analysis. The
+%           restriction vector is saved alond with the diffusion analysis.
 %
 % Uses: trackDiffusionAnalysis1
 %       loadIndividualMovies
@@ -19,12 +22,37 @@ function [] = runDiffusionAnalysis();
 %
 % Daniel Nunez, July 9, 2008
 
+%% EXPLANATION of restriction values:
+% rest = [stat da minfr minlft maxlft minint maxint minmot maxmot]
+%                     
+% stat  =   object status, possible values are 1, 2, 3
+%           1: object appears and disappears in the movie
+%           2: object is present for entire length of the movie
+%           3: object is present in either first or last frame of the movie
+% da    =   appearance/disappearance status, possible values are 1,0,-1
+%           1: marks first frame
+%           0: marks middle frame
+%           -1: marks last frame
+% minfr =   minimum lifetime/length in FRAMES - e.g. 4, to exclude tracking
+%           artifacts of false detection positives
+% minlft =  minimum lifetime in SECONDS - e.g. 60 to select for productive
+%           clathrin-coated pits
+% maxlft =  maximum lifetime in SECONDS - e.g. 25 to select for abortive
+%           clathrin-coated pits
+%
+% These latter criteria allow you to select e.g. the brightest 10% of the
+% population, or the faster 50%.
+% 
+% EXAMPLE:  to select the positions where productive pits appear
+%           rest1 = [1 1 4 60 300]
+%           to select the positions where abortive pits are located in each
+%           frame
+%           rest2 = [1 0 4 8 25]
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%
 %LOAD MOVIES
 [experiment] = loadIndividualMovies();
-
-%GET LIFETIME CUTT-OFFS TO DEFINE POPULATIONS
-[startAbortLifetime,endAbortLifetime,startProdLifetime]=plotPopulationProbabilityDistribution();
-rest = [1 1 4 startProdLifetime 300;1 1 4 startAbortLifetime endAbortLifetime];
 
 %FOR EACH MOVIE
 for iexp = 1:length(experiment)
@@ -84,7 +112,7 @@ for iexp = 1:length(experiment)
 
     %SAVE RESULTS
     mkdir([experiment(iexp).source filesep 'diffusionAnalysis']);
-    secureSave([experiment(iexp).source filesep 'diffusionAnalysis' filesep 'diffusionAnalysisResults' datestr(now,'yyyymmdd')],'diffAnalysisRes','errFlag','alphaValues','checkAsym');
+    secureSave([experiment(iexp).source filesep 'diffusionAnalysis' filesep 'diffusionAnalysisResults' datestr(now,'yyyymmdd')],'diffAnalysisRes','errFlag','alphaValues','checkAsym','restrict');
     
     %clear trackInfo so that it doesn't interfere with the trackInfo of the
     %next movie (because there are two names for this variable depending on
