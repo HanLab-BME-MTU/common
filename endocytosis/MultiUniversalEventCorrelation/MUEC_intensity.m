da68c5dccd58b773af00d0af183286f9e73d532d
Dinah Loerke <dinah.loerke@du.edu>
2009-01-20 23:50:55 +0000

    updated version: improved documentation
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3102 0bcb941b-74d1-0310-bb26-e6636707f451

--- endocytosis/MultiUniversalEventCorrelation/MUEC_intensity.m
+++ endocytosis/MultiUniversalEventCorrelation/MUEC_intensity.m
@@ -1,24 +1,49 @@
 function [result] = MUEC_intensity(data, restvector, dvector, tvector, channel); 
-% Multichannel Universal Event Correlator
+% MUEC (Multichannel Universal Event Correlator) Intensity: correlates
+% tracking events with intensity
 % 
-% SYNOPSIS [result] = MUEC_intensity(data, restvector, dvector, tvector); 
+% SYNOPSIS [result] = MUEC_intensity(data, restvector, dvector, tvector, channel); 
 %
-% INPUT:    data:   data structure
-%           restvector: restriction vector; if restvector is a string
-%           instead of a vector, then the function will try to read the
-%           MPP from the structure field of the corresponding name
-%           dvector: distance vector         
-%           tvector: time shift vector
+% INPUT:    data:       data structure
+%           restvector: restriction vector
+%                   NOTE1: if restvector is a string instead of a vector, 
+%                   then the function will try to read the vector from the 
+%                   structure field of the corresponding name
+%                   NOTE2: standard format for the restriction vector is
+%                   [lstat dstat minfr minsec maxsec], where
+%                   lstat = lifetime status [nan,1,2,3]
+%                   dstat = appearance/disappearance status [-1,0,1]
+%                   minfr = minimum lifetime in frames
+%                   minsec = minimum lifetime in seconds
+%                   maxsec = maximum lifetime in seconds
+%                   EXAMPLE: [nan -1 4 60 800] extracts the disappearance
+%                   events of objects that live >4 frames and between
+%                   60-800 seconds (determined from .framerate), lifetime
+%                   status is irrelevant
+%           dvector:    distance vector in pixels (e.g. 2 for 2 pix radius)       
+%           tvector:    time shift vector (e.g. [-5:10])
+%           channel (optional): structure field in data from which to read
+%               the intensity data; if no value is specified, the function
+%               will try to find image data at the path location specified
+%               in the structure field '.channel2'
 %
-% OUTPUT:   results:        
+% OUTPUT:   result:  contains the fields:
+%                   .parfield = averaged parameter results above background
+%                   .parerror = standard error of the mean (all events)
+%                   .restrictions = restriction vector (see above)
+%                   .distvector = distance vector (see above)
+%                   .timevector = tvector (see above)
+%                   .numtraj = total number of trajectories      
 %
-% last modified: Dinah Loerke, August 28, 2008
+% Dinah Loerke
+% last modified:    August 28, 2008
+%                   January 20, 2009
 %
 % NOTE: the assumption is that the entered tvector (e.g. [-5:10]) is in
 % frames and corresponds to the fastest framerate present in the analyzed 
 % data (fframerate); data with slower framerates are also collected for the
 % same number of frames pre- and post-event, but in the final averaging
-% analysis, the data are extrapolated only for the time range of the
+% analysis, the data are interpolated only for the time range of the
 % fastest rate
 
 
@@ -26,33 +51,43 @@ od = cd;
 nr = 10;
 refplane = find(tvector==0);
 
-if isfield(data,'framerate')
-    scvec = data(1).framerate;
-else
-    scvec = 1;
-end
+%% select image files from which intensity is to be read; if no specific
+% location is specified, the function will try to read the image locations
+% from the structure field 'channel2'
 
 for i=1:length(data)
     
     % select first image for the second (intensity) channel
     if nargin>4
-        path2 = getfield(data,{1,i},channel);
+        if isfield(data, channel)
+            path2 = getfield(data,{1,i},channel);
+        else
+            error(['no field of the specified name (,'channel,') is found');
+        end
     else
-        path2 = data(i).channel2;
+        if isfield(data, channel2)
+            path2 = data(i).channel2;
+        else
+            error('function requires the existence of a field called .channel2');
+        end
     end
     
-    % change to source directory
-    cd(path2);         
+    % change to specified directory
+    cd(path2); 
+    % select first image file
     [imageName, imagePath] = uigetfile('.tif',['Select first original intensity image for movie #',num2str(i)]); 
-    
+    % make a list of all complete image file names (including path) for
+    % later upload of the images
     completeImageName = strcat(imagePath, imageName);
     imageStackList = getFileStackNames(completeImageName);
     
     Ch2stackList(i).list = imageStackList;
     
 end
+pause(0.1);
 
 
+%% loop over all movies
 for i=1:length(data)
     
     fprintf('movie #%02d',i);
@@ -66,12 +101,19 @@ for i=1:length(data)
     trackInfo  = loadfile.trackInfo;
     framerate = data(i).framerate;
     
-    % load reference image, if one exists
+    % load reference image - this is a binary mask that shows the outline
+    % of the cell; the mask is required because the function will choose
+    % random positions inside the cell as reference positions for the
+    % background signal, and it is desirable to choose the reference
+    % positions within the area of the cell (as opposed to the empty area 
+    % on the coverslip next to it)
     cpath = [data(i).source,filesep,'SubregionsMask'];
     cd(cpath);
     image = imread('mask0001.tif');
     mask = image';
-       
+    
+    % if necessary, read restriction vector from the specified structure
+    % field
     if isstr(restvector)
         restrictions = getfield( data(i),restvector );
     else
@@ -79,16 +121,17 @@ for i=1:length(data)
        restrictions = restvector;
     end
 
-
+    % extract the positions with the desired properties
     MPMglobal = CorrelateData2Pos_extractMPM2L(trackinfopath, restrictions, tvector, framerate, 1);
     
-    % if the red and green color channels are shifted - i.e. if a field 
-    % colorShiftVector exists - then the clathrin pit event positions in
+    % if the intensity belongs to a second color channel that is shifted
+    % with respect to the tracking channel - i.e. if a field 
+    % colorShiftVector exists - then the event positions in
     % MPMglobal have to be shifted by the shift vector
     % NOTE: What do the dimensions of the shiftvector mean? For example, a 
     % shift of shiftvec=[-10,-5]) means that image2 is shifted in such a 
-    % way that the point im1(1,1) in image1 (clathrin) overlays point 
-    % im2(11,6) in image2 (actin). Thus, the shift has to be subtracted
+    % way that the point im1(1,1) in image1 (e.g. clathrin) overlays point 
+    % im2(11,6) in image2 (e.g. actin). Thus, the shift has to be subtracted
     % from the positions in the clathrin channel to obtain the correct
     % coordinates in the actin channel. Positions outside the shifted image
     % dimensions have to be set to nan
@@ -111,15 +154,15 @@ for i=1:length(data)
         end
     end
             
-            
-    pause(0.1);
-    
+                
     % compact MPMglobal (get rid of nan rows)
     projpos_global = nanmean(MPMglobal(:,:,1),2);
     fpos_global = find(isfinite(projpos_global));
     MPMglobal = MPMglobal(fpos_global,:,:);
         
-    % make random positions for reference measurement
+    % make random positions for reference measurement; every event in
+    % MPMglobal will be supplemented by 10 points randomly chosen in the
+    % cell area (but in the same time frame as the event)
     MPMbas = MPMglobal(:,:,1);
     goodpos = find( MPMglobal(:,:,2) == refplane );
     MPMbas_use = nan*MPMbas;
@@ -142,12 +185,12 @@ for i=1:length(data)
     [lx1,ly1,lz1] = size(MPMglobal);
     [lx2,ly2,lz2] = size(MPMrandomshift);
     
-    MPMall = [MPMglobal ; MPMrandomshift];
-    
+    MPMall = [MPMglobal ; MPMrandomshift];    
         
     imageStackList = Ch2stackList(i).list;    
     total_frame_num = length(imageStackList);
     
+    % read all images for this movies
     for k=1:total_frame_num
         
         cimage = imread(imageStackList{k});
@@ -158,32 +201,25 @@ for i=1:length(data)
         
     end
     
-    
+    % perform the correlation
     CORRresults_all = CorrelateData2Pos(MPMall, intensityImageStack, dvector, 'intensity');
     
-    % extract kinetic scores at desired positions
-    %CORRresults_pos = CorrelateData2Pos(MPMglobal, data(i).ksnamelist, dvector, 'kscores');
-    
-    % extract kinetic scores for random positions
-    %CORRresults_rand = CorrelateData2Pos(MPMrandomshift, data(i).ksnamelist, dvector, 'kscores');
-    
-    % global results
-    %ResultsGlobal(:,:,i) = CORRresults_pos;
-    %ResultsRandom(:,:,i) = CORRresults_rand;
-    
+    % the results contain a matrix of intensity values for all points     
     ResultsGlobalData(i).intmatrix = CORRresults_all(1:lx1,:,:);
     ResultsRandomData(i).intmatrix = CORRresults_all(lx1+1:lx1+lx2,:,:);
     ResultsNumTraj(i) = lx1;
         
 end % of for i-loop
 
+% return to original directory
 cd(od);
 
 
 %% the raw data is now processed for averaging
 
-% average results taking into account possible variable framerates
-
+% NOTE: the function will average the results taking into account possibly 
+% variable framerates; if necessary, the slower movie results are 
+% interpolated to the fastest available framerate
 for i=1:length(data)
     cfr = data(i).framerate;
     if i==1
@@ -200,9 +236,12 @@ figure; hold on;
 
 for i=1:length(data)
     
-   
+    % event results
     ResultsGlobalCurr       = ResultsGlobalData(i).intmatrix;
+    % results of the random reference points
     ResultsRandomCurr       = ResultsRandomData(i).intmatrix;
+    % results of the random reference points are averaged and written into
+    % a matrix matching ResultsGlobalCurr for subtraction
     ResultsRandomCurrAV     = nanmean(ResultsRandomCurr,1);
     ResultsRandomCurrAVMAT  = repmat(ResultsRandomCurrAV,ResultsNumTraj(i),1);
     ResultsGlobalCurrCORR   = ResultsGlobalCurr - ResultsRandomCurrAVMAT;
@@ -218,8 +257,7 @@ for i=1:length(data)
         ResultsGlobalCurrCORR   = ResultsGlobalCurr_EP' - ResultsRandomCurrAVMAT;
     end      
  
-    
-    
+    % add the background-subtracted results to a global results matrix    
     if i==1
         ResultsGlobalAll = ResultsGlobalCurrCORR;
     else
@@ -243,13 +281,21 @@ end
 
 
 
-
-
-
 % average the results of individual movies
 ResultsGlobalAVE = nanmean(ResultsGlobalAll,1);
 ResultsGlobalError = nanstd(ResultsGlobalAll,[],1)/sqrt(sum(ResultsNumTraj));
 
+% write results
+result.parfield = ResultsGlobalAVE;
+result.parerror = ResultsGlobalError;
+
+result.restrictions = restvector;
+result.distvector = dvector;
+result.timevector = tvector;
+result.numtraj = sum(ResultsNumTraj);
+
+
+% display results
 figure
 [rx,ry,rz] = size(ResultsGlobalAVE);
 if rz==1
@@ -266,14 +312,7 @@ axis([amin amax -2.5e-7 2.5e-7]);
 xlabel('time point relative to CCP event (sec)');
 ylabel('relative kinetic score');
 
-% display Results
-result.parfield = ResultsGlobalAVE;
-result.parerror = ResultsGlobalError;
 
-result.restrictions = restvector;
-result.distvector = dvector;
-result.timevector = tvector;
-result.numtraj = sum(ResultsNumTraj);
 
 end % of function
 
