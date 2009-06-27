0d489d3441a982d11dd36ed1e64fb2b5acba4f5a
Jonas Dorn <jonas.dorn@gmail.com>
2009-06-27 02:08:46 +0000

    plotTransparent: Jonas' version has proper widths for acute angles, too, as long as the aspect ratio is 1:1. I have not been able to get the aspectRatio correction to work - somewhere there is a problem with the width correction. Also, I have added the possibility to plot into specific axes.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3493 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/plotTransparent.m
+++ graphics/plotTransparent.m
@@ -1,7 +1,8 @@
-function h = plotTransparent(x,y,width,color,opacity,perpendicularOffset)
+function h = plotTransparent(varargin)
 
 %
-% h = plotTransparent(x,y,width,plotStyle)
+% h = plotTransparent(x,y,width,color,opacity,perpendicularOffset,useLimits)
+% h = plotTransparent(ah,...
 %
 %
 % Plots the input x-y data as a tansparent line with variable width.
@@ -28,6 +29,16 @@ function h = plotTransparent(x,y,width,color,opacity,perpendicularOffset)
 %                 imshow, but it allows to plot any 2D line, not just those
 %                 that can be described as y = f(x). Optional. Default: 1
 %
+%     correctAspectRatio - if 1, expected axes limits are used to adjust the width
+%                 of the line in x/y so that it doesn't appear deformed
+%                 without axes equal. If an axes handle has been given, the
+%                 dataAspectRatio of these axes will be used to calculate
+%                 the correction.
+%                 Default: false.
+%                 THIS OPTION DOES NOT WORK PROPERLY AT THE MOMENT
+%
+%     ah : handles of axes into which to plot
+%
 %                 
 %
 % Output: h   -    Handle to the patch object
@@ -38,47 +49,76 @@ function h = plotTransparent(x,y,width,color,opacity,perpendicularOffset)
 %           figure,
 %           plotTransparent(sin(phi),cos(phi)),axis equal
 %
+%           x = [1,2,3,4,4,6];y=[1,2,1,1,2,1];
+%           plotTransparent(x,y,0.1) 
+%           axis equal % if this is omitted, the plot will not look well.
+%
 % Hunter Elliott, 6/2009
 % adapted for any 2D lines by Jonas
-
-% note: it turns out that Hunter's version looks equally crappy with
-% unequal axes (example: figure,plotTransparent(rand(25,1),[],[],[],[],0) )
-% Therefore, I set the perpendicularOffset as default.
 
 
 if nargin  == 0
     error('plotTransparent needs at least one nonempty input argument')
 end
 
-if nargin < 2 || isempty(y)
+if isscalar(varargin{1}) && ishandle(varargin{1}) &&  strcmp(get(varargin{1},'Type'),'axes')
+    ah = varargin{1};
+    varargin(1) = [];
+else
+    ah = [];
+end
+
+% overwrite nargin
+numArgIn = length(varargin);
+
+if numArgIn  == 0
+    error('plotTransparent needs at least one nonempty input argument')
+end
+
+x = varargin{1}(:);
+
+if numArgIn < 2 || isempty(varargin{2})
     if isempty(x)
         error('plotTransparent needs at least one nonempty x or y')
     end
     y = x;
     x = [];
+else
+    y = varargin{2}(:);
 end
 
 if isempty(x)
-    x = 1:length(y);
+    x = (1:length(y))';
 end
 
-x = x(:);
-y = y(:);
-
-if nargin < 3 || isempty(width)
+if numArgIn < 3 || isempty(varargin{3})
     width = nanstd(y) / 20;
+else
+    width = varargin{3}(:);
 end
 
-if nargin < 4 || isempty(color)
+if numArgIn < 4 || isempty(varargin{4})
     color = 'b';
+else
+    color = varargin{4};
 end
 
-if nargin < 5 || isempty(opacity)
+if numArgIn < 5 || isempty(varargin{5})
     opacity = .4;
+else
+    opacity = varargin{5};
 end
 
-if nargin < 6 || isempty(perpendicularOffset)
+if numArgIn < 6 || isempty(varargin{6})
     perpendicularOffset = true;
+else
+    perpendicularOffset = varargin{6};
+end
+
+if numArgIn < 7 || isempty(varargin{7})
+    useLimits = false;
+else
+    useLimits = varargin{7};
 end
 
 n = length(x);
@@ -109,28 +149,74 @@ end
 
 
 if ~perpendicularOffset
-    % Hunter's version
-    xFill = [x(:)' x(end:-1:1)'];
-    yFill = [(y(:)' - width(:)') (y(end:-1:1)' + width(end:-1:1)') ];
+    % Hunter's version - updated by Jonas
+    offset = [zeros(size(x,1),1),width];
     
 else
     % Jonas' version
     
     xy = [x,y];
+    
+    
+   
+    
     % find derivative to have proper corners
     [n_xy,e_xy] = normList(diff(xy));
-    offset = [-e_xy(:,2),e_xy(:,1)];
-    % offset should be average of normal to the line segments at intersections
-    % not using bsxfun here because of LCCB
-    offset = (width * [1 1]) .* [offset(1,:);(offset(2:end,:) + offset(1:end-1,:))/2;offset(end,:)];
-    % create x,y for fill (could also use multiple patches if multiple colors
-    % are needed
     
-    xFill = [xy(:,1) + offset(:,1); xy(end:-1:1,1) - offset(end:-1:1,1)];
-    yFill = [xy(:,2) + offset(:,2); xy(end:-1:1,2) - offset(end:-1:1,2)];
+     
+    
+    
+    
+    offset = [-e_xy(:,2),e_xy(:,1)];
+    
+    % check whether to use limits
+    if useLimits
+        if isempty(ah)
+            fh = figure('visible','off');
+            tmpAx = axes('Parent',fh);
+            plot(x,y);
+            aspectRatio = get(tmpAx,'DataAspectRatio');
+            delete(fh)
+        else
+            aspectRatio = get(ah,'DataAspectRatio');
+        end
+        stretchRatio = aspectRatio(1)/aspectRatio(2);
+        % offset gets stretched in x vs y. However, the line gets stretched
+        % in x vs y as well. Therefore, in order to look perpendicular,
+        % both x and y have to be corrected
+        offset(:,1) = offset(:,1) * (stretchRatio);
+        offset(:,2) = offset(:,2) / (stretchRatio);
+        % this gives the right angles, but unfortunately, the correction is
+        % wrong. However, I am unable to figure this out right now.
+    end
+    
+    % offset should be average of normal to the line segments at
+    % intersections. In order to get constant line thickness, the averaged
+    % offset vector needs to be stretched by 1/sin(alpha), otherwise, there
+    % is an apparent narrowing of the line, especially at acute angles
+    tmp = [offset(1,:);(offset(2:end,:) + offset(1:end-1,:))/2;offset(end,:)];
+    tmp(2:end-1,:) = tmp(2:end-1,:) ./...
+        (dot(offset(1:end-1,:),tmp(2:end-1,:),2) * [1,1]);
+    % Adjust with based on user input. Not using bsxfun here because of LCCB
+    offset = (width * [1 1]) .* tmp;
+    
+    
+    
 end
+    
+    
+    
+    % create x,y for fill (could also use multiple patches if multiple colors
+    % are needed 
+    xFill = [x + offset(:,1); x(end:-1:1) - offset(end:-1:1,1)];
+    yFill = [y + offset(:,2); y(end:-1:1) - offset(end:-1:1,2)];
 
+    if ~isempty(ah)
+        set(gca,'DataAspectRatio',aspectRatio,'NextPlot','add')
+    end
 ph = fill(xFill,yFill,color,'EdgeColor','none','FaceAlpha',opacity);
+% hold on, plot(xFill,yFill,'k')
+% plot(x,y,'ok')
 
 if nargout > 0
     h = ph;
