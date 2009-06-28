75a638de18c026cddc200ebaf8ed1addaabab3f5
Jonas Dorn <jonas.dorn@gmail.com>
2009-06-28 22:40:47 +0000

    plotTransparent: there is now an option for constant thickness of the line with only y-offset
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3494 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/plotTransparent.m
+++ graphics/plotTransparent.m
@@ -22,12 +22,24 @@ function h = plotTransparent(varargin)
 %     opacity - How transparent the line is. If 1, the line is opaque. If
 %     0, the line is invisible.
 %
-%     perpendicularOffset - if 1, the code assumes equal axes and thus calculates the
-%                 line such that it is expanded at a right angle to the
-%                 individual line segments. This only looks good if you set
-%                 'axis equal' or if you plot onto an image created with
-%                 imshow, but it allows to plot any 2D line, not just those
-%                 that can be described as y = f(x). Optional. Default: 1
+%     perpendicularOffset - Optional. Default: 1
+%                 if 0, the offset is in y-direction only. No correction is
+%                   made
+%                 if 1, the offset is perpendicular to the line segments.
+%                   At plot points, the offset vector is the average of the
+%                   normal to the two line segments. The offset vector is
+%                   corrected so that the line has constant width. However,
+%                   unless the aspect ratio is 1 (axis equal, axis
+%                   image...), the line thickness will still look strange,
+%                   unless you set correctAspectRatio to 1 and unless you
+%                   manage to fix that option :)
+%                 if 2, the offset is in y-direction only, however, it is
+%                   corrected as in 1, so that the width of the line is
+%                   constant if you set the width to constant. This means,
+%                   that the length of the offset vector is not constant
+%                   numerically, but visually the band around the curve
+%                   looks to be the same thickness as long as the aspect
+%                   ratio is correct.
 %
 %     correctAspectRatio - if 1, expected axes limits are used to adjust the width
 %                 of the line in x/y so that it doesn't appear deformed
@@ -39,18 +51,18 @@ function h = plotTransparent(varargin)
 %
 %     ah : handles of axes into which to plot
 %
-%                 
+%
 %
 % Output: h   -    Handle to the patch object
 %
-% Example 
+% Example
 %
 %           phi = -pi:0.01:pi;
 %           figure,
 %           plotTransparent(sin(phi),cos(phi)),axis equal
 %
 %           x = [1,2,3,4,4,6];y=[1,2,1,1,2,1];
-%           plotTransparent(x,y,0.1) 
+%           plotTransparent(x,y,0.1)
 %           axis equal % if this is omitted, the plot will not look well.
 %
 % Hunter Elliott, 6/2009
@@ -148,72 +160,87 @@ end
 
 
 
-if ~perpendicularOffset
-    % Hunter's version - updated by Jonas
-    offset = [zeros(size(x,1),1),width];
-    
-else
-    % Jonas' version
-    
-    xy = [x,y];
-    
-    
-   
-    
-    % find derivative to have proper corners
-    [n_xy,e_xy] = normList(diff(xy));
-    
-     
-    
-    
-    
-    offset = [-e_xy(:,2),e_xy(:,1)];
-    
-    % check whether to use limits
-    if useLimits
-        if isempty(ah)
-            fh = figure('visible','off');
-            tmpAx = axes('Parent',fh);
-            plot(x,y);
-            aspectRatio = get(tmpAx,'DataAspectRatio');
-            delete(fh)
-        else
-            aspectRatio = get(ah,'DataAspectRatio');
+switch perpendicularOffset
+    case 0
+        % Hunter's version - updated by Jonas
+        offset = [zeros(size(x,1),1),width];
+        
+        
+    case {1,2}
+        % Jonas' version
+        
+        xy = [x,y];
+        
+        
+        
+        
+        % find derivative to have proper corners
+        [n_xy,e_xy] = normList(diff(xy));
+        
+        
+        
+        
+        
+        offset = [-e_xy(:,2),e_xy(:,1)];
+        
+        % check whether to use limits
+        if useLimits
+            if isempty(ah)
+                fh = figure('visible','off');
+                tmpAx = axes('Parent',fh);
+                plot(x,y);
+                aspectRatio = get(tmpAx,'DataAspectRatio');
+                delete(fh)
+            else
+                aspectRatio = get(ah,'DataAspectRatio');
+            end
+            stretchRatio = aspectRatio(1)/aspectRatio(2);
+            % offset gets stretched in x vs y. However, the line gets stretched
+            % in x vs y as well. Therefore, in order to look perpendicular,
+            % both x and y have to be corrected
+            offset(:,1) = offset(:,1) * (stretchRatio);
+            offset(:,2) = offset(:,2) / (stretchRatio);
+            % this gives the right angles, but unfortunately, the correction is
+            % wrong. However, I am unable to figure this out right now.
         end
-        stretchRatio = aspectRatio(1)/aspectRatio(2);
-        % offset gets stretched in x vs y. However, the line gets stretched
-        % in x vs y as well. Therefore, in order to look perpendicular,
-        % both x and y have to be corrected
-        offset(:,1) = offset(:,1) * (stretchRatio);
-        offset(:,2) = offset(:,2) / (stretchRatio);
-        % this gives the right angles, but unfortunately, the correction is
-        % wrong. However, I am unable to figure this out right now.
-    end
-    
-    % offset should be average of normal to the line segments at
-    % intersections. In order to get constant line thickness, the averaged
-    % offset vector needs to be stretched by 1/sin(alpha), otherwise, there
-    % is an apparent narrowing of the line, especially at acute angles
-    tmp = [offset(1,:);(offset(2:end,:) + offset(1:end-1,:))/2;offset(end,:)];
-    tmp(2:end-1,:) = tmp(2:end-1,:) ./...
-        (dot(offset(1:end-1,:),tmp(2:end-1,:),2) * [1,1]);
-    % Adjust with based on user input. Not using bsxfun here because of LCCB
-    offset = (width * [1 1]) .* tmp;
-    
-    
-    
+        
+        % offset should be average of normal to the line segments at
+        % intersections. In order to get constant line thickness, the averaged
+        % offset vector needs to be stretched by 1/sin(alpha), otherwise, there
+        % is an apparent narrowing of the line, especially at acute angles
+        switch perpendicularOffset
+            case 1
+                % offset vector is the average of the two line segments
+                tmp = [offset(1,:);(offset(2:end,:) + offset(1:end-1,:))/2;offset(end,:)];
+            case 2
+                % offset vector is in y-direction only
+                tmp = repmat([0,1],n,1);
+        end
+        tmp(2:end-1,:) = tmp(2:end-1,:) ./...
+            (dot(offset(1:end-1,:),tmp(2:end-1,:),2) * [1,1]);
+        if perpendicularOffset == 2
+            nanIdx = any(~isfinite(tmp),2);
+            if any(nanIdx)
+                tmp(nanIdx,:) = ones(sum(nanIdx),1) * [0,1];
+            end
+        end
+        % Adjust with based on user input. Not using bsxfun here because of LCCB
+        offset = (width * [1 1]) .* tmp;
+        
+        
+        
 end
-    
-    
-    
-    % create x,y for fill (could also use multiple patches if multiple colors
-    % are needed 
-    xFill = [x + offset(:,1); x(end:-1:1) - offset(end:-1:1,1)];
-    yFill = [y + offset(:,2); y(end:-1:1) - offset(end:-1:1,2)];
 
-    if ~isempty(ah)
-        set(gca,'DataAspectRatio',aspectRatio,'NextPlot','add')
-    end
+
+
+% create x,y for fill (could also use multiple patches if multiple colors
+% are needed
+xFill = [x + offset(:,1); x(end:-1:1) - offset(end:-1:1,1)];
+yFill = [y + offset(:,2); y(end:-1:1) - offset(end:-1:1,2)];
+
+if ~isempty(ah)
+    set(gca,'DataAspectRatio',aspectRatio,'NextPlot','add')
+end
 ph = fill(xFill,yFill,color,'EdgeColor','none','FaceAlpha',opacity);
 % hold on, plot(xFill,yFill,'k')
 % plot(x,y,'ok')
