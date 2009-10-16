7193c1fe390c8c1f1d7252a3c821cd77397faf53
Francois Aguet <francois_aguet@hms.harvard.edu>
2009-10-16 19:26:54 +0000

    Code tried to convert NaNs to 'logical' type (crashes). New version returns 'double'.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3651 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/isEven.m
+++ mathfun/isEven.m
@@ -3,21 +3,16 @@ function out = isEven(in)
 %
 % SYNOPSIS out = isEven(in)
 %
-% INPUT    in :  input (array) of numbers to be tested. IsEven only works
-%                   properly for integers. 
+% INPUT    in :  input (array) of numbers to be tested. IsEven only works properly for integers. 
 % OUTPUT   out:  array of size(in) with 
 %                   1 for even integers and zero
 %                   0 for odd integers
 %                 NaN for non-integers
 %
 % c: jonas 5/05
+% Last modified 10/16/2009 - Francois Aguet
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% out = remainder of input + 1 divided by two
-out = mod(in + 1,2);
-
-% set NaNs for not 0 or 1
-out(find(~(out==1 | out==0))) = NaN;
-
-% make logical
-out = logical(out);
+% NaNs cannont be converted to logical type (true or false only)
+out = mod(in+1, 2);
+out((out ~= 0) & (out ~= 1)) = NaN;
