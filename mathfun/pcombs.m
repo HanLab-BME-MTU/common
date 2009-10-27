8a1e00e851f56470d536948485fb35ff4fb8d63b
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2009-10-27 21:57:12 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3663 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/pcombs.m
+++ mathfun/pcombs.m
@@ -1,7 +1,7 @@
 function C = pcombs(v)
-% This function generate all pairwise combinations of v, where v is a
-% vector of length n. The number of possible pairwise combinations is
-% n(n-1) / 2.
+% This function generate all pairwise combinations of a vector v of length
+% n. The number of possible pairwise combinations is n (n - 1) / 2. The
+% elements of v must be all different.
 %
 % e.g.:
 % v = 1 2 4 3
@@ -12,6 +12,8 @@ function C = pcombs(v)
 %   1  3
 %   2  3
 %   4  3
+
+assert(min(v == unique(v)) == 1);
 
 [I J] = meshgrid(v, v');
 I = triu(I, 1);
