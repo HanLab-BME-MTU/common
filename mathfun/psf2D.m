fddbbd67ae1295843f215fca04f47287e4e46415
Francois Aguet <francois_aguet@hms.harvard.edu>
2009-11-02 20:50:26 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3682 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/psf2D.m
+++ mathfun/psf2D.m
@@ -1,37 +1,37 @@
-function M = psf2D(pixSize,NA,lambda)
+function psf = psf2D(pixelSize, NA, lambda)
 
-%PSF2D generates a 2D point spread function model (Airy disc)
+%PSF2D generates a 2-D point spread function (Airy disc)
 %
-% SYNOPSIS M = psf2D(pixSize,NA,lambda)
+% SYNOPSIS psf = psf2D(pixelSize, NA, lambda)
 %
-% INPUT    pixSize : pixel size in object space (in um)
+% INPUT    pixelSize : pixel size in object space (in um)
 %          NA      : numerical aperture of the objective lens
 %          lambda  : wavelength of light (in um)
 %
-% OUTPUT   M       : filter mask (odd dimensions) representing the Airy disk
-%                    the filter is normalized to sum(M(:)) = 1 
+% OUTPUT   psf       : filter mask (odd dimensions) representing the Airy disk
+%                      the filter is normalized to sum(psf(:)) = 1 
 %
 % NOTE     the function calculates the Airy disk radius according to 
 %          R0 = 0.61 * lambda / NA
 %          
-% REMARK - M is not normalized here!
-%
 % Alexandre Matov, January 7th, 2003
+% Francois Aguet, 11/02/2009
 
-R0 = 0.61 * lambda / NA;
-R1 = 7.02 / 3.83 * R0;    % position of the second root of the Bessel function 
+%bessel1_zero1 = 3.8317059702075;
+bessel1_zero2 = 7.0155866698156;
 
-% dim = 2*ceil(R1/pixSize)+1;
+z0 = 2*pi/lambda*NA;
 
-[x,y] = meshgrid(-ceil(R1/pixSize):ceil(R1/pixSize));
+%R0 = bessel1_zero1 / z0;
+R1 = bessel1_zero2 / z0;
 
-d = sqrt(x.^2+y.^2)+ 0.01 ;    % shift center by a 1/100 pixel to avoid division-by-0
-                               % in the next step
-ds = d*pixSize/R0*3.83;
-psfs = (besselj(1,ds)./ds);
-psf = psfs.*conj(psfs);
+b = ceil(R1/pixelSize);
+[x,y] = meshgrid(-b:b);
 
-% normalization to sum(M(:))=1;
-M = psf / sum(psf(:));                                 
-%M=psf/max(psf(:));
+z = sqrt(x.^2 + y.^2) * pixelSize * z0;
 
+psf = besselj(1,z) ./ z;
+psf = psf .* conj(psf);
+psf(b+1,b+1) = 0.25; % limit at J_1(0)/0
+
+psf = psf / sum(psf(:));
