dccfbd65773f08369755e81502848f2d4907117d
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2010-01-19 23:20:41 +0000

    Added a help section to setupMovieArray.m, and it now returns a proper error message when no movieData are found in the specified directory. Added the function clearMovieAnalysisDirector.m, which is used to remove old analysis results/directories.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3899 0bcb941b-74d1-0310-bb26-e6636707f451

--- movieManagement/setupMovieArray.m
+++ movieManagement/setupMovieArray.m
@@ -1,5 +1,31 @@
 function movieArray = setupMovieArray(parentDirectory)
 
+% 
+% movieArray = setupMovieArray;
+% 
+% movieArray = setupMovieArray(parentDirectory);
+% 
+% This function finds the movieData structure for every movie in the
+% specified parent directory. These movieData structures should be saved in
+% files named movieData.mat, and should be formatted as created by
+% setupMovieData.m
+% 
+% Input:
+% 
+%   parentDirectory - The directory containing all the movies (and
+%   movieData.mat files).
+% 
+% Output:
+% 
+%   movieArray - A cell-array containing all the movieData structures for
+%   the movies in the parent directory.
+%
+%
+%
+% Hunter Elliott
+% Sometime in 2008?
+
+
 %Get the parent directory if not input
 if nargin < 1 || isempty(parentDirectory)
     parentDirectory = uigetdir('','Select the parent directory containing all the movies:');
@@ -14,9 +40,12 @@ else
     fList = searchFiles('movieData.mat',[],parentDirectory,1,'new',1);
 end
 
-%Allow the user to select among the files found
-[iSel,selectedFiles] = listSelectGUI(fList,[],'move');
-
+if ~isempty(fList)
+    %Allow the user to select among the files found
+    [iSel,selectedFiles] = listSelectGUI(fList,[],'move');
+else
+    error('No movieData.mat files found in specified directory! Check directory!')    
+end
 %Load all of the movie data files and put them in an array
 nFiles = length(selectedFiles);
 
