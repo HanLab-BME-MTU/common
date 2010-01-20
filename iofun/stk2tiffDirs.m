efe7f5ea145f09883e5d666c33db9cc392e7e796
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-01-20 23:04:19 +0000

    Converts STK files into directories containing TIFFs. Works on multiple STKs.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3915 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ iofun/stk2tiffDirs.m
@@ -0,0 +1,35 @@
+function stk2tiffDirs(stkpath)
+%
+% function stk2tiffDirs(stkpath)
+%
+% Splits STKs in input directory into folders with TIFF files.
+%
+% Input: path to directory containing STK files
+%
+% Francois Aguet, 01/20/2010
+
+if (nargin == 0 || isempty(stkpath))
+   stkpath = uigetdir('Select directory containing the STK files:'); 
+   if (stkpath == 0)
+       return;
+   end;
+end;
+
+stkpath = [stkpath filesep];
+stkList = dir([stkpath '*.stk']);
+
+N = length(stkList);
+
+for k = 1:N
+    fprintf('Converting: %s\n', stkList(k).name);
+    stkname = strrep(stkList(k).name(1:end-4), ' ', '_');
+    dirname = [stkpath stkname filesep];
+    if ~(exist(dirname, 'dir')==7)
+        mkdir(dirname);
+    end;
+    stack = stackRead([stkpath stkList(k).name]);
+    nf = size(stack,3);    
+    for z = 1:nf
+        imwrite(stack(:,:,z), [dirname stkname '_' num2str(z, ['%0' num2str(length(num2str(nf))) 'd']) '.tif'], 'tif');
+    end;
+end;
