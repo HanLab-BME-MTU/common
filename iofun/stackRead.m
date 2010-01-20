8382b14c49c35351a7a6e581d74f5c0a37dcbdc7
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-01-20 21:31:31 +0000

    Now works without an input argument.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3913 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/stackRead.m
+++ iofun/stackRead.m
@@ -2,15 +2,20 @@ function [stack, stackinfo] = stackRead(stackpath)
 %
 % [stack, stackinfo] = stackRead(stackpath)
 %
-% Wrapper function for François Nédélec's tiffread.m
+% Wrapper function for François Nedelec's tiffread.m
 % This works on STK files as well as multipage TIFFs.
 % Outputs:
 %
 %   stack     : 3D data array
 %   stackinfo : Any other information included in original file
 %
-% François Aguet, 01/2010
+% Francois Aguet, 01/2010
 
-stackinfo = tiffread(stackpath);
+if (nargin == 0 || isempty(stackpath))
+    stackinfo = tiffread();
+else
+    stackinfo = tiffread(stackpath);
+end
+
 stack = cat(3,stackinfo(:).data);
 stackinfo = rmfield(stackinfo, 'data');
