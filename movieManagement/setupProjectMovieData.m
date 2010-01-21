c9c403ef4ebad9cf8215bb0a36e2b8394288ecb3
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2010-01-21 00:20:01 +0000

    Completely re-wrote setupProjectMovieData.m so that it supports the new movieData format. Also made it suck less in general.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3919 0bcb941b-74d1-0310-bb26-e6636707f451

--- movieManagement/setupProjectMovieData.m
+++ movieManagement/setupProjectMovieData.m
@@ -1,40 +1,53 @@
-function setupProjectMovieData(projectFolder,pixSize,timeInterval)
+function setupProjectMovieData(projectFolder,pixSize,timeInterval,forceReplace)
 
 
-% setupProjectMovieData(projectFolder,pixSize,timeInterval)
+% setupProjectMovieData(projectFolder,pixSize,timeInterval,forceReplace)
 %
-%Description:
-%A shitty little function that takes movie folders (which must be set up as
-%done in setMovieFolders.m) and creates their movieData using the same
-%pixel size and time interval on each.
+% This function takes movie folders (which must be set up as
+% done in setMovieFolders.m) and creates their movieData using the same
+% pixel size and time interval on each.
 %
-%ALPHA VERSION use with care
 %
-%Hunter Elliott 2/2009
+% Input:
+%   projectFolder - this is the folder which contains all the movie folders.
+%   This is the same folder you specified when using setupMovieFolders.m
 %
-%Input:
-%projectFolder - this is the folder which contains all the movie folders.
-%This is the same folder you specified when using setupMovieFolders.m
-%pixSize - image pixel size in nanometers for all movies in folder
-%timeInterval - time interval between images in seconds for all movies in
-%folder
+%   pixSize - image pixel size in nanometers for all movies in folder
+%
+%   timeInterval - time interval between images in seconds for all movies in
+%   folder
+%   
+%   forceReplace - If true, a new movieData will be created even if there
+%   is an existing one in the same directory. This will erase all
+%   processing that has been logged in the movieData.
+%   Optional. Default is false.
+%
+% 
+% Output: 
+% 
+%   The newly created movieData structures will be saved in each movie's
+%   analysis directory as a file named movieData.mat.
+% 
+% 
+% Hunter Elliott 
+% Re-written 1/20/2010
+%
 
+if nargin < 3 || isempty(pixSize) || isempty(timeInterval)
+    error('You must specify a project directory, a time interval and a pixel size!')
+end
 
+if nargin < 4 || isempty(forceReplace)
+    forceReplace = false;
+end
 
 %Get the folders for each movie
 movieFolders = dir([projectFolder filesep]);
+movieFolders = movieFolders(arrayfun(@(x)(x.isdir && ... %Retain only the directories. Do it this way so it works on linux and PC
+    ~(strcmp(x.name,'.') || strcmp(x.name,'..'))),movieFolders)); 
 
-nFiles = length(movieFolders);
-isDir = false(nFiles,1);
-for j = 1:nFiles
-    if movieFolders(j).isdir && ~strcmp(movieFolders(j).name,'.') && ~strcmp(movieFolders(j).name,'..')
-        isDir(j) = true;
-    end
-end
    
-
-iDir = find(isDir);
-nMovies = length(iDir);
+nMovies = length(movieFolders);
 
 
 %Set common properties
@@ -42,134 +55,84 @@ movieData.pixelSize_nm = pixSize;
 movieData.timeInterval_s = timeInterval;
 
 %Go through each and try to set up the movieData
-for j = iDir'
+for j = 1:nMovies
+            
+    disp(['Setting up movie ' num2str(j) ' of ' num2str(nMovies)])        
     
-    goAhead = 0;
+    %Re-set fields from previous movieData
+    movieData.nImages = [];
+    movieData.imSize =  [];
+    movieData.channelDirectory = {};
     
-    disp(['Setting up movie ' num2str(j-min(iDir)+1) ' of ' num2str(nMovies)])
     
-    %Reset the movie data
-    movieData.imageDirectory = [];
-    movieData.analysisDirectory = [];
-    if isfield(movieData,'channelDirectory');
-        movieData = rmfield(movieData,'channelDirectory');
-    end        
-
     %Switch to folder
     cd([projectFolder filesep movieFolders(j).name]);
     
     %Check for an existing movieData
-    if ~exist('movieData.mat','file')
+    if forceReplace || ~exist('movieData.mat','file')
     
         %Set this as the analysis folder
         movieData.analysisDirectory = pwd;    
 
-        %Look for images folder, make sure it contains images/channels
-        imCheck = dir('*images*');    
-        nDir = length(imCheck);
-        dirGood = zeros(1,nDir);    
+        %Look for sub-folder named "images"        
+        if exist([pwd filesep 'images'],'dir')
+            
+            %Set this as the movie's image directory
+            movieData.imageDirectory = [movieData.analysisDirectory filesep 'images'];
+                        
+            %Check for sub directories containing the images for each
+            %channel
+            chanDir = dir([pwd filesep 'images']);
+            chanDir = chanDir(arrayfun(@(x)(x.isdir && ... %Retain only the directories.
+                            ~(strcmp(x.name,'.') || strcmp(x.name,'..'))),chanDir));
+            
+            %Check if it is a single- or multiple-channel movie and set up the channels            
+            if ~isempty(chanDir) > 0
+            
+                %Determine the number of images and image size in each channel
+                %directory.
+                for i = 1:length(chanDir)
 
-        %Check the folders that were found
-        for k = 1:length(imCheck)        
-            %If its a folder which contains images
-            if imCheck(k).isdir && ~isempty(dir([imCheck(k).name filesep '*.tif']))
-                dirGood(k) = 1;
-            end                           
-        end
+                    imFiles = dir([pwd filesep 'images' filesep chanDir(i).name filesep '*.tif']);
 
-        %Now finish setting up the movieData             
-        if sum(dirGood) == 1
-            movieData.imageDirectory = [movieData.analysisDirectory filesep imCheck(dirGood).name];        
-            goAhead = 1;
-        elseif sum(dirGood) > 1
-            disp(['More than one sub-folder named "images" in ' movieData.analysisDirectory '...  cannot setup movieData!!'])
-        elseif sum(dirGood) == 0        
-
-            %Check if it is a multi-channel movie
-            isChanDir = zeros(1,nDir);
-            for k = 1:nDir                        
-                %Check for sub-directories containing images                
-                tmpDir = dir([movieData.analysisDirectory filesep imCheck(k).name filesep]);
-                nImages = zeros(length(tmpDir),1);
-                for m = 1:length(tmpDir)
-                    if tmpDir(m).isdir && ~strcmp(tmpDir(m).name,'.') && ~strcmp(tmpDir(m).name,'..')
-                        nImages(m,k) = length(dir([movieData.analysisDirectory filesep imCheck(k).name filesep tmpDir(m).name filesep '*.tif']));
+                    movieData.nImages(i) = length(imFiles);
+                    movieData.channelDirectory{i} = chanDir(i).name;
+                    if movieData.nImages(i) > 0
+                        %Load the header for one image to determine size
+                        imInfo = imfinfo([pwd filesep 'images' filesep chanDir(i).name filesep imFiles(1).name]);
+                        movieData.imSize(1,i) = imInfo.Height;
+                        movieData.imSize(2,i) = imInfo.Width;                
                     end
-                end
 
-                %If all the sub-directories contain same number of images
-                if any(nImages(:,k)>0)
-                    isChanDir(k) = 1;
-                end                                    
+
+                end
+            else %If it's a single-channel movie
+                movieData.channelDirectory{1} = '';
+                imFiles = dir([pwd filesep 'images' filesep '*.tif']);
+                movieData.nImages = length(imFiles);
+                if movieData.nImages > 0
+                    %Load the header for one image to determine size
+                    imInfo = imfinfo([pwd filesep 'images' filesep imFiles(1).name]);
+                    movieData.imSize(1,1) = imInfo.Height;
+                    movieData.imSize(2,1) = imInfo.Width;                
+                end                                
             end
-
-            %Make sure there was only one found
-            if sum(isChanDir) == 1
-
-                %Set up the folder and sub-folders
-                movieData.imageDirectory = [movieData.analysisDirectory filesep imCheck(isChanDir).name];
-                tmpDir = dir([movieData.imageDirectory filesep]);
-                nGood = 0;
-                for m = 1:length(tmpDir)
-                    if nImages(m,isChanDir) > 0
-                        nGood = nGood + 1;
-                        movieData.channelDirectory{nGood} = tmpDir(m).name;
-                    end
-                end
-                goAhead = 1;
-
-            elseif sum(isChanDir) > 1
-                disp(['More than one images folder with sub-directories containing images was found in ' movieData.analysisDirectory '...  cannot setup movieData!!'])
+            %Make sure at least one image was found!
+            if sum(movieData.nImages(:)) > 0
+                %If everything worked out, save the newly created
+                %movieData.
+                updateMovieData(movieData);
             else
-                disp(['Could not find a suitable image directory in ' movieData.analysisDirectory '...  cannot setup movieData!!'])                        
-            end
-
-
-
-
-            %Check if a frame number is specified in the directory name. This
-            %is basically only for use with stimulation/drug perfusion
-            %experiments. 
-
-
-
-            %Get only the directory from the path:
-            r1 = regexpi(movieData.analysisDirectory,filesep);                
-            iLastFileSep = max(r1);
-            fName = movieData.analysisDirectory(iLastFileSep+1:end);
-            r2 = regexpi(fName,'_');
-            iLastUscore = max(r2);        
-            termString = fName(iLastUscore+1:end);
-
-            %If there was something after the underscore...
-            if ~isempty(termString)
-
-                frameNum = str2num(termString);                        
-
-                %Check if it is an integer, and if so store it in movieData
-                if ~isempty(frameNum) && ~isnan(frameNum) && round(frameNum) == frameNum
-                    movieData.stimulation.iFrame = frameNum;
-                end        
-
-            end
-
-
+               disp(['No images found in the image directory for movie ' pwd ' - cannot setup movieData!'])
+            end            
+        else
+           disp(['Movie folder ' pwd ' has no sub-directory named "images" - cannot setup movieData!']) 
         end
-
-        %Specify the shade correction directories
-        movieData.FRETprocessing.fretShadeDirectory = [movieData.analysisDirectory filesep 'fretShade'];
-        movieData.FRETprocessing.dicShadeDirectory = [movieData.analysisDirectory filesep 'dicShade'];    
-        movieData.FRETprocessing.donorShadeDirectory = [movieData.analysisDirectory filesep 'cfpShade'];
-
-        if goAhead
-            %Finalize and save the movieData
-            setupMovieData(movieData);
-        end    
-
     else
         disp(['Movie ' num2str(j) ' of ' num2str(nMovies) ' already had a movieData.mat! Doing nothing...'])
         
     end
 end
+
 %Return to original folder
 cd(projectFolder);
