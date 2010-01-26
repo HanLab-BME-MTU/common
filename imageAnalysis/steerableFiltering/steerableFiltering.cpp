1224ee04fdd42f8ea6a68470bbed346c8e24df57
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-01-26 20:22:39 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@3965 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ imageAnalysis/steerableFiltering/steerableFiltering.cpp
@@ -0,0 +1,108 @@
+#include <mex.h>
+
+#include <image.hpp>
+#include <compute_nms.hpp>
+#include <steerable_filtering.hpp>
+
+void mexFunction(int nlhs, mxArray *plhs[],
+		 int nrhs, const mxArray *prhs[])
+{
+  /////////////////////////////////////////////////
+  // Check number of input and output parameters //
+  /////////////////////////////////////////////////
+
+  if (nrhs < 3)
+    mexErrMsgTxt("Three input arguments required.");
+
+  if (nlhs > 3)
+    mexErrMsgTxt("Too many output arguments");
+
+  ////////////////////////////
+  // Check input parameters //
+  ////////////////////////////
+
+  if (mxGetNumberOfDimensions(prhs[0]) != 2)
+    mexErrMsgTxt("Invalid dimension for I argument.");
+
+  if (!mxIsDouble(prhs[0]))
+    mexErrMsgTxt("I is not a double-precision matrix.");
+
+  if (!mxIsChar(prhs[1]))
+    mexErrMsgTxt("method argument is not a string.");
+
+  if (mxGetNumberOfElements(prhs[2]) != 1)
+    mexErrMsgTxt("Invalid dimension for sigma argument.");
+
+  //////////////////////////
+  // Get input parameters //
+  //////////////////////////
+
+  const mwSize* size = mxGetDimensions(prhs[0]);
+  double* ptr = mxGetPr(prhs[0]);
+  image<double> ima(ptr, size[1], size[0]);
+
+  int str_length = mxGetNumberOfElements(prhs[1]);
+
+  char* buf = new char[str_length];
+
+  mxGetString(prhs[1], buf, str_length + 1);
+
+  std::string method_name(buf, str_length);
+
+  delete[] buf;
+
+  const std::string valid_method_names[] = {
+    "2ndGaussian",
+    "UnserM1",
+    "UnserM2"
+  };
+
+  int method_id = 0;
+
+  while (method_id < 3 && method_name != valid_method_names[method_id])
+    ++method_id;
+
+  if (method_id == 3)
+    mexErrMsgTxt((method_name + " is not a valid method").c_str());
+
+  ptr = mxGetPr(prhs[2]);
+
+  if (*ptr <= 0)
+    mexErrMsgTxt("sigma must be strictly positive.");
+
+  double sigma = *ptr;
+  
+  ///////////////////////
+  // Compute filtering //
+  ///////////////////////
+  
+  image<double> res(ima.width(), ima.height(), 2); // for nms safety
+  image<double> theta(ima.width(), ima.height());
+
+  switch (method_id)
+    {
+    case 0: snd_gaussian_filtering(ima, sigma, res, theta); break;
+    case 1: unser_m1_filtering(ima, sigma, res, theta); break;
+    case 2: unser_m2_filtering(ima, sigma, res, theta); break;
+    }
+
+  ////////////////////////////
+  // Allocate output arrays //
+  ////////////////////////////
+
+  if (nlhs > 0) res.image2mxArray(plhs[0]);
+  if (nlhs > 1) theta.image2mxArray(plhs[1]);
+
+  if (nlhs > 2)
+    {
+      /////////////////////////////////////
+      // Compute non-maximal suppression //
+      /////////////////////////////////////
+
+      image<double> nms(ima.width(), ima.height());
+  
+      compute_nms(res, theta, nms);
+
+      nms.image2mxArray(plhs[2]);
+    }
+}
---
+++ imageAnalysis/tweakTransform.m
@@ -0,0 +1,50 @@
+function imProd = tweakTransform(baseImage,inImage,dX)
+
+%This function calculates the mean squared error between two images after
+%one is transformed by the transformation specifed by the vector dX. The
+%vector should be a vectorized version of the transformation matrix as used
+%by t imtransform. 
+
+%Assemble the transformation matrix
+tMat = zeros(3);
+
+%If limited to homogenous matrices and projective transformations
+
+%Check the number of parameters input and use this to determine the
+%transformation type.
+if length(dX(:)) == 2
+    tMat = eye(3);
+    tMat(3,1:2) = dX(:)';
+    tType = 'projective';    
+elseif length(dX(:)) == 6
+    tMat(3,3) = 1;
+    tMat(1:6) = dX(:);
+    tType = 'projective';
+elseif length(dX(:)) == 9;   %If non-homogenous allowed in proj. xform
+    tMat(:) = dX(:);
+    tType = 'projective';
+elseif length(dX(:)) == 20;  %If polynomial transform
+    tType = 'polynomial';
+    tMat = zeros(10,2);
+    tMat(:) = dX(:);
+end
+
+%Create the transform
+if strcmp(tType,'projective')
+    xForm = maketform('projective',tMat);
+else
+    xForm = cp2tform(ones(10,2),ones(10,2)+rand(10,2)/10,'polynomial',3);
+    xForm.tdata = tMat;
+end
+
+%Transform the input image with the new transformation
+inImage = imtransform(inImage,xForm,'XData',[1 size(baseImage,2)],'YData',[1 size(baseImage,1)],'FillValues',1);
+
+%Calculate the product of the two images. Ignore a border around the edge
+%to prevent biasing by the filled-in pixels not present in image
+brdr = 20;
+imDiff = abs(baseImage(brdr:end-brdr,brdr:end-brdr) - inImage(brdr:end-brdr,brdr:end-brdr));
+imProd = sum(sum(imDiff ./ baseImage(brdr:end-brdr,brdr:end-brdr)));
+
+
+
