e38810df5fa3f0c5bef6c5ca40cbbf4b5e7d0e97
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-02-01 19:21:53 +0000

    Corrected a bug with constant border extension.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4021 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/padarrayXT.m
+++ mathfun/padarrayXT.m
@@ -314,7 +314,7 @@ if nargin > 2
         end
     end
 end
-    
+
 % Check the input array type
 if strcmp(method,'constant') && ~(isnumeric(a) || islogical(a))
     id = sprintf('Images:%s:badTypeForConstantPadding', mfilename);
@@ -322,3 +322,14 @@ if strcmp(method,'constant') && ~(isnumeric(a) || islogical(a))
     msg2 = 'to be numeric or logical for constant padding.';
     error(id,'%s\n%s',msg1,msg2);
 end
+
+
+function out = mkconstarray(class, value, size)
+%MKCONSTARRAY creates a constant array of a specified numeric class.
+%   A = MKCONSTARRAY(CLASS, VALUE, SIZE) creates a constant array 
+%   of value VALUE and of size SIZE.
+
+%   Copyright 1993-2003 The MathWorks, Inc.  
+%   $Revision: 1.8.4.1 $  $Date: 2003/01/26 06:00:35 $
+
+out = repmat(feval(class, value), size);
