ef4f9fd8c03c86ac10bb9cc5772480ecf32f911e
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-02-02 20:44:41 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4031 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageProcessing/fastGauss3D.m
+++ imageProcessing/fastGauss3D.m
@@ -28,7 +28,7 @@ function out=fastGauss3D(img,sigma,fSze,correctBorder,filterMask,reduceNanImage)
 % revamped by jonas
 
 %===============
-% check input
+%% check input
 %===============
 if nargin < 1 || isempty(img);
     error('Please pass nonempty image to fastGauss3D')
@@ -86,12 +86,6 @@ if correctBorder == 2
         correctBorder = 1;
     else
         addBorderOld;
-        
-        halFsze = zeros(1,3);
-        halFsze(1:length(fSze)) = floor(fSze/2); %half filter size
-        
-        
-        img = padarrayXT(img, [halFsze halFsze halFsze], 'symmetric');
         convnOpt = 'valid';
     end
 end
@@ -99,7 +93,7 @@ if correctBorder == 1
     %correct for border effects. Create nanMask first. It's a logical mask
     %that takes up 1/8th the space of the original. As long as more than
     %1/8th of the image contains Nan, it's smaller than a list of indices
-    nanMask = isnan(img);
+    nanMask = isnan(img); 
     fullMask = [];
     if reduceNanImage && any(nanMask(:))
         goodRCZcell = cell(dims,1);
@@ -117,9 +111,9 @@ if correctBorder == 1
             nanMask = nanMask(goodRCZcell{:});
         end
     end
-    
+        
     % pass nanMask so that we don't need to recalc again
-    img = padarrayXT(img, floor(fSze/2)*ones(1,dims), 'symmetric');
+    img = addBorder(img,floor(fSze/2),nanMask);
     convnOpt = 'valid';
 end
 
@@ -151,12 +145,130 @@ switch dims
 end
 
 
-if ~isempty(nanMask) && reduceNanImage && ~isempty(fullMask)
-    % rebuild full image
-    img = out;
-    out = NaN(size(fullMask));
-    out(~fullMask) = img(~nanMask);
-else
-    % simply ensure that NaNs are in the right place
+if ~isempty(nanMask)
+    if reduceNanImage && ~isempty(fullMask)
+        % rebuild full image
+        img = out;
+        out = NaN(size(fullMask));
+        out(~fullMask) = img(~nanMask);
+    else
+        % simply ensure that NaNs are in the right place
     out(nanMask) = NaN;
+    end
+end
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    function addBorderOld
+        %ADDBORDER adds a border around 3D images for filtering
+        %adds a border of halFsze around the img, and fill it with pixels whose
+        %value is computed as follows:
+        %for all 3 dimensions i (=6 sides) of the array, the nanmedian of the first or last
+        %halFsze(i)+1 pixels in this dimension, respectively, is computed. The
+        %edges and corners are filled with means of the adjacent sides or edges,
+        %respectively
+        %c: 030514 jonas
+
+        %init
+        halFsze = zeros(1,3);
+        halFsze(1:length(fSze)) = floor(fSze/2); %half filter size
+        oldSize = size(img);
+        if length(oldSize) < 3
+            error('image too small for 3D filtering: no 3rd dimension!')
+        elseif any(oldSize < halFsze+1)
+            error('image too small for 3D filtering: filter is larger than image!')
+        end
+
+        newImg = zeros(oldSize+2*halFsze);
+        newImg(halFsze(1)+1:end-halFsze(1),halFsze(2)+1:end-halFsze(2),halFsze(3)+1:end-halFsze(3)) = img;
+
+        %alternatively, this could be made with feval and for-loop, but it would be a
+        %chore, too
+
+        %2 sides first direction
+        inSide11 = img(1:halFsze(1)+1,:,:);
+        side11val = nanmedian(inSide11(:));
+        newImg(1:halFsze(1),halFsze(2)+1:end-halFsze(2),halFsze(3)+1:end-halFsze(3)) = side11val;
+
+        inSide12 = img(end-halFsze(1):end,:,:);
+        side12val = nanmedian(inSide12(:));
+        newImg(end-halFsze(1)+1:end,halFsze(2)+1:end-halFsze(2),halFsze(3)+1:end-halFsze(3)) = side12val;
+        %2 sides second direction
+        inSide21 = img(:,1:halFsze(2)+1,:);
+        side21val = nanmedian(inSide21(:));
+        newImg(halFsze(1)+1:end-halFsze(1),1:halFsze(2),halFsze(3)+1:end-halFsze(3)) = side21val;
+
+        inSide22 = img(:,end-halFsze(2):end,:);
+        side22val = nanmedian(inSide22(:));
+        newImg(halFsze(1)+1:end-halFsze(1),end-halFsze(2)+1:end,halFsze(3)+1:end-halFsze(3)) = side22val;
+
+        %2 sides third direction
+        inSide31 = img(:,:,1:halFsze(3)+1);
+        side31val = nanmedian(inSide31(:));
+        newImg(halFsze(1)+1:end-halFsze(1),halFsze(2)+1:end-halFsze(2),1:halFsze(3)) = side31val;
+
+        inSide32 = img(:,:,end-halFsze(3):end);
+        side32val = nanmedian(inSide32(:));
+        newImg(halFsze(1)+1:end-halFsze(1),halFsze(2)+1:end-halFsze(2),end-halFsze(3)+1:end) = side32val;
+
+        %4 edges parallel to first direction
+        edge2131val = nanmean([side21val,side31val]);
+        newImg(:,1:halFsze(2),1:halFsze(3)) = edge2131val;
+
+        edge2231val = nanmean([side22val,side31val]);
+        newImg(:,end-halFsze(2)+1:end,1:halFsze(3)) = edge2231val;
+
+        edge2132val = nanmean([side21val,side32val]);
+        newImg(:,1:halFsze(2),end-halFsze(3)+1:end) = edge2132val;
+
+        edge2232val = nanmean([side22val,side32val]);
+        newImg(:,end-halFsze(2)+1:end,end-halFsze(3)+1:end) = edge2232val;
+        % 4 edges parallel to second direction
+        edge1131val = nanmean([side11val,side31val]);
+        newImg(1:halFsze(1),:,1:halFsze(3)) = edge1131val;
+        edge1231val = nanmean([side12val,side31val]);
+        newImg(end-halFsze(1)+1:end,:,1:halFsze(3)) = edge1231val;
+        edge1132val = nanmean([side11val,side32val]);
+        newImg(1:halFsze(1),:,end-halFsze(3)+1:end) = edge1132val;
+        edge1232val = nanmean([side12val,side32val]);
+        newImg(end-halFsze(1)+1:end,:,end-halFsze(3)+1:end) = edge1232val;
+        % 4 edges parallel to third direction
+        edge1121val = nanmean([side11val,side21val]);
+        newImg(1:halFsze(1),1:halFsze(2),:) = edge1121val;
+
+        edge1221val = nanmean([side12val,side21val]);
+        newImg(end-halFsze(1)+1:end,1:halFsze(2),:) = edge1221val;
+
+        edge1122val = nanmean([side11val,side22val]);
+        newImg(1:halFsze(1),end-halFsze(2)+1:end,:) = edge1122val;
+        edge1222val = nanmean([side12val,side22val]);
+        newImg(end-halFsze(1)+1:end,end-halFsze(2)+1:end,:) = edge1222val;
+        %corner 000
+        newImg(1:halFsze(1),1:halFsze(2),1:halFsze(3)) = nanmean([edge2131val,edge1131val,edge1121val]);
+        %corner 100
+        newImg(end-halFsze(1)+1:end,1:halFsze(2),1:halFsze(3)) = nanmean([edge2131val,edge1231val,edge1221val]);
+        %corner 010
+        newImg(1:halFsze(1),end-halFsze(2)+1:end,1:halFsze(3)) = nanmean([edge2231val,edge1131val,edge1122val]);
+
+        %corner 110
+        newImg(end-halFsze(1)+1:end,end-halFsze(2)+1:end,1:halFsze(3)) = nanmean([edge2231val,edge1231val,edge1222val]);
+
+        %corner 001
+        newImg(1:halFsze(1),1:halFsze(2),end-halFsze(3)+1:end) = nanmean([edge2132val,edge1132val,edge1121val]);
+
+        %corner 101
+        newImg(end-halFsze(1)+1:end,1:halFsze(2),end-halFsze(3)+1:end) = nanmean([edge2132val,edge1232val,edge1221val]);
+
+        %corner 011
+        newImg(1:halFsze(1),end-halFsze(2)+1:end,end-halFsze(3)+1:end) = nanmean([edge2232val,edge1132val,edge1122val]);
+        %corner 111
+        newImg(end-halFsze(1)+1:end,end-halFsze(2)+1:end,end-halFsze(3)+1:end) = nanmean([edge2232val,edge1232val,edge1222val]);
+
+
+        % assign img
+        img = newImg;
+    end
 end
