2ba381b85c02761e1027dce38f248fb5e1be3f6f
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-02-09 01:10:15 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4066 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ mathfun/dLSegment2D.m
@@ -0,0 +1,40 @@
+function F = dLSegment2D(X, Y, xc, yc, A, sigmaPSF, l, theta)
+% Diffraction-limited Segment Model
+% F = dLSegment2D(X, Y, xc, xy, amplitude, sigmaPSF, l, theta);
+%
+% parameters:
+% (X, Y)       2 vectors representing the 2-dimensional domain (e.g X =
+%              -10:.1:10, Y = -5:.1:5
+%
+% (xc,yc)      center of the segment
+%
+% A            amplitude of the segment
+%
+% sigmaPSF     half width of the gaussian PSF model.
+%
+% l            length of the segment
+%
+% theta        orientation of the segment
+%
+% output:
+% F is a NxM matrix where N = numel(X) and M = numel(Y).
+
+ct = cos(theta);
+st = sin(theta);
+
+lDiv2 = l / 2;
+
+c0 = sqrt(2) * sigmaPSF;
+c1 = sqrt(2) / (sigmaPSF * pi) * erf(lDiv2 / c0);
+c1 = A / c1 * 1 / (c0 * pi);
+
+[X Y] = meshgrid(X,Y);
+
+X = X - xc;
+Y = Y - yc;
+
+F = c1 * exp(-((st * X - ct * Y) / c0).^2) .* ...
+         (erf((lDiv2 - ct * X - st * Y) / c0) + ...
+          erf((lDiv2 + ct * X + st * Y) / c0));
+      
+end
