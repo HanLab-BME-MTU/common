743f9d5d4ad13b8b6328373c8ef6f00a6450dc7d
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2010-02-18 15:35:00 +0000

    Added a function for renaming files (usually produced by metamorph) with endings like _t1 , _t2 etc. to _t01, _t02 etc. so that the dir and ls commands return them in the correct order.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4176 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ iofun/fixImageNumbers.m
@@ -0,0 +1,47 @@
+function fixImageNumbers(directory)
+% 
+% fixImageNumbers(directory)
+% 
+% This function goes through every numbered image file in the specified
+% directory and renames files which have an ending like _t1, _t2 etc. To
+% _t01, _t02 so that dir/ls returns them in the correct order.
+% 
+% Hunter Elliott, 11/2009
+%
+
+stackFiles = imDir(directory);
+
+nStack = length(stackFiles);
+
+nDig = floor(log10(nStack))+1;
+fString = ['%0' num2str(nDig) '.0f'];
+
+
+if nStack > 0
+    
+    disp(['Renaming ' num2str(nStack) ' image files...'])
+    
+    for i = 1:nStack
+        
+        %Get the index of the last t
+        iT = max(regexp(stackFiles(i).name,'t'));
+        
+        iFrame = str2double(stackFiles(i).name(iT+1:end-4));
+        
+        newName = [stackFiles(i).name(1:iT) num2str(iFrame,fString) stackFiles(i).name(end-4:end)];
+        
+        if ~strcmp(newName,stackFiles(i).name)
+            movefile([directory filesep stackFiles(i).name],...
+                [directory filesep newName]);                
+        end
+    end
+else 
+    disp('No image files found in specified directory...')
+    return
+end
+
+disp('Finished!')
+
+
+
+
