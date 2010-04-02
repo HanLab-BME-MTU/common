8039e6dd6655ecf07164e0042d9a8d87923c39b1
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2010-04-02 21:08:51 +0000

    added a function for determining the topology/graph structure of binary skeleton images
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4431 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ imageProcessing/skel2graph.m
@@ -0,0 +1,125 @@
+function [vertices,edges] = skel2graph(skelIn,nConn)
+%SKEL2GRAPH converts a binary 3D skeleton matrix into a graph structure with nodes and edges 
+% 
+% [vertices,edges] = skel2graph(skelIn)
+% 
+% [vertices,edges] = skel2graph(skelIn,nConn)
+% 
+% Input:
+% 
+%   skelIn - The binary, 3D matrix containing a skeleton (maximum width is
+%   1)
+% 
+%   nConn - Optional. The connectivity number to use (6,18 or 26)
+% 
+% 
+% Output:
+% 
+%   vertices - A Mx3 matrix of the coordinates of vertices in the
+%   skeleton/graph (end-points or junction points), where M is the number
+%   of vertices
+% 
+%   edges - An Nx2 matrix of the index of the vertices that each edge
+%   connects.
+% 
+% 
+% Hunter Elliott
+% 4/2/2010
+
+
+%% ---------- Input ----------- %%
+
+%NOTE: At some point this should be generalized to 2D or 3D. Also, the
+%exact paths the edges follow should be returned.
+
+showPlots = true;
+
+if nargin < 2 || isempty(nConn)
+    nConn = 26;
+end
+
+
+%Validate that the input mask is in fact a skeleton(not too thick?)??
+
+
+%% ------- Detection -----  %%
+%Detect edges and vertices
+
+%Get the neighborhood for this connectivity
+nHood = bwnHood3D(nConn);
+
+%Calculate local neighbor number
+nNeighbors = bwNneighbors(skelIn,nHood);
+
+%Find end-points
+vertMat = (nNeighbors == 1) & skelIn;
+
+%Find junction points
+junctionPoints = (nNeighbors > 2) & skelIn;
+
+%Combine the end-points and junctions and label them
+[vertMat,nVerts] = bwlabeln(vertMat | junctionPoints,nConn);
+
+%Get edges
+edgeMat = nNeighbors == 2 & skelIn;
+
+%Label these edges
+[edgeMat,nEdges] = bwlabeln(edgeMat,nConn);
+
+
+%% ------ Connectivity ----- %%
+%Determine which edges are connected to which vertices
+
+edges = zeros(nEdges,2);
+vertices = zeros(nVerts,3);
+
+%Go through each vertex (these may in fact be clusters of points depending
+%on the connectivity and skeleton structure)
+for j = 1:nVerts
+    
+    %Get the index of the point(s)
+    currInd = find(vertMat == j);
+    
+    %Convert this to matrix coord
+    [currM,currN,currP] = ind2sub(size(skelIn),currInd);
+    
+    %Average/store these coord
+    vertices(j,:) = [mean(currM),mean(currN),mean(currP)];        
+
+
+end
+
+
+%Go through each edge...
+for j = 1:nEdges
+            
+    %Find vertices which this edge connects
+    edges(j,:) = unique(vertMat(imdilate(edgeMat == j,nHood) & vertMat));           
+  
+    
+end
+    
+
+%RETURN EDGE PATHS ALSO???
+
+
+if showPlots
+   
+    fsFigure(.5);
+    title('Edges')    
+    hold on
+    spy3d(skelIn,'.k')
+    cols = jet(nEdges);    
+    arrayfun(@(x)(spy3d(edgeMat == x & skelIn,'.','color',cols(x,:),'MarkerSize',15)),1:nEdges)
+    
+    
+    fsFigure(.5);
+    title('Vertices')
+    hold on
+    spy3d(skelIn,'.k')
+    cols = jet(nVerts);    
+    arrayfun(@(x)(spy3d(vertMat == x & skelIn,'.','color',cols(x,:),'MarkerSize',15)),1:nVerts)
+    
+    
+end
+
