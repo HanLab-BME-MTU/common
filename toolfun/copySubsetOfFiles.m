7d186ca8705191bc6d0881fa74c432297ea7e467
Khuloud Jaqaman <khuloud_jaqaman@hms.harvard.edu>
2010-04-05 21:33:24 +0000

    added new function copySubsetOfFiles, similar to moveSubsetOfFiles
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4439 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/copySubsetOfFiles.m
+++ toolfun/copySubsetOfFiles.m
@@ -1,18 +1,18 @@
-function moveSubsetOfFiles(dirSourceTarget,indices2move,filenameBase,...
+function copySubsetOfFiles(dirSourceTarget,indices2copy,filenameBase,...
     filenameExt,digits4Enum,renumber)
-%MOVESUBSETOFFILES moves a subset of files from one directory to another, with the possibility of renumbering
+%COPYSUBSETOFFILES copies a subset of files from one directory to another, with the possibility of renumbering
 %
-%SYNPOSIS moveSubsetOfFiles(dirSourceTarget,indices2move,filenameBase,...
+%SYNPOSIS copySubsetOfFiles(dirSourceTarget,indices2copy,filenameBase,...
 %    filenameExt,digits4Enum,renumber)
 %
 %INPUT  dirSourceTarget: 2-by-1 cell array indicating source directory to
-%                     move files from and target directory to move files
+%                     copy files from and target directory to copy files
 %                     to.
-%       indices2move: Row vector of file indices to move.
+%       indices2copy: Row vector of file indices to copy.
 %       filenameBase: File name base (everything before the numbers).
 %       filenameExt : File name extension (everything after the ".").
 %       digits4Enum : Number of digits used to enumerate files.
-%       renumber    : 1 to renumber the moved files, starting with 1 and
+%       renumber    : 1 to renumber the copied files, starting with 1 and
 %                     using increments of 1; 0 otherwise. When files are
 %                     renumbered, "new_" is inserted after the filenameBase
 %                     and before the numbers.
@@ -20,13 +20,13 @@ function moveSubsetOfFiles(dirSourceTarget,indices2move,filenameBase,...
 %
 %OUTPUT NONE
 %
-%Khuloud Jaqaman, February 2010
+%Khuloud Jaqaman, April 2010
 
 %% Input
 
 %check whether correct number of input arguments was used
 if nargin < 5
-    disp('--moveSubsetOfFiles: Incorrect number of input arguments!');
+    disp('--copySubsetOfFiles: Incorrect number of input arguments!');
     return
 end
 
@@ -42,8 +42,8 @@ end
 %% Action!
 
 %to keep the user informed of progress...
-progressText(0,'Moving files');
-numFiles2move = length(indices2move);
+progressText(0,'Copying files');
+numFiles2copy = length(indices2copy);
 iDummy = 0;
 
 %based on renumbering choice ...
@@ -52,39 +52,39 @@ if renumber %if file names are renumbered
     %initialize index for renumbering
     iNew = 0;
     
-    %go over files to be moved
-    for iFile = indices2move
+    %go over files to be copied
+    for iFile = indices2copy
         
         %get file name
-        file2move = [filenameBase sprintf(['%0' num2str(digits4Enum) 'i'],iFile) '.' filenameExt];
+        file2copy = [filenameBase sprintf(['%0' num2str(digits4Enum) 'i'],iFile) '.' filenameExt];
         
         %construct new file name
         iNew = iNew + 1;
         newName = [filenameBase 'new_' sprintf(['%0' num2str(digits4Enum) 'i'],iNew) '.' filenameExt];
         
-        %move file
-        movefile(fullfile(sourceDir,file2move),fullfile(targetDir,newName));
+        %copy file
+        copyfile(fullfile(sourceDir,file2copy),fullfile(targetDir,newName));
         
         %to keep the user informed of progress...
         iDummy = iDummy + 1;
-        progressText(iDummy/numFiles2move,'Moving files');
+        progressText(iDummy/numFiles2copy,'Copying files');
         
     end
     
 else %if file names stay the same
     
-    %go over files to be moved
-    for iFile = indices2move
+    %go over files to be copied
+    for iFile = indices2copy
         
         %get file name
-        file2move = [filenameBase sprintf(['%0' num2str(digits4Enum) 'i'],iFile) '.' filenameExt];
+        file2copy = [filenameBase sprintf(['%0' num2str(digits4Enum) 'i'],iFile) '.' filenameExt];
         
-        %move file
-        movefile(fullfile(sourceDir,file2move),targetDir);
+        %copy file
+        copyfile(fullfile(sourceDir,file2copy),targetDir);
         
         %to keep the user informed of progress...
         iDummy = iDummy + 1;
-        progressText(iDummy/numFiles2move,'Moving files');
+        progressText(iDummy/numFiles2copy,'Copying files');
         
     end
     
