5843709d6df9e683eb2f8367845acecaef85e98a
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-04-06 21:20:39 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4449 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/locmax1d.m
+++ mathfun/locmax1d.m
@@ -1,17 +1,34 @@
-function indx=locmax1d(x)
-%LOCMAX1D returns a list of local maxima in vector x
+function indx=locmax1d(x, winSize)
+%LOCMAX1D returns a list of local maxima in vector x within a window.
 %
-% SYNOPSIS indx=locmax1d(x)
+% SYNOPSIS indx=locmax1d(x, n)
 %
-% INPUT x : data vector
+% INPUT 
+%   x:  data vector
+%
+%   winSize:  (optional) length of the window. Must be odd. Default value
+%   is 3
 %
 % OUTPUT indx : index list to local maxima in x
 
 % STARTED GD 29-Nov-1999
+% MODIFIED SB 6-Apr-2010
 
-indx = [];
-for( i = 2:(length(x)-1))
-   if(x(i)>x(i-1) & x(i)>x(i+1))
-      indx = [indx,i];
-   end;
-end;
+if nargin > 1 && ~isempty(winSize)
+    if mod(winSize,2) == 0
+        error('winSize must be odd.');
+    end
+else
+    winSize = 3;
+end
+
+hside = floor(winSize/2);
+n = length(x);
+
+ind = hside+1:n-hside;
+
+xMax = arrayfun(@(pos) max(x(pos-hside:pos+hside)), ind);
+
+indx = find(x(ind) == xMax(:)) + hside;
+
+
--- mathfun/locmin1d.m
+++ mathfun/locmin1d.m
@@ -1,17 +1,32 @@
-function indx=locmin1d(x)
-%LOCMIN1D returns a list of local minima in vector x
+function indx=locmin1d(x,winSize)
+%LOCMIN1D returns a list of local minima in vector x within a window.
 %
-% SYNOPSIS indx=locmin1d(x)
+% SYNOPSIS indx=locmin1d(x, n)
 %
-% INPUT x : data vector
+% INPUT 
+%   x:  data vector
+%
+%   winSize:  (optional) length of the window. Must be odd. Default value
+%   is 3
 %
 % OUTPUT indx : index list to local minima in x
 
 % STARTED GD 29-Nov-1999
+% MODIFIED SB 6-Apr-2010
 
-indx = [];
-for( i = 2:(length(x)-1))
-   if(x(i)<x(i-1) & x(i)<x(i+1))
-      indx = [indx,i];
-   end;
-end;
+if nargin > 1 && ~isempty(winSize)
+    if mod(winSize,2) == 0
+        error('winSize must be odd.');
+    end
+else
+    winSize = 3;
+end
+
+hside = floor(winSize/2);
+n = length(x);
+
+ind = hside+1:n-hside;
+
+xMax = arrayfun(@(pos) min(x(pos-hside:pos+hside)), ind);
+
+indx = find(x(ind) == xMax(:)) + hside;
