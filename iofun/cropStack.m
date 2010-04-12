d0e31e1acdfd9c7f52bfdf56608b765fce8ed7c5
Achim Besser <achim.besser@hms.harvard.edu>
2010-04-12 16:17:53 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4466 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/cropStack.m
+++ iofun/cropStack.m
@@ -1,7 +1,7 @@
-function area=cropStackNoCompression(area,path)
+function area=cropStack(area,output_path,first_image_path,num_images_to_crop)
 % cropStack lets the user crop a region of interest from an image stack
 %
-% SYNOPSIS   area=cropStack(area,path)
+% SYNOPSIS   area=cropStackk(area,output_path,first_image_path,num_images_to_crop)
 %
 % INPUT      area [y0 x0 y x]
 %                           y0 : y coordinate of the top-left corner
@@ -10,79 +10,103 @@ function area=cropStackNoCompression(area,path)
 %                           x  : x coordinate of the bottom-right corner
 %            Pass area=[] to manually draw a region of interest
 %
-%            path (optional) output path - if path is not passed, the
+%            output_path (optional) output path - if path is not passed, the
 %            user will be prompted to select the path through a save
 %            dialog. If a directory is specified which does not exist, 
 %            it will be created (if possible).
+%
+%            first_image_path (optional) is the path to the first image to
+%            be cropped. If path is not passed, the user will be prompted
+%            to select the path through a save dialog.
+%
+%            num_images_to_crop (optional). 
+%            num_images_to_crop=[] : will prompt you to enter the number of images to
+%                                    be cropped.
+%            num_images_to_crop=0  : all images will be cropped.
+%            num_images_to_crop=n  : n images will be cropped. 
 
 % Check input parameters
 if nargin==0
    error('Not enough input arguments');
-elseif nargin==1
+elseif nargin==1 || isempty(output_path)
    manualSave=1;
-elseif nargin==2
-    manualSave=0;
-    if isempty(path) % In case the user passed an empty path instead of dropping it
-        manualSave=1;
-    end
 else
-    error('Wrong number of arguments');
+   manualSave=0;
 end
 
 % Creating output directory if needed
-if manualSave==0 & exist(path)~=7
+if manualSave==0 & exist(output_path)~=7
     % Directory does not exist - create it
-    if path(2)==':' % Compatibility with Windows OS
+    if output_path(2)==':' % Compatibility with Windows OS
         % Drive letter specified
-        mkdir(path(1:3),path(4:end));
+        mkdir(output_path(1:3),output_path(4:end));
     else
-        mkdir(path);
+        mkdir(output_path);
     end
-    fprintf(1,'Directory %s successfully created.\n',path);
+    fprintf(1,'Directory %s successfully created.\n',output_path);
 end
 
-% Load First image
-[fName,dirName] = uigetfile(...
-    {'*.tif;*.tiff;*.jpg;*.jpeg;*.TIF','Image Files (*.tif,*.tiff,*.jpg,*.jpeg)';
-    '*.tif','TIF files (*.tif)'
-    '*.tiff','TIFF files (*.tiff)'
-    '*.jpg;','JPG files (*.jpg)'
-    '*.jpeg;','JPEG files (*.jpeg)'
-    '*.*','All Files (*.*)'},...
-    'Select first image');
-if(isa(fName,'char') & isa(dirName,'char'))
-    % Recover all file names from the stack
-    outFileList=getFileStackNames([dirName fName]);
+if nargin<3 || isempty(first_image_path)
+    % Load First image
+    [fName,dirName] = uigetfile(...
+        {'*.tif;*.tiff;*.jpg;*.jpeg;*.TIF','Image Files (*.tif,*.tiff,*.jpg,*.jpeg)';
+        '*.tif','TIF files (*.tif)'
+        '*.tiff','TIFF files (*.tiff)'
+        '*.jpg;','JPG files (*.jpg)'
+        '*.jpeg;','JPEG files (*.jpeg)'
+        '*.*','All Files (*.*)'},...
+        'Select first image');
+    if(isa(fName,'char') & isa(dirName,'char'))
+        % Recover all file names from the stack
+        first_image_path=[dirName,filesep,fName];
+        outFileList=getFileStackNames(first_image_path);        
+        % Number of files 
+        n=length(outFileList);
+    else
+        area=[];
+        return
+    end
+else
+    outFileList=getFileStackNames(first_image_path);        
     % Number of files 
     n=length(outFileList);
-else
-    area=[];
-    return
 end
 
-% The user can decide the number of images to be cropped
-prompt={'Specify the number of images to be cropped'};
-dlg_title='User input requested';
-num_lines=1;
-def={num2str(n)};
-answer=fix(str2num(char(inputdlg(prompt,dlg_title,num_lines,def))));
+if nargin<4 || isempty(num_images_to_crop)
+    % The user decides the number of images to be cropped
+    prompt={'Specify the number of images to be cropped'};
+    dlg_title='User input requested';
+    num_lines=1;
+    def={num2str(n)};
+    num_images_to_crop=fix(str2num(char(inputdlg(prompt,dlg_title,num_lines,def))));
 
-% Check the selected number
-if isempty(answer)
-    area=[];
-    return
-end
+    % Check the selected number
+    if isempty(num_images_to_crop)
+        area=[];
+        return
+    end
 
-if answer<1 | answer>n
-    fprintf(1,'Invalid number of images specified. Using the default value (%d).\n',answer);
-else
-    % Crop outFileList
-    n=answer;
+    if num_images_to_crop>n
+        fprintf(1,'Invalid number of images specified. Using the default value (%d).\n',num_images_to_crop);
+    else
+        % Crop outFileList
+        n=num_images_to_crop;
+        outFileList=outFileList(1:n);
+    end
+elseif num_images_to_crop==0
     outFileList=outFileList(1:n);
+else
+    if num_images_to_crop>n
+        fprintf(1,'Invalid number of images specified. Using the default value (%d).\n',num_images_to_crop);
+    else
+        % Crop outFileList
+        n=num_images_to_crop;
+        outFileList=outFileList(1:n);
+    end
 end
 
 % Read first image
-imgOne=double(imread([dirName fName]));
+imgOne=double(imread(first_image_path));
 
 if isempty(area)
     h=figure;
@@ -125,8 +149,8 @@ if x>=size(imgOne,2), x=size(imgOne,2); end
 
 % Select output directory
 if manualSave==1
-    path=uigetdir('','Select output directory');
-    if path==0
+    output_path=uigetdir('','Select output directory');
+    if output_path==0
         area=[];
         return
     end
@@ -149,7 +173,7 @@ for i=1:n
     imgC=img(y0:y,x0:x);
    
     % Prepare filename with path
-    filename=[path,filesep,'crop_',fName,fno,fext];
+    filename=[output_path,filesep,'crop_',fName,fno,fext];
    
     % Write file to disk
     if strcmp(fext,'.tif') || strcmp(fext,'.tiff') || strcmp(fext,'.TIF') || strcmp(fext,'.jpg') || strcmp(fext,'.jpeg')
