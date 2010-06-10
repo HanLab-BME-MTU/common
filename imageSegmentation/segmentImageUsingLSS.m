a14592d5f991a3313c3995b193f343d32a066c6f
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-06-10 22:46:48 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4910 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageSegmentation/segmentImageUsingLSS.m
+++ imageSegmentation/segmentImageUsingLSS.m
@@ -2,12 +2,12 @@ function [BW,phi,iter] = segmentImageUsingLSS(ima, domain, phi, nu, h, maxIter)
 
 %% ------ Parse inputs --------- %%
 
-if nargin ~= 5
+if nargin ~= 6
     error('Invalid number of input argument.');
 end
 
-if ~isinteger(h)
-    error('scale parameter h needs to be an integer.');
+if h ~= floor(h) || h < 0
+    error('scale parameter h needs to be a positive integer.');
 end
 
 [nrows,ncols] = size(ima);
@@ -28,9 +28,9 @@ if isempty(phi)
     % Create an initial level set from a cone function
     cx = floor(ncols/2);
     cy = floor(nrows/2);
-    radius = (2/3) * min(nrows,ncols);
-    [X Y] = meshgrid(1:ncols - cx, 1:nrows - cy);
-    phi = -sqrt(X.^2 + Y.^2 - radius.^2);
+    radius = (1/3) * min(nrows,ncols);
+    [X Y] = meshgrid((1:ncols) - cx, (1:nrows) - cy);
+    phi = -sqrt(X.^2 + Y.^2) + radius;
     
 elseif size(phi,1) ~= nrows || size(phi,2) ~= ncols
     error('ima size and phi size differ.');
@@ -38,7 +38,7 @@ end
 
 %% ------ Initialization ------ %%
 
-% resize image, phi and domain to a squared power of 2 size
+% STEP 1: resize image, phi and domain to a squared power of 2 size width
 
 newSize = 2^p;
 
@@ -55,7 +55,7 @@ tmp = phi;
 phi = zeros(newSize);
 phi(1:nrows,1:ncols) = tmp;
 
-% down sample level set using a iterative bicubic method.
+% STEP 2: down sample level set using a iterative bicubic method.
 
 scaledPhi = phi;
 
@@ -63,17 +63,76 @@ for s = 1:h
     scaledPhi = imresize(scaledPhi, .5);
 end
 
-% Initialize B-spline coefficients from scaled phi
+% STEP 3: initialize B-spline coefficients from scaled phi
 
-bSplineCoeffs = convertRowSignalToBSplineCoefficients(scaledPhi);
-bSplineCoeffs = convertRowSignalToBSplineCoefficients(bSplineCoeffs');
-bSplineCoeffs = bSplineCoeffs';
+BS = convertRowSignalToBSplineCoefficients(scaledPhi);
+BS = convertRowSignalToBSplineCoefficients(BS');
+BS = BS';
 
-% Normalize B-spline coefficients according to eq. 15
+% STEP 4: normalize B-spline coefficients according to eq. 15
 
-bSplineCoeffs = bSplineCoeffs / max(abs(bSplineCoeffs(:)));
+BS = BS / max(abs(BS(:)));
 
-% Upload the new down sampled level set from B-spline coefficients
+% STEP 5: update level set value from down-sampled B-spline coefficients
+% using B3-spline interpolation
+
+[X,Y] = meshgrid(1:newSize,1:newSize);
+
+% Rescale levelset coordinates into the B-spline space (side with = 2^(p-h))
+xScaled = X / 2^h;
+yScaled = Y / 2^h;
+
+% The subsequent code can be replaced by the following line (TO BE CHECKED)
+%phi = interp2(X / 2^h, Y / 2^h, bSplineCoeffs, X, Y, 'spline');
+
+% Compute the interpolation indexes
+xIndex = arrayfun(@(i) floor(xScaled) + i, -1:2, 'UniformOutput', false);
+xIndex = cat(3,xIndex{:});
+
+yIndex = arrayfun(@(i) floor(yScaled) + i, -1:2, 'UniformOutput', false);
+yIndex = cat(3,yIndex{:});
+
+% Compute weights
+xW = xScaled - xIndex(:,:,2);
+yW = yScaled - yIndex(:,:,2);
+
+xWeight = zeros([size(xScaled), 4]);
+xWeight(:,:,4) = (1/6) * xW.^3;
+xWeight(:,:,1) = (1/6) + (1/2) * xW .* (xW - 1) - xWeight(:,:,4);
+xWeight(:,:,3) = xW + xWeight(:,:,1) - 2 * xWeight(:,:,4);
+xWeight(:,:,2) = 1 - xWeight(:,:,1) - xWeight(:,:,3) - xWeight(:,:,4);
+
+yWeight = zeros([size(yScaled), 4]);
+yWeight(:,:,4) = (1/6) * yW.^3;
+yWeight(:,:,1) = (1/6) + (1/2) * yW .* (yW - 1) - yWeight(:,:,4);
+yWeight(:,:,3) = yW + yWeight(:,:,1) - 2 * yWeight(:,:,4);
+yWeight(:,:,2) = 1 - yWeight(:,:,1) - yWeight(:,:,3) - yWeight(:,:,4);
+
+% Add mirror condition at border 
+BSpadded = padarrayXT(BS, [2 2], 'symmetric', 'both');
+
+% Compute interpolation for each X,Y = 
+% sum_k=1^4 yWeight(k) sum_l=1^4 xWeight(l) BSpadded(yIndex(l), xIndex(l))
+
+phi(:) = 0;
+tmp = zeros(2^p);
+
+for k = 1:4
+    tmp(:) = 0;
+    
+    yShifted = yIndex(:,:,k) + 2;
+    
+    for l = 1:4
+        xShifted = xIndex(:,:,l) + 2;
+        
+        ind = sub2ind(size(BSpadded), yShifted(:), xShifted(:));
+        
+        tmp = tmp + xWeight(:,:,l) .* reshape(BSpadded(ind), [512, 512]);
+        
+    end
+    
+    phi = phi + yWeight(:,:,k) .* tmp;
+end
 
 
 %% ------ Main loop ------- %%
@@ -134,8 +193,12 @@ end
 
 in(:,1) = sum / (1 - z^(2 * n - 2));
 
-in(:,2:end) = in(:,2:end) + z * in(:,1:end-1);
+for i = 2:n
+    in(:,i) = in(:,i) + z * in(:,i-1);
+end
 
 in(:,end) = (z * in(:,end-1) + in(:,end)) * z / (z * z - 1);
 
-in(:,end-1:-1:1) = z * (in(:,end:-1:2) - in(:,end-1:-1:1));
+for i = n-1:-1:1
+    in(:,i) = z * (in(:,i+1) - in(:,i));
+end
