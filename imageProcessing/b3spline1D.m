908d23b0c07f6c579d0940035fc7354d7af655ed
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-06-12 23:45:56 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@4925 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageProcessing/b3spline1D.m
+++ imageProcessing/b3spline1D.m
@@ -1,14 +1,14 @@
-% [c] = b3spline1D(signal, boundary)
+% [c] = b3spline1D(img, boundary)
 % 
-% Computes the cubic B-spline coefficients.
+% Computes the 1D cubic B-spline coefficients.
 %
 % Inputs: 
-%           signal   : input signal
+%           img      : input image
 %           boundary : boundary conditions: 'mirror' (default) or 'periodic'
 
 % Francois Aguet, June 2010
 
-function c = b3spline1D(signal, boundary)
+function cn = b3spline1D(img, boundary)
 
 if nargin<2
     boundary = 'mirror';
@@ -16,62 +16,57 @@ end
 
 % cubic spline parameters
 c0 = 6;
-a = -2+sqrt(3);
+z1 = -2+sqrt(3);
 
-c = prefilter(reshape(signal, [1 length(signal)]), c0, a, boundary);
-c = reshape(c, size(signal));
+N = size(img,2);
+cp = zeros(size(img));
+cn = zeros(size(img));
 
-
-function cn = prefilter(signal, c0, z1, mode)
-N = length(signal);
-cp = zeros(size(signal));
-cn = zeros(size(signal));
-
-if (strcmp(mode, 'mirror')==1)
-    cp(1) = getCausalInit_Mirror(signal, z1);
+if (strcmp(boundary, 'mirror')==1)
+    cp(:,1) = getCausalInit_Mirror(img, z1);
     for k = 2:N
-        cp(k) = signal(k) + z1*cp(k-1);
+        cp(:,k) = img(:,k) + z1*cp(:,k-1);
     end;
-    cn(N) = getAntiCausalInit_Mirror(cp, z1);
+    cn(:,N) = getAntiCausalInit_Mirror(cp, z1);
     for k = N-1:-1:1
-        cn(k) = z1*(cn(k+1) - cp(k));
+        cn(:,k) = z1*(cn(:,k+1) - cp(:,k));
     end;
-elseif (strcmp(mode, 'periodic')==1)
-    cp(:,1) = getCausalInit_Periodic(signal, z1);
+elseif (strcmp(boundary, 'periodic')==1)
+    cp(:,1) = getCausalInit_Periodic(img, z1);
     for k = 2:N
-        cp(k) = signal(k) + z1*cp(k-1);
+        cp(:,k) = img(:,k) + z1*cp(:,k-1);
     end;
     cn(:,N) = getAntiCausalInit_Periodic(cp, z1);
     for k = N-1:-1:1
-        cn(k) = z1*(cn(k+1) - cp(k));
+        cn(:,k) = z1*(cn(:,k+1) - cp(:,k));
     end
 else
-    error('Mode: unknown boundary conditions');
+    error('Boundary: unknown boundary conditions');
 end;
 cn = c0*cn;
 
 
-function c0 = getAntiCausalInit_Mirror(signal, a)
-N = length(signal);
-c0 = (a/(a*a-1))*(signal(N)+a*signal(N-1));
+function c0 = getAntiCausalInit_Mirror(img, a)
+N = size(img,2);
+c0 = (a/(a*a-1))*(img(:,N)+a*img(:,N-1));
 
 
-function c0 = getAntiCausalInit_Periodic(signal, a)
-N = length(signal);
-signal = [signal(N) signal signal(1:N-3)];
-k = 0:2*N-3;
-c0 = -a/(1-a^N)*sum(a.^k.*signal);
+function c0 = getAntiCausalInit_Periodic(img, a)
+N = size(img,2);
+img = [img(:,N) img img(:,1:N-3)];
+k = repmat(0:2*N-3, [size(img,1) 1]);
+c0 = -a/(1-a^N)*sum(a.^k.*img, 2);
 
 
-function out = getCausalInit_Mirror(signal, a)
-N = length(signal);
-k = 0:2*N-3;
-signal = [signal signal(end-1:-1:2)];
-out = sum(signal.*a.^k) / (1 - a^(2*N-2));
+function out = getCausalInit_Mirror(img, a)
+N = size(img,2);
+k = repmat(0:2*N-3, [size(img,1) 1]);
+img = [img img(:,end-1:-1:2)];
+out = sum(img.*a.^k,2) / (1 - a^(2*N-2));
 
 
-function out = getCausalInit_Periodic(signal, a)
-N = length(signal);
-k = 0:N-1;
-signal = [signal(:,1) signal(:,N:-1:2)];
-out = sum(signal.*a.^k)/(1-a^N);
+function out = getCausalInit_Periodic(img, a)
+N = size(img,2);
+k = repmat(0:N-1, [size(img,1) 1]);
+img = [img(:,1) img(:,N:-1:2)];
+out = sum(img.*a.^k,2)/(1-a^N);
