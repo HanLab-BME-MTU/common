25f5967f67f49bf07c3e540ef96fa884fe70cbbf
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-08-05 17:45:54 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5088 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/pcombs.m
+++ mathfun/pcombs.m
@@ -1,23 +1,44 @@
-function C = pcombs(v)
+function C = pcombs(v,keepDiag)
 % This function generate all pairwise combinations of a vector v of length
-% n. The number of possible pairwise combinations is n (n - 1) / 2. The
-% elements of v must be all different.
+% n. The number of possible pairwise combinations is n x (n - 1) / 2 with
+% keepDiag == false (default), n x (n + 1) / 2 otherwise. The elements of v
+% must be all different.
 %
 % e.g.:
-% v = 1 2 4 3
-% C = 
+% pcombs([1 2 4 3], false)
+% ans = 
 %   1  2
 %   1  4
-%   2  4
 %   1  3
+%   2  4
 %   2  3
 %   4  3
+%
+% pcombs([1 2 4 3], true)
+% ans =
+%   1  1
+%   1  2
+%   1  4
+%   1  3
+%   2  2
+%   2  4
+%   2  3
+%   4  4
+%   4  3
+%   3  3
 
-assert(all(v == unique(v)));
+if nargin < 2 || isempty(keepDiag)
+    keepDiag = false;
+end
+
+assert(islogical(keepDiag));
+
+assert(length(v) == length(unique(v)));
 
 [I J] = meshgrid(v, v');
-I = triu(I, 1);
-J = triu(J, 1);
+k = ~keepDiag;
+I = triu(I, k);
+J = triu(J, k);
 C = horzcat(nonzeros(J), nonzeros(I));
 
 end
