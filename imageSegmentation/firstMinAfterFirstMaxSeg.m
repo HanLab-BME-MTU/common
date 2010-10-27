2fdf6a03a1899f828b3d5b49206b1bb6cf8ab498
Achim Besser <achim.besser@hms.harvard.edu>
2010-10-27 19:50:59 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5330 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageSegmentation/firstMinAfterFirstMaxSeg.m
+++ imageSegmentation/firstMinAfterFirstMaxSeg.m
@@ -1,4 +1,4 @@
-function mask = firstMinAfterFirstMaxSeg(thismask, closureRadius, sigmaGauss)
+function mask = firstMinAfterFirstMaxSeg(thismask, closureRadius, sigmaGauss, holes)
 %FIRSTMINAFTERFIRSTMAXSEG Detects cell edge by thresholding the intensity histogram at the first minimum after the highest maximum
 %
 %SYNOPSIS mask = firstMinAfterFirstMaxSeg(thismask, closureRadius)
@@ -7,6 +7,10 @@ function mask = firstMinAfterFirstMaxSeg(thismask, closureRadius, sigmaGauss)
 %          (optional) closureRadius        performed image closing on the
 %                                          segmentation mask with a disk
 %                                          with radius = closureRadius
+%          (optional) holes                if holes==1 the mask might
+%                                          contain holes. If holes==0 all
+%                                          holes will be filled up. The
+%                                          default is no holes (holes=0).
 %
 %Output:   mask                            segmented mask
 %
@@ -19,6 +23,11 @@ end
 if nargin < 3 || isempty(sigmaGauss)
     sigmaGauss=1;
 end
+
+if nargin < 4 || isempty(holes)
+    holes=0;
+end
+
 
 %make sure that image is in double format
 thismask = double(thismask);
@@ -53,18 +62,20 @@ mask = (L == tmp2);
 closureBrush = strel('disk',closureRadius);
 mask = imclose(cast(mask,'double'),closureBrush);
 mask = cast(mask,'logical');
-mask = double(imfill(double(mask),'holes'));
-%mask(1,:) = 0;
-L2 = bwlabel(mask==0);
-s2  = regionprops(L2, 'Area','PixelIdxList');
+if holes==0
+    mask = double(imfill(double(mask),'holes'));
+    %mask(1,:) = 0;
+    L2 = bwlabel(mask==0);
+    s2  = regionprops(L2, 'Area','PixelIdxList');
 
-% find the background with the highest average intensity
-% assign the rest as foreground
-numberOfBlobs = size(s2, 1);
-for k=1:numberOfBlobs
-    PixelIdxList = s2(k).PixelIdxList;
-    meanGL(k) = mean(thismask(PixelIdxList));
+    % find the background with the highest average intensity
+    % assign the rest as foreground
+    numberOfBlobs = size(s2, 1);
+    for k=1:numberOfBlobs
+        PixelIdxList = s2(k).PixelIdxList;
+        meanGL(k) = mean(thismask(PixelIdxList));
+    end
+    [tmp1, tmp2] = min(meanGL);
+    maskbg = (L2 == tmp2);
+    mask = (maskbg == 0);
 end
-[tmp1, tmp2] = min(meanGL);
-maskbg = (L2 == tmp2);
-mask = (maskbg == 0);
--- mathfun/locmax2d.m
+++ mathfun/locmax2d.m
@@ -4,7 +4,13 @@ function fImg=locmax2d(img,mask,keepFlat)
 %    SYNOPSIS fImg=(img,mask)
 %
 %    INPUT    img    image matrix
-%             mask   [m n] defines the operator window dimensions
+%             mask   EITHER a vector [m n] that defines the operator window
+%                    dimensions (all ones).
+%                    OR a structural element, a matrix, that contains only
+%                    0/1. Structural elements such as discs can be defined
+%                    easily using the built-in matlab function "strel".
+%                    The input matrix must have an odd number of columns
+%                    and rows. 
 %             keepFlat Optional input variable to choose whether to remove
 %                      "flat" maxima or to keep them. Default is 0, to
 %                      remove them. - KJ
@@ -26,6 +32,46 @@ if nargin<2
    error('Please define all parameters');
 end
 
+% If mask is a vector with two entries, then, these two entries define the
+% number of rows and columns of a rectangular mask filled with ones:
+if length(mask(:))==2
+    % make sure the mask elements are odd numbers (only then, the 
+    % local max operator is properly defined)
+    indx = find(~mod(mask,2));
+    mask(indx) = mask(indx) + 1;
+    rows=mask(1);
+    cols=mask(2);
+    % number of non-zero elements:
+    numEl=prod(mask);
+    
+    % generate the real mask:
+    mask=ones(mask);    
+else
+    % mask is a flat structural element, with an odd number of cols and
+    % rows. Note that this excludes the only possible overlap case [1 1] 
+    % which would be treated as identical operation above.
+    
+    % first check that mask has the right size:
+    [rows cols]=size(mask);
+    if ~mod(rows,2) || ~mod(cols,2)
+        % There is no simple way of extending a general mask, thus:
+        error('Mask must have an odd number of rows and columns!');
+    end
+    
+    % get matrix dimensions:
+    [rows cols]=size(mask);
+    
+    % Check that the matrix contains only 0/1. This check could be omitted:
+    checkMat = (mask==1 | mask==0);
+    if sum(checkMat(:))<rows*cols
+        % There is no simple way of extending a general mask, thus:
+        error('The mask may contain binary values only!');
+    end    
+    
+    % number of non-zero elements:
+    numEl=sum(mask(:));
+end
+
 if nargin < 3 || isempty(keepFlat)
     keepFlat = 0;
 end
@@ -36,15 +82,10 @@ end
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% make sure the mask elements are odd numbers (only then, the local max operator is 
-% properly defined)
-indx = find(~mod(mask,2));
-mask(indx) = mask(indx) + 1;
-
 % apply a max filter
-fImg = ordfilt2(img,prod(mask),ones(mask));
+fImg = ordfilt2(img,numEl,mask);
 if keepFlat == 0 %change made by KJ
-    fImg2 = ordfilt2(img,prod(mask)-1,ones(mask));
+    fImg2 = ordfilt2(img,numEl-1,mask);
     fImg(fImg2==fImg)=0;
 end
 
@@ -54,6 +95,6 @@ fImg(fImg ~= img) = 0;
 
 % set image border to zero
 auxM = zeros(size(img));
-auxM(fix(mask(1)/2)+1:end-fix(mask(1)/2),fix(mask(2)/2)+1:end-fix(mask(2)/2)) = ...
-    fImg(fix(mask(1)/2)+1:end-fix(mask(1)/2),fix(mask(2)/2)+1:end-fix(mask(2)/2));
+auxM(fix(rows/2)+1:end-fix(rows/2),fix(cols/2)+1:end-fix(cols/2)) = ...
+    fImg(fix(rows/2)+1:end-fix(rows/2),fix(cols/2)+1:end-fix(cols/2));
 fImg=auxM;
