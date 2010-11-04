6d7ddecd6f227c2b27efc7122cba971efe35944c
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-11-04 05:40:56 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5370 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/name2wavelength.m
+++ toolfun/name2wavelength.m
@@ -1,15 +1,31 @@
+
+% Values from http://www.olympusfluoview.com/applications/fpcolorpalette.html
+
 % Francois Aguet, October 2010
 
 function lambda = name2wavelength(name)
 
-% values from http://www.olympusfluoview.com/applications/fpcolorpalette.html
 switch name
+    case 'CFP'
+        lambda = 475e-9;
     case 'GFP'
         lambda = 509e-9;
     case 'EGFP'
         lambda = 507e-9;
+    case 'Alexa488'
+        lambda = 519e-9;
+    case 'YFP'
+        lambda = 527e-9;
+    case 'Alexa555'
+        lambda = 565e-9;
+    case 'Alexa568'
+        lambda = 603e-9;
+    case 'RFP'
+        lambda = 607e-9;
     case 'mCherry'
         lambda = 610e-9;
+    case 'Alexa647'
+        lambda = 665e-9;
     otherwise
         if isnumeric(name)
             lambda = name;
