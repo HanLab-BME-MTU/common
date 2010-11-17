81b50b997bc6b8f9a7558e2d515013a3cfa9f09f
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-11-17 16:06:48 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5438 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/name2wavelength.m
+++ toolfun/name2wavelength.m
@@ -16,19 +16,23 @@ function lambda = convert(name)
 switch name
     case 'CFP'
         lambda = 475e-9;
-    case 'GFP'
-        lambda = 509e-9;
     case 'EGFP'
         lambda = 507e-9;
+    case 'GFP'
+        lambda = 509e-9;
     case 'Alexa488'
         lambda = 519e-9;
     case 'YFP'
         lambda = 527e-9;
     case 'Alexa555'
         lambda = 565e-9;
+    case {'dTomato', 'tdTomato'}
+        lambda = 581e-9;
+    case 'DsRed'
+        lambda = 583e-9;
     case 'Alexa568'
         lambda = 603e-9;
-    case 'RFP'
+    case {'RFP', 'mRFP'}
         lambda = 607e-9;
     case 'mCherry'
         lambda = 610e-9;
