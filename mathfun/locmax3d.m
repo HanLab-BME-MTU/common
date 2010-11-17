945028d566a5c9fe20f9522c08d8a72bc2474dab
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-11-17 04:59:48 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5436 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ mathfun/locmax3d.m
@@ -0,0 +1,45 @@
+% Francois Aguet, 16 November 2010
+
+function lm = locmax3d(img, mask)
+
+if numel(mask)==1
+    wx = mask;
+    wy = mask;
+    wz = mask;
+    if mod(wx,2)==0 || mod(wy,2)==0 || mod(wz,0)==0
+        error('Mask dimensions must be odd integers');
+    end
+    mask = ones(wy,wx);
+elseif numel(mask)==3
+    wx = mask(1);
+    wy = mask(2);
+    wz = mask(3);
+    if mod(wx,2)==0 || mod(wy,2)==0 || mod(wz,0)==0
+        error('Mask dimensions must be odd integers');
+    end    
+    mask = ones(wy,wx);
+end
+
+[ny,nx,nz] = size(img);
+lm = zeros(ny,nx,nz);
+
+for z = 1:nz
+    tmp = ordfilt2(img(:,:,z), wx*wy, mask);
+    tmp(tmp ~= img(:,:,z)) = 0;
+    lm(:,:,z) = tmp;    
+end
+
+img = zeros(ny,nx,nz);
+
+b = (wz-1)/2;
+for z = 1+b:nz-b
+    img(:,:,z) = max(lm(:,:,z-b:z+b), [], 3);
+end
+
+lm(lm~=img) = 0;
+
+% also set xy-borders to zero
+b = (wx-1)/2;
+lm(:,[1:1+b end-b:end],:) = 0;
+b = (wy-1)/2;
+lm([1:1+b end-b:end],:,:) = 0;
