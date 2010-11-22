7098d322d57f735a946baeec3354efc742919869
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-11-22 04:49:05 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5455 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ toolfun/findFilesInSubDirs.m
@@ -0,0 +1,17 @@
+%fileList = findFilesInSubDirs(topdir, filename)
+% Finds all files containing 'filename' in their name in all subdirectories of 'topdir'.
+
+% Francois Aguet, November 21, 2010
+
+function fileList = findFilesInSubDirs(topdir, filename)
+
+dirs = getSubDirs(topdir);
+fileList = cell(1,length(dirs));
+for k = 1:length(dirs)
+    tt = dir([dirs{k} filesep '*' filename '*']);
+    if ~isempty(tt)
+        fileList{k} = arrayfun(@(x) [dirs{k} filesep x.name], tt, 'UniformOutput', false);
+    end
+end
+
+fileList = vertcat(fileList{:});
