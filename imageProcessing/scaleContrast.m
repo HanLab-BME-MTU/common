070b3933c77778a780a7258c2374d4ffd5b35acd
Francois Aguet <francois_aguet@hms.harvard.edu>
2010-11-22 14:12:57 +0000

    Added argument for output dynamic range
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5457 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageProcessing/scaleContrast.m
+++ imageProcessing/scaleContrast.m
@@ -1,20 +1,18 @@
-function out = scaleContrast(in, varargin)
-switch(nargin)
-    case 1
-        maxI = max(in(:));
-        minI = min(in(:));
-        v = 255;
-    case 2
-        dynamicRange = varargin{1};
-        minI = dynamicRange(1);
-        maxI = dynamicRange(2);
-        v = 255;
-    case 3
-    otherwise
-        disp('Unsupported number of arguments');
-end;
-if ((maxI-minI) ~= 0)
-    out = (in-minI)/(maxI-minI)*v;
+function out = scaleContrast(in, rangeIn, rangeOut)
+
+if nargin<2 
+    rangeIn = [min(in(:)) max(in(:))];
+    rangeOut = [0 255];
+end
+if nargin<3 || isempty(rangeOut)
+    rangeOut = [0 255];
+end
+if isempty(rangeIn)
+    rangeIn = [min(in(:)) max(in(:))]; 
+end
+
+if rangeIn(2)-rangeIn(1) ~= 0
+    out = (in-rangeIn(1)) / (rangeIn(2)-rangeIn(1)) * (rangeOut(2)-rangeOut(1)) + rangeOut(1);
 else
     out = zeros(size(in));
-end;
+end
