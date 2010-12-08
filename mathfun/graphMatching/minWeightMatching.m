61fb276afd6c7a4f8808ff0ae4e0fec882208195
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2010-12-08 00:46:33 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5503 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/graphMatching/minWeightMatching.m
+++ mathfun/graphMatching/minWeightMatching.m
@@ -1,4 +1,4 @@
-function  = minWeightMatching(D, nonLinkMarker)
+function [U V] = minWeightMatching(D, nonLinkMarker)
 
 % D must be squared
 % D must be symetric. Only the lower triangular part will be considered
@@ -6,6 +6,10 @@ function  = minWeightMatching(D, nonLinkMarker)
 % D > 0
 
 [n m] = size(D);
+
+if nargin < 2 || isemtpy(nonLinkMarker)
+  nonLinkMarker = max(D(:)) + 1;
+end
 
 if n ~= m
   error('Input matrix must be squared.');
@@ -15,7 +19,10 @@ if any(D < 0)
   error('Weights must be >= 0');
 end
 
+% FIXME
+
 [u, v, w] = find(tril(D));
 
-M = minWeightMatchingMEX(n, [u v], w);
+M = perfectMatchingMEX(n, [u v], w);
 
+% TODO: remove edge where w == nonLinkMarker
