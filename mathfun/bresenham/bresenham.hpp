5b20e7995b89de70d004fc36dde91aa1fb1c259f
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2011-03-10 21:03:31 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5837 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/bresenham/bresenham.hpp
+++ mathfun/bresenham/bresenham.hpp
@@ -1,22 +1,22 @@
 #ifndef BRESENHAM_HPP
 # define BRESENHAM_HPP
 
-# include <point_2d.hpp>
+# include <vector.hpp>
 
 template <typename T, typename F>
-void bresenham(const point_2d<T>& p1,
-	       const point_2d<T>& p2,
+void bresenham(const vector<2,T>& p1,
+	       const vector<2,T>& p2,
 	       F& func)
 {
   int l, q, dqr, dqru;
       
-  point_2d<T> p, incr1, incr2;
+  vector<2,T> p, incr1, incr2;
       
-  int dy = p2.y - p1.y;
-  int dx = p2.x - p1.x;
+  int dy = p2[1] - p1[1];
+  int dx = p2[0] - p1[0];
       
-  incr1.y = dy < 0 ? (dy = -dy, -1) : 1;
-  incr1.x = dx < 0 ? (dx = -dx, -1) : 1;
+  incr1[1] = dy < 0 ? (dy = -dy, -1) : 1;
+  incr1[0] = dx < 0 ? (dx = -dx, -1) : 1;
       
   if (dy >= dx)
     {
@@ -24,7 +24,7 @@ void bresenham(const point_2d<T>& p1,
       dqru = dqr - (dy << 1);
       q = dqr - dy;
       l = dy;
-      incr2.y = incr1.y;
+      incr2[1] = incr1[1];
     }
   else
     {
@@ -32,7 +32,7 @@ void bresenham(const point_2d<T>& p1,
       dqru = dqr - (dx << 1);
       q = dqr - dx;
       l = dx;
-      incr2.x = incr1.x;
+      incr2[0] = incr1[0];
     }
       
   p = p1;
