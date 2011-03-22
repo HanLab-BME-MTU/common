d756b2978a018ffc503215b32618d80f798c81ef
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-03-22 19:09:38 +0000

    bug for uint8/uint16 inputs fixed
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@5973 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageProcessing/scaleContrast.m
+++ imageProcessing/scaleContrast.m
@@ -1,18 +1,14 @@
 function out = scaleContrast(in, rangeIn, rangeOut)
 
-if nargin<2 
+if nargin<2 || isempty(rangeIn)
     rangeIn = [min(in(:)) max(in(:))];
-    rangeOut = [0 255];
 end
 if nargin<3 || isempty(rangeOut)
     rangeOut = [0 255];
 end
-if isempty(rangeIn)
-    rangeIn = [min(in(:)) max(in(:))]; 
-end
 
 if rangeIn(2)-rangeIn(1) ~= 0
-    out = (in-rangeIn(1)) / (rangeIn(2)-rangeIn(1)) * (rangeOut(2)-rangeOut(1)) + rangeOut(1);
+    out = (double(in)-rangeIn(1)) / diff(rangeIn) * diff(rangeOut) + rangeOut(1);
 else
     out = zeros(size(in));
 end
