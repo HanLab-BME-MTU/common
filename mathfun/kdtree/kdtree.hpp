916a3cf2b072649a9423093bba2fd5ab054b662e
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2011-04-03 07:13:38 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@6078 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/kdtree/kdtree.hpp
+++ mathfun/kdtree/kdtree.hpp
@@ -6,167 +6,146 @@
 
 # include <vector.hpp>
 
-template <unsigned n, typename T>
-class cmp
+template <unsigned N, typename T>
+class KDTreeVisitor;
+
+template <unsigned N, typename T>
+struct KDTraits
 {
-public:
-  cmp(int dim)
-  {
-    assert(dim >= 0 && dim < n);
-
-    dim_ = dim;
-  }
-
-  bool operator()(const vector<n, T> & p1, const vector<n, T> & p2) const
-  {
-    return p1[dim_] < p2[dim_];
-  }
-
-private:
-  int dim_;
+	typedef typename std::vector< vector<N, T> > points_type;
+	typedef typename std::vector< unsigned >::iterator idx_iterator_type;
+	typedef std::pair<unsigned, double> pair_type;
 };
 
-template <unsigned n, typename T>
-class kdtree
+template <unsigned N, typename T>
+class KDTree
 {
 public:
-  typedef typename std::vector< vector<n, T> > points_t;
-  typedef typename points_t::iterator points_iterator;
+	virtual ~KDTree() {}
+	
+	virtual void accept(KDTreeVisitor<N, T> & v) const = 0;
+};
 
-  kdtree(const points_iterator first,
-	 const points_iterator last,
-	 unsigned dim = 0)
-    : pt_(0), lt_(0), gt_(0)
+# include <KDTreeVisitor.hpp>
+
+template <unsigned N, typename T>
+class KDNode : public KDTree<N, T>
+{
+public:
+	KDNode(KDTree<N, T>* lt, KDTree<N, T>* gt, unsigned dim, double value) : lt_(lt), gt_(gt), dim_(dim), value_(value) {}
+	
+	~KDNode() {
+		delete lt_;
+		delete gt_;
+	}
+	
+	void accept(KDTreeVisitor<N, T> & v) const { v.visit(*this); }
+	
+	unsigned dim() const { return dim_; }
+	double value() const { return value_; }
+	
+	const KDTree<N, T> *lt() const { return lt_; }
+	const KDTree<N, T> *gt() const { return gt_; }
+	
+private:
+	KDTree<N, T> *lt_, *gt_;
+	unsigned dim_;
+	double value_;
+};
+
+template <unsigned N, typename T>
+class KDLeaf : public KDTree<N, T>
+{
+public:
+	KDLeaf(unsigned index) : index_(index) {}
+	
+	void accept(KDTreeVisitor<N, T> & v) const { v.visit(*this); }
+
+	unsigned index() const { return index_; }
+	
+private:
+	unsigned index_;
+};
+
+template <unsigned N, typename T>
+class KDTreeBuilder
+{
+private:
+	struct Incr_
+	{
+		Incr_() { current = 0; }
+		
+		int operator()() { return current++; }
+
+		int current;		
+	};
+
+	struct Cmp_
+	{
+	public:
+		Cmp_() {}
+		
+		Cmp_(const typename KDTraits<N, T>::points_type & points, unsigned k) : points_(points), k_(k) {}
+		
+		bool operator()(unsigned i, unsigned j) const
+		{
+			return points_[i][k_] < points_[j][k_];
+		}
+		
+	private:
+		typename KDTraits<N, T>::points_type points_;
+		
+		unsigned k_;
+	};
+	
+public:
+	KDTreeBuilder(const typename KDTraits<N, T>::points_type & points) : points_(points)
+	{
+		for (int k = 0; k < N; ++k)
+			cmp_[k] = Cmp_(points_, k);
+	}
+	
+	KDTree<N, T>* build() const
+	{
+		if (points_.empty())
+			return NULL;
+    
+		std::vector<unsigned> indices(points_.size());
+		std::generate(indices.begin(), indices.end(), Incr_());
+    
+		return build_(indices.begin(), indices.end());
+	}
+	
+private:
+	KDTree<N, T>* build_(const typename KDTraits<N, T>::idx_iterator_type & first,
+											 const typename KDTraits<N, T>::idx_iterator_type & last,
+											 unsigned k = 0) const
   {
     if (last - first > 1)
-      {
-	// sort the vector of points along the 'dim' coordinate
-	sort(first, last, cmp<n, T>(dim));
-	// take the median
-	points_iterator median = first + (last - first) / 2;
+    {
+			unsigned k2 = (k + 1) & (N - 1);
 
-	pt_ = &(*median);
-
-	lt_ = new kdtree(first, median, (dim + 1) & (n - 1));
-	gt_ = new kdtree(median, last, (dim + 1) & (n - 1));
-      }
+      // sort the vector of points along the Kth coordinate
+      sort(first, last, cmp_[k]);
+      
+    	// take the median
+      typename KDTraits<N, T>::idx_iterator_type median = first + (last - first) / 2;
+      
+      double value = points_[*median][k];
+      
+      KDTree<N, T> * lt = build_(first, median, k2);
+      KDTree<N, T> * gt = build_(median, last, k2);
+      
+      return new KDNode<N, T>(lt, gt, k, value);
+    }
     else
-      {
-	// this kdtree is a leaf
-	pt_ = &(*first);
-      }
-  }
-
-  ~kdtree()
-  {
-    if (lt_)
-      {
-	delete lt_;
-	delete gt_;
-      }
-  }
-
-  double closest_point(const vector<n, T> & query,
-		       vector<n, T> & closest,
-		       unsigned dim = 0) const
-  {
-    unsigned next_dim = (dim + 1) & (n - 1);
-
-    if (lt_)
-      {
-	// top-down
-	double min_dist = query[dim] < (*pt_)[dim] ?
-	  lt_->closest_point(query, closest, next_dim) :
-	  gt_->closest_point(query, closest, next_dim);
-
-	// bottom-up
-
-	if (query[dim] < (*pt_)[dim])
-	  {
-	    // The closest point has been found in the lt_ child. See
-	    // whether there isn't any closer point in the gt_ child
-	    // within min_dist from the closest point.
-	    if ((*pt_)[dim] - query[dim] < min_dist)
-	      {
-		vector<n, T> tmp_closest;
-		double tmp_dist = gt_->closest_point(query,
-						     tmp_closest,
-						     next_dim);
-		if (tmp_dist < min_dist)
-		  {
-		    closest = tmp_closest;
-		    min_dist =  tmp_dist;
-		  }
-	      }
-	  }
-	else
-	  {
-	    // The closest point has been found in the gt_ child. See
-	    // whether there isn't any closer point in the lt_ child
-	    // within min_dist from the closest point.
-	    if (query[dim] - (*pt_)[dim] < min_dist)
-	      {
-		vector<n, T> tmp_closest;
-		double tmp_dist = lt_->closest_point(query,
-						     tmp_closest,
-						     next_dim);
-
-		if (tmp_dist < min_dist)
-		  {
-		    closest = tmp_closest;
-		    min_dist = tmp_dist;
-		  }
-	      }
-	  }
-
-	return min_dist;
-      }
-    else
-      {
-	closest = *pt_;
-	return dist(query, *pt_);
-      }
+      return new KDLeaf<N, T>(*first);
   }
   
-  // F is a binary operator
-  template <typename F>
-  void ball_query(const vector<n,T> & center,
-		  const double radius,
-		  F & f,
-		  unsigned dim = 0) const
-  {
-    unsigned next_dim = (dim + 1) & (n - 1);
-
-    if (lt_)
-      {
-	double d = center[dim] - (*pt_)[dim];
-
-	if (d > 0)
-	  {
-	    if (d < radius)
-	      lt_->ball_query(center, radius, f, next_dim);
-	    gt_->ball_query(center, radius, f, next_dim);
-	  }
-	else
-	  {
-	    if (-d < radius)
-	      gt_->ball_query(center, radius, f, next_dim);
-	    lt_->ball_query(center, radius, f, next_dim);
-	  }
-      }
-    else
-      {
-	// this kdtree is a leaf
-	double d = dist(center, *pt_);
-
-	if (d <= radius)
-	  f(d, *pt_);
-      }
-  }
-
 private:
-  const vector<n, T>* pt_;
-  kdtree* lt_, * gt_;
+  typename KDTraits<N, T>::points_type points_;
+	
+	Cmp_ cmp_[N];
 };
 
 #endif /* KDTREE_HPP */
