8bdaaeacfcb880144b06238dc6c67b1586c39bd4
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2011-04-04 19:19:30 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@6097 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/kdtree/testKDTreeBallQuery.m
+++ mathfun/kdtree/testKDTreeBallQuery.m
@@ -1,13 +1,15 @@
 function testKDTreeBallQuery
 
-X = rand(5000,2);
+X = rand(100000,2);
 C = [.5 .5];
 R = .2;
 
 [idx,d] = KDTreeBallQuery(X,C,R);
+assert(all(d{1} <= R));
 
 plot(X(:,1),X(:,2), 'b.');
 hold on;
-ezplot(@(x,y) sqrt((x-C(1)).^2 + (y-C(2)).^2 - R^2), [0, 1, 0, 1]);
+t = 0:pi/100:2*pi;
+plot(C(1) + R * cos(t),C(2) + R * sin(t), 'g-');
 plot(X(idx{1},1), X(idx{1},2), 'r.');
 axis equal;
