e85a13398e1fcf197b9124a718d2f05233fe7a50
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2011-04-05 16:09:05 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@6102 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/kdtree/KDTree.hpp
+++ mathfun/kdtree/KDTree.hpp
@@ -7,7 +7,7 @@
 
 # include <vector.hpp>
 
-#ifndef UNIX || MAC
+#if defined(_WIN32) || defined(_WIN64)
 double log2(double n) { return log(n) / log(2.0); }
 #endif
 
@@ -15,187 +15,187 @@ template <unsigned N, typename T>
 class KDTree
 {
 public:
-	typedef vector<N, T> point_type;
-	typedef typename std::vector<point_type> points_type;
-	typedef typename std::vector< unsigned >::iterator idx_iterator_type;
-	typedef std::pair<double, unsigned> pair_type;
-	typedef std::multimap<double, unsigned> set_type;
+  typedef vector<N, T> point_type;
+  typedef typename std::vector<point_type> points_type;
+  typedef typename std::vector< unsigned >::iterator idx_iterator_type;
+  typedef std::pair<double, unsigned> pair_type;
+  typedef std::multimap<double, unsigned> set_type;
 	
 public:
-	KDTree(const points_type & points) : points_(points), tree_(NULL)
-	{
-		int n = points_.size();
+  KDTree(const points_type & points) : points_(points), tree_(NULL)
+  {
+    int n = points_.size();
 
-		if (n == 0)
-			return;
+    if (n == 0)
+      return;
 		
-		// n must be less than 2^32 - 1 since indices are stored on signed int
-		assert(n <= std::numeric_limits<int>::max());
+    // n must be less than 2^32 - 1 since indices are stored on signed int
+    assert(n <= std::numeric_limits<int>::max());
 		
-		std::vector<unsigned> indices(n);
-		std::generate(indices.begin(), indices.end(), Incr_());
+    std::vector<unsigned> indices(n);
+    std::generate(indices.begin(), indices.end(), Incr_());
 		
-		int h = (int) ceil(log2(n)) + 1;
-		int nnodes = (1 << (h+1)) - 1;
-		tree_ = new int[nnodes];
+    int h = (int) ceil(log2(n)) + 1;
+    int nnodes = (1 << (h+1)) - 1;
+    tree_ = new int[nnodes];
 		
-		build_(indices.begin(), indices.end());
-	}
+    build_(indices.begin(), indices.end());
+  }
 
-	~KDTree()
-	{
-		delete[] tree_;
-	}
+  ~KDTree()
+  {
+    delete[] tree_;
+  }
 	
-	pair_type closest_point(const point_type & query, unsigned idx = 0, unsigned k = 0) const
-	{
-		unsigned idx_lt = (idx << 1) + 1;
-		unsigned idx_gt = (idx << 1) + 2;
-		unsigned k2 = (k + 1) & (N - 1);
+  pair_type closest_point(const point_type & query, unsigned idx = 0, unsigned k = 0) const
+  {
+    unsigned idx_lt = (idx << 1) + 1;
+    unsigned idx_gt = (idx << 1) + 2;
+    unsigned k2 = (k + 1) & (N - 1);
 		
-		if (tree_[idx_lt] != -1)
-		{
-			double value = points_[tree_[idx]][k];
+    if (tree_[idx_lt] != -1)
+      {
+	double value = points_[tree_[idx]][k];
 						
-			// top-down
-			pair_type res = query[k] < value ?
-			closest_point(query, idx_lt, k2) :
-			closest_point(query, idx_gt, k2);
+	// top-down
+	pair_type res = query[k] < value ?
+	  closest_point(query, idx_lt, k2) :
+	  closest_point(query, idx_gt, k2);
 		
-			// bottom-up
-			if (query[k] < value)
-			{
-				// The closest point has been found in the lt_ child. See
-				// whether there isn't any closer point in the gt_ child
-				// within min_dist from the closest point.
-				if (value - query[k] < res.first)
-				{
-					pair_type res2 = closest_point(query, idx_gt, k2);
+	// bottom-up
+	if (query[k] < value)
+	  {
+	    // The closest point has been found in the lt_ child. See
+	    // whether there isn't any closer point in the gt_ child
+	    // within min_dist from the closest point.
+	    if (value - query[k] < res.first)
+	      {
+		pair_type res2 = closest_point(query, idx_gt, k2);
 					
-					if (res2.first < res.first)
-						res = res2;
-				}
-			}
-			else
-			{
-				// The closest point has been found in the gt_ child. See
-				// whether there isn't any closer point in the lt_ child
-				// within min_dist from the closest point.
-				if (query[k] - value < res.first)
-				{
-					pair_type res2 = closest_point(query, idx_lt, k2);
+		if (res2.first < res.first)
+		  res = res2;
+	      }
+	  }
+	else
+	  {
+	    // The closest point has been found in the gt_ child. See
+	    // whether there isn't any closer point in the lt_ child
+	    // within min_dist from the closest point.
+	    if (query[k] - value < res.first)
+	      {
+		pair_type res2 = closest_point(query, idx_lt, k2);
 					
-					if (res2.first < res.first)
-						res = res2;
-				}
-			}
+		if (res2.first < res.first)
+		  res = res2;
+	      }
+	  }
 			
-			return res;
-		}
-		else
-		{
-			// idx is a leaf
-			return std::make_pair(dist(query, points_[tree_[idx]]), tree_[idx]);
-		}
-	}
+	return res;
+      }
+    else
+      {
+	// idx is a leaf
+	return std::make_pair(dist(query, points_[tree_[idx]]), tree_[idx]);
+      }
+  }
 	
-	void ball_query(const point_type & center, double radius, set_type & res, unsigned idx = 0, unsigned k = 0) const
-	{
-		unsigned idx_lt = (idx << 1) + 1;
-		unsigned idx_gt = (idx << 1) + 2;
-		unsigned k2 = (k + 1) & (N - 1);
+  void ball_query(const point_type & center, double radius, set_type & res, unsigned idx = 0, unsigned k = 0) const
+  {
+    unsigned idx_lt = (idx << 1) + 1;
+    unsigned idx_gt = (idx << 1) + 2;
+    unsigned k2 = (k + 1) & (N - 1);
 
-		if (tree_[idx_lt] != -1)
-		{
-			double value = points_[tree_[idx]][k];
+    if (tree_[idx_lt] != -1)
+      {
+	double value = points_[tree_[idx]][k];
 			
-			double d = center[k] - value;
+	double d = center[k] - value;
 			
-			if (d > 0)
-			{
-				if (d < radius)
-					ball_query(center, radius, res, idx_lt, k2);
-				ball_query(center, radius, res, idx_gt, k2);
-			}
-			else
-			{
-				if (-d < radius)
-					ball_query(center, radius, res, idx_gt, k2);
-				ball_query(center, radius, res, idx_lt, k2);
-			}			
-		}
-		else
-		{
-			//idx is a leaf
-			double d = dist(center, points_[tree_[idx]]);
+	if (d > 0)
+	  {
+	    if (d < radius)
+	      ball_query(center, radius, res, idx_lt, k2);
+	    ball_query(center, radius, res, idx_gt, k2);
+	  }
+	else
+	  {
+	    if (-d < radius)
+	      ball_query(center, radius, res, idx_gt, k2);
+	    ball_query(center, radius, res, idx_lt, k2);
+	  }			
+      }
+    else
+      {
+	//idx is a leaf
+	double d = dist(center, points_[tree_[idx]]);
 			
-			if (d <= radius)
-				res.insert(std::make_pair(d, tree_[idx]));
-		}
-	}
+	if (d <= radius)
+	  res.insert(std::make_pair(d, tree_[idx]));
+      }
+  }
 	
 private:
-	void build_(const idx_iterator_type & first,
-							const idx_iterator_type & last,
-							unsigned idx = 0,
-							unsigned k = 0)
-	{
-		unsigned idx_lt = (idx << 1) + 1;
-		unsigned idx_gt = (idx << 1) + 2;
+  void build_(const idx_iterator_type & first,
+	      const idx_iterator_type & last,
+	      unsigned idx = 0,
+	      unsigned k = 0)
+  {
+    unsigned idx_lt = (idx << 1) + 1;
+    unsigned idx_gt = (idx << 1) + 2;
 
-		if (last - first > 1)
-		{
-			unsigned k2 = (k + 1) & (N - 1);
+    if (last - first > 1)
+      {
+	unsigned k2 = (k + 1) & (N - 1);
 
-      // sort the vector of points along the kth coordinate
-      sort(first, last, Cmp_(points_, k));
+	// sort the vector of points along the kth coordinate
+	sort(first, last, Cmp_(points_, k));
       
     	// take the median as the split value
-      idx_iterator_type median = first + (last - first) / 2;
-			tree_[idx] = *median;
+	idx_iterator_type median = first + (last - first) / 2;
+	tree_[idx] = *median;
 			
-			// build children
-			build_(first, median, idx_lt, k2);
-			build_(median, last, idx_gt, k2);
-    }
+	// build children
+	build_(first, median, idx_lt, k2);
+	build_(median, last, idx_gt, k2);
+      }
     else
-		{
-			tree_[idx] = *first;
-			tree_[idx_lt] = -1;
-			tree_[idx_gt] = -1;
-		}
-	}
+      {
+	tree_[idx] = *first;
+	tree_[idx_lt] = -1;
+	tree_[idx_gt] = -1;
+      }
+  }
 	
 private:
-	struct Incr_
-	{
-		Incr_() { current = 0; }
+  struct Incr_
+  {
+    Incr_() { current = 0; }
 		
-		int operator()() { return current++; }
+    int operator()() { return current++; }
 
-		int current;		
-	};
+    int current;		
+  };
 
-	struct Cmp_
-	{
-	public:
-		Cmp_(const points_type & points, unsigned k) : points_(points), k_(k) {}
+  struct Cmp_
+  {
+  public:
+    Cmp_(const points_type & points, unsigned k) : points_(points), k_(k) {}
 		
-		bool operator()(unsigned i, unsigned j) const
-		{
-			return points_[i][k_] < points_[j][k_];
-		}
+    bool operator()(unsigned i, unsigned j) const
+    {
+      return points_[i][k_] < points_[j][k_];
+    }
 		
-	private:
-		const points_type & points_;
+  private:
+    const points_type & points_;
 		
-		unsigned k_;
-	};
+    unsigned k_;
+  };
 	
 private:
-	const points_type & points_;
+  const points_type & points_;
 	
-	int* tree_;
+  int* tree_;
 };
 
 #endif /* KDTREE_HPP */
