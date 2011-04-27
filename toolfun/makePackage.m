cdd83d5a90c619c907da3e8b2f7e5d14894c4c37
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2011-04-27 20:47:46 +0000

    M    toolfun/makePackage.m - Removed *.c files from the list of files included in the package.
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@6363 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/makePackage.m
+++ toolfun/makePackage.m
@@ -1,5 +1,5 @@
 function makePackage(outDir)
-%MAKEPACKAGE builds the list of selected packages
+% Build the list of selected packages
 % 
 % makePackage(outDir)
 % 
@@ -13,7 +13,7 @@ function makePackage(outDir)
 %   outDir - The directory to copy all the package files to.
 %
 %
-% $LastChangedBy$ $LastChangedDate$
+% Sebastien Besson, 4/2011
 
 % List of all available packages
 fullPackageList={'SegmentationPackage';'BiosensorsPackage';'UTrackPackage'};
@@ -37,7 +37,7 @@ if nargin < 1 || isempty(outDir)
     outDir = uigetdir(pwd,'Select output dir:');
 end
     
-%Get m files from packages directory
+%% Get m files from packages directory
 disp('Getting file list...')
 packageFuns= cellfun(@(x) dir([x filesep '*.m']),packageDir,'UniformOutput',false);
 packageFuns=vertcat(packageFuns{:});
@@ -63,10 +63,12 @@ tbs = toolboxesUsed(packageFuns);
 disp('The package uses the following toolboxes:')
 disp(tbs)
 
-% Additional functions can be found under three types of format:
+%% Additional files can be found under four types of format:
 %   * GUIs may have associated *.fig
 %   * Processes and GUIs may have associated *.pdf
-%   * Mex-files can be found under many format depending on the OS
+%   * Mex-files have many extensions depending on the OS
+%   * Icons or other MAT-files
+
 % Split functions into paths, filenames and extensions for search
 [packageFunsPaths packageFunsNames packageFunsExt]=...
     cellfun(@fileparts,packageFuns,'UniformOutput',false);
@@ -77,18 +79,19 @@ isDocFile = logical(cellfun(@(x,y) exist([x filesep 'doc' filesep y '.pdf'],'fil
 packageDocs = cellfun(@(x,y) [x filesep 'doc' filesep y '.pdf'],...
     packageFunsPaths(isDocFile),packageFunsNames(isDocFile),'UniformOutput',false);
 
-% Get GUI-associated fig files
+% Get GUI FIG files
 isGUIFile =logical(cellfun(@(x) exist([x(1:end-2) '.fig'],'file'),packageFuns));
 packageFigs = cellfun(@(x) [x(1:end-2) '.fig'],packageFuns(isGUIFile),'UniformOutput',false);
 
-% Add all versions of MEX files
+% List all files which are neither M-files nor FIG-files nor MAT-files
 uniquePackageFunsExt = unique(packageFunsExt);
 matExt={'.fig';'.m';'.mat'};
 matExtIndx = ismember(uniquePackageFunsExt,matExt);
 mexExt=uniquePackageFunsExt(~matExtIndx);
 mexFunsIndx = find(ismember(packageFunsExt,mexExt));
 
-% List all files in the same folder as found MEX-files
+% Retrieve all mex-files
+% List all files in the same folder as these found MEX-files
 packageMexList=arrayfun(@(x)  dir([packageFunsPaths{x} filesep '*.*']),...
     mexFunsIndx,'Unif',false);
 packageMexFunsPaths=packageFunsPaths(mexFunsIndx);
@@ -97,16 +100,22 @@ packageMexFunsNames = @(x) strcat([packageMexFunsPaths{x} filesep],...
 packageMexFuns = arrayfun(@(x) packageMexFunsNames(x),1:numel(mexFunsIndx),'Unif',false);
 packageMexFuns =vertcat(packageMexFuns{:});
 
+% Remove C-files
+cFiles=~cellfun(@isempty,regexp(packageMexFuns,'.c$','once'));
+packageMexFuns(cFiles)=[];
+
 % Add icons
 packageIcons = which('lccbGuiIcons.mat');
 
 % Concatenate all files but the documentation
 packageFiles=vertcat(packageFuns,packageFigs,packageIcons,packageMexFuns);
 
+%% Export package files
 % Create package output directory if non-existing
 disp('Creating/cleaning release directory...')
 mkClrDir(outDir);
 
+% Copy function files
 nFiles = numel(packageFiles);
 disp(['Copying all '  num2str(nFiles) ' files ...'])
 for j = 1:nFiles
@@ -119,9 +128,9 @@ disp('Creating/cleaning release documentation directory...')
 docDir=[outDir filesep 'doc'];
 mkClrDir(docDir);
 
+% Copy documentation files
 nDocFiles = numel(packageDocs);
 disp(['Copying all '  num2str(nDocFiles) ' files ...'])
-
 for nfile=1:numel(packageDocs)
     iLFS = max(regexp(packageDocs{nfile},filesep));
     copyfile(packageDocs{nfile},[docDir filesep packageDocs{nfile}(iLFS+1:end)]);
