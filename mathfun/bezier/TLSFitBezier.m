10aca36a9628fb9bca0395e0f9fc6ab0ee9a8014
Sylvain Berlemont <sylvain.berlemont@hms.harvard.edu>
2011-05-19 21:20:41 +0000

    Add TLSFitBezier.m
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@6565 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/bezier/TLSFitBezier.m
+++ mathfun/bezier/TLSFitBezier.m
@@ -1,17 +1,81 @@
-function [P t r] = TLSFitBezier(X, n, varargin) %#ok<INUSD,STOUT>
+function [P t res] = TLSFitBezier(X, n, varargin)
 % TLSFitBezier computes the best total least squares fit of a nth-degree
 % Bezier curve to a set of ordered data points.
 %
-% Inputs:
-% X       a m x d array representing a set of d-dimensional points
-% n       the degree of the Bezier curve.
+% Required Inputs:
+% X              A m x d array representing a set of d-dimensional points
+% n              Degree of the Bezier curve.
+% 
+% Optional Inputs:
+% MaxFunEvals    Aaximum number of fonctional evaluations during lsqnonlin.
+% MaxIter        Maximum number of interations during lsqnonlin.
+% Display        Verbose mode during lsqnonlin.
+% TolX           Tolerance on the solution (i.e. t) during lsqnonlin.
+% TolFun         Tolerance on the functional during lsqnonlin.
 %
 % Outputs:
-% P       a n+1 x d array representing the set of d-dimensional control
-%         points defining the Bezier curve.
+% P              A n+1 x d array representing the set of d-dimensional
+%                control points defining the Bezier curve.
 %
-% t       a m x 1 array of parameter value for each input points. t belongs
-%         to [0, 1].
+% t              A m x 1 array of parameter value for each input points. t
+%                belongs to [0, 1].
 %
-% res     a m x 1 array of residue, i.e the distance between each point and
-%         the fitted curve.
+% res            A m x 1 array of residue, i.e the orthogonal distance
+%                between a data point and the fitted curve.
+%
+% Sylvain Berlemont, May 2011
+
+% Parse inputs
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('X', @isnumeric);
+ip.addRequired('n', @(n) n > 0);
+ip.addParamValue('MaxFunEvals', 1e4, @isscalar);
+ip.addParamValue('MaxIter', 1e4, @isscalar);
+ip.addParamValue('Display', 'off', @isstring);
+ip.addParamValue('TolX', 1e-8, @isscalar);
+ip.addParamValue('TolFun', 1e-8, @isscalar);
+
+ip.parse(X, n, varargin{:});
+maxFunEvals = ip.Results.MaxFunEvals;
+maxIter = ip.Results.MaxIter;
+display = ip.Results.Display;
+tolX = ip.Results.TolX;
+tolFun = ip.Results.TolFun;
+
+% Define options of lsqnonlin algorithm
+opts = optimset('Jacobian', 'off', ...
+  'MaxFunEvals', maxFunEvals, ...
+  'MaxIter', maxIter, ...
+  'Display', display, ...
+  'TolX', tolX, ...
+  'Tolfun', tolFun);
+
+% Define initial vector of nodes t0
+m = size(X,1);
+t = linspace(0,1,m)';
+
+% Solve the non-linear optimization on t
+Cnj = arrayfun(@(j) nchoosek(n,j), 0:n);
+[t, ~, res] = lsqnonlin(@(t) r(t, X, Cnj, n), t, zeros(size(t)), ones(size(t)), opts);
+
+% Compute the control points
+B = bsxfun(@times, bsxfun(@power, t, 0:n) .* bsxfun(@power, 1 - t, n:-1:0), Cnj);
+x = B \ X(:,1);
+y = B \ X(:,2);
+P = [x y];
+
+function F = r(t, X, Cnj, n)
+    
+% Compute Berstein Matrix
+B = bsxfun(@times, bsxfun(@power, t, 0:n) .* bsxfun(@power, 1 - t, n:-1:0), Cnj);
+
+% Compute the QR decomposition of B
+% Q1 is a m x (n+1) matrix
+Q1 = qr(B,0);
+
+% Q2 is a m x (m - n + 1) matrix
+% Q2 * Q2' is a m x m matrix
+Q2Q2t = eye(numel(t)) - Q1 * Q1';
+
+F = sqrt((Q2Q2t * X(:,1)).^2 + (Q2Q2t * X(:,2)).^2);
