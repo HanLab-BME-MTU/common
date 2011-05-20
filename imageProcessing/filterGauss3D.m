1d2a240a25af5880ee04963174dc1b777f09ae74
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-05-20 17:08:30 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@6581 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ imageProcessing/filterGauss3D.m
@@ -0,0 +1,26 @@
+function [out] = filterGauss3D(input, sigma, borderCondition)
+% filterGauss2D :	filters a data volume with a 3-D Gaussian mask
+%
+%    [out, G] = filterGauss3D(image, sigma, borderCondition);
+%
+%    INPUT: image           : 3-D input array
+%           sigma           : standard deviation of the Gaussian
+%           borderCondition : input for 'padarrayXT'. Default: 'symmetric'
+%                             Options: 'symmetric', 'replicate', 'circular', 'antisymmetric', or a constant value
+%
+%    OUTPUT: out : filtered volume
+%
+% Francois Aguet, added 01/21/2010
+
+if nargin < 3 || isempty(borderCondition)
+    borderCondition = 'symmetric';
+end
+
+w = ceil(3*sigma); % cutoff radius of the gaussian kernel
+x = -w:w;
+g = exp(-x.^2/(2*sigma^2));
+g = g/sum(g);
+
+out = convn(padarrayXT(input, [w w w], borderCondition), g', 'valid');
+out = convn(out, g, 'valid');
+out = convn(out, reshape(g, [1 1 2*w+1]), 'valid');
