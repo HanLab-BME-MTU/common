be6a941263c1c115f68bce69a6eef8ea3b3814fd
Anindya Chanda <anindya_chanda@hms.harvard.edu>
2011-06-22 20:42:23 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@6837 0bcb941b-74d1-0310-bb26-e6636707f451

--- detectionAlgorithms/subResFeatures2D/scriptDetectGeneral.m
+++ detectionAlgorithms/subResFeatures2D/scriptDetectGeneral.m
@@ -1,10 +1,10 @@
 
 %% movie information
-movieParam.imageDir = '/home/kj35/orchestra/groups/lccb-receptors/Grinstein/Nico/2011-02-28_CD36Tubulin/CD36Tubulin_1/images/mov_03_every3rd/'; %directory where images are
-movieParam.filenameBase = 'mov_03_new_'; %image file name base
+movieParam.imageDir = 'C:/Users/Anindya/Desktop/EAA1/Untreated/EAA/Images/'; %directory where images are
+movieParam.filenameBase = 'Untreated_EAA_'; %image file name base
 movieParam.firstImageNum = 1; %number of first image in movie
-movieParam.lastImageNum = 148; %number of last image in movie
-movieParam.digits4Enum = 4; %number of digits used for frame enumeration (1-4).
+movieParam.lastImageNum = 8; %number of last image in movie
+movieParam.digits4Enum = 8; %number of digits used for frame enumeration (1-4).
 
 %% detection parameters
 detectionParam.psfSigma = 1.6; %point spread function sigma (in pixels)
@@ -12,7 +12,7 @@ detectionParam.testAlpha = struct('alphaR',0.005,'alphaA',0.005,'alphaD',0.005,'
 detectionParam.visual = 0; %1 to see image with detected features, 0 otherwise
 detectionParam.doMMF = 1; %1 if mixture-model fitting, 0 otherwise
 detectionParam.bitDepth = 16; %Camera bit depth
-detectionParam.alphaLocMax = 0.01; %alpha-value for initial detection of local maxima
+detectionParam.alphaLocMax = 0.05; %alpha-value for initial detection of local maxima
 detectionParam.numSigmaIter = 10; %maximum number of iterations for PSF sigma estimation
 detectionParam.integWindow = 0; %number of frames before and after a frame for time integration
 
@@ -23,8 +23,8 @@ detectionParam.integWindow = 0; %number of frames before and after a frame for t
 % detectionParam.background = background;
 
 %% save results
-saveResults.dir = '/home/kj35/orchestra/groups/lccb-receptors/Grinstein/Nico/2011-02-28_CD36Tubulin/CD36Tubulin_1/images/mov_03_every3rd/'; %directory where to save input and output
-saveResults.filename = 'detectionAll1.mat'; %name of file where input and output are saved
+saveResults.dir = 'C:/Users/Anindya/Desktop/EAA1/Untreated/EAA/Analysis/'; %directory where to save input and output
+saveResults.filename = 'Untreated_EAA.mat'; %name of file where input and output are saved
 % saveResults = 0;
 
 %% run the detection function
