18e34e86e1b90020d1534985cf69c63ac4573796
Pascal Berard <pascal_berard@hms.harvard.edu>
2011-07-13 00:22:29 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@6960 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ mathfun/bezier/testTLSFitBezierWeightedConstraint1.m
@@ -0,0 +1,32 @@
+function testTLSFitBezierWeightedConstraint1()
+clc;
+clear all;
+
+n = 1;
+m = 50;
+x = linspace(0,2 * pi, m);
+y = sin(x) + rand(size(x)) * .0;
+
+figure(1);
+plot(x,y,'ro');
+hold on;
+
+W = ones(m,2);
+W([1:10,m-10:m],1) = 0.1;
+W([1:10,m-10:m],2) = 0.1;
+% W([1:5,m-5:m],1) = 10;
+% W([1:5,m-5:m],2) = 10;
+
+[P1, t1] = TLSFitBezierConstraint1([x' y'], n);
+[P2, t2] = TLSFitBezierWeightedConstraint1([x' y'],W, n);
+
+C1 = renderBezier(P1, t1);
+C2 = renderBezier(P2, t2);
+
+plot(C1(:,1),C1(:,2),'b.');
+plot(C2(:,1),C2(:,2),'r.');
+
+axis equal;
+hold off;
+
+P2
