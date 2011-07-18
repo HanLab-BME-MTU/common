673b21a39c5ecc905e4ba8442f08d14a71cba983
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2011-07-18 19:39:12 +0000

    Cleaned toolfun
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7007 0bcb941b-74d1-0310-bb26-e6636707f451

--- deprecated/clipNegative.m
+++ deprecated/clipNegative.m
@@ -5,5 +5,7 @@ function y = clipNegative(x)
 %
 %    where y = 0 if x < 0; y = x otherwise
 
+fprintf(2,['Warning: ''' mfilename ''' is deprecated. Use normpdf instead.\n']);
+
 aux = (x >= 0);
 y = aux .* x;
