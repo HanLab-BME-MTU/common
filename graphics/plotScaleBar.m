399b59abe54811e592f862d9b7074307ffd64edc
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-07-21 20:33:33 +0000

    Changed for better consistency
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7031 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/plotScaleBar.m
+++ graphics/plotScaleBar.m
@@ -9,30 +9,31 @@
 %                  'FontSize'
 %
 % Example: plotScalebar(500, [], 'Label', '500 nm', 'Location', 'SouthEast');
-
-
-% Note: there is a bug in the 'patch' function of Matlab2010b, hence the 0.999 factor.
 %
-% Francois Aguet, March 14 2011
+%
+% Note: there is a bug in '-depsc2' as of Matlab2010b, which misprints patches.
+%       When printing, use 'depsc' instead.
+
+% Francois Aguet, March 14 2011 (last modified 07/21/2011)
 
 
 function plotScaleBar(width, varargin)
-
-% fixed parameters
-b = 40;
 
 ip = inputParser;
 ip.CaseSensitive = false;
 ip.addRequired('width', @isscalar);
 ip.addParamValue('Handle', gca, @ishandle)
-ip.addParamValue('Location', 'southwest', @ischar);
-ip.addParamValue('Label', [], @ischar);
+ip.addParamValue('Location', 'southwest', @(x) any(strcmpi(x, {'northeast', 'southeast', 'southwest', 'northwest'})));
+ip.addParamValue('Label', [], @(x) ischar(x) || isempty(x));
 ip.addParamValue('FontName', 'Helvetica', @ischar);
-ip.addParamValue('FontSize', 10, @isscalar);
+ip.addParamValue('FontSize', [], @isscalar);
+ip.addParamValue('Color', [1 1 1], @(x) isvector(x) && numel(x)==3);
+
 ip.parse(width, varargin{:});
 label = ip.Results.Label;
 fontName = ip.Results.FontName;
 fontSize = ip.Results.FontSize;
+color = ip.Results.Color;
 
 XLim = get(ip.Results.Handle, 'XLim');
 YLim = get(ip.Results.Handle, 'YLim');
@@ -40,49 +41,51 @@ YLim = get(ip.Results.Handle, 'YLim');
 lx = diff(XLim);
 ly = diff(YLim);
 
-height = ly/b; % height of the scale bar
-dx = lx/b; % offset from image border
+height = width/10; % height of the scale bar
+dx = ly/20;
 
 if ~isempty(label)
+    if isempty(fontSize)
+        fontSize = 1.5*height;        
+    end
     % get height of default text bounding box
-    h = text(0, 0, ' ', 'FontName', fontName, 'FontSize', fontSize);
+    h = text(0, 0, ' ', 'FontUnits', 'pixels', 'FontName', fontName, 'FontSize', fontSize);
     extent = get(h, 'extent');
-    textHeight = extent(4);
+    textHeight = extent(4) + height;
     delete(h);
 else
     textHeight = 0;
 end
 
-
-textProps = {'Color', 'w',...
+textProps = {'Color', color,...
                 'VerticalAlignment', 'Top',...
                 'HorizontalAlignment', 'Center',...
-                'FontName', fontName, 'FontSize', fontSize};%, 'FontUnits', 'normalized'};
+                'FontName', fontName, 'FontSize', fontSize};
 
 hold on;
 set(gcf, 'InvertHardcopy', 'off');
 switch ip.Results.Location
     case 'northeast'
         fill([lx-width lx lx lx-width]-dx, [height height 0 0]+dx,...
-            [1 1 1]*0.9999, 'EdgeColor', 'none');
+            color, 'EdgeColor', 'none');
         if ~isempty(label)
             text(lx-dx-width/2, dx+height, label, textProps{:});
         end
     case 'southeast'
-        patch([lx-width lx lx lx-width]-dx, [ly ly ly-height ly-height]-max(dx,textHeight),...
-            [1 1 1]*0.9999, 'EdgeColor', 'none');
+        fill([lx-width lx lx lx-width]-dx, [ly ly ly-height ly-height]-max(dx,textHeight),...
+            color, 'EdgeColor', 'none');
         if ~isempty(label)
-            text(lx-dx-width/2, ly-max(dx,0.9*textHeight), label, textProps{:});
+            text(lx-dx-width/2, ly-max(dx,0.95*textHeight), label, textProps{:});
         end
     case 'southwest'
         fill([0 width width 0]+dx, [ly ly ly-height ly-height]-max(dx,textHeight),...
-            [1 1 1]*0.9999, 'EdgeColor', 'none');
+            color, 'EdgeColor', 'none');
         if ~isempty(label)
-            text(dx+width/2, ly-max(dx,0.9*textHeight), label, textProps{:});
+            text(dx+width/2, ly-max(dx,0.95*textHeight), label, textProps{:});
         end
     case 'northwest'
         fill([0 width width 0]+dx, [height height 0 0]+dx,...
-            [1 1 1]*0.9999, 'EdgeColor', 'none');
+            color, 'EdgeColor', 'none');
         if ~isempty(label)
             text(dx+width/2, dx+height, label, textProps{:});
         end
