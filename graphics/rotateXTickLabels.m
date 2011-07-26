74170de7e980c0f101647ef0781e845ddf8e8f84
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-07-26 05:43:36 +0000

    Function for rotating the x-axis labels
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7061 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ graphics/rotateXTickLabels.m
@@ -0,0 +1,73 @@
+%ROTATEXTICKLABELS rotates the labels on the x-axis
+%
+% INPUTS:      ha : axis handle of the plot to be modified
+%         'Angle' : rotation in degrees. Default: 45
+
+% Francois Aguet, 22 Feb 2011
+% Last modified: 26 July 2011
+
+function rotateXTickLabels(ha, varargin)
+
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('ha', @ishandle);
+ip.addParamValue('Angle', 45, @(x) isscalar(x) && (0<=x && x<=90));
+ip.parse(ha, varargin{:});
+
+hx = get(ha, 'XLabel');
+xlabelExtent = get(hx, 'Extent');
+
+
+xa = get(ha, 'XTick');
+xla = get(ha, 'XTickLabel');
+set(ha, 'XTickLabel', []);
+
+fontName = get(ha, 'FontName');
+fontSize = get(ha, 'FontSize');
+pos = get(ha, 'Position');
+
+
+YLim = get(ha, 'Ylim');
+width = diff(get(gca, 'XLim'));
+height = diff(YLim);
+
+
+% get height of default text bounding box
+h = text(0, 0, ' ', 'FontName', fontName, 'FontSize', fontSize);
+extent = get(h, 'extent');
+textHeight = extent(4);
+shift = textHeight/sqrt(2)/4 * width/height;
+delete(h);
+
+
+ht = arrayfun(@(k) text(xa(k)-shift, YLim(1)-0.01*height, xla{k},...
+    'FontName', fontName, 'FontSize', fontSize,...
+    'Units', 'data', 'VerticalAlignment', 'top', 'HorizontalAlignment', 'right',...
+    'Rotation', ip.Results.Angle),...
+    1:length(xa), 'UniformOutput', false);
+
+% largest extent
+maxHeight = cellfun(@(k) get(k, 'extent'), ht, 'UniformOutput', false);
+maxHeight = vertcat(maxHeight{:});
+maxHeight = max(maxHeight(:,4));
+maxHeightN = maxHeight/height * pos(4); % data units -> normalized
+
+xlh = xlabelExtent(4)/height * pos(4); % x-label height in normalized units
+
+% space needed: xlh+maxHeightN-pos(2)
+sp = xlh+maxHeightN-pos(2);% - textHeight/height*pos(4);
+hfig = get(ha, 'Parent');
+fpos = get(hfig, 'Position');
+fpos(4) = fpos(4)*(1+sp);
+set(hfig, 'Position', fpos);
+
+newAxesHeight = pos(4)/(1+sp);
+newTopMargin = (1-pos(2)-pos(4))/(1+sp);
+pos(2) = 1 - newTopMargin - newAxesHeight;
+pos(4) = newAxesHeight;
+set(ha, 'Position', pos);
+
+% shift x-label
+pos = get(hx, 'Position');
+pos(2) = pos(2) - maxHeight;
+set(hx, 'Position', pos, 'VerticalAlignment', 'middle');
