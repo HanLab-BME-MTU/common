fc14c7928f05912d0921a46d58ff57d7162f1356
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-07-27 15:17:46 +0000

    Help file
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7073 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ imageProcessing/steerableFiltering/steerableDetector.m
@@ -0,0 +1,22 @@
+%[res, theta, nms, filterBank] = steerableDetector(img, M, sigma) performs edge/ridge detection through a generalization of Canny's alorithm based on steerable filters
+%
+% Inputs: 
+%         img : input image
+%           M : order of the filter, between 1 and 5.
+%             : Odd orders: edge detectors, M = 1 is equivalent to Canny's detector.
+%             : Even orders: ridge detectors.
+%       sigma : standard deviation of the Gaussian kernel on which the filters are based
+%
+% Outputs: 
+%         res : response to the filter
+%       theta : orientation map
+%         nms : non-maximum-suppressed response
+%  filterBank : templates used in the computation of the steerable filter response
+%
+% For more information, see:
+% M. Jacob et al., IEEE Trans. Image Proc., 26(8) 1007-1019, 2004.
+
+% Francois Aguet, 07/12/2011 (last modified Jul 14, 2011).
+% Adapted from SteerableJ package (2008).
+
+function [res, theta, nms, filterBank] = steerableDetector(img, M, sigma) %#ok<STOUT,INUSD>
