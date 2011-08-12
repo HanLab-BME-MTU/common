6ef3db3d6d2b4a4292d6b0fb37e541c83fad7d70
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2011-08-12 19:17:23 +0000

    Added detectVectorFieldOutliers
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7229 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ mathfun/detectVectorFieldOutliers.m
@@ -0,0 +1,77 @@
+function [outlierIndex, r] = detectVectorFieldOutliers(data,varargin)
+% detectVectorFieldOutlier detect and return the outliers in a vector field
+%
+% Synopsis:        outlierIdx = detectOutliers(data)
+%                  [outlierIdx,r] = detectOutliers(data,2)
+%
+% This function detects outliers within a vectorial field using an extended
+% version of the 'median test' of Westerweel et al. 1994, adapted for PTV.
+% After constructing a Delaunay tessellation, the algorithm calculates the
+% normalized fluctuation with respect to the neighborood median residual
+% for each vertex. A threshold is then applied to this quantity to extract
+% the outliers.
+%
+% Input:
+%      data - a vector field, i.e. a matrix of size nx4 where the two first
+%      columns give the positions and the two last the displacement
+%
+%      threshold (optional) - a threshold for the detection criterion.
+%      Usually values are between 2-4 depending on the stringency.
+%
+% Output
+%      outlierIndx - the index of the outlier along the first dimension of
+%      data
+%
+%      r - the values of the normalized fluctuation for each element of the
+%      vector field
+%
+% For more information, see:
+% J. Westerweel & F. Scarano, Exp. Fluids, 39 1096-1100, 2005.
+% J. Duncan et al., Meas. Sci. Technol., 21 057002, 2010.
+
+% Sebastien Besson, Aug 2011
+
+% Input check
+ip=inputParser;
+ip.addRequired('data',@(x) size(x,2)==4);
+ip.addOptional('threshold',2,@isscalar);
+ip.addParamValue('epsilon',.1,@isscalar);
+ip.parse(data,varargin{:})
+threshold=ip.Results.threshold;
+epsilon=ip.Results.epsilon;
+
+% Generate a Delaunay tessellation
+tri=DelaunayTri(data(:,1),data(:,2));
+
+% Get the triangulation edges and calculate all distances
+edges= tri.edges;
+dp=(data(edges(:,2),1:2)-data(edges(:,1),1:2));
+D= sqrt(sum(dp.^2,2));
+
+% Convert the edge list into an adjacency list 
+nodes = unique([edges(:,1)' edges(:,2)']);
+N=cell(numel(nodes),1);
+E=cell(numel(nodes),1);
+for e=1:size(edges,1); 
+    N{edges(e,1)}=[N{edges(e,1)},edges(e,2)]; 
+    E{edges(e,1)}=[E{edges(e,1)},e]; 
+    N{edges(e,2)}=[N{edges(e,2)},edges(e,1)]; 
+    E{edges(e,2)}=[E{edges(e,2)},e];
+end
+
+% Measure weighted local and neighborhood velocities
+options = {1:size(data,1),'Unif',false};
+d =arrayfun(@(x)D(E{x}),options{:});
+localVel=arrayfun(@(x)data(x,3:4)/(median(d{x})+epsilon),options{:});
+neighVel=arrayfun(@(x)data(N{x},3:4)./repmat(d{x}+epsilon,1,2),options{:});
+
+% Get median weighted neighborood velocity
+medianVel=cellfun(@median,neighVel,'Unif',false);
+
+% Calculate normalized fluctuation using neighborood residuals
+medianRes=arrayfun(@(x) median(abs(neighVel{x}-repmat(medianVel{x},size(neighVel{x},1),1))),options{:});
+normFluct = arrayfun(@(x) abs(localVel{x}-medianVel{x})./(medianRes{x}+epsilon),options{:});
+r=cellfun(@norm, normFluct);
+
+% Filter outliers using threshold
+outlierIndex = find(r>threshold);
