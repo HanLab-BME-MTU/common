23c8a06bb5d4931c9c204824a3d5abdd420a6998
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-09-08 21:15:36 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7330 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ imageProcessing/nonMaximumSuppression.m
@@ -0,0 +1,25 @@
+%nms = nonMaximumSuppression(res, th)
+%
+% Inputs:   res : response
+%            th : orientation
+%
+% Uses the grid conventions of steerableDetector()
+
+% Francois Aguet
+
+function nms = nonMaximumSuppression(res, th)
+
+resXT = padarrayXT(res, [1 1], 'symmetric');
+
+[ny,nx] = size(res);
+[x,y] = meshgrid(1:nx,1:ny);
+[xi,yi] = meshgrid(0:nx+1, 0:ny+1);
+
+% +1 interp
+A1 = interp2(xi, yi, resXT, x+sin(th), y-cos(th));
+
+% -1 interp
+A2 = interp2(xi, yi, resXT, x-sin(th), y+cos(th));
+
+nms = res;
+nms(res<=A1 | res<=A2) = 0;
