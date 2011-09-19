2bb9f4165661b147dd3023ba18993f7d18ce160b
Francois Aguet <francois_aguet@hms.harvard.edu>
2011-09-19 21:06:37 +0000

    NaNs in input are excluded from calculation
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7395 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageProcessing/localAvgStd2D.m
+++ imageProcessing/localAvgStd2D.m
@@ -1,22 +1,33 @@
 % Computes the local average and standard deviation within a square window of side 'w'.
 
 % Francois Aguet
-% Last modified on 10/14/2010
+% Last modified on 09/19/2011
 
-function [avg sigma] = localAvgStd2D(image, w)
+function [avg sigma] = localAvgStd2D(img, w)
 
 if mod(w+1, 2)
     error('The window length w should be an odd integer.');
 end;
 
+nanMask = isnan(img);
+
 b = (w-1)/2;
-image = padarray(image, [b b], 'replicate');
 
+% kernel
 h = ones(1,w);
-E = conv2(h/w, h, image, 'valid');
-E2 = conv2(h, h, image.^2, 'valid');
 
-sigma = E2 - E.^2;
+% count of non-NaN elements
+n = conv2(h, h, padarray(double(~nanMask), [b b], 'replicate'), 'valid');
+img(nanMask) = 0;
+
+img = padarray(img, [b b], 'replicate');
+E = conv2(h, h, img, 'valid');
+E2 = conv2(h, h, img.^2, 'valid');
+
+sigma = E2 - E.^2./n;
 sigma(sigma<0) = 0;
-sigma = sqrt(sigma/(w*w - 1));
-avg = E/w;
+sigma = sqrt(sigma./(n - 1));
+avg = E./n;
+
+avg(nanMask) = NaN;
+sigma(nanMask) = NaN;
