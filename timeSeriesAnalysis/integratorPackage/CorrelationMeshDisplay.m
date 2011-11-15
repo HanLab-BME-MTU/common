ff22403f5529a2842a419f94bfbcd014df71139d
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2011-11-15 20:13:39 +0000

    Implemented double slider
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7802 0bcb941b-74d1-0310-bb26-e6636707f451

--- timeSeriesAnalysis/integratorPackage/CorrelationMeshDisplay.m
+++ timeSeriesAnalysis/integratorPackage/CorrelationMeshDisplay.m
@@ -7,6 +7,7 @@ classdef CorrelationMeshDisplay < MovieDataDisplay
     end
     properties (SetAccess = protected)
         slider;
+        slider2;
     end
     methods
         function obj=MeshDisplay(varargin)
@@ -23,7 +24,7 @@ classdef CorrelationMeshDisplay < MovieDataDisplay
             [nx,ny,nz,nz2] = size(data.Z);
             if ~isfield(data,'X'), data.X=repmat(1:nx,1,ny); end
             if ~isfield(data,'Y'), data.Y= repmat(1:ny,nx,1); end
-            h(1)=mesh(data.X(:,:,1),data.Y,data.Z(:,:,1),'FaceColor','interp');
+            h(1)=mesh(data.X(:,:,1,1),data.Y,data.Z(:,:,1,1),'FaceColor','interp');
            
             xlabel('Lag (s)')
             ylabel('Window number')
@@ -32,11 +33,11 @@ classdef CorrelationMeshDisplay < MovieDataDisplay
             if ~isempty(data.bounds)
                 hold on
 %                 [cL,~] = size(data.Z);
-                upline  = repmat(data.bounds(1,:,1),nx,1);
-                h(2)=mesh(data.X(:,:,1),data.Y,upline,'FaceColor',[63/255 63/255 63/255]);
+                upline  = repmat(data.bounds(1,:,1,1),nx,1);
+                h(2)=mesh(data.X(:,:,1,1),data.Y,upline,'FaceColor',[63/255 63/255 63/255]);
                 
-                dline  = repmat(data.bounds(2,:,1),nx,1);
-                h(3)=mesh(data.X(:,:,1),data.Y,dline,'FaceColor',[63/255 63/255 63/255]);
+                dline  = repmat(data.bounds(2,:,1,1),nx,1);
+                h(3)=mesh(data.X(:,:,1,1),data.Y,dline,'FaceColor',[63/255 63/255 63/255]);
             end
             
             if nz>1
@@ -51,22 +52,39 @@ classdef CorrelationMeshDisplay < MovieDataDisplay
                     'Tag','slider_depth','BackgroundColor','white',...
                     'Callback',@(hObject,event) updateDraw(obj,h,data));
             end
+            if nz2>1
+                axesPos = get(get(h(1),'Parent'),'Position');
+                mainFig = get(get(h(1),'Parent'),'Parent');
+                set(mainFig,'Toolbar','figure');
+                obj.slider2 = uicontrol(mainFig,'Style','slider',...
+                    'Units','normalized',...
+                    'Position',[axesPos(1) 1-.06 axesPos(3) .03],...
+                    'Value',1,'Min',1,'Max',nz,...
+                    'SliderStep',[1/(nz2-1)  5/(nz2-1)],...
+                    'Tag','slider_depth','BackgroundColor','white',...
+                    'Callback',@(hObject,event) updateDraw(obj,h,data));
+            end
             
             set(h,'Tag',tag);
         end
         function updateDraw(obj,h,data)
-            [nx,ny,nz] = size(data.Z);
+            [nx,ny,nz,nz2] = size(data.Z);
             if nz>1,
-                depth = round(get(obj.slider,'Value'));
+                zdepth = round(get(obj.slider,'Value'));
             else
-                depth=1;
+                zdepth=1;
             end
-            set(h(1),'XData',data.X(:,:,depth),'ZData',data.Z(:,:,depth));
+            if nz2>1,
+                zdepth2 = round(get(obj.slider2,'Value'));
+            else
+                zdepth2=1;
+            end
+            set(h(1),'XData',data.X(:,:,zdepth,zdepth2),'ZData',data.Z(:,:,zdepth,zdepth2));
             if ~isempty(data.bounds)
-                upline  = repmat(data.bounds(1,:,depth),nx,1);
-                set(h(2),'XData',data.X(:,:,depth),'ZData',upline);                
-                dline  = repmat(data.bounds(2,:,depth),nx,1);
-                set(h(3),'XData',data.X(:,:,depth),'ZData',dline);
+                upline  = repmat(data.bounds(1,:,zdepth,zdepth2),nx,1);
+                set(h(2),'XData',data.X(:,:,zdepth,zdepth2),'ZData',upline);                
+                dline  = repmat(data.bounds(2,:,zdepth,zdepth2),nx,1);
+                set(h(3),'XData',data.X(:,:,zdepth,zdepth2),'ZData',dline);
             end
 
         end
