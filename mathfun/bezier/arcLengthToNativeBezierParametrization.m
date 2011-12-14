5b9f1299b5de4654fe1f3f1da55b1a5d72fa01b2
Pascal Berard <pascal_berard@hms.harvard.edu>
2011-12-14 20:07:07 +0000

    Added arcLengthToNativeBezierParametrization.m
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7951 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ mathfun/bezier/arcLengthToNativeBezierParametrization.m
@@ -0,0 +1,75 @@
+function t = arcLengthToNativeBezierParametrization(cP,s)
+% function t = arcLengthToNativeBezierParametrization(cP,s)
+% arcLengthToNativeBezierParametrization transforms an arc length 
+% parametrization into the 'native' parametrization of a Bezier curve by 
+% iteratively splitting the search interval.
+%
+% Required Inputs:
+% cP             A n+1 x 3 array representing the three-dimensional control 
+%                points of the Bezier curve of degree n.
+%
+% s              Any array representing the arc length nodes for which the
+%                corresponding 'native' nodes should be computed.
+% 
+% Optional Inputs:
+%
+% Outputs:
+% t              Array representing the transformed ('native') node values
+%
+% Pascal Berard, December 2011
+
+% Define parameters
+iterMax = 1000;
+precision = 0.0001;
+
+% Transform line vectors and matrices into column vectors
+sizeS = size(s);
+s = reshape(s,numel(s),1);
+
+% Test if all s are between 0 and 1
+if all(s<=1&s>=0)
+    nS = numel(s);
+    tLow = zeros(nS,1);
+    tUpper = ones(nS,1);
+    sLow = zeros(nS,1);
+    sUpper = ones(nS,1);
+    t = zeros(nS,1);
+    
+    length = lengthBezier(cP);
+    
+    for iter=1:iterMax
+        tGuess = (tLow+tUpper)/2;
+        sGuess = arrayfun(@(a)lengthBezier(cP,0,a)/length,tGuess);
+        
+        % Determine bounds
+        guessTooHigh = sGuess > s;
+        tUpper(guessTooHigh) = tGuess(guessTooHigh);
+        sUpper(guessTooHigh) = sGuess(guessTooHigh);
+        tLow(~guessTooHigh) = tGuess(~guessTooHigh);
+        sLow(~guessTooHigh) = sGuess(~guessTooHigh);
+        
+        % Precision test
+        sDelta = arrayfun(@(a,b) lengthBezier(cP,a,b)/length,tLow,tUpper);
+        
+        ok = sDelta < precision; % Precision reached
+
+        % Exit when at least the required precision is reached
+        if all(ok)
+            % Linear interpolation of t
+            f = (s-sLow)/(sUpper-sLow);
+            t = tLow + f*(tUpper-tLow);
+            break;
+        end
+    end
+    
+    if iter == iterMax
+        disp('WARNING: transformArcLengthParametrization: iterMax reached! Result may be inaccurate!');
+    end
+    
+    % Restore the input vector/matrix shape
+    t = reshape(t,sizeS);
+else
+    disp('WARNING: transformArcLengthParametrization: The input parameter s must be between 0 and 1!');
+end
+
+end
