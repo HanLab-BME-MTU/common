6a757e4bb8d7e803a647a135291169cf30e25f6a
Khuloud Jaqaman <khuloud_jaqaman@hms.harvard.edu>
2011-12-16 22:36:48 +0000

    updated uTrackGUI documentation
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@7969 0bcb941b-74d1-0310-bb26-e6636707f451

--- detectionAlgorithms/subResFeatures2D/scriptDetectGeneral.m
+++ detectionAlgorithms/subResFeatures2D/scriptDetectGeneral.m
@@ -1,30 +1,32 @@
 
 %% movie information
-movieParam.imageDir = '/home/kj35/files/LCCB/receptors/endoCells/AnindyaChanda/20111018_LatranculinTreatment_TSP1_treated/Before_Lat/Images/'; %directory where images are
-movieParam.filenameBase = 'Stream_unperturbed_'; %image file name base
+movieParam.imageDir = '/home/kj35/files/LCCB/receptors/superres/testing/syntheticImages/10p89p4_highnoise/'; %directory where images are
+movieParam.filenameBase = 'image10p89p4_highnoise_'; %image file name base
 movieParam.firstImageNum = 1; %number of first image in movie
-movieParam.lastImageNum = 100; %number of last image in movie
-movieParam.digits4Enum = 3; %number of digits used for frame enumeration (1-4).
+movieParam.lastImageNum = 9; %number of last image in movie
+movieParam.digits4Enum = 1; %number of digits used for frame enumeration (1-4).
 
 %% detection parameters
-detectionParam.psfSigma = 1; %point spread function sigma (in pixels)
-detectionParam.testAlpha = struct('alphaR',0.001,'alphaA',0.4,'alphaD',1,'alphaF',0); %alpha-values for detection statistical tests
+detectionParam.psfSigma = 1.5; %point spread function sigma (in pixels)
+detectionParam.testAlpha = struct('alphaR',0.05,'alphaA',0.05,'alphaD',0.05,'alphaF',0); %alpha-values for detection statistical tests
 detectionParam.visual = 0; %1 to see image with detected features, 0 otherwise
-detectionParam.doMMF = 1; %1 if mixture-model fitting, 0 otherwise
+detectionParam.doMMF = 0; %1 if mixture-model fitting, 0 otherwise
 detectionParam.bitDepth = 16; %Camera bit depth
-detectionParam.alphaLocMax = 0.25; %alpha-value for initial detection of local maxima
+detectionParam.alphaLocMax = 0.0005; %alpha-value for initial detection of local maxima
 detectionParam.numSigmaIter = 0; %maximum number of iterations for PSF sigma estimation
 detectionParam.integWindow = 0; %number of frames before and after a frame for time integration
 
-%absolute background info and parameters...
-background.imageDir = '/home/kj35/files/LCCB/receptors/endoCells/AnindyaChanda/20111018_LatranculinTreatment_TSP1_treated/Before_Lat/bg/';
-background.filenameBase = 'crop_Stream_unperturbed_';
-background.alphaLocMaxAbs = 0.001;
-detectionParam.background = background;
+detectionParam.calcMethod = 'g';
+
+% %absolute background info and parameters...
+% background.imageDir = '/home/kj35/files/LCCB/receptors/Galbraiths/data/talinAndCellEdge/110916_Cs1C4_Talin/bgTalin/';
+% background.filenameBase = 'crop_110916_Cs1C4_CHO_mEos2Talin_A_';
+% background.alphaLocMaxAbs = 0.01;
+% detectionParam.background = background;
 
 %% save results
-saveResults.dir = '/home/kj35/files/LCCB/receptors/endoCells/AnindyaChanda/20111018_LatranculinTreatment_TSP1_treated/Before_Lat/analysisKJ/'; %directory where to save input and output
-saveResults.filename = 'detection11Frames001to100.mat'; %name of file where input and output are saved
+saveResults.dir = '/home/kj35/files/LCCB/receptors/superres/testing/'; %directory where to save input and output
+saveResults.filename = 'localizationGauss0M.mat'; %name of file where input and output are saved
 % saveResults = 0;
 
 %% run the detection function
--- trackWithGapClosing/Kalman/scriptTrackGeneral.m
+++ trackWithGapClosing/Kalman/scriptTrackGeneral.m
@@ -1,8 +1,8 @@
 
 %% general gap closing parameters
-gapCloseParam.timeWindow = 7; %maximum allowed time gap (in frames) between a track segment end and a track segment start that allows linking them.
-gapCloseParam.mergeSplit = 1; %1 if merging and splitting are to be considered, 2 if only merging is to be considered, 3 if only splitting is to be considered, 0 if no merging or splitting are to be considered.
-gapCloseParam.minTrackLen = 2; %minimum length of track segments from linking to be used in gap closing.
+gapCloseParam.timeWindow = 6; %maximum allowed time gap (in frames) between a track segment end and a track segment start that allows linking them.
+gapCloseParam.mergeSplit = 0; %1 if merging and splitting are to be considered, 2 if only merging is to be considered, 3 if only splitting is to be considered, 0 if no merging or splitting are to be considered.
+gapCloseParam.minTrackLen = 1; %minimum length of track segments from linking to be used in gap closing.
 
 %optional input:
 gapCloseParam.diagnostics = 1; %1 to plot a histogram of gap lengths in the end; 0 or empty otherwise.
@@ -14,10 +14,10 @@ costMatrices(1).funcName = 'costMatRandomDirectedSwitchingMotionLink';
 
 %parameters
 
-parameters.linearMotion = 0; %use linear motion Kalman filter.
+parameters.linearMotion = 1; %use linear motion Kalman filter.
 
 parameters.minSearchRadius = 2; %minimum allowed search radius. The search radius is calculated on the spot in the code given a feature's motion parameters. If it happens to be smaller than this minimum, it will be increased to the minimum.
-parameters.maxSearchRadius = 4.5; %maximum allowed search radius. Again, if a feature's calculated search radius is larger than this maximum, it will be reduced to this maximum.
+parameters.maxSearchRadius = 5; %maximum allowed search radius. Again, if a feature's calculated search radius is larger than this maximum, it will be reduced to this maximum.
 parameters.brownStdMult = 3; %multiplication factor to calculate search radius from standard deviation.
 
 parameters.useLocalDensity = 1; %1 if you want to expand the search radius of isolated features in the linking (initial tracking) step.
@@ -27,7 +27,7 @@ parameters.kalmanInitParam = []; %Kalman filter initialization parameters.
 % parameters.kalmanInitParam.searchRadiusFirstIteration = 10; %Kalman filter initialization parameters.
 
 %optional input
-parameters.diagnostics = []; %if you want to plot the histogram of linking distances up to certain frames, indicate their numbers; 0 or empty otherwise. Does not work for the first or last frame of a movie.
+parameters.diagnostics = 18; %if you want to plot the histogram of linking distances up to certain frames, indicate their numbers; 0 or empty otherwise. Does not work for the first or last frame of a movie.
 
 costMatrices(1).parameters = parameters;
 clear parameters
@@ -40,17 +40,17 @@ costMatrices(2).funcName = 'costMatRandomDirectedSwitchingMotionCloseGaps';
 %parameters
 
 %needed all the time
-parameters.linearMotion = 0; %use linear motion Kalman filter.
+parameters.linearMotion = 1; %use linear motion Kalman filter.
 
 parameters.minSearchRadius = 2; %minimum allowed search radius.
-parameters.maxSearchRadius = 4.5; %maximum allowed search radius.
+parameters.maxSearchRadius = 5; %maximum allowed search radius.
 parameters.brownStdMult = 3*ones(gapCloseParam.timeWindow,1); %multiplication factor to calculate Brownian search radius from standard deviation.
 
-parameters.brownScaling = [0.25 0.01]; %power for scaling the Brownian search radius with time, before and after timeReachConfB (next parameter).
+parameters.brownScaling = [0.5 0.01]; %power for scaling the Brownian search radius with time, before and after timeReachConfB (next parameter).
 % parameters.timeReachConfB = 3; %before timeReachConfB, the search radius grows with time with the power in brownScaling(1); after timeReachConfB it grows with the power in brownScaling(2).
 parameters.timeReachConfB = gapCloseParam.timeWindow; %before timeReachConfB, the search radius grows with time with the power in brownScaling(1); after timeReachConfB it grows with the power in brownScaling(2).
 
-parameters.ampRatioLimit = [0.7 4]; %for merging and splitting. Minimum and maximum ratios between the intensity of a feature after merging/before splitting and the sum of the intensities of the 2 features that merge/split.
+parameters.ampRatioLimit = []; %for merging and splitting. Minimum and maximum ratios between the intensity of a feature after merging/before splitting and the sum of the intensities of the 2 features that merge/split.
 
 parameters.lenForClassify = 5; %minimum track segment length to classify it as linear or random.
 
@@ -59,7 +59,7 @@ parameters.nnWindow = gapCloseParam.timeWindow; %number of frames before/after t
 
 parameters.linStdMult = 1*ones(gapCloseParam.timeWindow,1); %multiplication factor to calculate linear search radius from standard deviation.
 
-parameters.linScaling = [0.25 0.01]; %power for scaling the linear search radius with time (similar to brownScaling).
+parameters.linScaling = [1 0.01]; %power for scaling the linear search radius with time (similar to brownScaling).
 % parameters.timeReachConfL = 4; %similar to timeReachConfB, but for the linear part of the motion.
 parameters.timeReachConfL = gapCloseParam.timeWindow; %similar to timeReachConfB, but for the linear part of the motion.
 
@@ -85,9 +85,9 @@ kalmanFunctions.timeReverse = 'kalmanReverseLinearMotion';
 %% additional input
 
 %saveResults
-saveResults.dir = '/home/kj35/files/LCCB/receptors/Galbraiths/data/alphaVandCellEdge/110114/Cs3_CHO03/Cs3_CHO03B/analysisAlphaV/new/tracks/'; %directory where to save input and output
-% saveResults.filename = 'tracks1Detection10.mat'; %name of file where input and output are saved
-% saveResults = 0; %don't save results
+% saveResults.dir = '/home/kj35/files/LCCB/receptors/trialStuff/Bree/CrowdSourcingProblem/forKhuloud/'; %directory where to save input and output
+% saveResults.filename = 'movie38TracksTest8.mat'; %name of file where input and output are saved
+saveResults = 0; %don't save results
 
 %verbose
 verbose = 1;
@@ -97,16 +97,23 @@ probDim = 2;
 
 %% tracking function call
 
-% [tracksFinal,kalmanInfoLink,errFlag] = trackCloseGapsKalmanSparse(movieInfo,...
-%     costMatrices,gapCloseParam,kalmanFunctions,probDim,saveResults,verbose);
+[tracksFinal,kalmanInfoLink,errFlag] = trackCloseGapsKalmanSparse(movieInfo,...
+    costMatrices,gapCloseParam,kalmanFunctions,probDim,saveResults,verbose);
 
-for i = 1 : 5
-    movieInfoTmp((i-1)*1200+1:min(i*1200,6800)) = movieInfo((i-1)*1200+1:min(i*1200,6800));
-    saveResults.filename = ['tracks1All01_' sprintf('%02i',i) '.mat'];
-    [tracksFinal,kalmanInfoLink,errFlag] = trackCloseGapsKalmanSparse(movieInfoTmp,...
-        costMatrices,gapCloseParam,kalmanFunctions,probDim,saveResults,verbose);
-    clear movieInfoTmp
-end
+% for i = 1 : 11
+%     movieInfoTmp((i-1)*1200+1:i*1200) = movieInfo((i-1)*1200+1:i*1200);
+%     saveResults.filename = ['tracks2All_' sprintf('%02i',i) '.mat'];
+%     [tracksFinal,kalmanInfoLink,errFlag] = trackCloseGapsKalmanSparse(movieInfoTmp,...
+%         costMatrices,gapCloseParam,kalmanFunctions,probDim,saveResults,verbose);
+%     clear movieInfoTmp
+% end
+% i=12;
+% movieInfoTmp((i-1)*1200+1:14800) = movieInfo((i-1)*1200+1:14800);
+% saveResults.filename = ['tracks2All_' sprintf('%02i',i) '.mat'];
+% [tracksFinal,kalmanInfoLink,errFlag] = trackCloseGapsKalmanSparse(movieInfoTmp,...
+%     costMatrices,gapCloseParam,kalmanFunctions,probDim,saveResults,verbose);
+% clear movieInfoTmp
+
 
 % for startFrame = 1 : 400 : 48000
 %     endFrame = startFrame + 399;
