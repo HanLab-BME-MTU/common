205f424bb883b643ad7b0c7b280414de9830c9de
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-01-09 20:46:54 +0000

    Updated help
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@8013 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageProcessing/steerableFiltering/steerableDetector.m
+++ imageProcessing/steerableFiltering/steerableDetector.m
@@ -2,9 +2,11 @@
 %
 % Inputs: 
 %         img : input image
-%           M : order of the filter, between 1 and 5.
-%             : Odd orders: edge detectors, M = 1 is equivalent to Canny's detector.
-%             : Even orders: ridge detectors.
+%           M : order of the filter, between 1 and 5
+%             : Odd orders: edge detectors, M = 1 is equivalent to Canny's detector
+%             : Even orders: ridge detectors
+%               Higher orders provide better orientation selectivity and are less sensitive to noise,
+%               at a small trade-off in computational cost.
 %       sigma : standard deviation of the Gaussian kernel on which the filters are based
 %
 % Outputs: 
@@ -16,7 +18,7 @@
 % For more information, see:
 % M. Jacob et al., IEEE Trans. Image Proc., 26(8) 1007-1019, 2004.
 
-% Francois Aguet, 07/12/2011 (last modified Jul 14, 2011).
+% Francois Aguet, 07/12/2011 (last modified Nov 17, 2011).
 % Adapted from SteerableJ package (2008).
 
 function [res, theta, nms, filterBank] = steerableDetector(img, M, sigma) %#ok<STOUT,INUSD>
