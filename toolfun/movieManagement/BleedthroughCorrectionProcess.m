636c7ce04e2dd432e16c224849e33f5e885edf00
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2012-01-30 15:54:04 +0000

    Changed BleedthroughCorrectionProcess into ImageProcessingProcess
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@8094 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/movieManagement/BleedthroughCorrectionProcess.m
+++ toolfun/movieManagement/BleedthroughCorrectionProcess.m
@@ -1,9 +1,7 @@
-classdef BleedthroughCorrectionProcess < ImageCorrectionProcess
-    
+classdef BleedthroughCorrectionProcess < ImageProcessingProcess   
     %A class for performing bleedthrough correction on images.
     %
     %Hunter Elliott, 5/2010
-    %
 
     methods (Access = public)
         
@@ -11,36 +9,18 @@ classdef BleedthroughCorrectionProcess < ImageCorrectionProcess
                                               
             if nargin == 0
                 super_args = {};
-            else                
-                
+            else                       
                 super_args{1} = owner;
                 super_args{2} = BleedthroughCorrectionProcess.getName;
                 super_args{3} = @bleedthroughCorrectMovie;                               
                 if nargin<2, outputDir = owner.outputDirectory_; end
                 if nargin < 3 || isempty(funParams) 
-                    
                     funParams=BleedthroughCorrectionProcess.getDefaultParams(owner,outputDir);
                 end
-                
                 super_args{4} = funParams;    
-                
-                if nargin > 3           
-                    %Set the correction image paths to the shadeImage paths
-                    %input.
-                    super_args{7} = shadeImagePaths;                
-                end
-                
-                if nargin > 4
-                    super_args{5} = inImagePaths;
-                end
-                
-                if nargin > 5
-                    super_args{6} = outImagePaths;
-                end                
-                
             end
             
-            obj = obj@ImageCorrectionProcess(super_args{:});
+            obj = obj@ImageProcessingProcess(super_args{:});
         end   
         
     end
