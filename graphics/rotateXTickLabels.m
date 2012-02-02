1c9352bc5a5fd340612b4e62c707acefcd3331a2
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-02-02 23:32:00 +0000

    Figure size adjustment for cases when rotated labels are outside of bounds
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@8131 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/rotateXTickLabels.m
+++ graphics/rotateXTickLabels.m
@@ -16,24 +16,19 @@ ip.addParamValue('Interpreter', 'tex', @(x) any(strcmpi(x, {'tex', 'latex', 'non
 ip.addParamValue('AdjustFigure', true, @islogical);
 ip.parse(ha, varargin{:});
 
-hx = get(ha, 'XLabel');
-xlabelExtent = get(hx, 'Extent');
-
-
 xa = get(ha, 'XTick');
 xla = get(ha, 'XTickLabel');
 if ischar(xla) % if singleton labels
     xla = num2cell(xla);
 end
-set(ha, 'XTickLabel', []);
+set(ha, 'XTickLabel', [], 'Units', 'pixels');
 
 fontName = get(ha, 'FontName');
 fontSize = get(ha, 'FontSize');
-pos = get(ha, 'Position');
 
-
+XLim = get(gca, 'XLim');
 YLim = get(ha, 'Ylim');
-width = diff(get(gca, 'XLim'));
+width = diff(XLim);
 height = diff(YLim);
 
 
@@ -51,26 +46,37 @@ ht = arrayfun(@(k) text(xa(k)-shift, YLim(1)-0.01*height, xla{k},...
     'Rotation', ip.Results.Angle, 'Interpreter', ip.Results.Interpreter),...
     1:length(xa), 'UniformOutput', false);
 
-% largest extent
-maxHeight = cellfun(@(k) get(k, 'extent'), ht, 'UniformOutput', false);
-maxHeight = vertcat(maxHeight{:});
-maxHeight = max(maxHeight(:,4));
-maxHeightN = maxHeight/height * pos(4); % data units -> normalized
+% largest extent (relative to axes units)
+extents = cellfun(@(k) get(k, 'extent'), ht, 'UniformOutput', false);
+extents = vertcat(extents{:});
 
-xlh = xlabelExtent(4)/height * pos(4); % x-label height in normalized units
+pos = get(ha, 'Position');
+
+lmargin = -min(extents(:,1))/width * pos(3); % normalized units in fig. frame
+
+hx = get(ha, 'XLabel');
+maxHeight = max(extents(:,4));
+if ~strcmpi(get(hx, 'String'), ' ')
+    xlheight = get(hx, 'Extent');
+    xlheight = xlheight(4);
+else
+    xlheight = 0;
+end
+bmargin = (maxHeight+xlheight)/height * pos(4); % data units -> normalized
 
 if ip.Results.AdjustFigure
-    % space needed: xlh+maxHeightN-pos(2)
-    sp = xlh+maxHeightN-pos(2);% - textHeight/height*pos(4);
     hfig = get(ha, 'Parent');
     fpos = get(hfig, 'Position');
-    fpos(4) = fpos(4)*(1+sp);
-    set(hfig, 'Position', fpos, 'PaperPositionMode', 'auto');
+    % expand figure window
     
-    newAxesHeight = pos(4)/(1+sp);
-    newTopMargin = (1-pos(2)-pos(4))/(1+sp);
-    pos(2) = 1 - newTopMargin - newAxesHeight;
-    pos(4) = newAxesHeight;
+    if lmargin > pos(1)
+        fpos(3) = fpos(3) + lmargin-pos(1);
+        pos(1) = lmargin;
+    end
+    fpos(4) = fpos(4) + bmargin-pos(2);
+    pos(2) = bmargin;
+
+    set(hfig, 'Position', fpos, 'PaperPositionMode', 'auto');
     set(ha, 'Position', pos);
 end
 
