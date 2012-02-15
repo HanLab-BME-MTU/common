cfe6ed708f6649a800b0a3a3e158e8d91a50cea8
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-02-15 20:12:15 +0000

    small optimizations
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@8256 0bcb941b-74d1-0310-bb26-e6636707f451

--- mex/include/stats.h
+++ mex/include/stats.h
@@ -218,11 +218,11 @@ unsigned char adtest(double *data, int N, int adCase, double mu, double sigma, d
     free(sdata);
     
     // Look-up table for critical values
-    double alphaVec[4] = {0.01, 0.025, 0.05,  0.1};
-    double ctable[16] = {3.857, 3.070, 2.492, 1.933, // case 1
-                         1.573, 1.304, 1.105, 0.908, // case 2
-                         3.690, 2.904, 2.323, 1.760, // case 3
-                         1.092, 0.918, 0.787, 0.656};// case 4
+    static const double alphaVec[4] = {0.01, 0.025, 0.05,  0.1};
+    static const double ctable[16] = {3.857, 3.070, 2.492, 1.933, // case 1
+                                      1.573, 1.304, 1.105, 0.908, // case 2
+                                      3.690, 2.904, 2.323, 1.760, // case 3
+                                      1.092, 0.918, 0.787, 0.656};// case 4
     
     // get critical value from lookup table
     int ai = 5;
