199cd2bb77e6b5aaefbc16a1088cc85f002dcfab
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2012-02-29 20:56:06 +0000

    Add plusTipTracker package to the movie graphical interface
    - Add radiobutton to launch PlusTipTrackerPackage
    - Fix bug in uTrackPackageGUI launching function
    
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@8325 0bcb941b-74d1-0310-bb26-e6636707f451

--- gui/uTrackGUI/uTrackPackageGUI.m
+++ gui/uTrackGUI/uTrackPackageGUI.m
@@ -8,7 +8,7 @@ function varargout = uTrackPackageGUI(varargin)
 % Sebastien Besson 5/2011
 %
 
-if isa(varargin{1},'MovieList')
+if nargin>0 && isa(varargin{1},'MovieList')
     varargout{1} = packageGUI(@UTrackPackage,[varargin{1}.getMovies{:}],varargin{2:end});
 else
     varargout{1} = packageGUI(@UTrackPackage,varargin{:});
