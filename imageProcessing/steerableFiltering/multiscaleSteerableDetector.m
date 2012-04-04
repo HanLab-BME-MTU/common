24e1b0ebdb813e130680e069575133ebc867545c
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-04-04 20:03:17 +0000

    Added help
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@8509 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageProcessing/steerableFiltering/multiscaleSteerableDetector.m
+++ imageProcessing/steerableFiltering/multiscaleSteerableDetector.m
@@ -1,3 +1,32 @@
+% [res, theta, nms, scaleMap] = multiscaleSteerableDetector(img, M, varargin) performs multi-scale steerable filtering
+% 
+% Multi-scale filtering is performed by computing the steerable filter response at individual scales,
+% and selecting the per-pixel maximum response across scales.
+% The resulting scale map is used to produce an orientation map.
+%
+% Inputs: 
+%            img : input image
+%              M : order of the filter, between 1 and 5
+%                : Odd orders: edge detectors, M = 1 is equivalent to Canny's detector
+%                : Even orders: ridge detectors
+%                  Higher orders provide better orientation selectivity and are less sensitive to noise,
+%                  at a small trade-off in computational cost.
+% {'sigmaArray'} : array of scales (standard deviation of the Gaussian kernel)
+%                  across which the filters are evaluated. Default: [1 2 4]
+%    
+%
+% Outputs: 
+%         res : response to the filter across 
+%       theta : orientation map
+%         nms : non-maximum-suppressed response
+%    scaleMap : pixel map of scales for which the response is maximal
+%
+% Recommended usage: 
+%                    3rd/5th order for edge detection (M = 3 or 5)  
+%                    4th order for ridge detection (M = 4)
+%
+% See also steerableDetector.m
+
 % Francois Aguet, 02/01/2012
 
 function [res, theta, nms, scaleMap] = multiscaleSteerableDetector(img, M, varargin)
