5a59f451ef2ff981a3064d7d31684d7e1b51e5bb
Daniel Nunez <daniel_nunez@hms.harvard.edu>
2012-04-30 18:25:30 +0000

    updated help
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@8563 0bcb941b-74d1-0310-bb26-e6636707f451

--- SpatialClustering/plotLocalNeighborhoodDensityMap.m
+++ SpatialClustering/plotLocalNeighborhoodDensityMap.m
@@ -4,19 +4,35 @@ function [C, H] = plotLocalNeighborhoodDensityMap(mpm,imsize,varargin)
 %SYNOPSIS [C, H] = plotLocalNeighborhoodDensityMap(mpm,imsize,varargin))
 %
 %INPUT  mpm             : matrix of point coordinates [x1 y1; x2 y2;....xn yn]
-%       imsize          : image size of the form [numVerticalPixels numHorizontalPixels] 
+%       imsize          : image size of the form [numVerticalPixels numHorizontalPixels]
 %       dist(optional)  : vector of distances at which Ripley's K function
 %                         (i.e. number of neighbors )is sampled (1:20 is default)
 %       scale(optional) : intensity of clustering to map
 %       lr(optional)    : can specify Besag's L-function to use as
 %                         lr(length(dist),numOfPoints), use
 %                         RipleysKfunction.m
+%       mask(optional)      : user provided binary mask used to calculate area
+%                             and to correct point density
+%       calculateMask(opt)  : true to calculate mask from mpm positions, false
+%                             otherwise
+%       closureRadius(opt)  : radius for disk used in closure for calcuating
+%                             mask
+%       dilationRadius(opt) : radius for dialation in calculating mask
+%       doFill(optional)    : true to fill calculates mask, false to not fill
+%       plotMask(optional)  : true to display calculated mask, false to not
+%
 %
 %OUTPUT C               : contour matrix C as described in CONTOURC
 %       H               : handle H to a contourgroup object
 %
 %Daniel Nunez, April 2012
-
+%
+%Note: This algorithm essentially assigns the value of lr evaluated at a
+%specified distance (the scale) for each possition specified in the mpm.
+%For sparce data, additional random points are created for which the lr is
+%calculated. These additional points do not factor into the lr measured
+%for other points, and therefore serve as a way to sample the neighbor
+%density localy. This was addapted from Getis and Franklin (1987).
 
 
 ip = inputParser;
@@ -27,25 +43,40 @@ ip.addRequired('imsize', @isnumeric);
 ip.addParamValue('dist', 1:20, @isnumeric);
 ip.addParamValue('scale', 1, @isscalar);
 ip.addParamValue('lr', [], @isnumeric);
+ip.addParamValue('mask', [], @islogical);
+ip.addParamValue('calculateMask', false, @islogical);
+ip.addParamValue('closureRadius', 20, @islogical);
+ip.addParamValue('dilationRadius', 5, @islogical);
+ip.addParamValue('doFill', false, @islogical);
+ip.addParamValue('plotMask', false, @islogical);
 ip.parse(mpm, imsize, varargin{:});
 dist = ip.Results.dist;
 lr = ip.Results.lr;
+areamask  = ip.Results.mask;
+imsizS = [imsize(2) imsize(1)];
 
 %MAKE MASK
-imsizS = [imsize(2) imsize(1)];
-%[areamask] = makeCellMaskDetections(mpm,closureRadius,...
-%dilationRadius,doFill,imsize,plotMask,[]);
-%CALCULATE NORMALIZED AREA FROM MASK
-%normArea = bwarea(areamask);
-% CREATE CORRECTION FACTOR MATRIX FOR THIS MOVIE using all objects
-%corrFacMat  = makeCorrFactorMatrix(imsizS, dist, 10, areamask');
+if ip.Results.calculateMask
+    [areamask] = makeCellMaskDetections(mpm,ip.Results.closureRadius,...
+        ip.Results.dilationRadius,ip.Results.doFill,imsize,ip.Results.plotMask,[]);
+end
+
+if ~isempty(areamask)
+    %CALCULATE NORMALIZED AREA FROM MASK
+    normArea = bwarea(areamask);
+    % CREATE CORRECTION FACTOR MATRIX FOR THIS MOVIE using all objects
+    corrFacMat  = makeCorrFactorMatrix(imsizS, dist, 10, areamask');
+else
+    corrFacMat = [];
+    normArea = [];
+end
 
 
 %Calculate L-ripley
 if isempty(lr)
     lr = nan(length(dist),size(mpm,1));
     for i = 1:length(mpm)
-        [~,lr(:,i)]=RipleysKfunction(mpm([1:i-1 i+1:length(mpm)],:),mpm(i,:),imsizS,dist); %,corrFacMat,normArea);
+        [~,lr(:,i)]=RipleysKfunction(mpm([1:i-1 i+1:length(mpm)],:),mpm(i,:),imsizS,dist,corrFacMat,normArea);
     end
 end
 
@@ -58,12 +89,12 @@ end
 
 
 %add random points to make sure plot fills out nicely
-percentPoints = 0.5;
-mpmRandSample = repmat(imsize,0.25*imsize(1)*imsize(2),1).*...
-    rand(0.25*imsize(1)*imsize(2),2);
+percentPoints = 4;
+mpmRandSample = repmat(imsize,percentPoints*imsize(1)*imsize(2),1).*...
+    rand(percentPoints*imsize(1)*imsize(2),2);
 lrRandSample = nan(length(dist),size(mpm,1));
 for i = 1:length(mpmRandSample)
-    [~,lrRandSample(:,i)]=RipleysKfunction(mpm,mpmRandSample(i,:),imsizS,dist); %,corrFacMat,normArea);
+    [~,lrRandSample(:,i)]=RipleysKfunction(mpm,mpmRandSample(i,:),imsizS,dist,corrFacMat,normArea);
 end
 for i = 1:size(lrRandSample,2)
     M(max(1,round(mpmRandSample(i,2))),max(1,round(mpmRandSample(i,1)))) = lrRandSample(ip.Results.scale,i);
@@ -71,7 +102,7 @@ end
 
 
 %plot contours
-V = linspace(min(M(:)),max(M(:)),10);
+%V = linspace(min(M(:)),max(M(:)),10);
 figure
 [C H] = contourf(M);
 clabel(C,H);
