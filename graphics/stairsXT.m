23741c7475c2f4cec645c924eef067ce78cda85c
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-05-22 15:36:08 +0000

    New function for plotting histograms as a step trace
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@8634 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ graphics/stairsXT.m
@@ -0,0 +1,34 @@
+%stairsXT(x, f, varargin) plots a histogram as a single 'step' trace. 
+
+% Francois Aguet, 5/21/2012
+
+function stairsXT(x, f, varargin)
+
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('x');
+ip.addRequired('f');
+ip.addParamValue('EdgeColor', 'k', @(s) ischar(s));
+ip.addParamValue('FaceColor', []);
+ip.addParamValue('Bounds', 'closed', @(x) any(strcmpi(x, {'open', 'closed'})));
+ip.addParamValue('LineWidth', 1.5);
+ip.parse(x, f, varargin{:});
+
+if numel(x)>2
+    dx = x(2)-x(1);
+else
+    dx = 1;
+end
+x = [x x(end)+dx];
+f = [f f(end)];
+if strcmpi(ip.Results.Bounds, 'closed')
+    x = [x(1) x x(end)];
+    f = [0 f 0];
+end
+
+[xb,yb] = stairs(x-0.5, f);
+hold on;
+if ~isempty(ip.Results.FaceColor)
+    patch(xb, yb, ip.Results.FaceColor, 'EdgeColor', 'none')
+end
+plot(xb, yb, '-', 'Color', ip.Results.EdgeColor, 'LineWidth', ip.Results.LineWidth);
