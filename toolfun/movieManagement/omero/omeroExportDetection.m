65d0a1ab4220ca51f1d19aeff3812a5e2bcf6836
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2012-08-17 16:13:40 +0000

    Add namespace to detection ROI export
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9009 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/movieManagement/omero/omeroExportDetection.m
+++ toolfun/movieManagement/omero/omeroExportDetection.m
@@ -19,8 +19,9 @@ function omeroExportDetection(movieData,movieInfo)
 % Output:
 %
 
-% Sebastien Besson, Jun 2012
+% Sebastien Besson, Jun 2012 (last modified Aug 2012)
 
+ns = 'hms-detection';
 
 % Input check
 ip=inputParser;
@@ -28,24 +29,25 @@ ip.addRequired('movieData',@(x) isa(x,'MovieData') && x.isOmero());
 ip.addRequired('movieInfo',@isstruct);
 ip.parse(movieData,movieInfo);
 
-image = movieData.getImage;
+% Retrieve image and update service
+image = movieData.getImage();
 updateService = movieData.getSession().getUpdateService();
 
-progressText(0,'Exporting ROIs')
-roi=omero.model.RoiI();
+%  Create roi to attach to the image
+progressText(0, 'Exporting ROIs')
+roi = omero.model.RoiI();
 roi.setImage(image);
+roi.setNamespaces(ns);
 
 for t=1:size(movieInfo,1)
     
-    for i = 1:size(movieInfo(t).xCoord,1)
-        
+    for i = 1:size(movieInfo(t).xCoord,1)        
         point = omero.model.PointI;
         point.setCx(omero.rtypes.rdouble(movieInfo(t).xCoord(i,1)));
         point.setCy(omero.rtypes.rdouble(movieInfo(t).yCoord(i,1)));
         point.setTheT(omero.rtypes.rint(t-1));
         point.setTheZ(omero.rtypes.rint(0));
         roi.addShape(point);
-
     end
 
     progressText(t/size(movieInfo,1))
