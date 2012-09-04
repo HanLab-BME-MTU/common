34fe318b1a917942441bd207a4684bc1622ed1b1
Deepak Chittajallu <deepak_chittajallu@hms.harvard.edu>
2012-09-04 21:51:00 +0000

    Deepak - STABLE
    Made some minor fixes to the imseries* functions (added code for AdjustImageIntensityRange and ComputeImageDynamicRange inside the files to make them standalone).
    
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9118 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/imageSeriesVisualization/imseriesmaskshow.m
+++ graphics/imageSeriesVisualization/imseriesmaskshow.m
@@ -650,3 +650,41 @@ volSupLim = floor([ data.plane(data.curPlane).xlim(2) data.plane(data.curPlane).
 blnInside = all( cp <= volSupLim ) && all( cp >= volInfLim );
 
 end
+
+%%
+function [ imCorrected ] = AdjustImageIntensityRange( im, ImageIntensityRange )
+    
+    imCorrected = mat2gray( im, ImageIntensityRange ) * range(ImageIntensityRange);
+    
+end
+
+%%
+function [ intensityRange ] = ComputeImageDynamicRange( im, cover_percent )
+
+    [p,x] = hist( im, 255 );   
+    p = p / sum(p);
+    
+    min_xlow = [];
+    min_xhigh = [];
+    min_xdiff = [];
+    min_xcover = [];
+    
+    for i = 1:numel(x)
+        for j = i+1:numel(x)
+    
+            if sum( p(i:j) ) < 0.01 * cover_percent
+                continue;
+            end
+            
+            if isempty(min_xdiff) || (x(j) - x(i)) < min_xdiff
+                min_xlow = x(i);
+                min_xhigh = x(j);
+                min_xdiff = x(j) - x(i);              
+            end
+        end
+    end
+    
+    w = 0.5 * (x(2) - x(1));
+    intensityRange = [min_xlow-w, min_xhigh+w];
+    
+end
