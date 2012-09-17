826ac2abf190969dac0ce40f8aff440571f5cfc1
Marco Vilela <marco_vilela@hms.harvard.edu>
2012-09-17 12:54:06 +0000

    -maximum and average velocity of a protrusion or retraction block
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9161 0bcb941b-74d1-0310-bb26-e6636707f451

--- timeSeriesAnalysis/misc/findingProtRetrTime.m
+++ timeSeriesAnalysis/misc/findingProtRetrTime.m
@@ -1,4 +1,4 @@
-function [PersTime,newBlock] = findingProtRetrTime(block,TS,deltaT)
+function [PersTime,newBlock,maxVeloc,meanVeloc] = findingProtRetrTime(block,TS,deltaT)
 %This is the core function to calculate the protrusion/retrection time
 %It is part of a set of function (see below)
 %
@@ -8,9 +8,10 @@ function [PersTime,newBlock] = findingProtRetrTime(block,TS,deltaT)
 %        TS     - Time series
 %        deltaT - Time between points (Sampling rate)
 %
-%Output: PersTime - vector with the persistence time for each block
-%        newBlock - Two blocks are fused when they are separeted by one point and this point does not cross the x-axis.
-%
+%Output: PersTime  - vector with the persistence time for each block
+%        newBlock  - Two blocks are fused when they are separeted by one point and this point does not cross the x-axis.
+%        maxVeloc  - maximum velocity of a block
+%        meanVeloc - mean velocity of a block
 % See also: getPersistenceTime, getEdgeMotionPersistence
 %
 %Marco Vilela, 2012
@@ -42,57 +43,60 @@ ip.addRequired('deltaT',@isscalar);
 
         if newBlock{iB}(1) == 1
 
-            deltaTr = rightBorder(TS,newBlock{iB},deltaT,nPoint);
-            deltaTl = 0;
+            [deltaTr,newBlock{iB}] = rightBorder(TS,newBlock{iB},deltaT,nPoint);
+            deltaTl                = 0;
 
         elseif newBlock{iB}(end) == nPoint
 
-            deltaTl = leftBorder(TS,newBlock{iB},deltaT);
-            deltaTr = 0;
+            [deltaTl,newBlock{iB}] = leftBorder(TS,newBlock{iB},deltaT);
+            deltaTr                = 0;
 
         else
 
-            deltaTr = rightBorder(TS,newBlock{iB},deltaT,nPoint);
-            deltaTl = leftBorder(TS,newBlock{iB},deltaT);
+            [deltaTr,newBlock{iB}] = rightBorder(TS,newBlock{iB},deltaT,nPoint);
+            [deltaTl,newBlock{iB}] = leftBorder(TS,newBlock{iB},deltaT);
 
         end
 
         PersTime(iB)  = (length(newBlock{iB})-1)*deltaT + deltaTl + deltaTr;
-
+        maxVeloc(iB)  = max(TS(newBlock{iB}));
+        meanVeloc(iB) = nanmean(TS(newBlock{iB}));
 
         clear deltaTl;clear deltaTr;
     end
 
 end%End of main function
 
-function [deltaTr] = rightBorder(TS,newBlock,deltaT,nPoint)
+function [deltaTr,newBlock] = rightBorder(TS,newBlock,deltaT,nPoint)
 
         endPoint = find( TS(newBlock(end)+1:end) < 0 );%finding the zero-crossing point
         
         if ~isempty(endPoint)
             rZeroC2  = endPoint(1) + newBlock(end);
             rZeroC1  = rZeroC2  - 1;
+            newBlock = [newBlock;[newBlock(end):rZeroC1]'];
             deltaTr  = deltaT*TS(rZeroC1)/abs( diff( TS(rZeroC1:rZeroC2) ) );
         else
             rZeroC2  = nPoint;
             rZeroC1  = newBlock(end);
             deltaTr  = deltaT*(rZeroC2 - rZeroC1);
         end
-       deltaTr = deltaTr + deltaT*(rZeroC1 - newBlock(end)); 
+       %deltaTr = deltaTr + deltaT*(rZeroC1 - newBlock(end)); 
 end
 
-function [deltaTl] = leftBorder(TS,newBlock,deltaT)
+function [deltaTl,newBlock] = leftBorder(TS,newBlock,deltaT)
     
     ftPoint  = find( TS(1:newBlock(1)) < 0 );%finding the zero-crossing point
     
     if ~isempty(ftPoint)
         lZeroC1  = ftPoint(end) ;
         lZeroC2  = lZeroC1  + 1;
+        newBlock = [[lZeroC2:newBlock(1)]';newBlock];
         deltaTl  = deltaT*TS(lZeroC2)/abs( diff( TS(lZeroC1:lZeroC2) ) );
     else
         lZeroC1  = 1;
         lZeroC2  = newBlock(end);
         deltaTl  = deltaT*(lZeroC2 - lZeroC1);
     end
-    deltaTl = deltaTl + deltaT*(newBlock(1) - lZeroC2);
+    %deltaTl = deltaTl + deltaT*(newBlock(1) - lZeroC2);
 end
