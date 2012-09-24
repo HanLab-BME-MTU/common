d9cdb4b88a1506440d26c917e601cf3520a959c3
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2012-09-24 22:16:43 +0000

    Fix getChannelPaths() method for OMERO movies
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9185 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/movieManagement/MovieData.m
+++ toolfun/movieManagement/MovieData.m
@@ -136,10 +136,9 @@ classdef  MovieData < MovieObject
             assert(all(ismember(iChan,1:numel(obj.channels_))),...
                 'Invalid channel index specified! Cannot return path!');
             
-            if isa(obj.channels_(1).channelPath_,'omero.model.Pixels')
-                id = obj.channels_(1).channelPath_.getId.getValue;
-                chanPaths = arrayfun(@(x) ['Image ' num2str(id) ': Channel ' num2str(x)],...
-                    iChan,'UniformOutput',false);
+            if obj.isOmero()
+                chanPaths = arrayfun(@(x) ['Image ' num2str(obj.omeroId_) ...
+                    ': Channel ' num2str(x)], iChan, 'UniformOutput', false);
             else
                 chanPaths = arrayfun(@(x)obj.channels_(x).channelPath_,iChan,...
                     'UniformOutput',false);
