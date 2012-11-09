1b0c9cd26a8a959629b680c725d63cdd8237323c
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2012-11-09 03:49:26 +0000

    Clean iofun folder
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9388 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/frameGrabber/private/makeMex.m
+++
@@ -1,4 +0,0 @@
-%% compile commands for all mex files in this directory
-clear mex;
-% mex -DVERBOSE mexDTFgHandler.c olimg32.lib olfg32.lib -f dtFgOpts.bat
-mex mexDTFgHandler.c olimg32.lib olfg32.lib -f dtFgOpts.bat
