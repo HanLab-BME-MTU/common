7d805249b4029574027b46f54cc3d8ba5121db76
Marco Vilela <marco_vilela@hms.harvard.edu>
2012-11-12 19:26:37 +0000

    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9410 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ mathfun/bootStrapMean.m
@@ -0,0 +1,32 @@
+function [conf,meanS] = bootStrapMean(variable,alpha,nBoot)
+%This subFunction bootstrap the mean value of the input "variable"
+% bootci calculates the confidence interval at alpha level based on the
+% percentile (corrected for bias) of the distribution for the speficic
+% stats
+%
+% Usage: [conf,meanS] = bootStrapMean(variable,alpha,nBoot)
+%
+% Input:
+%       variable - series of points - a given stats or raw data 
+%       alpha    - confidence level - see bootci
+%       nBoot    - number of boostrap samples
+%
+% Output:
+%        conf  - confidence interval for the mean value
+%        meanS - mean value 
+%
+% Marco Vilela, 2012
+
+ variable(isnan(variable)) = [];
+
+opt = statset('UseParallel','never');
+if matlabpool('size')
+    opt = statset('UseParallel','always');
+end
+
+[conf,meanSample] = bootci(nBoot,{@mean,variable},'alpha',alpha,...
+    'type','bca','Options',opt);
+
+meanS = mean(meanSample);
+
+end
