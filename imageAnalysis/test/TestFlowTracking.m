712029e8aabe1fcc10d7ce6e194a0e49d3fb3cb4
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2012-11-21 09:12:28 +0000

    Add failing flow tracking test with large depth stack
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9455 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageAnalysis/test/TestFlowTracking.m
+++ imageAnalysis/test/TestFlowTracking.m
@@ -18,8 +18,8 @@ classdef TestFlowTracking < TestCase
         end
         
         function testSingleGaussianSpot(self)
-        % Test the displacement of a single 2D Gaussian spot
-        
+            % Test the displacement of a single 2D Gaussian spot
+            
             v0 = [0 0; 1 0; 0 1; 1 1; .5 .5; 5.5 5.5; 9.5 9.5];
             for i = 1 : size(v0,1)
                 self.setUpGaussianSpot(v0(i,:));
@@ -28,14 +28,26 @@ classdef TestFlowTracking < TestCase
             end
         end
         
+        
+        function testStackDepth(self)
+            % Test the depth integration of the cross-correlation score
+            
+            v0 = [1 1];
+            for depth = 2 : 15
+                self.setUpGaussianSpot(v0, depth);
+                v = self.f(self.stack, self.imsize/2, 20, 20);
+                assertElementsAlmostEqual(v, v0 ,'relative',1e-3);
+            end
+        end
+        
         function testMaxFlowSpeed(self)
             % Test the maximum flow speed parameter
-
+            
             % Set up a single 2D Gaussian spot moving by [v0 v0]
             v0 = 5;
             vth = repmat(v0, 1, 2);
             self.setUpGaussianSpot(vth);
-
+            
             % Test maximum speeds larger than v0
             for maxSpd = v0+1 : 2 : 20;
                 v = self.f(self.stack, self.imsize/2, 20, 20,...
@@ -50,22 +62,31 @@ classdef TestFlowTracking < TestCase
                 assertTrue(all(isnan(v)));
             end
         end
-
-    
-        function setUpGaussianSpot(self, v) 
+        
+        
+        function setUpGaussianSpot(self, v, varargin)
             % Generate a n x m x2 stack with a single 2D Gaussian spot
-            % moving with a velocity v
+            % moving with a velocity v (in pixels/frame)
             
-            assert(numel(v) == 2, 'Velocity must be a 2D vector');
+            % Check input
+            ip = inputParser;
+            ip.addRequired('v', @(x) isvector(x) && numel(v) == 2);
+            ip.addOptional('depth', 2, @isscalar);
+            ip.parse(v, varargin{:});
+            depth = ip.Results.depth;
             
+            % Initialize Gaussian standard deviatiaion and amplitude
             sigma = 3;
             A = 1e4;
-            self.stack = zeros([self.imsize 2]);
-            self.stack(:,:,1) = simGaussianSpots(self.imsize(1), self.imsize(2), sigma,...
-                'x' ,self.imsize(1)/2, 'y', self.imsize(2)/2, 'A',A);
-            self.stack(:,:,2) = simGaussianSpots(self.imsize(1), self.imsize(2), sigma,...
-                'x' ,self.imsize(1)/2+v(1), 'y', self.imsize(2)/2+v(2), 'A',A);
+            
+            % Generate the stack
+            self.stack = zeros([self.imsize depth]);
+            for i = 1 : depth
+                self.stack(:,:,i) = simGaussianSpots(...
+                    self.imsize(1), self.imsize(2), sigma,...
+                    'x' ,self.imsize(1)/2 + (i - 1) * v(1),...
+                    'y', self.imsize(2)/2 + (i - 1) * v(2), 'A',A);
+            end
         end
     end
-
 end
