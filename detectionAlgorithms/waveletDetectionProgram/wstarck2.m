600d0615f5d84ff4d4f6b0748bf4caefd570f606
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-11-29 21:11:15 +0000

    Cleaned up
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9494 0bcb941b-74d1-0310-bb26-e6636707f451

--- detectionAlgorithms/waveletDetectionProgram/wstarck2.m
+++ detectionAlgorithms/waveletDetectionProgram/wstarck2.m
@@ -1,43 +1,36 @@
-function w = wstarck2(x,k,irecon)
-% WSTARCK2 reconstructs the image without noise by summing all significant
-% coefficients in each detail.  
-%  Y = WSTARCK2(X,K,IRECON), where X is the image to be processed.
-%    K is the level of decomposition for wavelet transformation.
-%    IRECON is the flag to indicate if the ind_th approximation of X (or
-%    low pass component) should be added.  When IRECON = 0, WSTARCK2 just
-%    calcualted reconstructed image up to order k without kth detail.
+%[Y] = WSTARCK2(IMG,K,IRECON) denoises the input image  by hard thresholding in the wavelet subbands.
 %
-%    Reference:  "Olivo-Marin J.C. 2002. Extraction of spots in biological
-%    images using multiscale products. Pattern Recognit. 35: 1989�1996."
-% 
-% Last updated:  Shann-Ching Sam Chen, April 8, 2008
-%                add description to the function and rewrote the inner loop 
-%                in a vectorized form to speed up computation
+% Inputs:
+%       img : input image   
+%         k : decomposition level of the wavelet transform
+%    irecon : flag to include (1) or not (0) the low-pass component in the reconstruction
+%
+% Reference:  
+% [1] Olivo-Marin, "Extraction of spots in biological images using multiscale products," Pattern Recoginition 35, pp. 1989-1996, 2002.
+%
+% Note: The implementation is incorrect, the thresholding mask should be calculated
+%       separately for each scale instead of being cumulative. Since this code has
+%       been used for published work, and the bug has effects on downstream functions,
+%       it is left as is. -FA
 
-[ly,lx]=size(x);		% Get the original image size
+% Shann-Ching Sam Chen, 2008. Last modified by Francois Aguet.
 
-%------------------------------
-%   SETUP ARRAYS
-%------------------------------
-yl = zeros(ly, lx, k + 1);
+function y = wstarck2(img, k, irecon)
 
-tx = x;
-yl(:,:,1) = x;
-w = zeros(ly,lx);
-s = zeros(ly,lx);
+[ny,nx] = size(img);
+
+y = zeros(ny,nx);
+s = zeros(ny,nx);
+
+% Calculate 'a trous' wavelet transform (spline wavelets)
+W = awt(img, k);
 
 for ind = 1:k
-    yl(:,:,ind+1) = wtlo2(wtlo2(tx, ind)', ind)';
-    %wi = awt(tx, ind);
-    %yl(:,:,ind+1) = wi(:,:,end);
-    
-    yh = yl(:,:,ind) - yl(:,:,ind+1); % This is the ind_th detail or residue
-    tx = yl(:,:,ind+1);   % This is the ind_th approximation of x (or low pass component)  
-    s(abs(yh)>=3*std(yh(:))) = 1;
-    
-    w = w + s.*yh;
+    tmp = W(:,:,ind);
+    s(abs(tmp)>=3*std(tmp(:))) = 1;
+    y = y + s.*tmp;
 end
 
 if irecon==1
-    w = w + tx;
+    y = y + W(:,:,end);
 end
