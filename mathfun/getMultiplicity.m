e7e1b8b14aa93466d8831b945eacce672c45d351
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-11-29 15:49:00 +0000

    Add sorted full array to output
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9487 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/getMultiplicity.m
+++ mathfun/getMultiplicity.m
@@ -1,15 +1,16 @@
-% [r sdata] = getMultiplicity(data) returns the occurrences/Multiplicity of the elements of 'data'
+% [r, udata, sdata] = getMultiplicity(data) returns the occurrences/Multiplicity of the elements of 'data'
 %
 % Inputs:
 %         data : n-dimensional input array
 %
 % Outputs: 
 %          rep : # of occurrences for each element of 'data'
-%        sdata : sorted 1-D array of unique values in 'data'
+%        udata : sorted 1-D array of unique values in 'data'
+%        sdata : sorted 1-D array of values in 'data'
 
-% Francois Aguet, 03/02/2012
+% Francois Aguet, 03/02/2012 (modified on 10/29/2012)
 
-function [rep sdata] = getMultiplicity(data)
+function [rep, udata, sdata] = getMultiplicity(data)
 sdata = sort(data(:))';
 rep = diff([0 find([diff(sdata)~=0 1])]);
-sdata = unique(sdata);
+udata = unique(sdata);
