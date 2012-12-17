47aa19b2dcf8b4c1102f37ed8b661b7d96cb586f
Francois Aguet <francois_aguet@hms.harvard.edu>
2012-12-17 22:32:05 +0000

    Added option to preserve points at image borders
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9581 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/locmax3d.m
+++ mathfun/locmax3d.m
@@ -1,24 +1,32 @@
-% Francois Aguet, 16 November 2010
+%[lm] = locmax3d(img, wdims, varargin) searches for local maxima in 3D 
 
-function lm = locmax3d(img, mask)
+% Francois Aguet (last modified 12/17/2012)
 
-if numel(mask)==1
-    wx = mask;
-    wy = mask;
-    wz = mask;
+function [lm] = locmax3d(img, wdims, varargin)
+
+ip = inputParser;
+ip.CaseSensitive = false;
+ip.addRequired('img', @isnumeric);
+ip.addRequired('wdims', @isnumeric);
+ip.addParamValue('ClearBorder', true, @islogical);
+ip.parse(data, varargin{:});
+
+if numel(wdims)==1
+    wx = wdims;
+    wy = wdims;
+    wz = wdims;
     if mod(wx,2)==0 || mod(wy,2)==0 || mod(wz,0)==0
         error('Mask dimensions must be odd integers');
     end
-    mask = ones(wy,wx);
-elseif numel(mask)==3
-    wx = mask(1);
-    wy = mask(2);
-    wz = mask(3);
+elseif numel(wdims)==3
+    wx = wdims(1);
+    wy = wdims(2);
+    wz = wdims(3);
     if mod(wx,2)==0 || mod(wy,2)==0 || mod(wz,0)==0
         error('Mask dimensions must be odd integers');
     end    
-    mask = ones(wy,wx);
 end
+mask = ones(wy,wx);
 
 [ny,nx,nz] = size(img);
 lm2D = zeros(ny,nx,nz);
@@ -30,14 +38,19 @@ end
 lm = zeros(ny,nx,nz);
 
 b = (wz-1)/2;
-for z = 1+b:nz-b
-    lm(:,:,z) = max(lm2D(:,:,z-b:z+b), [], 3);
+for z = 1:nz
+    lm(:,:,z) = max(lm2D(:,:,max(1,z-b):min(nz,z+b)), [], 3);
 end
 
+% if max. filter response is equal to input, point is a local maximum
 lm(lm~=img) = 0;
 
-% also set xy-borders to zero
-b = (wx-1)/2;
-lm(:,[1:b end-b+1:end],:) = 0;
-b = (wy-1)/2;
-lm([1:b end-b+1:end],:,:) = 0;
+% set borders to zero
+if ip.Results.ClearBorder
+    b = (wx-1)/2;
+    lm(:,[1:b end-b+1:end],:) = 0;
+    b = (wy-1)/2;
+    lm([1:b end-b+1:end],:,:) = 0;
+    b = (wz-1)/2;
+    lm(:,:,[1:b end-b+1:end]) = 0;
+end
