01bbbcb1fb0a4729a25e0a946475963fb0001ba5
Marco Vilela <marco_vilela@hms.harvard.edu>
2012-12-20 22:15:20 +0000

    fixed normalization
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9593 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/modifiedKendallCorr.m
+++ mathfun/modifiedKendallCorr.m
@@ -7,25 +7,32 @@ function  [tau,tauAmp] = modifiedKendallCorr(x,varargin)
 %Based on "A New Measure of Rank Correlation", M.G.Kendall. Biometrika, Vol. 30, No 1/2, pp 81-93
 %Marco Vilela, 2012
 
+ 
 
+ 
 
+ 
 ip = inputParser;
 ip.addRequired('x',@(x) isvector(x));
-ip.addParamValue('y',x,@(x) isvector(x));
-ip.addParamValue('local',numel(x)-1,@isscalar);
-ip.addParamValue('alpha',0.05,@isscalar);
-ip.addParamValue('maxLag',0,@isscalar);
+ip.addOptional('y',x,@(x) isvector(x));
+ip.addOptional('local',numel(x)-1,@isscalar);
+ip.addOptional('alpha',0.05,@isscalar);
+ip.addOptional('maxLag',0,@isscalar);
 
+ 
 
+ 
 ip.parse(x,varargin{:});
 local    = ip.Results.local;
 alpha    = ip.Results.alpha;
 y        = ip.Results.y;
 maxLag   = ip.Results.maxLag;
 
+ 
 x = x(:);
 y = y(:);
 
+ 
 if ~all( size(x) == size(y) )
     error('x and y have different size')
 else
@@ -36,27 +43,34 @@ else
     end
 end
 
-if (nObs - local) < maxLag
+ 
+if maxLag > 0 && (nObs - local) < maxLag
     disp('maximum lag is too large. Reducing it to the max allowed')
-    maxLag = (nObs - local) - 1;
+    maxLag = max((nObs - local) - 1,0);
 end
 
+ 
 
+ 
 normalization       = localN*(2*nObs - localN - 1)/2;
 contM               = getCorrMatrix(ones(size(x)),local,[]);
 [contX,ampM(:,:,1)] = getCorrMatrix(x,local,contM);
 [contY,ampM(:,:,2)] = getCorrMatrix(y,local,contM);
 
+ 
 %Matrix with [-1 0 1] for mismatch, no-match and match of slopes
 match = contX.*contY;
 tau   = sum(match(:))/normalization;
 
+ 
 %Matrix with mean of [delta(i,j)] 
 meanAmp  = mean(abs(ampM),3);
 matchAmp = match.*meanAmp;
 tauAmp   = sum(matchAmp(:))/sum(meanAmp(:));
 
+ 
 
+ 
 contYr        = contY;
 contXl        = contX;
 ampM(:,:,3:4) = ampM(:,:,1:2);% 1 is for x and 2 for y
@@ -65,32 +79,43 @@ tauL          = NaN(1,maxLag-1);
 tauAmpR       = NaN(1,maxLag-1);
 tauAmpL       = NaN(1,maxLag-1);
 
+ 
 
+ 
 for iLag = 1:maxLag
     %Change this code - create a template that change with lag
     currNorm = normalization - local*iLag;
+
     
     %Shifting Y to the past
     [contYr,ampM(:,:,[2 4]),tauR(iLag),tauAmpR(iLag)]...
              = calculateCorr(contYr,contX,ampM(:,:,[2 4]),currNorm);
+
     
     %Shifting X to the past
     [contXl,ampM(:,:,[1 3]),tauL(iLag),tauAmpL(iLag)]...
              = calculateCorr(contXl,contY,ampM(:,:,[1 3]),currNorm);
+
     
 end
 
+ 
 tau    = [fliplr(tauL) tau tauR];
 tauAmp = [fliplr(tauAmpL) tauAmp tauAmpR];
 
+ 
 end%END OF MAIN FUNCTION
 
+ 
 function [contTS,ampTSdiff] = getCorrMatrix(TS,neigh,contM)
 
+ 
 
+ 
 hTS = hankel(TS);hTS(1,:) = [];
 hTS(:,neigh+1:end)        = [];
 
+ 
 if isempty(contM)   
     contTS    = hTS;
 else
@@ -101,20 +126,26 @@ else
     contTS    = sign(ampTSdiff);
 end
 
+ 
 end%END OF getCorrMatrix
 
+ 
 function [contZ,ampM,tau,tauAmp] = calculateCorr(contZ,contA,ampM,currNorm)
 %contZ - comes from the time series being shifted
 %contA - reference time series
 
+ 
     %Updating counter matrix - shifting up and adding zeros to the last row
     contZ  = circshift(contZ,-1);contZ(end,:) = 0;
 
+ 
     %Updating amplitude matrix 
     ampM(:,:,2) = circshift(ampM(:,:,2),-1);ampM(end,:,2) = 0;
+
     
     %Finding matches
     match    = contA.*contZ;
+
     
     %Calculating the mean amplitude of the slopes
     meanAmp  = mean( abs( ampM ),3);
@@ -123,5 +154,6 @@ function [contZ,ampM,tau,tauAmp] = calculateCorr(contZ,contA,ampM,currNorm)
     %
     tauAmp   = sum(matchAmp(:))/sum(meanAmp(:));
     tau      = sum(match(:))/currNorm;
+
     
 end
