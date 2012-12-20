cb4606c2ba578f0e3fbe24f22b6edd609ec3382f
Marco Vilela <marco_vilela@hms.harvard.edu>
2012-12-20 18:55:14 +0000

    Added modified Kendall
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9591 0bcb941b-74d1-0310-bb26-e6636707f451

--- timeSeriesAnalysis/misc/nanCrossCorrelation.m
+++ timeSeriesAnalysis/misc/nanCrossCorrelation.m
@@ -1,4 +1,4 @@
-function [xCorr,bounds,pVal] = nanCrossCorrelation(x,y,type,mLag,neighborhood)
+function [xCorr,bounds,pVal] = nanCrossCorrelation(x,y,type,maxLag,local)
 %Inputs are column vectors
 %Under Construction
 %
@@ -8,7 +8,8 @@ ip = inputParser;
 ip.addRequired('x',@isvector);
 ip.addRequired('y',@isvector);
 ip.addParamValue('type','Pearson', @ischar)
-ip.addParamValue('mLag',10,@isscalar);
+ip.addParamValue('maxLag',10,@isscalar);
+ip.addParamValue('local',numel(x)-1,@isscalar);
 
 x = x(:);
 y = y(:);
@@ -34,8 +35,8 @@ switch type
         SX = flipud(buffer(xIn,nObs,nObs - 1));%delay x(t-n)
         SY = flipud(buffer(yIn,nObs,nObs - 1));%delay y(t-n)
 
-        SX(mLag+2:end,:) = [];
-        SY(mLag+2:end,:) = [];
+        SX(maxLag+2:end,:) = [];
+        SY(maxLag+2:end,:) = [];
         
         CCR   = normalization*xIn'*SY';
         CCL   = normalization*yIn'*SX';
@@ -43,14 +44,8 @@ switch type
         pVal  = pvalPearson('b',xCorr,nObs);
     case 'Kendall'
         
-        for iLag = 1:mLag
-            
-            [CCR(iLag),rPval(iLag)]   = modifiedCorr(xIn(1:end-iLag+1),yIn(iLag:end),neighborhood,'type',type);
-            [CCL(iLag),lPval(iLag)]   = modifiedCorr(xIn(iLag:end),yIn(1:end-iLag+1),neighborhood,'type',type);
-            
-        end
-        xCorr = [fliplr(CCR) CCL(2:end)];
-        pVal  = [fliplr(rPval) lPval(2:end)];
+        [xCorr(:,1),xCorr(:,2)] = modifiedKendallCorr(x,'local',local,'maxLag',maxLag);
+        
     case 'Spearman'
         
 end
