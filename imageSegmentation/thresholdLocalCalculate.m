c46c8390650037d55c623f137eafbfc30e2aca70
Liya Ding <liya_ding@hms.harvard.edu>
2012-12-26 23:18:14 +0000

    local thresholding code organized
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9607 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ imageSegmentation/thresholdLocalCalculate.m
@@ -0,0 +1,178 @@
+function [level_img, level_whole] = thresholdLocalCalculate(imageIn,choice_of_threshold,level_local_radius, pace, varargin)
+% local thresholding based on global thresholding level using one of the
+% three methods "Otsu","Rosin",or "FluorescenceImage"
+%
+% level_img = thresholdOtsu_local(imageIn,level_local_radius, pace, showPlots)
+% 
+% This function selects a threshold for the input fluorescence image using
+% the thresholding method the user askes for as in choice_of_threshold,
+% then find the local thresholds also
+% decided by thresholding method the user askes to get a local threshold
+% map. After some smoothing on this threshold map(not included here) can used to segment the input
+% image.
+% 
+% Input:
+% 
+%   imageIn:            2D input image to be thresholded.
+%   choice_of_threshold: string input "Otsu","Rosin",or "FluorescenceImage"
+%   level_local_radius: the radius of local patch
+%   pace:               the pace to calculate local threshold, mostly to
+%                       speed up the process which is computational expensive
+%
+% Output:
+% 
+%   level_img - The intensity value image selected for local thresholding,
+%               before further smoothing over it
+%
+% Liya Ding, 1/2012, cleaned 12/2012
+
+ip=inputParser;
+ip.addRequired('imageIn',@isnumeric);
+ip.addRequired('level_local_radius',@isnumeric);
+ip.addRequired('pace',@isnumeric);
+ip.addOptional('showPlots',0,@isnumeric)
+ip.parse(imageIn, level_local_radius, pace, varargin{:});
+showPlots=ip.Results.showPlots;
+
+% Define the half pace 
+half_pace = round(pace-1)/2;
+
+% Convert to double if necessary
+imageIn = double(imageIn);
+
+% Find nonzero values (if due to masking)
+nzInd = find(imageIn);
+
+% Get minumum and maximum pixel values in image
+minSignal = min(imageIn(nzInd));
+maxSignal = max(imageIn(nzInd));
+
+% Normalize nonzero value between 0 and 1
+imageInNorm = zeros(size(imageIn));
+imageInNorm(nzInd) = (imageIn(nzInd)- minSignal) / (maxSignal - minSignal);
+
+try
+    % Obtain the global threshold using the method user askes for
+    
+    switch choice_of_threshold
+        case 'Otsu'
+            level = thresholdOtsu(imageInNorm);
+        case 'Rosin'
+            level = thresholdRosin(imageInNorm);
+        case 'FluorescenceImage'
+            level = thresholdFluorescenceImage(imageInNorm);
+    end
+    
+ level_whole = level*(maxSignal - minSignal)+minSignal;
+catch
+    level = nan;
+    level_whole = nan;
+end
+
+% Initialized the threshold map(level_img) as setting everywhere as the global level
+level_img = imageIn*0+level_whole;
+
+x_grid = level_local_radius + 1 : pace : size(level_img,1) - level_local_radius;
+y_grid = level_local_radius + 1 : pace : size(level_img,2) - level_local_radius;
+
+
+level_local_radius = round(level_local_radius);
+half_pace = round(half_pace);
+pace=round(pace);
+
+% For every patch according to the pace(grid)
+for img_x = level_local_radius + 1 : pace : size(level_img,1) - level_local_radius
+    for img_y = level_local_radius + 1 : pace : size(level_img,2) - level_local_radius
+% 
+%         img_x = round(img_x);
+%         img_y = round(img_y);
+        
+        % Cropping a local image path
+        local_img = imageIn(img_x - level_local_radius : img_x + level_local_radius ,img_y - level_local_radius:img_y + level_local_radius );
+        
+        % Find nonzero values (due to masking)
+        nzInd = find(local_img);
+        
+        % Get minumum and maximum pixel values in image
+        minSignal = min(local_img(nzInd));
+        maxSignal = max(local_img(nzInd));
+        
+        % Normalize nonzero value between 0 and 1
+        imageInNorm = zeros(size(local_img));
+        imageInNorm(nzInd) = (local_img(nzInd)- minSignal) / (maxSignal - minSignal);
+        
+        % Find the threshold for this patch
+               
+        try
+            
+            switch choice_of_threshold
+                case 'Otsu'
+                    level = thresholdOtsu(imageInNorm);
+                case 'Rosin'
+                    level = thresholdRosin(imageInNorm);
+                case 'FluorescenceImage'
+                    level = thresholdFluorescenceImage(imageInNorm);
+            end
+            
+        catch
+            level = [];
+        end
+        
+        
+        % level could be empty if input patch is all background zeros
+        if(isempty(level))
+            level_img(img_x-half_pace:img_x+half_pace,img_y-half_pace:img_y+half_pace) = level_whole;
+            % For the boundary
+            if img_x == x_grid(1)
+                level_img(1:img_x+half_pace,img_y-half_pace:img_y+half_pace) = level_whole;
+            end
+            if img_x == x_grid(end)
+                level_img(img_x-half_pace:end,img_y-half_pace:img_y+half_pace) = level_whole;
+            end
+            if img_y == y_grid(1)
+                level_img(img_x-half_pace:img_x+half_pace,1:img_y+half_pace) = level_whole;
+            end
+            if img_y == y_grid(end)
+                level_img(img_x-half_pace:img_x+half_pace,img_y-half_pace:end) = level_whole;
+            end
+        else
+            
+             level = level*(maxSignal - minSignal)+minSignal;       
+            
+            level_img(img_x-half_pace:img_x+half_pace,img_y-half_pace:img_y+half_pace) = level;
+            if img_x == x_grid(1)
+                level_img(1:img_x+half_pace,img_y-half_pace:img_y+half_pace) = level;
+            end
+            if img_x == x_grid(end)
+                level_img(img_x-half_pace:end,img_y-half_pace:img_y+half_pace) = level;
+            end
+            if img_y == y_grid(1)
+                level_img(img_x-half_pace:img_x+half_pace,1:img_y+half_pace) = level;
+            end
+            if img_y == y_grid(end)
+                level_img(img_x-half_pace:img_x+half_pace,img_y-half_pace:end) = level;
+            end
+        end
+    end
+end
+
+
+% For the corners
+
+CL = level_local_radius + pace;
+
+level_img(1:CL, 1:CL)= ...
+    mean2(level_img(CL+1:CL+10, 1:CL))/2 +...
+    mean2(level_img(1:CL,CL+1:CL+10))/2;
+
+level_img(1:CL, end-CL:end)= ...
+    mean2(level_img(CL+1:CL+10, end-CL:end))/2 +...
+    mean2(level_img(1:CL,end-CL-10:end-CL-1))/2;
+ 
+level_img(end-CL:end, 1:CL)= ...
+    mean2(level_img(end-CL-10:end-CL-1, 1:CL))/2 +...
+    mean2(level_img(end-CL:end,CL+1:CL+10))/2;
+
+level_img(end-CL:end, end-CL:end)= ...
+     mean2(level_img(end-CL-10:end-CL-1,end-CL:end))/2 +...
+     mean2(level_img(end-CL:end,end-CL-10:end-CL))/2;
--- imageSegmentation/thresholdOtsu_local.m
+++ imageSegmentation/thresholdOtsu_local.m
@@ -1,11 +1,11 @@
-function [level,bw_out] = thresholdOtsu_local(imageIn,level_local_radius, pace, lowerbound, varargin)
-% local thresholding based on global thresholding level using Otsu's method
+function  [level,bw_out] = thresholdOtsu_local(imageIn,level_local_radius, pace, lowerbound, varargin)
+% local thresholding based on thresholding level using Otsu's method
 %
 % [level,bw_out] = thresholdOtsu_local(imageIn,level_local_radius, pace, showPlots)
 % 
 % This function selects a threshold for the input fluorescence image using
 % Ostu method(function thresholdOtsu()), then find the local thresholds also
-% decided by Ostu method(function thresholdOtsu()) to get a local threshold
+% decided by Otsu method(function thresholdOtsu()) to get a local threshold
 % map. After some smoothing this threshold map is used to segment the input
 % image.
 % 
@@ -15,164 +15,19 @@ function [level,bw_out] = thresholdOtsu_local(imageIn,level_local_radius, pace, 
 %   level_local_radius: the radius of local patch
 %   pace:               the pace to calculate local threshold, mostly to
 %                       speed up the process which is computational expensive
+%   lowerbound:         the percentage of global threshold the user want to
+%                       set as the lowerbound of the local thresholding
 %   showPlots:          If true, a plot of the histogram and an overlay of the mask
 %                       on the image will be shown. 
 %
 % Output:
 % 
 %   level - The intensity value selected for global thresholding.
+%   bw_out - the output segmentation result
 %
+
 % Liya Ding, 1/2012
 
-ip=inputParser;
-ip.addRequired('imageIn',@isnumeric);
-ip.addRequired('level_local_radius',@isnumeric);
-ip.addRequired('pace',@isnumeric);
-ip.addRequired('lowerbound',@isnumeric);
-ip.addOptional('showPlots',0,@isnumeric)
-ip.parse(imageIn, level_local_radius, pace, varargin{:});
-showPlots=ip.Results.showPlots;
-
-% level_local_radius = 15;
-% pace = 3;
-half_pace = round(pace-1)/2;
-
-% Convert to double if necessary
-imageIn = double(imageIn);
-
-% A light gaussian smoothing
-H1 = fspecial('gaussian',3);
-imageIn =  imfilter(imageIn, H1,'same','replicate');
-
-% Find nonzero values (due to masking)
-nzInd = find(imageIn);
-
-% Get minumum and maximum pixel values in image
-minSignal = min(imageIn(nzInd));
-maxSignal = max(imageIn(nzInd));
-
-% Normalize nonzero value between 0 and 1
-imageInNorm = zeros(size(imageIn));
-imageInNorm(nzInd) = (imageIn(nzInd)- minSignal) / (maxSignal - minSignal);
-
-% Gaussian smoothing of the normalized image, to get a better global mask
-H = fspecial('gaussian',7);
-sm_imageInNorm = imfilter(imageInNorm, H,'same','replicate');
-level = graythresh(sm_imageInNorm);
-level_sm_norm = level;
-
-% the glocal level determined for input image
-level_whole = level*(maxSignal - minSignal)+minSignal;
-
-% Initialized the threshold map(level_img) as setting everywhere as the global level
-level_img = imageIn*0+level_whole;
-
-x_grid = level_local_radius + 1 : pace : size(level_img,1) - level_local_radius;
-y_grid = level_local_radius + 1 : pace : size(level_img,2) - level_local_radius;
-
-% For every patch according to the pace(grid)
-for img_x = level_local_radius + 1 : pace : size(level_img,1) - level_local_radius
-    for img_y = level_local_radius + 1 : pace : size(level_img,2) - level_local_radius
-
-        % Cropping a local image path
-        local_img = imageIn(img_x - level_local_radius : img_x + level_local_radius ,img_y - level_local_radius:img_y + level_local_radius );
-        
-        % Find nonzero values (due to masking)
-        nzInd = find(local_img);
-        
-        % Get minumum and maximum pixel values in image
-        minSignal = min(local_img(nzInd));
-        maxSignal = max(local_img(nzInd));
-        
-        % Normalize nonzero value between 0 and 1
-        imageInNorm = zeros(size(local_img));
-        imageInNorm(nzInd) = (local_img(nzInd)- minSignal) / (maxSignal - minSignal);
-        
-        % Find the threshold for this patch
-        level = graythresh(imageInNorm);
-        level = level*(maxSignal - minSignal)+minSignal;
-        
-        % level could be empty if input patch is all background zeros
-        if(isempty(level))
-            level_img(img_x-half_pace:img_x+half_pace,img_y-half_pace:img_y+half_pace) = level_whole;
-            if img_x == x_grid(1)
-                level_img(1:img_x+half_pace,img_y-half_pace:img_y+half_pace) = level_whole;
-            end
-            if img_x == x_grid(end)
-                level_img(img_x-half_pace:end,img_y-half_pace:img_y+half_pace) = level_whole;
-            end
-            if img_y == y_grid(1)
-                level_img(img_x-half_pace:img_x+half_pace,1:img_y+half_pace) = level_whole;
-            end
-            if img_y == y_grid(end)
-                level_img(img_x-half_pace:img_x+half_pace,img_y-half_pace:end) = level_whole;
-            end
-        else
-            level_img(img_x-half_pace:img_x+half_pace,img_y-half_pace:img_y+half_pace) = level;
-            if img_x == x_grid(1)
-                level_img(1:img_x+half_pace,img_y-half_pace:img_y+half_pace) = level;
-            end
-            if img_x == x_grid(end)
-                level_img(img_x-half_pace:end,img_y-half_pace:img_y+half_pace) = level;
-            end
-            if img_y == y_grid(1)
-                level_img(img_x-half_pace:img_x+half_pace,1:img_y+half_pace) = level;
-            end
-            if img_y == y_grid(end)
-                level_img(img_x-half_pace:img_x+half_pace,img_y-half_pace:end) = level;
-            end
-        end
-    end
-end
-
-
-% For the corners
-
-CL = level_local_radius + pace;
-
-level_img(1:CL, 1:CL)= ...
-    mean2(level_img(CL+1:CL+10, 1:CL))/2 +...
-    mean2(level_img(1:CL,CL+1:CL+10))/2;
-
-level_img(1:CL, end-CL:end)= ...
-    mean2(level_img(CL+1:CL+10, end-CL:end))/2 +...
-    mean2(level_img(1:CL,end-CL-10:end-CL-1))/2;
- 
-level_img(end-CL:end, 1:CL)= ...
-    mean2(level_img(end-CL-10:end-CL-1, 1:CL))/2 +...
-    mean2(level_img(end-CL:end,CL+1:CL+10))/2;
-
-level_img(end-CL:end, end-CL:end)= ...
-     mean2(level_img(end-CL-10:end-CL-1,end-CL:end))/2 +...
-     mean2(level_img(end-CL:end,end-CL-10:end-CL))/2;
-
-% Smooth the threshold map
-H = fspecial('gaussian',pace);
-level_img = imfilter(level_img,H,'replicate','same');
-
-% Segmentation using the threshold map
-imageMask = imageIn >= level_img;
-
-% Get a global mask to eliminate segmentation of detailed noise in the
-% background, with a slight lowered threshold to include some boundary
-% parts, fill holes and dilate a little to avoid masking off target region
-
-% second_group =imageIn(find(imageIn>level_whole));
-% new_level_for_second_group = level_whole+lowerbound/100*std(second_group);
-% imageMask_whole = imageIn>new_level_for_second_group;
-
-imageMask_whole = sm_imageInNorm >= level_sm_norm*lowerbound/100;
-imageMask_whole = imfill(imageMask_whole,'holes');
-% imageMask_whole = imdilate(imageMask_whole,ones(5,5));
-
-% The final segmentation is the intersect of both mask.
-bw_out =  imageMask.*imageMask_whole;
-
-% The output level is the gobal threshold 
-level = level_whole;
-
-if(showPlots==1)
-    figure(1); hold off;
-    imagesc(imageIn); colormap(gray); hold on
-    contour(bw_out,'r')
-end
+% this function is re-organized in the function called thresholdLocal with
+% the option of 'Otsu'
+[level,bw_out] = thresholdLocalSeg(imageIn,'Otsu', level_local_radius, pace, lowerbound);
