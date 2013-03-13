d8f729c965dfccbcd26cf7d4993782f652ce95bc
Marco Vilela <marco_vilela@hms.harvard.edu>
2013-03-13 13:16:59 +0000

    -F
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9851 0bcb941b-74d1-0310-bb26-e6636707f451

--- timeSeriesAnalysis/misc/preWhitening.m
+++ timeSeriesAnalysis/misc/preWhitening.m
@@ -3,10 +3,11 @@ function [out,trend,imf] = preWhitening(TS,varargin)
 %series TS
 %
 %Synopsis:
-%         [out,trend,imf]=preWhitening(TS,method)  
+%         [out,trend,imf]=preWhitening(TS,varargin)  
 %
 %Input: TS          - matrix(# of observations,# of variables)
 %       method      - string - 'imf' or 'ar' (Default is 'imf')
+%       alpha       - alpha value for the stationary tests    
 %       removeMean  - logical - true to remove the time series mean value
 %
 %Output
@@ -18,7 +19,7 @@ function [out,trend,imf] = preWhitening(TS,varargin)
 %Reference
 %Z. Wu, N. E. Huang, S. R. Long and C.-K. Peng, On the trend, detrending, and the
 %variability of nonlinear and non-stationary time series, Proc. Natl. Acad. Sci. USA
-%104 (2007) 14889?14894
+%104 (2007) 14889-14894
 %
 %See also : removeMeanTrendNaN
 %
@@ -27,11 +28,13 @@ function [out,trend,imf] = preWhitening(TS,varargin)
 %%Parsing input
 ip=inputParser;
 ip.addRequired('TS',@isnumeric);
-ip.addOptional('method','imf',@ischar);
-ip.addOptional('removeMean',false,@islogical);
+ip.addParamValue('method','imf',@ischar);
+ip.addParamValue('alpha',0.01,@isnumeric);
+ip.addParamValue('removeMean',false,@islogical);
 
 ip.parse(TS,varargin{:})
 method     = ip.Results.method;
+alpha      = ip.Results.alpha;
 removeMean = ip.Results.removeMean;
 
 %% Initialization
@@ -40,20 +43,20 @@ maxOrder    = 8;%Upper bound on the arx model order - empirical
 trend       = nan(nVar,nObs);
 out         = TS;
 imf         = [];
-h           = inf(2,nVar);
+hypot       = inf(2,nVar);
 
 %%
 for iVar=1:nVar
     
-    h(1,iVar) = kpsstest(TS(iVar,:));%Ho is stationary - 0 
-    h(2,iVar) = vratiotest(TS(iVar,:),'alpha',0.01);%Ho is a random walk - 1
+    hypot(1,iVar) = kpsstest(TS(iVar,:));%Ho is stationary - 0 
+    hypot(2,iVar) = vratiotest(TS(iVar,:),'alpha',alpha);%Ho is a random walk - 1
     
-    if or(h(1,iVar),~h(2,iVar))
+    if or(hypot(1,iVar),~hypot(2,iVar))
         switch method
             case 'ar'
                 
                 ts       = iddata(TS(iVar,:),[],1);
-                model    = arxstruc(ts,ts,[1:maxOrder]');
+                model    = arxstruc(ts,ts,(1:maxOrder)');
                 bestM    = selstruc(model,'aic');
                 finalM   = ar(ts,bestM(1));
                 IR       = polydata(finalM);
@@ -61,12 +64,16 @@ for iVar=1:nVar
                 out(iVar,:) = filter(IR,1,TS(iVar,:));
                 
             case 'imf'
-                %imf = empiricalModeDecomp( TS(:,i) )' ;
+                
                 imf  = emd(TS(iVar,:));
                 %Testing each IMF
-                for j = 1:size(imf,1) 
+                nImf = size(imf,1);
+                rW   = inf(1,nImf);
+                sS   = inf(1,nImf);
+                
+                for j = 1:nImf
                     rW(j) = vratiotest( imf(j,:) );
-                    sS(j) = kpsstest( imf(j,:) );
+                    sS(j) = kpsstest(   imf(j,:) );
                 end
                 %
                 
@@ -75,18 +82,18 @@ for iVar=1:nVar
                     for j=numel(range):-1:1
                         trend = sum( imf( range(j:end), : ), 1 );
                         DTs   = TS(iVar,:) - trend;
-                        h     = kpsstest(DTs);
-                        if ~h
+                        hypot     = kpsstest(DTs);
+                        if ~hypot
                             out(iVar,:) = DTs;
                             break;
                         end
                     end
                 end
                 
-                if h
+                if hypot
                     
                     out(iVar,:) = preWhitening(TS(iVar,:),'method','ar');
-                    h           = kpsstest(out(iVar,:));
+                    hypot           = kpsstest(out(iVar,:));
                     
                 end
         end
