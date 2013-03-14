3f17824470b31258de4d21772210a90aac206e85
Marco Vilela <marco_vilela@hms.harvard.edu>
2013-03-14 15:45:54 +0000

    clean up
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9863 0bcb941b-74d1-0310-bb26-e6636707f451

--- timeSeriesAnalysis/misc/timeSeriesPreProcessing.m
+++ timeSeriesAnalysis/misc/timeSeriesPreProcessing.m
@@ -1,10 +1,26 @@
 function  [outTS,exclude] = timeSeriesPreProcessing(TS,varargin)
-%This function performe the following time series operations:
+%This function performes the following time series operations:
 %   - remove outliers
 %   - interpolate NaN gaps (be careful with the size of the gap)
 %   - remove trend (different types)
 %
-%TS (nVar,nPoints)
+%Usage:
+%       [outTS,exclude] = timeSeriesPreProcessing(TS,varargin)
+%
+%Input:
+%       TS - time series. Format (nVar,nPoints)
+%       alpha - alpha level to test signal's imf against white noise imf's. (see getTimeSeriesTrend)
+%       nSurr - number of TS surragates created to build the white noise imf distribution
+%       minLength - minimal TS length
+%       trendType - see getTimeSeriesTrend
+%       gapSize   - length of the NaN gap to be interpolated - usually 1
+%       outLevel  - used to detect outliers (see detectOutliers)
+%
+%Output:
+%       outTS   - resulting TS after operations
+%       exclude - list of variables that were excluded because some of the minimal length requirement
+%
+%Marco Vilela, 2012
 
 ip = inputParser;
 ip.addRequired('TS',@(x) ismatrix(x));
@@ -52,7 +68,7 @@ end
 if trendT > -1
     
     auxTS = getTimeSeriesTrend(outTS,'trendType',trendT,'nSurr',nSurr,'alpha',alpha);
-    outTS = auxTS.detrendTS;    
+    outTS = auxTS.dTS;    
 end
 
 %% Removing by minimum length
