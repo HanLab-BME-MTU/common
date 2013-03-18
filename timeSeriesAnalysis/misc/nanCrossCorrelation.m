6052b5bafbe9bd5502bec1644817af0e796eb869
Marco Vilela <marco_vilela@hms.harvard.edu>
2013-03-18 18:55:42 +0000

    wrote a help
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@9891 0bcb941b-74d1-0310-bb26-e6636707f451

--- timeSeriesAnalysis/misc/nanCrossCorrelation.m
+++ timeSeriesAnalysis/misc/nanCrossCorrelation.m
@@ -1,6 +1,26 @@
 function [xCorr,bounds,lags,pVal] = nanCrossCorrelation(x,y,varargin)
-%Inputs are column vectors
-%Under Construction
+% This function calculates 3 measures of correlation between two vectors x and y.
+% Measures: Pearson, Kendall and Spearman cross-correlation
+%
+%Usage:
+%       [xCorr,bounds,lags,pVal] = nanCrossCorrelation(x,y,varargin)
+%
+%Inputs:
+%       x and y  - vectors of the same size
+%       corrType - correlation type: 'Pearson', 'Kendall' or 'Spearman'
+%       maxLag   - number of lag shifts 
+%       local    - only used for Kendall type. Under development
+%       robust   - logical. Only used for the Pearson type. If true, uses robust regression and estimation of variance to calculate the 
+%                  coefficient. Default is false.
+%
+%Output:
+%       xCorr  - correlation coefficients
+%       bounds - Two-element vector indicating the approximate upper and lower
+%                confidence bounds, assuming the input series are uncorrelated.
+%       lags   - 
+%       pVal   - pValue for each coefficient
+%
+%
 %
 %Marco Vilela, 2012
 
@@ -10,23 +30,24 @@ ip.addRequired('y',@isvector);
 ip.addParamValue('corrType','Pearson', @ischar)
 ip.addParamValue('maxLag',0,@isscalar);
 ip.addParamValue('local',numel(x)-1,@isscalar);
+ip.addParamValue('robust',false,@islogical);
 
 ip.parse(x,y,varargin{:});
 local    = ip.Results.local;
 maxLag   = ip.Results.maxLag;
 corrT    = ip.Results.corrType;
+robustOn = ip.Results.robust;
 
 x = x(:);
 y = y(:);
 
 lags = -maxLag:maxLag;
 nObs = length(x);
-%xIn  = testSign(x);
-%yIn  = testSign(y);
+
 
 numSTD = 2;
-
 bounds = [numSTD;-numSTD]/sqrt(nObs);
+
 
 
 pVal = [];
@@ -52,13 +73,27 @@ switch corrT
         Y    = num2cell(Y,1);
         X    = num2cell(X,1);
         
-        posLag = cell2mat(cellfun(@(x,y) robustfit(x,y,'bisquare'),lagX,Y,'Unif',0));
-        negLag = cell2mat(cellfun(@(x,y) robustfit(x,y,'bisquare'),X,lagY,'Unif',0));
-        normalizationR = cell2mat(cellfun(@(x,y) mad(x,1)/mad(y,1),lagX,Y,'Unif',0));
-        normalizationL = cell2mat(cellfun(@(x,y) mad(x,1)/mad(y,1),X,lagY,'Unif',0));
-        
-        CCL   = normalizationR.*posLag(2,:);
-        CCR   = normalizationL.*negLag(2,:);
+        if robustOn
+            
+            posLag = cell2mat(cellfun(@(x,y) robustfit(x,y,'bisquare'),lagX,Y,'Unif',0));
+            negLag = cell2mat(cellfun(@(x,y) robustfit(x,y,'bisquare'),X,lagY,'Unif',0));
+            posLag(1,:) = [];
+            negLag(1,:) = [];
+            
+            normalizationR = cell2mat(cellfun(@(x,y) mad(x,1)/mad(y,1),lagX,Y,'Unif',0));
+            normalizationL = cell2mat(cellfun(@(x,y) mad(x,1)/mad(y,1),X,lagY,'Unif',0));
+            
+        else
+
+            posLag = cell2mat(cellfun(@(x,y) regress(x,y),lagX,Y,'Unif',0));
+            negLag = cell2mat(cellfun(@(x,y) regress(x,y),X,lagY,'Unif',0));
+            normalizationR = cell2mat(cellfun(@(x,y) nanstd(x)/nanstd(y),lagX,Y,'Unif',0));
+            normalizationL = cell2mat(cellfun(@(x,y) nanstd(x)/nanstd(y),X,lagY,'Unif',0));
+            
+        end            
+            
+        CCL   = normalizationR.*posLag;
+        CCR   = normalizationL.*negLag;
         xCorr = [fliplr(CCR) CCL(2:end)];
         pVal  = pvalPearson('b',xCorr,nObs);
     case 'Kendall'
@@ -72,17 +107,6 @@ xCorr = xCorr(:);
 
 end %ENd of main function
 
-function [TSout] = testSign(TS)
-%Removes the mean value if the TS oscillates between positive and negative values
-TSout = TS;
-positiveTest = sum( TS(isfinite(TS)) ) == sum( TS(isfinite(TS))>0 );
-negativeTest = sum( TS(isfinite(TS)) ) == sum( TS(isfinite(TS))<0 );
-if ~xor(positiveTest,negativeTest)
-    TSout = TS - nanmean(TS);
-end
-
-end %end testSign
-    
 function p = pvalPearson(tail, xCorr, nObs)
 %
 t = xCorr.*sqrt((nObs-2)./(1-xCorr.^2)); % 
@@ -101,6 +125,4 @@ switch tail
         
 end
 
-end
-
-        
+end     
