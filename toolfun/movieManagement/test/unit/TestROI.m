5648f40fee29c2cbc7c882b39995181fa078241f
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2013-04-26 11:16:03 +0000

    Add unit tests for process/package sharing across ROIs
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10042 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/movieManagement/test/unit/TestROI.m
+++ toolfun/movieManagement/test/unit/TestROI.m
@@ -4,6 +4,8 @@ classdef TestROI < TestCase
         movie
         roi = MovieData.empty(0,1);
         nRois = 5
+        process
+        package
     end
     
     methods
@@ -23,7 +25,7 @@ classdef TestROI < TestCase
             delete(self.movie);
         end
         
-        %% Single ROI tests
+        %% ROI methods tests
         function testGetROI(self)
             for i = 1 : self.nRois
                 assertEqual(self.movie.getROI(i), self.roi(i));
@@ -51,6 +53,70 @@ classdef TestROI < TestCase
             assertEqual(self.movie.rois_, self.roi(self.nRois));
             for i = 1 : self.nRois -1
                 assertFalse(self.roi(i).isvalid);
+            end
+        end
+        
+        % Shared process/package tests
+        function setUpSharedProcess(self)
+            self.process = MockProcess(self.movie);
+            self.movie.addProcess(self.process);
+            
+            self.nRois = self.nRois + 1;
+            self.roi(self.nRois) = self.movie.addROI('','');
+        end
+        
+        function setUpSharedPackage(self)
+            self.package = MockPackage(self.movie);
+            self.movie.addPackage(self.package);
+            
+            self.nRois = self.nRois + 1;
+            self.roi(self.nRois) = self.movie.addROI('','');
+        end
+        
+        function testSharedProcess(self)
+            self.setUpSharedProcess();
+            assertEqual(self.movie.getProcess(1), self.process);
+            for i = 1: self.nRois - 1
+                assertTrue(isempty(self.movie.getROI(i).processes_));
+            end
+            assertEqual(self.movie.getROI(self.nRois).getProcess(1), self.process);
+        end
+        
+        function testDeleteSharedProcess(self)
+            self.setUpSharedProcess();
+            self.movie.deleteProcess(1);
+            assertTrue(isempty(self.movie.processes_));
+            for i = 1: self.nRois
+                assertTrue(isempty(self.movie.getROI(i).processes_));
+            end
+        end
+        
+        function testReplaceSharedProcess(self)
+            self.setUpSharedProcess();
+            newprocess = MockProcess(self.movie);
+            self.movie.replaceProcess(1, newprocess);
+            assertEqual(self.movie.getProcess(1), newprocess);
+            for i = 1: self.nRois - 1
+                assertTrue(isempty(self.movie.getROI(i).processes_));
+            end
+            assertEqual(self.movie.getROI(self.nRois).getProcess(1), newprocess);
+        end
+        
+        function testSharedPackage(self)
+            self.setUpSharedPackage();
+            assertEqual(self.movie.getPackage(1), self.package);
+            for i = 1: self.nRois - 1
+                assertTrue(isempty(self.movie.getROI(i).packages_));
+            end
+            assertEqual(self.movie.getROI(self.nRois).getPackage(1), self.package);
+        end
+        
+        function testDeleteSharedPackage(self)
+            self.setUpSharedPackage();
+            self.movie.deletePackage(1);
+            assertTrue(isempty(self.movie.packages_));
+            for i = 1: self.nRois
+                assertTrue(isempty(self.movie.getROI(i).packages_));
             end
         end
     end
