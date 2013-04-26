41c870dd560f054d6d44eeb72f5e02d93bdc6959
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2013-04-26 11:16:16 +0000

    Clean ROI integration tests (moved to test/unit)
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10043 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/movieManagement/test/integration/TestMovieDataROI.m
+++ toolfun/movieManagement/test/integration/TestMovieDataROI.m
@@ -53,34 +53,6 @@ classdef TestMovieDataROI < TestCase
             end
         end
         
-        function testAddROI(self)
-            % Create ROI movie
-            roiFullPaths = self.setupROI(1);
-            
-            % Reload ROI Movie and test components
-            roiMovie = MovieData.load(roiFullPaths{1});
-            assertEqual(roiMovie.parent_, self.movie);
-            assertEqual(roiMovie.getROIMask(), true(self.movie.imSize_));
-            assertEqual(roiMovie.getAncestor(), self.movie);
-            assertEqual(roiMovie.getAncestor().getDescendants(), roiMovie);
-        end
-        
-        function testAddMultipleROIs(self)
-            % Create 3 ROIs
-            nROIs = 5;
-            roiFullPaths = self.setupROI(nROIs);
-            assertEqual(numel(self.movie.rois_), nROIs);
-            
-            % Reload ROI Movie and test components
-            roiMovies(1, nROIs) = MovieData();
-            for i = 1 : nROIs
-                roiMovies(i) = MovieData.load(roiFullPaths{i});
-                assertEqual(roiMovies(i).getAncestor(), self.movie);
-            end
-            
-            % Test getAncestor/getDescendants methods
-            assertEqual(self.movie.getDescendants(), roiMovies);
-        end
         
         function testDeleteROI(self)
             % Create ROI movie
@@ -129,79 +101,6 @@ classdef TestMovieDataROI < TestCase
             
             % Remove relocated movie
             rmdir(relocatedMoviePath,'s');
-        end
-        
-        
-        %% Process/package deletion
-        
-        function testDeleteSharedProcess(self)
-            
-            % Create process in main movie
-            self.movie.addProcess(ThresholdProcess(self.movie));
-            
-            % Create ROI movies
-            self.setupROI(2);
-            assertEqual(numel(self.movie.processes_),1);
-            assertEqual(numel(self.movie.rois_(1).processes_),1);
-            assertEqual(numel(self.movie.rois_(2).processes_),1);
-            
-            % Delete process from ROI 1
-            self.movie.rois_(1).deleteProcess(1);
-            assertEqual(numel(self.movie.processes_),0);
-            assertEqual(numel(self.movie.rois_(1).processes_),0);
-            assertEqual(numel(self.movie.rois_(2).processes_),0);
-            
-            % Reload movie and check process has been deleted in the parent
-            % and in the ROI movies
-            self.movie.save();
-            reloadedMovie = MovieData.load(self.movie.getFullPath);
-            assertEqual(numel(reloadedMovie.processes_),0);
-            assertEqual(numel(reloadedMovie.rois_(1).processes_),0);
-            assertEqual(numel(reloadedMovie.rois_(2).processes_),0);
-            
-        end
-        
-        function testDeleteSharedPackage(self)
-            
-            % Create process in main movie
-            self.movie.addPackage(SegmentationPackage(self.movie));
-            
-            % Create ROI movies
-            self.setupROI(2);
-            assertEqual(numel(self.movie.packages_),1);
-            assertEqual(numel(self.movie.rois_(1).packages_),1);
-            assertEqual(numel(self.movie.rois_(2).packages_),1);
-            
-            % Delete process
-            self.movie.rois_(1).deletePackage(1);
-            assertEqual(numel(self.movie.packages_),0);
-            assertEqual(numel(self.movie.rois_(1).packages_),0);
-            assertEqual(numel(self.movie.rois_(2).packages_),0);
-            
-            % Reload movie and check process has been deleted in the parent
-            % and in the ROI movies
-            self.movie.save();
-            reloadedMovie = MovieData.load(self.movie.getFullPath);
-            assertEqual(numel(reloadedMovie.packages_),0);
-            assertEqual(numel(reloadedMovie.rois_(1).packages_),0);
-            assertEqual(numel(reloadedMovie.rois_(2).packages_),0);
-            
-        end
-        
-        function testReplaceSharedProcess(self)
-            % Create process
-            self.movie.addProcess(ThresholdProcess(self.movie));
-            oldprocess = self.movie.getProcess(1);
-            
-            % Create ROI movies
-            self.setupROI(2);
-            
-            % Replace process
-            self.movie.replaceProcess(1, MaskRefinementProcess(self.movie))
-            assertTrue(isa(self.movie.getProcess(1),'MaskRefinementProcess'));
-            assertTrue(isa(self.movie.rois_(1).getProcess(1),'MaskRefinementProcess'));
-            assertTrue(isa(self.movie.rois_(2).getProcess(1),'MaskRefinementProcess'));
-            assertFalse(oldprocess.isvalid);
         end
     end
 end
