933615c1004accfbfd7af5b68bd5158e46fe620f
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2013-05-17 12:52:25 +0000

    A    machineLearning/clustering/MeanShift/estimateOptimalBandwidth.m - per point bandwidth estimate A machineLearning/clustering/MeanShift/VariableBandwidthMeanShiftClustering.m - uses per-point bandwidths from above. beta version, use with caution
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10122 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ machineLearning/clustering/MeanShift/estimateOptimalBandwidth.m
@@ -0,0 +1,114 @@
+function [H,He] = estimateOptimalBandwidth(X,HRange,varargin)
+%ESTIMATEOPTIMALBANDWIDTH estimates the optimal mean shift bandwidth for each point
+%
+%H = estimateOptimalBandwidth(X,HRange)
+%
+%   This function implements the bandwidth estimation in [1] to determine,
+%   within the specified bandwidth range, what the optimal mean-shift
+%   bandwidth is to use for each point in the input dataset. These
+%   per-point bandwidths can then be used in variable-bandwidth mean-shift
+%   clustering.
+%
+%
+%
+% References:
+% [1] D. Comaniciu, "An Algorithm for Data-Driven Bandwidth Selection", IEEE
+% T.P.A.M.I, 25 #2 (2003)
+%% ----------------- Input ---------------- %%
+
+ip = inputParser;
+ip.addParamValue('nH',20,@isposint);%Number of bandwidths to try within specified range
+ip.addParamValue('w',1,@isposint);%Window size for calculating local jenson-shannon divergence.
+
+
+ip.parse(varargin{:});
+
+p = ip.Results;
+
+nH = p.nH;%decreases annoyingness of code.
+
+[n,d] = size(X);
+
+
+%% ------------ Init ------------ %%
+
+Htry = logspace(log10(HRange(1)),log10(HRange(2)),p.nH);
+
+
+
+
+%% --------- Per-Bandwidth Clustering ----- %%
+%Cluster at each bandwidth, and estimate the mean and sigma of the density
+%mode underlying each detected cluster at each bandwidth.
+
+nC = zeros(nH,1);
+sigEst = cell(nH,1);%Per-cluster estimated sigmas
+ppSig = nan(n,nH,d);%Sigmas estimated for each point at each bandwidth. We assume diagonal covariance matrices.
+ppMu = nan(n,nH,d);%Mean for each point at each bandwidth
+
+
+for ih = 1:nH
+    
+    [clInf,ptInd,ptTraj] = MeanShiftClustering(X,Htry(ih),'method','standard','minClusterDistance',Htry(ih));
+    
+    nC(ih) = numel(clInf);
+    
+    sigEst{ih} = nan(nC(ih),d);
+    for u = 1:nC(ih)%for each cluster
+        
+        %Do least-squares estimate of sigma for this cluster
+        
+        mu = clInf(u).ptClusterCenter;%Center of this cluster is assumed to be mean
+        Yi = cellfun(@(x)(x(1:end-1,:)),ptTraj(ptInd == u),'Unif',0);%All traj points for this cluster, exlcluding first which is the point itself
+        Yi = vertcat(Yi{:});
+        Mi = cellfun(@(x)(diff(x,1,1)),ptTraj(ptInd == u),'Unif',0);
+        Mi = vertcat(Mi{:});
+        %tu = size(Yi,1);%Number of traj points this cluster            
+        
+        sig2v = nan(1,d);
+        for v = 1:d
+            num = sum(Mi(:,v) .* (mu(v)-Yi(:,v)));
+            den = sum(Mi(:,v) .^2);
+            sig2v(v) = Htry(ih)^2 * (num/den - 1);                
+        end
+
+        sigEst{ih}(u,:) = sqrt(sig2v);
+        
+        %Assign the sigmas to each point in the cluster for per-point
+        %sigmas
+        %NOTE: We should actually not estimate if #pts too low...
+        ppSig(ptInd == u,ih,:) = repmat(sigEst{ih}(u,:),[nnz(ptInd==u) 1]);
+        ppMu(ptInd == u,ih,:) = repmat(mu,[nnz(ptInd==u) 1]);        
+
+    end
+    
+end
+
+%% ----------- Get optimal bandwidth for each point ------- %%
+%This is done by finding the bandwidths at which the estimated mu and sigma
+%were most stable.
+
+%Since our clustering currently only supports diagonal, isotropic covariane
+%matrices, we just use the mean estimated sigma. 
+ppSigMean = nanmean(real(ppSig),3);%Temp - Min better? Max? (real is in case bandwidth range was too wide and some estimated sigmas^2 were negative)
+ppMuMean = nanmean(real(ppMu),3);
+
+ljsAll = nan(n,nH);
+H = nan(n,1);
+He = nan(n,1);
+minJS = nan(n,1);
+iMinJS = nan(n,1);
+
+for j = 1:n
+    ljsAll(j,:) = localJensenShannon(ppMuMean(j,:),ppSigMean(j,:),p.w);%Calculates difference between estimated distributions at neighboring bandwidths
+    [minJS(j),iMinJS(j)] = min(ljsAll(j,:));
+    H(j) = ppSigMean(j,iMinJS(j));%The optimal bandwidth is that which minizes this local difference, and is therefore most stable.
+    He(j) = Htry(iMinJS(j));%And the bandwidth at which this was estimated.
+    
+end
+
+%Clip estimates to input sigma range.
+H(H<HRange(1)) = HRange(1);
+H(H>HRange(2)) = HRange(2);
+
+
