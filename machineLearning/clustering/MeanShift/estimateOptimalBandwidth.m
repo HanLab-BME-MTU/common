1dd0db7810dbaf1d77f193b9959aa68856c959d3
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2013-05-21 18:29:17 +0000

    M    machineLearning/clustering/MeanShift/estimateOptimalBandwidth.m - returns additional outputs
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10127 0bcb941b-74d1-0310-bb26-e6636707f451

--- machineLearning/clustering/MeanShift/estimateOptimalBandwidth.m
+++ machineLearning/clustering/MeanShift/estimateOptimalBandwidth.m
@@ -1,4 +1,4 @@
-function [H,He] = estimateOptimalBandwidth(X,HRange,varargin)
+function [H,ppSig,ppMu,ljsAll] = estimateOptimalBandwidth(X,HRange,varargin)
 %ESTIMATEOPTIMALBANDWIDTH estimates the optimal mean shift bandwidth for each point
 %
 %H = estimateOptimalBandwidth(X,HRange)
@@ -19,7 +19,7 @@ function [H,He] = estimateOptimalBandwidth(X,HRange,varargin)
 ip = inputParser;
 ip.addParamValue('nH',20,@isposint);%Number of bandwidths to try within specified range
 ip.addParamValue('w',1,@isposint);%Window size for calculating local jenson-shannon divergence.
-
+ip.addParamValue('NumParallel',6,@isposint);
 
 ip.parse(varargin{:});
 
@@ -35,6 +35,17 @@ nH = p.nH;%decreases annoyingness of code.
 Htry = logspace(log10(HRange(1)),log10(HRange(2)),p.nH);
 
 
+%Setup parallel workers if requested
+if p.NumParallel > 1
+    currPoolSize = matlabpool('size');    
+    if currPoolSize ~= p.NumParallel       
+        if currPoolSize > 0            
+            matlabpool('close')
+        end        
+        matlabpool('open',p.NumParallel);        
+    end       
+end
+
 
 
 %% --------- Per-Bandwidth Clustering ----- %%
@@ -43,46 +54,58 @@ Htry = logspace(log10(HRange(1)),log10(HRange(2)),p.nH);
 
 nC = zeros(nH,1);
 sigEst = cell(nH,1);%Per-cluster estimated sigmas
+muEst = cell(nH,1);%Per-cluster estimated mus
 ppSig = nan(n,nH,d);%Sigmas estimated for each point at each bandwidth. We assume diagonal covariance matrices.
 ppMu = nan(n,nH,d);%Mean for each point at each bandwidth
 
+ptInd = nan(n,nH);
 
-for ih = 1:nH
+parfor ih = 1:nH
     
-    [clInf,ptInd,ptTraj] = MeanShiftClustering(X,Htry(ih),'method','standard','minClusterDistance',Htry(ih));
+    [clInf,ptInd(:,ih),ptTraj] = MeanShiftClustering(X,Htry(ih),'method','standard','minClusterDistance',Htry(ih));
     
     nC(ih) = numel(clInf);
     
     sigEst{ih} = nan(nC(ih),d);
+    muEst{ih} = nan(nC(ih),d);
     for u = 1:nC(ih)%for each cluster
         
         %Do least-squares estimate of sigma for this cluster
         
-        mu = clInf(u).ptClusterCenter;%Center of this cluster is assumed to be mean
-        Yi = cellfun(@(x)(x(1:end-1,:)),ptTraj(ptInd == u),'Unif',0);%All traj points for this cluster, exlcluding first which is the point itself
+        muEst{ih}(u,:) = clInf(u).ptClusterCenter;%Center of this cluster is assumed to be mean
+        Yi = cellfun(@(x)(x(1:end-1,:)),ptTraj(ptInd(:,ih) == u),'Unif',0);%All traj points for this cluster, exlcluding first which is the point itself
         Yi = vertcat(Yi{:});
-        Mi = cellfun(@(x)(diff(x,1,1)),ptTraj(ptInd == u),'Unif',0);
+        Mi = cellfun(@(x)(diff(x,1,1)),ptTraj(ptInd(:,ih) == u),'Unif',0);
         Mi = vertcat(Mi{:});
         %tu = size(Yi,1);%Number of traj points this cluster            
         
         sig2v = nan(1,d);
         for v = 1:d
-            num = sum(Mi(:,v) .* (mu(v)-Yi(:,v)));
+            num = sum(Mi(:,v) .* (muEst{ih}(u,v)-Yi(:,v)));
             den = sum(Mi(:,v) .^2);
             sig2v(v) = Htry(ih)^2 * (num/den - 1);                
         end
 
         sigEst{ih}(u,:) = sqrt(sig2v);
         
-        %Assign the sigmas to each point in the cluster for per-point
-        %sigmas
-        %NOTE: We should actually not estimate if #pts too low...
-        ppSig(ptInd == u,ih,:) = repmat(sigEst{ih}(u,:),[nnz(ptInd==u) 1]);
-        ppMu(ptInd == u,ih,:) = repmat(mu,[nnz(ptInd==u) 1]);        
+        
 
     end
     
 end
+
+%Assign the sigmas to each point in the cluster for per-point sigmas.
+%We do this outside the loop to allow parallelization
+for ih = 1:nH
+    
+    for u = 1:nC(ih)
+        %NOTE: We should actually not estimate if #pts too low...
+        ppSig(ptInd(:,ih) == u,ih,:) = repmat(sigEst{ih}(u,:),[nnz(ptInd(:,ih)==u) 1]);
+        ppMu(ptInd(:,ih) == u,ih,:) = repmat(muEst{ih}(u,:),[nnz(ptInd(:,ih)==u) 1]);        
+    end
+    
+end
+
 
 %% ----------- Get optimal bandwidth for each point ------- %%
 %This is done by finding the bandwidths at which the estimated mu and sigma
