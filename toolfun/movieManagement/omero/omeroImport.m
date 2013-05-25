ab2c46e2b40bba9463478e9016d5c2846aaf8dcb
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2013-05-25 12:58:40 +0000

    Update omeroImport using built-in functions of OMERO.matlab
    - Replace use of pojos by metadataService calls
    - Fix numerical aperture reading
    
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10136 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/movieManagement/omero/omeroImport.m
+++ toolfun/movieManagement/omero/omeroImport.m
@@ -25,60 +25,54 @@ function movie = omeroImport(session,imageID,varargin)
 
 % Input check
 ip=inputParser;
-ip.addRequired('session',@(x) isa(x,'omero.api.ServiceFactoryPrxHelper'));
-ip.addRequired('imageID',@isscalar);
+ip.addRequired('session', @(x) isa(x,'omero.api.ServiceFactoryPrxHelper'));
+ip.addRequired('imageID', @isscalar);
 ip.addOptional('outputDirectory',[],@ischar);
 ip.parse(session,imageID,varargin{:});
 
-% Get images
-proxy = session.getContainerService();
-id = toJavaList(imageID, 'java.lang.Long');
-imageList = proxy.getImages('omero.model.Image', id, omero.sys.ParametersI());
-images = toMatlabList(imageList);
-assert(~isempty(images) && ~isempty(images(1)), 'No image found');
-image=images(1);
+% Retrieve image and pixels
+image = getImages(session, imageID);
+assert(~isempty(image), 'No image of id %g found', imageID);
+pixels = image.getPrimaryPixels();
 
-% Retrieve pixels
-svc=session.getPixelsService();
-pixels=svc.retrievePixDescription(image.getPixels(0).getId.getValue);
+% Create properties cell array based on existing metadata
+metadataService = session.getMetadataService;
+movieArgs={}; 
 
-
-movieArgs={}; % Create properties cell array based on existing metadata
-
-
-
-pixelsA=pojos.PixelsData(pixels);
-
-% Get pixel size
-pixelSize = pixelsA.getPixelSizeX;
+% Read pixel size
+pixelSize = pixels.getPhysicalSizeX;
 if ~isempty(pixelSize)
-    assert(isequal(pixelSize,pixelsA.getPixelSizeY),'Pixel size different in x and y');
-%     movieArgs=horzcat(movieArgs,'pixelSize_',pixelSize*1e3);
+    assert(isequal(pixelSize, pixels.getPhysicalSizeY),...
+        'Pixel size different in x and y');
+    movieArgs = horzcat(movieArgs, 'pixelSize_', pixelSize.getValue * 1e3);
 end
 
-% Get camera bit depth
+% Read camera bit depth
 camBitdepth = pixels.getPixelsType.getBitSize.getValue/2;
 if ~isempty(camBitdepth)
     movieArgs=horzcat(movieArgs,'camBitdepth_',camBitdepth);
 end
 
-% Get time interval
+% Read time interval
 timeInterval = pixels.getTimeIncrement();
 if ~isempty(timeInterval)
     movieArgs=horzcat(movieArgs,'timeInterval_', timeInterval.getValue());
 end
 
-% Get the lens numerical aperture
-imageA = pojos.ImageAcquisitionData(image);
-objective = imageA.getObjective;
-if ~isempty(objective)
-    lensNA = imageA.getObjective.getLensNA;
-    movieArgs=horzcat(movieArgs,'numAperture_',double(lensNA));
+% Read the lens numerical aperture
+objectiveSettings = image.getObjectiveSettings;
+if ~isempty(objectiveSettings)
+    instrument = metadataService.loadInstrument(objectiveSettings.getId.getValue);
+    objective = instrument.copyObjective().get(0);
+    if ~isempty(objective)
+        lensNA = objective.getLensNA().getValue();
+        movieArgs=horzcat(movieArgs,'numAperture_',double(lensNA));
+    end
 end
 
 
 % Read number of channels, frames and stacks
-nChan =  pixelsA.getSizeC;
+nChan =  pixels.getSizeC().getValue();
 
 % Set output directory (based on image extraction flag)
 movie=MovieData;
@@ -96,30 +90,28 @@ end
 % Create movie channels
 movieChannels(1,nChan)=Channel();
 channelArgs=cell(1,nChan);
-channels = pixels.copyChannels;
+pixelsId = toJavaList(pixels.getId.getValue,'java.lang.Long');
+omeroChannels = metadataService.loadChannelAcquisitionData(pixelsId);
 for i=1:nChan
     channelArgs{i}={};
-
-    channelD = pojos.ChannelData(i-1,channels.get(i-1));
+    omeroChannel = omeroChannels.get(i-1);
+    
     % Read excitation wavelength
-    exwlgth=channelD.getExcitationWavelength();
+    exwlgth = omeroChannel.getExcitationWave().getValue();
     if ~isempty(exwlgth) && exwlgth ~= -1
         channelArgs{i}=horzcat(channelArgs{i},'excitationWavelength_',exwlgth);
     end
     
     % Read emission wavelength
-    emwlgth=channelD.getEmissionWavelength();
+    emwlgth=omeroChannel.getEmissionWave().getValue();
     if ~isempty(emwlgth) && emwlgth ~= -1 && emwlgth ~= 1 % Bug
         channelArgs{i}=horzcat(channelArgs{i},'emissionWavelength_',emwlgth);
     end
     
-    % Read channelName
-    chanName=channelD.getName;
+    % Read channel xame
+    chanName = omeroChannels.get(0).getName.getValue;
     if isempty(chanName), chanName = ['Channel_' num2str(i)]; end
     
-    % Create new channel
-%     channelPath{i}=dataPath;
-%     end
     movieChannels(i)=Channel('',channelArgs{i}{:});
 end
 
