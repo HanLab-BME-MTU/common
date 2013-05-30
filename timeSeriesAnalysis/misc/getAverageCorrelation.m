06d65f456f92493ec5d6f98bc9c796c6f1f22e0a
Marco Vilela <marco_vilela@hms.harvard.edu>
2013-05-30 14:57:37 +0000

    made it generic
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10179 0bcb941b-74d1-0310-bb26-e6636707f451

--- timeSeriesAnalysis/misc/getAverageCorrelation.m
+++ timeSeriesAnalysis/misc/getAverageCorrelation.m
@@ -1,19 +1,17 @@
-function [muCC,muCI,lags,xCorr,excluded] = getAverageCorrelation(TS1,varargin)
+function [muCC,muCI,lags,xCorr] = getAverageCorrelation(TS1,varargin)
 %This function estimates the average autocorrelation or cross-correlation given TS1 or TS1,TS2 inputs matrices, respectively.
 %USAGE:
 %
-%       for ACF:        [muACF,muCI,lags,allACF] = getAverageCorrelation(TS1,varargin)
+%       for ACF: [muACF,muCI,lags,allACF]  = getAverageCorrelation(TS1,varargin)
 %
-%       for CCF: [muCC,muCI,lags,xCorr,excluded] = getAverageCorrelation(TS1,'TS2',TS2,varargin)
+%       for CCF: [muCC,muCI,lags,allxCorr] = getAverageCorrelation(TS1,'TS2',TS2,varargin)
 %
 %Input:
-%       TS1 - matrix of time series (# of variables,# of Observations) 
-%            format based on the output of the windowing package        
-%            The number of variables is the number of windows   
+%       TS1         - matrix of time series (# of variables,# of Observations) 
+%                     format based on the output of the windowing package        
+%                     The number of variables is the number of windows   
 %
 %       TS2         - same formart as TS1. Use to estimate the cross-correlation between each row of TS1 and TS2(default:[])
-%       excludeWin1 - variables(or windows) from TS1 to be excluded from the calculations(default:[])
-%       excludeWin2 - same as excludeWin1 but for TS2(default:[])
 %       nBoot       - number of bootstrap samples(default:1e3)
 %       alpha       - alpha value used to estimate the confidence interval for the average correlation(default:0.05)
 %       corrTyep    - correlation type: Pearson, Spearman or Kendall
@@ -26,28 +24,21 @@ function [muCC,muCI,lags,xCorr,excluded] = getAverageCorrelation(TS1,varargin)
 %       muCI  - confidence interval for muCC given the input alpha
 %       lags  - lags for muCC
 %       xCorr - ACF or CCF for each row of TS1/TS2 
-%       excluded - variables(windows) excluded = union(excludeWin1,excludeWin2)
 %
 %Marco Vilela, 2012
 
 ip = inputParser;
 ip.addRequired('TS1',@(x) isnumeric(x));
 [nVar1,nObs1] = size(TS1);
-
-ip.addOptional('excludeWin1',     [],@(x) isnumeric(x));
-ip.addOptional('TS2',     TS1,@(x) isnumeric(x));
-ip.addOptional('excludeWin2',     [],@(x) isnumeric(x));
-
+ip.addOptional('TS2',       TS1,@(x) isnumeric(x));
 ip.addParamValue('nBoot',   1e3,@isscalar);
 ip.addParamValue('alpha',   .05,@isscalar);
 ip.addParamValue('corrType','Pearson', @ischar)
-ip.addParamValue('local',length(TS1)-1,@isscalar);
-ip.addParamValue('maxLag',0,@isscalar);
-ip.addParamValue('robust',false,@islogical);
+ip.addParamValue('local',    length(TS1)-1,@isscalar);
+ip.addParamValue('maxLag',   0,@isscalar);
+ip.addParamValue('robust',   false,@islogical);
 
 ip.parse(TS1,varargin{:});
-exclude1 = ip.Results.excludeWin1;
-exclude2 = ip.Results.excludeWin2;
 nBoot    = ip.Results.nBoot;
 alpha    = ip.Results.alpha;
 TS2      = ip.Results.TS2;
@@ -69,15 +60,11 @@ if nObs1 ~= nObs2
 end
 
 %% Calculating pair-wise correlations
-excluded        = union(exclude2,exclude1);
-TS1(excluded,:) = [];
-TS2(excluded,:) = [];
 
-nVar  = size(TS1,1);
-xCorr = nan(2*maxLag + 1,nVar);
-bound = nan(2,nVar);
+xCorr = nan(2*maxLag + 1,nVar1);
+bound = nan(2,nVar1);
 
-for iVar = 1:nVar
+for iVar = 1:nVar1
     
     [xCorr(:,iVar),bound(:,iVar),lags] = nanCrossCorrelation(TS1(iVar,:),TS2(iVar,:),'corrType',corrT,'maxLag',maxLag,'local',local,'robust',robust);
     
