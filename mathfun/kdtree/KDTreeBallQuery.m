9819469e5f4cbccbaa8596a20c814e3323f7157e
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-06-24 22:41:04 +0000

    Restored old KD-Tree functions due to a (unresolvable?) bug with the implementation in 'extern'
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10326 0bcb941b-74d1-0310-bb26-e6636707f451

--- mathfun/kdtree/KDTreeBallQuery.m
+++ mathfun/kdtree/KDTreeBallQuery.m
@@ -1,50 +1,36 @@
-%function [idx, dist] = KDTreeBallQuery(inPts, queryPts, radii) finds all of the points which are within the specified radius of the query points
+%KDTREEBALLQUERY finds all of the points which are within the specified radius of the query points
+% 
+% [idx, dist] = KDTreeBallQuery(inPts,queryPts,radii)
+% 
+% This function returns the indices of the input points which are within
+% the specified radii of the query points. Supports 1D, 2D or 3D point sets. 
+% In other words, this returns all the indices of the input points which are
+% contained in the spheres whose centers are given by the query points and
+% whose radii are given by the input radii vector.
 %
-% Returns the indices of the input points which are within the specified radii of
-% the query points. Supports 1D, 2D or 3D point sets.
+% Input:
+% 
+%     inPts - an MxK matrix specifying the input points to test for distance
+%     from the query points, where M is the number of points and K is the
+%     dimensionality of the points.
+% 
+%     queryPts - an NxK matrix specifying the query points, e.g. the centers of
+%     the spheres within which input points will be found.
+% 
+%     radii - an Nx1 vector or a scalar specifying the distances from each 
+%     query point to find input points, e.g. the radii of the spheres within 
+%     which input points will be found. If scalar, the same radius is used
+%     for all query points.
+%     NOTE: This value should be of class double, or strange behavior may
+%     occur.
+% 
+% 
+% Output:
+% 
+%   idx - Nx1 cell array, the n-th element of which gives the indices of
+%   the input points which are within the n-th radii of the n-th query
+%   point.
+% 
+%   dist - Nx1 cell array, the n-th element of which gives the corresponding 
+%   distances between the input points and the n-th query point.
 %
-% Inputs:
-%
-%        inPts : MxK matrix specifying the M input points in K dimensions.
-%
-%     queryPts : NxK matrix specifying the N query points in K dimensions.
-%
-%        radii : Nx1 vector or a scalar specifying the distances from each
-%                query point to find input points. If scalar, the same radius
-%                is used for all query points.
-%
-% Outputs:
-%
-%          idx : Nx1 cell array, the n-th element of which gives the indices of
-%                the input points which are within the n-th radius of the n-th
-%                query point.
-%
-%         dist : Nx1 cell array, the n-th element of which gives the corresponding
-%                distances between the input points and the n-th query point.
-%
-% Notes:
-%
-%     The KD-Tree is built each time this function is called, which is suboptimal when multiple
-%     queries of the same tree are performed. To build the tree once and query it multiple times,
-%     the function calls below should be used instead. Building a KD-Tree takes O(n*log(n)) time;
-%     range querying an already existing tree takes on the average O(n^(1-1/K) + m) time,
-%     where m = #reported points.
-
-% Deepak Chittajallu, Francois Aguet, 11/2012
-
-function [idx, dist] = KDTreeBallQuery(inPts, queryPts, radii)
-
-kdtreeobj = KDTree(inPts);
-
-numQueryPoints = size(queryPts,1);
-if isscalar(radii)
-    radii = radii + zeros(numQueryPoints,1);
-end
-
-idx = cell(numQueryPoints, 1);
-dist = cell(numQueryPoints, 1);
-for i = 1:numQueryPoints
-    [idx{i}, dist{i}] = kdtreeobj.ball( queryPts(i,:), radii(i) );
-    [dist{i}, si] = sort(dist{i});
-    idx{i} = idx{i}(si);
-end
