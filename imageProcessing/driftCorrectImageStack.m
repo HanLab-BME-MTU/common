2cea4ea7c6f4dd1e3511ae1d1fe84bf04812ce44
Hunter Elliott <hunter_elliott@hms.harvard.edu>
2013-07-25 20:46:16 +0000

    removed development plotting lines I forgot in driftCorrectImageStack.m
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10418 0bcb941b-74d1-0310-bb26-e6636707f451

--- imageProcessing/driftCorrectImageStack.m
+++ imageProcessing/driftCorrectImageStack.m
@@ -36,7 +36,6 @@ p = ip.Results;
 
 %% ----------- Init --------- %%
 
-ogClass = class(imStack);
 imSize = size(imStack);
 
 imStackCorr = imStack;
@@ -55,47 +54,29 @@ nCorr = numel(corrInd);
 
 [optimizer,metric] = imregconfig(p.Modality);
 
-showPlots = true;
-
 %% ----------- Correction --------- %%
 
 for j = 1:nCorr
        
     currRef = double(imStackCorr(:,:,refInd(j)));    
+    currCorr = double(imStack(:,:,corrInd(j)));
         
     %Get cross-orrelation between images to do an initial translation since imregister is slow (optimization
     %based)    
-    imXcorr = convnfft(currRef,rot90(imStack(:,:,corrInd(j)),2));
+    imXcorr = convnfft(currRef - mean(currRef(:)),rot90(currCorr,2)-mean(currCorr(:)));
     %Find max correlation and shift accordingly
     [~,iMax] = max(imXcorr(:));    
     [maxCorr(1), maxCorr(2)] = ind2sub(size(imXcorr),iMax);        
     [Xi,Yi] = meshgrid((1:imSize(2))+imSize(2)-maxCorr(2),(1:imSize(1))+imSize(1)-maxCorr(1));
-    currCorr = interp2(double(imStack(:,:,corrInd(j))),Xi,Yi,'nearest');%Lazy way to do shifting...
+    currCorr = interp2(currCorr,Xi,Yi,'nearest');%Lazy way to do shifting...
     currCorr(isnan(currCorr)) = p.FillValue;
-    
-    if showPlots
-        fsFigure(.75);
-        subplot(1,2,1)
-        imshow(cat(3,mat2gray(currRef),mat2gray(imStack(:,:,corrInd(j))),zeros(imSize(1:2))))        
-        subplot(1,2,2)
-        imshow(cat(3,mat2gray(currRef),mat2gray(currCorr),zeros(imSize(1:2))))        
-    end
-    
+       
     %Optionally improve the registration
     if p.DoRefinement
         imStackCorr(:,:,corrInd(j)) = imregister(currCorr,currRef,p.TransformType,optimizer,metric);
     else        
         imStackCorr(:,:,corrInd(j)) = currCorr;
     end
-    
-    if showPlots
-        fsFigure(.75);
-        subplot(1,2,1)
-        imshow(cat(3,mat2gray(currRef),mat2gray(currCorr),zeros(imSize(1:2))))        
-        subplot(1,2,2)
-        imshow(cat(3,mat2gray(currRef),mat2gray(imStackCorr(:,:,corrInd(j))),zeros(imSize(1:2))))
-    end
-    
     
 end
 
