aa49e900a8e2fa5da9b1b0f64444be29a936d082
Francois Aguet <francois_aguet@hms.harvard.edu>
2013-08-21 03:27:56 +0000

    Wrapper for fast TIFF stack loading
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10538 0bcb941b-74d1-0310-bb26-e6636707f451

---
+++ iofun/readtiff.m
@@ -0,0 +1,35 @@
+%readtiff() loads a tiff stack using libtiff
+% This is ~1.5-2x faster than imread, useful for large stacks
+
+% Francois Aguet, 05/21/2013
+
+function s = readtiff(filepath, range)
+
+w = warning('off', 'all'); % ignore unknown TIFF tags
+
+info = imfinfo(filepath);
+
+nx = info(1).Width;
+ny = info(1).Height;
+
+if nargin<2
+    N = numel(info);
+    range = 1:N;
+else
+    N = numel(range);
+end
+
+if info(1).BitDepth==16 && strcmpi(info(1).ColorType, 'grayscale')
+    s = zeros(ny,nx,N,'uint16');
+else
+    s = zeros(ny,nx,N);
+end
+
+t = Tiff(filepath, 'r');
+for i = 1:numel(range)
+   t.setDirectory(range(i));
+   s(:,:,i) = t.read();
+end
+t.close();
+
+warning(w);
