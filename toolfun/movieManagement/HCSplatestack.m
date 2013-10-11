0ef0c48ff4a492db316ea2f0c4caedf3d85a117a
Yichao Joy Xu <joyxuyichao@gmail.com>
2013-10-11 15:52:44 +0000

    clean up duplicated function.
    M    toolfun/movieManagement/HCSplatestack.m
    M    toolfun/movieManagement/HCSReader.m
    
    
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@10820 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/movieManagement/HCSplatestack.m
+++ toolfun/movieManagement/HCSplatestack.m
@@ -41,39 +41,3 @@ function renameSingledigitfiles(dirurl, file_lists, maxlength)
         movefile(strcat(dirurl, file_lists(i3).name), strcat(dirurl, file_lists(i3).name(1:ss), '0', file_lists(i3).name(ss+1:end)));
         end
     end
-
-
-function [starti, startsw] = getindexstart(filename)
-% % generate a-z string
-for n = 1:26
-    strr{n} = char(n -1 + 'a');
-end
-% generate 0-9 string
-for n = 1:10
-    stnum{n} = num2str(n-1);
-end
-index_char = regexp(filename, strr, 'ignorecase');
-startsw = []; starti = [];
-for i = 1:length(index_char) %it does not check the 'tif'
-    if ~isempty(index_char{i})
-        while ~isempty(index_char{i})
-            if index_char{i}(1)+3<length(filename)
-                if sum(strcmp(stnum, filename(index_char{i}(1)+1))) == 0
-                    index_char{i}(1) = [];
-                elseif sum(strcmp(stnum, filename(index_char{i}(1)+2))) > 0 ...
-                        && sum(strcmp(stnum, filename(index_char{i}(1)+3))) == 0 ...
-                        && isempty(starti)
-                    starti = index_char{i}(1);
-                elseif sum(strcmp({'s', 'w', 'S', 'W'}, filename(index_char{i}(1)))) > 0 ...
-                        && sum(strcmp(stnum, filename(index_char{i}(1)+1))) > 0
-                    startsw = [startsw index_char{i}(1)];
-                    break;
-                else
-                    index_char{i}(1) = [];
-                end
-            else
-                index_char{i}(1) = [];
-            end
-        end
-    end
-end
