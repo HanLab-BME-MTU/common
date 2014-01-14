896621d03251335f312562df7e708058d744009e
Francois Aguet <francois_aguet@hms.harvard.edu>
2014-01-14 23:16:15 +0000

    Cleaned up, help added
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@11021 0bcb941b-74d1-0310-bb26-e6636707f451

--- graphics/densityplot.m
+++ graphics/densityplot.m
@@ -1,48 +1,42 @@
+%[ha, dRange] = densityplot(x, y, varargin) generates a 2D histogram / density plot
+% from the input vectors
+%
+% Inputs:
+%          x : sample vector for the 1st dimension
+%          y : sample vector for the 2nd dimension
+%
+% Optional inputs:
+%         xv : bin centers for the 1st dimension
+%         yv : bin centers for the 2nd dimension
+%
+% Parameters ('specifier', value)
+%           'Parent' : handle of the parent axes
+%  'DisplayFunction' : intensity mapping, i.e., @sqrt
+
 % Francois Aguet, 02/19/2013
 
-function [ha2, dRange] = densityplot(x, y, varargin)
+function [ha, dRange] = densityplot(x, y, varargin)
 
 ip = inputParser;
 ip.CaseSensitive = false;
 ip.addOptional('xv', linspace(min(x), max(x), 100));
 ip.addOptional('yv', linspace(min(y), max(y), 100));
-ip.addParamValue('Handle', gca, @ishandle);
-ip.addParamValue('AdjustBorder', false, @islogical);
-ip.addParamValue('Div', 1, @isscalar);
-ip.addParamValue('NormX', false, @islogical);
-ip.addParamValue('DisplayFunction', @(x) x, @(x) isa(x, 'function_handle'));
+ip.addParameter('Parent', gca, @ishandle);
+ip.addParameter('Div', 1, @isscalar);
+ip.addParameter('NormX', false, @islogical);
+ip.addParameter('DisplayFunction', @(x) x, @(x) isa(x, 'function_handle'));
 ip.parse(varargin{:});
 xv = ip.Results.xv(:)';
 yv = ip.Results.yv(:)';
-ha = ip.Results.Handle;
+ha = ip.Results.Parent;
 
-XLim = xv([1 end]);
-YLim = yv([1 end]);
-dx = xv(2)-xv(1);
-dy = yv(2)-yv(1);
-if ip.Results.AdjustBorder % bug in eps files? png works fine
-    XLim = [xv(1)-dx/2 xv(end)+dx/2];
-    YLim = [yv(1)-dx/2 yv(end)+dy/2];
-end
-xv = [xv(1)-dx xv xv(end)+dx];
-yv = [yv(1)-dy yv yv(end)+dy];
 M = hist3([y(:) x(:)], {yv, xv});
 if ip.Results.NormX
     M = M./repmat(sum(M,1), [size(M,1) 1]);
-    %M = M./repmat(sum(M,2), [1 size(M,2)]);
 end
 
-% M = M(2:end-1,2:end-1);
 M = ip.Results.DisplayFunction(M/ip.Results.Div);
-% M(isinf(M)) = NaN;
 
 imagesc(xv, yv, M, 'Parent', ha);
-axis xy;
+axis(ha, 'xy', 'tight');
 dRange = [min(M(:)) max(M(:))];
-
-% re-plot axes on top to create box w/o ticks
-% ha2 = copyobj(ha, get(ha, 'Parent'));
-% set(ha2, 'XTick', [], 'YTick', [], 'Color', 'b', 'Box', 'on');
-axis([ha ], [XLim YLim]);
-% set(gcf, 'CurrentAxes', ha);% doesn't work
-ha2 = [];
