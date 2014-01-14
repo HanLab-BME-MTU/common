4bee029860248f9c592e4a78fee1076e955b8c36
Francois Aguet <francois_aguet@hms.harvard.edu>
2014-01-14 23:35:51 +0000

    Added optional waitbar
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@11024 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/readtiff.m
+++ iofun/readtiff.m
@@ -3,23 +3,26 @@
 
 % Francois Aguet, 05/21/2013
 
-function s = readtiff(filepath, range, info)
+function s = readtiff(filepath, varargin)
 
-w = warning('off', 'all'); % ignore unknown TIFF tags
-
-if nargin<3 || isempty(info)
-    info = imfinfo(filepath);
-end
-
-nx = info(1).Width;
-ny = info(1).Height;
-
-if nargin<2
+ip = inputParser;
+ip.addOptional('range', []);
+ip.addOptional('info', imfinfo(filepath));
+ip.addParamValue('ShowWaitbar', false, @islogical);
+ip.parse(varargin{:});
+info = ip.Results.info;
+range = ip.Results.range;
+if isempty(range)
     N = numel(info);
     range = 1:N;
 else
     N = numel(range);
 end
+
+w = warning('off', 'all'); % ignore unknown TIFF tags
+
+nx = info(1).Width;
+ny = info(1).Height;
 
 if info(1).BitDepth==16 && strcmpi(info(1).ColorType, 'grayscale')
     s = zeros(ny,nx,N,'uint16');
@@ -28,9 +31,20 @@ else
 end
 
 t = Tiff(filepath, 'r');
-for i = 1:numel(range)
-   t.setDirectory(range(i));
-   s(:,:,i) = t.read();
+if ~ip.Results.ShowWaitbar
+    for i = 1:numel(range)
+        t.setDirectory(range(i));
+        s(:,:,i) = t.read();
+    end
+else
+    [~,fname] = fileparts(filepath);
+    h = waitbar(0, ['Loading ' fname]);
+    for i = 1:numel(range)
+        t.setDirectory(range(i));
+        s(:,:,i) = t.read();
+        waitbar(i/numel(range))
+    end
+    close(h);
 end
 t.close();
 
