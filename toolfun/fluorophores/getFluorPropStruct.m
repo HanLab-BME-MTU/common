396d3a02e247eae6e52551bbcb93e64d494fd629
Francois Aguet <francois_aguet@hms.harvard.edu>
2014-02-21 00:23:53 +0000

    Added mruby2
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@11092 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/fluorophores/getFluorPropStruct.m
+++ toolfun/fluorophores/getFluorPropStruct.m
@@ -55,6 +55,8 @@ s(24).name = 'meos';
 s(24).lambda_em = 563e-9;
 s(25).name = 'turborfp';
 s(25).lambda_em = 574e-9;
+s(26).name = 'mruby2';
+s(26).lambda_em = 600e-9;
 
 [~,i] = sort([s.lambda_em]);
 s = s(i);
