4b63cf7008db59bb4f74b81f8b44c47bf4ed7e1b
Francois Aguet <francois_aguet@hms.harvard.edu>
2014-03-12 18:02:29 +0000

    Added multi-channel option
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@11134 0bcb941b-74d1-0310-bb26-e6636707f451

--- iofun/movies/makeMovieFFMPEG.m
+++ iofun/movies/makeMovieFFMPEG.m
@@ -28,27 +28,49 @@ ip.addParamValue('DynamicRange', []);
 ip.addParamValue('Scale', 1, @isscalar);
 ip.parse(stack, varargin{:});
 
-nf = size(stack, 3);
-fmt = ['%0' num2str(ceil(log10(nf))) 'd'];
 
 if isunix && ~system('which ffmpeg >/dev/null 2>&1')
     fprintf('Generating movie ... ');
     frameDest = ['.frames_tmp_mmffmpeg' filesep];
     [~,~] = mkdir(frameDest);
-    dRange = ip.Results.DynamicRange;
-    if isempty(dRange)
-        dRange = double([min(stack(:)) max(stack(:))]);
-    end
     
-    for fi = 1:nf
-        frame = scaleContrast(stack(:,:,fi), dRange);
-        if ip.Results.Scale ~= 1
-           frame = imresize(frame, ip.Results.Scale, 'nearest'); 
+    
+    if ~iscell(stack) % single channel
+        dRange = ip.Results.DynamicRange;
+        if isempty(dRange)
+            dRange = double([min(stack(:)) max(stack(:))]);
         end
-        imwrite(uint8(frame), [frameDest 'frame_' num2str(fi, fmt), '.png']);
+        
+        nf = size(stack, 3);
+        fmt = ['%0' num2str(ceil(log10(nf))) 'd'];
+        for fi = 1:nf
+            frame = scaleContrast(stack(:,:,fi), dRange);
+            if ip.Results.Scale ~= 1
+                frame = imresize(frame, ip.Results.Scale, 'nearest');
+            end
+            imwrite(uint8(frame), [frameDest 'frame_' num2str(fi, fmt), '.png']);
+        end
+    else
+        dRange = ip.Results.DynamicRange;
+        if isempty(dRange)
+            dRange = {double([min(stack{1}(:)) max(stack{1}(:))]),...
+                double([min(stack{2}(:)) max(stack{2}(:))])};
+        end
+        
+        nf = size(stack{1}, 3);
+        fmt = ['%0' num2str(ceil(log10(nf))) 'd'];
+        for fi = 1:nf
+            frame1 = scaleContrast(stack{1}(:,:,fi), dRange{1});
+            frame2 = scaleContrast(stack{2}(:,:,fi), dRange{2});
+            frame = cat(3, frame1, frame2, zeros(size(frame1)));
+            if ip.Results.Scale ~= 1
+                frame = imresize(frame, ip.Results.Scale, 'nearest');
+            end
+            imwrite(uint8(frame), [frameDest 'frame_' num2str(fi, fmt), '.png']);
+        end
     end
-    [ny,nx] = size(frame);
     
+    [ny,nx,~] = size(frame);
     fr = num2str(ip.Results.framerate);
     
     cmd = ['ffmpeg -y -r ' fr ' -i ' frameDest 'frame_' fmt '.png' ' -vf "scale=' num2str(2*floor(nx/2)) ':' num2str(2*floor(ny/2))...
