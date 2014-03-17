b9cc068d8553230b206decc0c1e65aa1e5631c7c
Sebastien Besson <sebastien_besson@hms.harvard.edu>
2014-03-17 20:18:49 +0000

    Refactor channel unit tests to inherit from TestLibrary
    git-svn-id: https://orchestra.med.harvard.edu/svn/lccb/matlab/projects/common/main@11169 0bcb941b-74d1-0310-bb26-e6636707f451

--- toolfun/movieManagement/test/unit/TestChannel.m
+++ toolfun/movieManagement/test/unit/TestChannel.m
@@ -1,7 +1,6 @@
-classdef TestChannelProperties < TestCase
+classdef TestChannel < TestLibrary & TestCase
     
     properties
-        channel
         emissionWavelength = 500;
         excitationWavelength = 600
         imageTypes = Channel.getImagingModes()
@@ -10,78 +9,77 @@ classdef TestChannelProperties < TestCase
     end
     
     methods
-        function self = TestChannelProperties(name)
+        function self = TestChannel(name)
             self = self@TestCase(name);
         end
         
         function setUp(self)
-            self.channel = Channel();
+            self.setUpChannels(1);
         end
         
         function tearDown(self)
-            delete(self.channel);
+            tearDown@TestLibrary(self);
         end
         
         %% Class test
         function testClass(self)
-            assertTrue(isa(self.channel,'Channel'));
+            assertTrue(isa(self.channels(1),'Channel'));
         end
         
         %% Individual property tests
         function testSetValidEmissionWavelength(self)
-            self.channel.emissionWavelength_ = self.emissionWavelength;
-            assertEqual(self.channel.emissionWavelength_, self.emissionWavelength);
+            self.channels(1).emissionWavelength_ = self.emissionWavelength;
+            assertEqual(self.channels(1).emissionWavelength_, self.emissionWavelength);
         end
         
         function testSetInvalidEmissionWavelength(self)
-            f= @() set(self.channel, 'emissionWavelength_', 0);
+            f= @() set(self.channels(1), 'emissionWavelength_', 0);
             assertExceptionThrown(f,'lccb:set:invalid');
         end
         
         function testSetValidExcitationWavelength(self)
-            self.channel.excitationWavelength_ = self.excitationWavelength;
-            assertEqual(self.channel.excitationWavelength_, self.excitationWavelength);
+            self.channels(1).excitationWavelength_ = self.excitationWavelength;
+            assertEqual(self.channels(1).excitationWavelength_, self.excitationWavelength);
         end
         
         function testSetInvalidExcitationWavelength(self)
-            f= @() set(self.channel, 'excitationWavelength_', 0);
+            f= @() set(self.channels(1), 'excitationWavelength_', 0);
             assertExceptionThrown(f,'lccb:set:invalid');
         end
         
         function testSetValidExposureTime(self)
-            self.channel.exposureTime_ = self.exposureTime;
-            assertEqual(self.channel.exposureTime_, self.exposureTime);
+            self.channels(1).exposureTime_ = self.exposureTime;
+            assertEqual(self.channels(1).exposureTime_, self.exposureTime);
         end
         
         function testSetInvalidExposureTime(self)
-            f= @() set(self.channel, 'exposureTime_', 0);
+            f= @() set(self.channels(1), 'exposureTime_', 0);
             assertExceptionThrown(f,'lccb:set:invalid');
         end
         function testSetValidiImageType(self)
+            self.setUpChannels(numel(self.imageTypes))
             for i = 1 : numel(self.imageTypes)
-                self.channel = Channel();
-                self.channel.imageType_ = self.imageTypes{i};
-                assertEqual(self.channel.imageType_, self.imageTypes{i});
+                self.channels(i).imageType_ = self.imageTypes{i};
+                assertEqual(self.channels(i).imageType_, self.imageTypes{i});
             end
         end
         
         function testSetInvalidImageType(self)
-            f= @() set(self.channel, 'imageType_', 0);
+            f= @() set(self.channels(1), 'imageType_', 0);
             assertExceptionThrown(f,'lccb:set:invalid');
         end
         
         function testSetValidFlurophores(self)
+            self.setUpChannels(numel(self.fluorophores))
             for i = 1 : numel(self.fluorophores)
-                self.channel = Channel();
-                self.channel.fluorophore_ = self.fluorophores{i};
-                assertEqual(self.channel.fluorophore_, self.fluorophores{i});
+                self.channels(i).fluorophore_ = self.fluorophores{i};
+                assertEqual(self.channels(i).fluorophore_, self.fluorophores{i});
             end
         end
         
         function testSetInvalidFlurophore(self)
-            f= @() set(self.channel, 'fluorophore_', 0);
+            f= @() set(self.channels(1), 'fluorophore_', 0);
             assertExceptionThrown(f,'lccb:set:invalid');
         end
-        
     end
 end
--- toolfun/movieManagement/test/unit/TestLibrary.m
+++ toolfun/movieManagement/test/unit/TestLibrary.m
@@ -3,27 +3,37 @@ classdef TestLibrary < handle
     %   Detailed explanation goes here
     
     properties
-        movie
+        channels = Channel.empty(1, 0)
+        movie = MovieData.empty(1, 0)
         nRois
-    end    
+    end
     
     methods
         
         function tearDown(self)
-            cellfun(@delete, self.movie.processes_);
-            cellfun(@delete, self.movie.packages_);
-            delete(self.movie);
+            arrayfun(@delete, self.channels);
+            if ~isempty(self.movie)
+                cellfun(@delete, self.movie.processes_);
+                cellfun(@delete, self.movie.packages_);
+                delete(self.movie);
+            end
         end
         
         function setUpMovieData(self, nchannels)
             if nargin > 1 && nchannels > 0
-                channels(nchannels, 1) = Channel();
-                self.movie = MovieData(channels, '');
+                self.setUpChannels(nchannels);
+                self.movie = MovieData(self.channels, '');
             else
                 self.movie = MovieData();
             end
         end
         
+        function setUpChannels(self, nChannels)
+            if nargin < 2, nChannels = 1; end
+            for i = 1 : nChannels
+                self.channels(i) = Channel(num2str(i));
+            end
+        end
         
         function setUpMovieList(self)
             self.movie = MovieList();
