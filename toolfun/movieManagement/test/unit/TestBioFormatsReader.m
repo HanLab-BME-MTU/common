0cedb615b4537e9955ca95e517b35ed4be3dfb06
Sebastien Besson <seb.besson@gmail.com>
2014-08-15 18:59:55 +0100

    Test getCurrentFile() as well as multi-series closing/reinitializaing

--- toolfun/movieManagement/test/unit/TestBioFormatsReader.m
+++ toolfun/movieManagement/test/unit/TestBioFormatsReader.m
@@ -54,12 +54,14 @@ classdef TestBioFormatsReader < TestCase
             self.reader = BioFormatsReader(self.id);
             assertTrue(isa(self.reader.getReader(),...
                 'loci.formats.IFormatReader'));
+            assertEqual(char(self.reader.formatReader.getCurrentFile()), self.id);
         end
         
         function testGetReaderInit(self)
             r = bfGetReader(self.id);
             self.reader = BioFormatsReader(self.id, 'reader', r);
             assertEqual(self.reader.getReader(), r);
+            assertEqual(char(self.reader.formatReader.getCurrentFile()), self.id);
         end
         
         function testGetReaderMultiSeries(self)
@@ -77,6 +79,8 @@ classdef TestBioFormatsReader < TestCase
             r = bfGetReader(self.id);
             self.reader = BioFormatsReader(self.id, 0, 'reader', r);
             self.reader(2) = BioFormatsReader(self.id, 1, 'reader', r);
+            assertEqual(char(self.reader(1).formatReader.getCurrentFile()), self.id);
+            assertEqual(char(self.reader(2).formatReader.getCurrentFile()), self.id);
             assertEqual(self.reader(1).formatReader.getSeries(), 0);
             assertEqual(self.reader(2).formatReader.getSeries(), 0);
             assertEqual(self.reader(1).getReader().getSeries(), 0);
@@ -87,9 +91,24 @@ classdef TestBioFormatsReader < TestCase
             self.reader = BioFormatsReader(self.id);
             assertTrue(isa(self.reader.getReader(),...
                 'loci.formats.IFormatReader'));
-            self.reader.formatReader.close()
-            assertTrue(isa(self.reader.getReader(),...
-                'loci.formats.IFormatReader'));
+            assertEqual(char(self.reader.formatReader.getCurrentFile()), self.id);
+            self.reader.formatReader.close();
+            assertTrue(isempty(self.reader.formatReader.getCurrentFile()));
+            self.reader.getReader();
+            assertEqual(char(self.reader.formatReader.getCurrentFile()), self.id);
+        end
+        
+        function testGetReaderMultiSeriesClose(self)
+            self.id = 'test&series=2.fake';
+            r = bfGetReader(self.id);
+            self.reader = BioFormatsReader(self.id, 0, 'reader', r);
+            self.reader(2) = BioFormatsReader(self.id, 1, 'reader', r);
+            self.reader(2).formatReader.close();
+            assertTrue(isempty(self.reader(1).formatReader.getCurrentFile()));
+            assertTrue(isempty(self.reader(2).formatReader.getCurrentFile()));
+            self.reader(1).getReader();
+            assertEqual(char(self.reader(1).formatReader.getCurrentFile()), self.id);
+            assertEqual(char(self.reader(2).formatReader.getCurrentFile()), self.id);
         end
     end
 end
