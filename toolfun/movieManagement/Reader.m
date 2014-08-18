fba89c6d3c4d50f77db04df8ab4212683e71e4f5
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2014-08-18 14:16:31 -0500

    Changed t to be a required parameter, as a more conservative change Replaced 'like' syntax with more backwards compatible with syntax Separated out the input validation and implementation for loadStack Created an loadImage_ stub for future interface changes

--- toolfun/movieManagement/Reader.m
+++ toolfun/movieManagement/Reader.m
@@ -22,8 +22,50 @@ classdef  Reader < handle
         loadImage(obj)
     end
     
-    methods
-        % loadStack
+    methods ( Access = public )
+        
+        function I = loadStack(obj, c, t, varargin)
+        % loadStack reads a Z-stack and returns a YXZ Matrix
+        %
+        %   loadStack(c,t,Z) returns a Z-stack for channel c and
+        %   time frame t  with Z planes indicated by the vector Z
+        %
+        %   loadStack(c,t) returns loadStack(c,t, 1: getSizeZ(c) )
+        %
+        % output: a 3D matrix with dimensions YXZ
+        %
+        % Note: Override only if need you need to overload or change how
+        % the input is checked. Otherwise override loadStack_.
+        %
+        % Example:
+        %   reader = movieData.getReader();
+        %   % The following are all equivalent
+        %   zStackMatrix = reader.loadStack(1,1,1:reader.getSizeZ(c));
+        %   zStackMatrix = reader.loadStack(1,1);
+        %   zStackMatrix = reader.loadStack(1);
+        %
+       
+            % Input check
+            ip = inputParser;
+            ip.addRequired('c', ...
+                @(x) isscalar(x) && ismember(x, 1 : obj.getSizeC() ) );
+            ip.addRequired('t', ...
+                @(x) isscalar(x) && ismember(x, 1 : obj.getSizeT(c) ) );
+            % Parse c first for validation check
+            %  since getSizeT and getSizeZ may depend on a valid c.
+            ip.parse(c, t);
+            ip.addOptional('z', 1 : obj.getSizeZ(c), ...
+                @(x) all(ismember(x, 1 : obj.getSizeZ(c) ) ) );
+            ip.parse(c, varargin{:});
+            t = ip.Results.t;
+            z = ip.Results.z;
+            
+            I = obj.loadStack_(c,t,z);
+        end
+    end
+    methods( Access = protected )
+        function I = loadStack_(obj, c, t, z)
+        % loadStack_ is a validation-free version called by loadStack
         %
         % Provides a generic implementation of loadStack.
         % Guarantees all readers will have a loadStack method if they have
@@ -36,33 +78,29 @@ classdef  Reader < handle
         % t defaults to 1
         % z defaults to 1:sizeZ
         %
-        % output a 3D matrix, YXZ
-        function I = loadStack(obj, c, varargin)
-            % Input check
-            ip = inputParser;
-            ip.addRequired('c', ...
-                @(x) isscalar(x) && ismember(x, 1 : obj.getSizeC() ) );
-            % Parse c first for validation check
-            %  since getSizeT and getSizeZ may depend on a valid c.
-            ip.parse(c);
-            ip.addOptional('t', 1, ...
-                @(x) isscalar(x) && ismember(x, 1 : obj.getSizeT(c) ) );
-            ip.addOptional('z', 1 : obj.getSizeZ(c), ...
-                @(x) all(ismember(x, 1 : obj.getSizeZ(c) ) ) );
-            ip.parse(c, varargin{:});
-            t = ip.Results.t;
-            z = ip.Results.z;
-
+        
             % Load first image to get class and dimensions.
-            first = obj.loadImage( c , t(1) , z(1) );
+            first = obj.loadImage_( c , t , z(1) );
             % "I" will be a YXZ matrix.
-            I = zeros( [ size(first) length(z) ] , 'like', first);
+            I = zeros( [ size(first) length(z) ] , class(first));
             I(:,:,1) = first;
             
             for zi = 2:length(z)
-                I(:,:,zi) = obj.loadImage( c , t(1) , z(zi) );
+                I(:,:,zi) = obj.loadImage_( c , t , z(zi) );
             end
                 
+        end
+    %end
+    %methods( Abstract, Access = protected )
+        % I = loadImage_(obj, c, t, z )
+        function I = loadImage_(obj, c, t, z )
+            % loadImage_ is a validation-free implementation called by
+            %   loadImage (or will be in the near future)
+            %
+            % Forward to validation version for now.
+            % This WILL be an abstract method in the future.
+
+            I = obj.loadImage(c,t,z);
         end
     end
     
