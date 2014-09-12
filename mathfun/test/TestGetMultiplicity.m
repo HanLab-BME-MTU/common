a00dd939d4fa928d93a1074b4e10921907583047
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2014-09-11 21:26:58 -0500

    Add tests for getMultiplicity and getMultiplicityInt

---
+++ mathfun/test/TestGetMultiplicity.m
@@ -0,0 +1,59 @@
+classdef TestGetMultiplicity < TestCase
+    properties
+        M
+        fcn
+        rep
+        udata
+        sdata
+    end
+    methods
+        function self = TestGetMultiplicity(varargin)
+            self = self@TestCase(varargin{:});
+            self.fcn = @getMultiplicity;
+        end
+        function setUp(self)
+            self.M = randi(intmax('uint16'),1e6,1);
+            self.udata = unique(self.M)';
+            self.sdata = sort(self.M)';
+            self.rep = histc(self.M, self.udata)';
+        end
+        function checkOutput(self,rep,udata,sdata)
+            assertEqual(rep, self.rep);
+            if(nargin > 2)
+                assertEqual(udata, self.udata);
+            end
+            if(nargin > 3)
+                assertEqual(sdata, self.sdata);
+            end
+        end
+        function testSingleOutput(self)
+            self.checkOutput(self.fcn(self.M));
+        end
+        function testDoubleOutput(self)
+            [rep, udata] = self.fcn(self.M);
+            self.checkOutput(rep,udata);
+        end
+        function testTripleOutput(self)
+            [rep, udata, sdata] = self.fcn(self.M);
+            self.checkOutput(rep,udata,sdata);
+        end
+        function testMatrixInput(self)
+            [rep, udata, sdata] = self.fcn(reshape(self.M,1e3,1e3));
+            self.checkOutput(rep,udata,sdata);
+        end
+        function testNonFiniteInput(self)
+            M = [NaN; Inf; self.M; Inf; NaN]; 
+            [rep, udata, sdata] = self.fcn(M);
+            self.checkOutput(rep,udata,sdata);
+        end
+        function testUint16Input(self)
+            [rep, udata, sdata] = self.fcn(uint16(self.M));
+            assertEqual( class(udata) , 'uint16');
+            assertEqual( class(sdata) , 'uint16');
+            self.checkOutput(rep,double(udata),double(sdata));
+        end
+        function tearDown(self)
+            self.M = [];
+        end
+    end
+end
