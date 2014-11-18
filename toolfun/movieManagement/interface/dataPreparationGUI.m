9c41b34de8c88ce5cc9daf838ce29e42158003d6
Assaf <azaritsky@biohpcws021.biohpc.swmed.edu>
2014-11-18 16:17:39 -0600

    fixed userData for dataPreparationGUI

--- toolfun/movieManagement/interface/dataPreparationGUI.m
+++ toolfun/movieManagement/interface/dataPreparationGUI.m
@@ -69,6 +69,7 @@ set(handles.text_copyright, 'String', getLCCBCopyright())
 handles.output = hObject;
 
 userData = get(handles.figure1, 'UserData');
+if isempty(userData), userData = struct(); end
 
 % Initialize the userData
 userData.projectDir = pwd;
@@ -592,6 +593,8 @@ function pushbutton_done_Callback(~, eventdata, handles)
 userData = get(handles.figure1, 'UserData');
 
 % Check all movies and channels have been attributed
+if isempty(userData.rawData), return; end
+    
 checkMovies = any(cellfun(@isempty,{userData.rawData.movieIndx}));
 if checkMovies,
     errordlg('All components have not been affected to a movie','modal');
