335e3535fadd77ad14d52d7e73fd7a2a465d26f1
Tae Kim <s154073@biohpcwsb35.biohpc.swmed.edu>
2015-07-27 14:52:41 -0500

    got rid of rest option, instead of error displays warning when layer is 0

--- toolfun/multipleProgressText.m
+++ toolfun/multipleProgressText.m
@@ -6,8 +6,6 @@ function [out] = multipleProgressText(text_, nStep_)
 %       Initializes the progressText
 %   [] = multipleProgressText(text_)
 %       Changes the optional text and updates progressText
-%   [] = multipleProgressText('reset')
-%       Resets the program
 %   [] = multipleProgressText()
 %       Updates progressText
 %
@@ -26,63 +24,76 @@ function [out] = multipleProgressText(text_, nStep_)
 
 %% Initialization
 %persistent
-persistent text iStep nStep frac weight level
+persistent text iStep nStep frac weight level warned
 if isempty(level)
     level = 0;
+end
+if isempty(text)
+    text = {};
+end
+if isempty(warned)
+    warned = false;
 end
 
 %% Input
 if nargin == 0
     iStep(level) = iStep(level) + 1;
 end
-if nargin == 1 && level == 1
+if nargin == 1
     iStep(level) = iStep(level) + 1;
-    text = text_;
-end
-if nargin == 1 && strcmp(text_, 'reset')
-    level = 0;
-    iStep = [];
-    nStep = [];
-    frac = [];
+    text{level} = text_;
 end
 if nargin == 2
     level = level + 1;
     iStep(level) = 0;
     nStep(level) = nStep_;
-    weight(level, 1) = 1 / prod(nStep(2:end));
     if level == 1
-        text = text_;
+        weight(1, 1) = 1;
+    else
+        weight(level, 1) = weight(level-1, 1) / nStep(level - 1);
     end
-end
-%input check
-%throws error if level < 1;
-if level < 1
-    error('Progress display error: number of steps do not match the number times the program was called -or- program was never initialized');
+    text{level} = text_;
 end
 
-%% Fraction calculation
-frac(level) = iStep(level) / nStep(level);
+if level > 0
+    %% Fraction calculation
+    frac(level) = iStep(level) / nStep(level);
 
-%% Progress Display
-if iStep(1) < nStep(1) && iStep(level) ~= nStep(level) && nargin ~= 2
-    progressText(frac * weight, text);
-elseif iStep(1) == nStep(1)
-    progressText(1, text);
+    %% Progress Display
+    if iStep(1) < nStep(1) && iStep(level) ~= nStep(level) && nargin ~= 2
+        progressText(frac * weight, text);
+    elseif iStep(1) == nStep(1)
+        progressText(1, text);
+    end
+
+    %% level check
+    if iStep(level) == nStep(level)
+        level = level - 1;
+        frac = frac(1:end-1);
+        weight = weight(1:end-1);
+        text = text(1:end-1);
+        iStep = iStep(1:end-1);
+        nStep = nStep(1:end-1);
+    end
+
+    %% Output
+    if nargout > 0
+        out.level = level;
+        out.iStep = iStep;
+        out.nStep = nStep;
+    end
+elseif ~warned
+    warned = true;
+    warning('Level 0 reached. The progressText maybe inaccurate');
 end
 
-%% level check
-if iStep(level) == nStep(level)
-    level = level - 1;
-    frac = frac(1:end-1);
-    weight = weight(1:end-1);
 end
 
-%% Output
-if nargout > 0
-    out.level = level;
-    out.iStep = iStep;
-    out.nStep = nStep;
-end
-
+%% Local function
+%generates fullText
+function [fullText] = getFullText(text)
+text = cellfun(@(x) [x ': '], text, 'UniformOutput', false);
+fullText = [text{:}];
+fullText = fullText(1:end-2);
 end
 
