a06ee65b7e89ccfa0e13c221ec943230f889e692
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2015-12-04 10:43:27 -0600

    parcellfun_progress: Create a discrete defaultDisplayFunction for progress and allow DisplayFunction to be defined. May have an error handling bug.

--- toolfun/parcellfun_progress.m
+++ toolfun/parcellfun_progress.m
@@ -11,78 +11,102 @@ function [ varargout ] = parcellfun_progress( func, varargin )
     ip.addParameter('UniformOutput',true,@islogical);
     ip.addParameter('ErrorHandler',@rethrow,@(x) isa(x,'function_handle'));
     ip.addParameter('UpdateInterval',1,@(x) validateattributes(x,{'numeric'},{'scalar'}));
+    ip.addParameter('DisplayFunc',@defaultDisplayFunc,@(x) isa(x,'function_handle'));
 
     % Find first non-cell input to begin parsing parameters
     for inIdx = 1:length(varargin)
        if(~iscell(varargin{inIdx}))
+            paramIdx = inIdx;
             break;
-       end 
+       elseif(inIdx == length(varargin))
+           paramIdx = inIdx + 1;
+       end
     end
     
-    assert(inIdx ~= 1,'parcellfun_progress:NotACell','Input #2 expected to be a cell array.');
+    if(paramIdx == 1)
+        error('parcellfun_progress:NotACell', ...
+         'Input #2 expected to be a cell array.');
+    end
 
-    ip.parse(varargin{inIdx:end});
+    ip.parse(varargin{paramIdx:end});
     in = ip.Results;
 
-    
-    
-    F = cellfun(@parfunc,varargin{:},'UniformOutput',false);
-    F = [F{:}];
-    
-    nRuns = length(F);
+    inSize = size(varargin{1});
+    nRuns = numel(varargin{1});
     nCompleted = 0;
     out = cell(nRuns,nout);
+    notComplete = true(nRuns,1);
     
-    fprintf('Parallel Cell Function:');
-    
-    nProgressOut = fprintf(' %g / %g Estimating time ...\n',0,nRuns);
-    backspaceChar = '\b';
+    F = cellfun(@parfunc,varargin{1:paramIdx-1},'UniformOutput',false);
+    F = [F{:}];
+    cleanup = onCleanup(@()  cancel(F));
     
     startDT = datetime('now');
-    
-    updateInterval = 1;
-    
-%     for idx = 1:nRuns
+    nRunsTime = startDT;
+    nProgressOut = in.DisplayFunc(nRuns,0,startDT,NaN,0);
+        
     while(nCompleted < nRuns)
+        completedIdx = [];
         currentOut = cell(1,nout);
-        [completedIdx,currentOut{:}] = fetchNext(F,updateInterval);
+        try
+            [completedIdx,currentOut{:}] = fetchNext(F,in.UpdateInterval);
+        catch err
+%             disp(err);
+            nCompleted = nCompleted + 1;
+            completedIdx = find([F.Read] & notComplete',1);
+            disp(completedIdx);
+            [currentOut{:}] = in.ErrorHandler(err);
+        end
         if(~isempty(completedIdx))
+            notComplete(completedIdx) = false;
             out(completedIdx,:) = currentOut;
             nCompleted = nCompleted + 1;
+            nRunsTime = datetime('now');
         end
-        bp = repmat(backspaceChar,1,nProgressOut);
-%         bp = '';
-        nowDT = datetime('now');
-        sinceStart = nowDT - startDT;
-        estDuration = sinceStart*nRuns/nCompleted;
-        remaining = estDuration - sinceStart;
-        nProgressOut = fprintf([bp ' %g / %g\n Total %02g:%02g:%02g -  Elapsed %02g:%02g:%02g = Remaining %02g:%02g:%02g\n'],...
-            nCompleted,nRuns, ...
-            floor(hours(estDuration)), ...
-            floor(minutes(estDuration)), ...
-            ceil(seconds(estDuration)), ...
-            floor(hours(sinceStart)), ...
-            floor(minutes(sinceStart)), ...
-            ceil(seconds(sinceStart)), ...
-            floor(hours(remaining)), ...
-            floor(minutes(remaining)), ...
-            ceil(seconds(remaining)) ...
-            ) - nProgressOut;
+        nProgressOut = in.DisplayFunc(nRuns,nCompleted,startDT,nRunsTime,nProgressOut);
     end
+%     failed = ~cellfun(@isempty,{F.Error});
+%     out(failed,:) = arrayfun(in.ErrorHandler,F(failed),'UniformOutput',false);
     
     % Convert 2D cell array into a 1D cell array of cells
     varargout = num2cell(out,1);
     if(in.UniformOutput)
         varargout = cellfun(@cell2mat,varargout,'UniformOutput',false);
         nArgOut = cellfun('prodofsize',varargout);
-        assert(numel(nArgOut) == 1 || all(nArgOut(1) == nArgOut(2:end)), ...
-            'parcellfun_progress:NonUniformOutput','Non-uniform output with UniformOutput set to true');
+        assert(numel(nArgOut) == 1 && all(nArgOut(1) == nArgOut(2:end)), ...
+            'parcellfun_progress:NonUniformOutput', ...
+            'Non-uniform output with UniformOutput set to true');
     end
+    % Make output reflect size of input
+    varargout = cellfun(@(out) reshape(out,inSize),varargout,'UniformOutput',false);
         
-
+    % Wrap func in parfeval for evaluation
     function F = parfunc(varargin)
         F = parfeval(func,nout,varargin{:});
     end
 
+end
+function nProgressOut = defaultDisplayFunc(nRuns,nCompleted,startDT,nRunsTime,nProgressOut)
+bp = repmat('\b',1,nProgressOut);
+    nowDT = datetime('now');
+    sinceStart = nowDT - startDT;
+    if(~nCompleted)
+        estDuration = 'xx:xx:xx';
+        remaining = 'xx:xx:xx';
+    else
+        estDuration = (nRunsTime-startDT)*nRuns/nCompleted;
+        remaining = estDuration - sinceStart;
+    end
+    nProgressOut = fprintf([bp ...
+        ' %g / %g\n' ...
+        '  Estimated %s\n' ...
+        '-   Elapsed %s\n' ...
+        '---------------------\n' ...
+        '= Remaining %s\n'],...
+        nCompleted,nRuns, ...
+        char(estDuration), ...
+        char(sinceStart), ...
+        char(remaining) ...
+        ) - nProgressOut;
 end
 
