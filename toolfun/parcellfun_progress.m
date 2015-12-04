f44de54ffc5aa2ec636a07a0fa74a1cf7311a4eb
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2015-12-04 16:58:52 -0600

    parcellfun_progress: Added cleanup, pool parameter, improved display, fixed error handler bug

--- toolfun/parcellfun_progress.m
+++ toolfun/parcellfun_progress.m
@@ -12,6 +12,7 @@ function [ varargout ] = parcellfun_progress( func, varargin )
     ip.addParameter('ErrorHandler',@rethrow,@(x) isa(x,'function_handle'));
     ip.addParameter('UpdateInterval',1,@(x) validateattributes(x,{'numeric'},{'scalar'}));
     ip.addParameter('DisplayFunc',@defaultDisplayFunc,@(x) isa(x,'function_handle'));
+    ip.addParameter('ParallelPool',[]);
 
     % Find first non-cell input to begin parsing parameters
     for inIdx = 1:length(varargin)
@@ -28,45 +29,73 @@ function [ varargout ] = parcellfun_progress( func, varargin )
          'Input #2 expected to be a cell array.');
     end
 
+    % Parse input parameters
     ip.parse(varargin{paramIdx:end});
     in = ip.Results;
+    
+    % Initialize parallel pool
+    if(isempty(in.ParallelPool))
+        in.ParallelPool = gcp;
+    end
 
+    % Initialize variables
+    % Size of cell input
     inSize = size(varargin{1});
+    % Number of elements in cell input (number of parallel tasks)
     nRuns = numel(varargin{1});
-    nCompleted = 0;
+    % Cell array to hold all output
     out = cell(nRuns,nout);
+    % Logical array to mark which jobs have completed
     notComplete = true(nRuns,1);
     
+    % Start parallel execution
+    startDT = datetime('now');
     F = cellfun(@parfunc,varargin{1:paramIdx-1},'UniformOutput',false);
     F = [F{:}];
-    cleanup = onCleanup(@()  cancel(F));
     
-    startDT = datetime('now');
-    nRunsTime = startDT;
+    % When this function completes, user quits, or exception occurs,
+    % cleanup
+    cleanup = onCleanup(@()  cancel(F));
+
+    % Number of workers completed
+    nCompleted = 0;
+    % Time it took to complete nRuns
+    nCompletedTime = startDT;
+    
+    % Number of bytes output by progress, for backspacing
     nProgressOut = in.DisplayFunc(nRuns,0,startDT,NaN,0);
-        
+    
+    % We will not update the estimate until we have processed all finished
+    % workers
+    last_nCompleted = nCompleted;
+    last_nRunsTime = nCompletedTime;
+   
+    
+    % Main output checking loop
     while(nCompleted < nRuns)
-        completedIdx = [];
-        currentOut = cell(1,nout);
-        try
-            [completedIdx,currentOut{:}] = fetchNext(F,in.UpdateInterval);
-        catch err
-%             disp(err);
-            nCompleted = nCompleted + 1;
-            completedIdx = find([F.Read] & notComplete',1);
-            disp(completedIdx);
-            [currentOut{:}] = in.ErrorHandler(err);
+        completedIdx = -1;
+        % Process finished workers available at the moment
+        while(~isempty(completedIdx))
+            currentOut = cell(1,nout);
+            try
+                [completedIdx,currentOut{:}] = fetchNext(F,1);
+            catch err
+                completedIdx = find([F.Read] & notComplete',1);
+                [currentOut{:}] = in.ErrorHandler(err);
+            end
+            if(~isempty(completedIdx))
+                notComplete(completedIdx) = false;
+                out(completedIdx,:) = currentOut;
+                nCompleted = nCompleted + 1;
+                nCompletedTime = datetime('now');
+            end
+            nProgressOut = in.DisplayFunc(nRuns,last_nCompleted,startDT,last_nRunsTime,nProgressOut);
         end
-        if(~isempty(completedIdx))
-            notComplete(completedIdx) = false;
-            out(completedIdx,:) = currentOut;
-            nCompleted = nCompleted + 1;
-            nRunsTime = datetime('now');
-        end
-        nProgressOut = in.DisplayFunc(nRuns,nCompleted,startDT,nRunsTime,nProgressOut);
+        % New estimate completed, update timers
+        last_nCompleted = nCompleted;
+        last_nRunsTime = nCompletedTime;
+        nProgressOut = in.DisplayFunc(nRuns,nCompleted,startDT,nCompletedTime,nProgressOut);
     end
-%     failed = ~cellfun(@isempty,{F.Error});
-%     out(failed,:) = arrayfun(in.ErrorHandler,F(failed),'UniformOutput',false);
     
     % Convert 2D cell array into a 1D cell array of cells
     varargout = num2cell(out,1);
@@ -82,31 +111,40 @@ function [ varargout ] = parcellfun_progress( func, varargin )
         
     % Wrap func in parfeval for evaluation
     function F = parfunc(varargin)
-        F = parfeval(func,nout,varargin{:});
+        F = parfeval(in.ParallelPool,func,nout,varargin{:});
     end
 
 end
-function nProgressOut = defaultDisplayFunc(nRuns,nCompleted,startDT,nRunsTime,nProgressOut)
-bp = repmat('\b',1,nProgressOut);
+function nProgressOut = defaultDisplayFunc(nRuns,nCompleted,startDT,nCompletedTime,nProgressOut)
+    bp = repmat('\b',1,nProgressOut);
     nowDT = datetime('now');
     sinceStart = nowDT - startDT;
     if(~nCompleted)
         estDuration = 'xx:xx:xx';
         remaining = 'xx:xx:xx';
     else
-        estDuration = (nRunsTime-startDT)*nRuns/nCompleted;
+        estDuration = (nCompletedTime-startDT)*nRuns/nCompleted;
         remaining = estDuration - sinceStart;
     end
-    nProgressOut = fprintf([bp ...
-        ' %g / %g\n' ...
-        '  Estimated %s\n' ...
-        '-   Elapsed %s\n' ...
-        '---------------------\n' ...
-        '= Remaining %s\n'],...
-        nCompleted,nRuns, ...
-        char(estDuration), ...
-        char(sinceStart), ...
-        char(remaining) ...
-        ) - nProgressOut;
+    if(nRuns ~= nCompleted)
+        nProgressOut = fprintf([bp ...
+            ' %g / %g = %3.0f%%\n' ...
+            '  Estimated %s\n' ...
+            '-   Elapsed %s\n' ...
+            '---------------------\n' ...
+            '= Remaining %s\n'],...
+            nCompleted,nRuns,nCompleted/nRuns*100, ...
+            char(estDuration), ...
+            char(sinceStart), ...
+            char(remaining) ...
+            ) - nProgressOut;
+    else
+        nProgressOut = fprintf([bp ...
+            ' %g / %g' ...
+            ' Elapsed %s\n'], ...
+            nCompleted,nRuns, ...
+            char(sinceStart) ...
+            ) - nProgressOut;
+    end
 end
 
