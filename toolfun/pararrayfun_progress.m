26cb480cc65105a56ff988e964ee806b5204e85e
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2015-12-07 15:32:07 -0600

    pararrayfun_progress: Check for accidental parameter inclusion before checking if we reached the end of the variables

--- toolfun/pararrayfun_progress.m
+++ toolfun/pararrayfun_progress.m
@@ -9,14 +9,16 @@ for paramIdx=1:length(varargin)
         break;
     end
 end
-
-if(mod(length(varargin)-paramIdx)==0 ...
+if(mod(length(varargin)-paramIdx,2)==0 ...
     && paramIdx > 1 ...
     && ischar(varargin{paramIdx-1}))
     % Remaining arguments for parameters is an odd number.
     % Previous argument could be a parameter name that
     %    happens to be the same size as other array input
     paramIdx = paramIdx - 1;
+elseif(paramIdx == length(varargin))
+    paramIdx = paramIdx + 1;
+else
 end
 
 % Just convert arrays to cells for now and use parcellfun_progress
