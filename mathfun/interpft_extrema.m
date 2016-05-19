be82d94ed3876c2a27557c090540fbb2864b0d89
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2016-05-18 21:02:54 -0500

    interpft_extrema: Initial vectorization up to 2D input where each column represents a discrete problem

--- mathfun/interpft_extrema.m
+++ mathfun/interpft_extrema.m
@@ -40,6 +40,7 @@ function [maxima,minima,maxima_value,minima_value,other,other_value] = interpft_
 
     % Tolerance for log(abs(root)) to be near zero, in which case the root is real
     TOL = eps*1e2;
+    
 
     s = size(x);
     scale_factor = s(1);
@@ -60,20 +61,28 @@ function [maxima,minima,maxima_value,minima_value,other,other_value] = interpft_
         % split nyquist frequency
         x_h(nyquist,:) = x_h(nyquist,:)/2;
         x_h = x_h([1:nyquist nyquist nyquist+1:end],:);
-        s(1) = s(1) + 1;
+%         s(1) = s(1) + 1;
     end
     % Wave number, unnormalized by number of points
     freq = [0:nyquist-1 -nyquist+1:1:-1]';
 
     % calculate derivatives, unscaled by 2*pi since we are only concerned with the signs of the derivatives
-    dx_h = x_h .* freq * 1i;
-    dx2_h = -x_h .* freq.^2;
+    dx_h = bsxfun(@times,x_h,freq * 1i);
+    dx2_h = bsxfun(@times,x_h,-freq.^2);
     
     % use companion matrix approach
     if(dx_h(end) ~= 0)
-        r = roots(fftshift(-dx_h./dx_h(end)));
+%         r = roots(fftshift(-dx_h./dx_h(end)));
+        dx_h = -fftshift(dx_h,1);
+        output_size = size(dx_h);
+        output_size(1) = output_size(1) - 1;
+        r = zeros(output_size);
+        for i=1:size(r,2)
+            r(:,i) = roots(dx_h(:,i));
+        end
         % keep only the real answers
-        r = r(abs(log(abs(r))) < TOL);
+%         r = r(abs(log(abs(r))) < TOL);
+        r(abs(log(abs(r))) > TOL) = NaN;
     else
         % no roots
         maxima = [];
@@ -96,39 +105,49 @@ function [maxima,minima,maxima_value,minima_value,other,other_value] = interpft_
     extrema(neg_extrema) = extrema(neg_extrema) + 2*pi;
     
     % calculate angles multiplied by wave number
-    theta = bsxfun(@times,extrema,freq');
+    theta = bsxfun(@times,extrema,shiftdim(freq,-ndims(extrema)));
     % waves
     waves = exp(1i*theta);
     % evaluate each wave by fourier coeffient
-    dx2 = waves*(dx2_h);
+%     dx2 = waves(:,:)*dx2_h;
+    dx2 = sum(bsxfun(@times,waves,shiftdim(dx2_h.',-1)),ndims(waves));
     
     % dx2 could be equal to 0, meaning inconclusive type
     % local maxima is when dx == 0, dx2 < 0
-    maxima = extrema(dx2 < 0);
+%     maxima = extrema(dx2 < 0);
+    maxima = NaN(size(extrema));
+    minima = maxima;
+    maxima(dx2 < 0) = extrema(dx2 < 0);
     % local minima is when dx == 0, dx2 > 0
-    minima = extrema(dx2 > 0);
+%     minima = extrema(dx2 > 0);
+    minima(dx2 > 0) = extrema(dx2 > 0);
     
     if(nargout > 2 || nargout == 0)
 	% calculate the value of the extrema if needed
-        maxima_value = waves*x_h/scale_factor;
-        minima_value = maxima_value(dx2 > 0);
-	if(nargout > 4)
-		% calculate roots which are not maxima or minima
-		other = extrema(dx2 == 0);
-		other_value = maxima_value(dx2 == 0);
-	end
-        maxima_value = maxima_value(dx2 < 0);
+%         maxima_value = waves*x_h/scale_factor;
+        extrema_value = sum(bsxfun(@times,waves,shiftdim(x_h.',-1)),ndims(waves))/scale_factor;
+        maxima_value = NaN(size(extrema_value));
+        minima_value = maxima_value;
+        minima_value(dx2 > 0) = extrema_value(dx2 > 0);
+        maxima_value(dx2 < 0) = extrema_value(dx2 < 0);
+        if(nargout > 4)
+            % calculate roots which are not maxima or minima
+            other = extrema(dx2 == 0);
+            other_value = extrema_value(dx2 == 0);
+        end
     end
     
     if(nargout == 0)
         % plot if no outputs requested
         if(~isempty(extrema))
 	    % Maxima will be green
-            plot([maxima maxima]',ylim,'g');
-            plot(maxima,real(maxima_value),'go');
+            real_maxima = maxima(isfinite(maxima));
+            plot([real_maxima real_maxima]',ylim,'g');
+            plot(real_maxima,real(maxima_value(isfinite(maxima_value))),'go');
 	    % Minima will be red
-            plot([minima minima]',ylim,'r');
-            plot(minima,real(minima_value),'ro');
+            real_minima = minima(isfinite(minima));
+            plot([real_minima real_minima]',ylim,'r');
+            plot(real_minima,real(minima_value(isfinite(minima_value))),'ro');
         else
             warning('No extrema');
         end
