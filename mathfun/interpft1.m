d30cf0424fa6db1ca85a2dd97d1fdbcd1f1fd845
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2016-07-28 02:43:22 -0500

    interpft1: Unify output scheme, implement Horner's method for evaluation

--- mathfun/interpft1.m
+++ mathfun/interpft1.m
@@ -7,6 +7,8 @@ function [vq] = interpft1(varargin)
 % v - known values at regular intervals corresponding to
 %     linspace(x(1),x(2)-x(1),size(v,1))
 % xq - abisssa to determine value of Fourier series at
+%      size(v,ii) must equal size(xq,ii) for ii > 1 if legacy is false
+%      OR xq is a column vector
 % method - (optional) interpolation method
 %          See interp1 for methods, also available:
 %          horner - Use Horner's method from chebfun
@@ -17,87 +19,134 @@ function [vq] = interpft1(varargin)
 %                  6 for cubic, pchip, v5cubic
 %                  3 for spline
 %                  10 for everything else
+% legacy - (optional) logical of whether to use legacy output behavior. See
+%          below. If true, output should match interp1 exactly.
+%          default: false
 %
 % horner and mmt are very accurate methods, but are slow
 % interp1 methods are typically faster but less accurate
 %
 % OUTPUT
-% vq - values of Fourier series at xq
+% vq - values of Fourier series at xq. Similar to interp1 conventions:
+% Shape of v    Shape of xq    Size of vq
+% ---------------------------------------
+% Vector        Vector         size(xq)
+% Vector        Matrix/N-D     size(xq)
+% Matrix/N-D    Row Vector     size(xq) *
+% Matrix/N-D    Column Vector  [length(xq) size(v,2), ... size(v,n)]
+% Matrix/N-D    Matrix/N-D     size(xq) *
+% * Behavior differs from interp1. Pass xq(:) for interp1 behavior and
+% reshape or set legacy to true.
 
 % Mark Kittisopikul
 % July 20th, 2016
 
 % TODO:
 % 1. Extract mapping code, consider wraparound(N)
-% 2. Do horner and mmt need wrapping?
 
-    [x,v,xq,method,fineGridFactor] = parseinputs(varargin{:});
-    switch(method)
-        case 'horner'
-            % use external package if availabile
-            str = which('trigtech.horner');
-            if(isempty(str))
-                warning('interpft1:chebfun_missing', ...
-                    'trigtech.horner in chebfun is not available. Using mmt method instead');
-                % Do mmt, repeated code
-                xq = xq - x(1);
-                xq = xq./(x(2) - x(1))*2*pi;
-                vq = matrixMultiplicationTransform(v,xq);
-                return;
-            else
-                % trigtech.horner is aligned on the domain [0,2)
-                % The output is not normalized by the number of points
-                xq = xq - x(1);
-                xq = xq./(x(2) - x(1))*2;
-                vq = trigtech.horner(xq(:),fftshift(fft(v)),isreal(v))./size(v,1);
-                v_sz = size(v);
-                xq_sz = size(xq);
-                if(xq_sz(2) == 1)
-                    xq_sz = xq_sz(1);
-                end
-                vq = reshape(vq,[xq_sz v_sz(2:end)]);
-                return;
-            end
-        case 'mmt'
-            xq = xq - x(1);
-            xq = xq./(x(2) - x(1))*2*pi;
-            vq = matrixMultiplicationTransform(v,xq);
-            return;
+    [x,v,xq,method,fineGridFactor,legacy] = parseinputs(varargin{:});
+    
+    if(legacy)
+        % Use interp1 output conventions
+        if(isvector(v))
+            vSz = [];
+        else
+            vSz = size(v);
+        end
+        if(isvector(xq) && ~isvector(v))
+            outSz = length(xq);
+        else
+            outSz = size(xq);
+        end
+        outSz = [outSz vSz(2:end)];
+        xq = xq(:);
     end
-    vft3 = interpft(v,size(v,1)*fineGridFactor);
-    vft3 = [vft3(end-2:end,:); vft3; vft3(1:4,:)];
-    if(~isempty(x))
-        % Map indices from [x(1) x(2)) to [0 1)
-        period = x(2) - x(1);
-        xq = xq - x(1);
-        xq = mod(xq,period);
-        xq = xq./period;
+    
+    % Map indices from [x(1) x(2)) to [0 1)
+    period = x(2) - x(1);
+    xq = xq - x(1);
+    xq = mod(xq,period);
+    xq = xq./period;
+
+    done = true;
+    
+    switch(method)
+        case 'chebfun'
+            % trigtech.horner is aligned on the domain [0,2)
+            % The output is not normalized by the number of points
+            xq = xq*2;
+            try
+                vq = trigtech.horner(xq(:),fftshift(fft(v),1),isreal(v))./size(v,1);
+            catch err
+                if(strcmp(err.identifier,'MATLAB:undefinedVarOrFunction'))
+                    warning('interpft1:chebfun_missing', ...
+                            'trigtech.horner in chebfun is not available. Using mmt method instead');
+                    vq = interp1(x,v,xq,'mmt',fineGridFactor);
+                    return;
+                else
+                    rethrow(err);
+                end
+            end
+            v_sz = size(v);
+            xq_sz = size(xq);
+            if(xq_sz(2) == 1)
+                xq_sz = xq_sz(1);
+            end
+            vq = reshape(vq,[xq_sz v_sz(2:end)]);
+        case 'horner'
+            % domain [0,2)
+            xq = xq*2;
+            vq = horner_vec_real(v,xq);
+        case 'mmt'
+            % domain [0,2*pi)
+            xq = xq*2*pi;
+            vq = matrixMultiplicationTransform(v,xq);
+        otherwise
+            done = false;
+    end
+    if(~done)
+        % Use interp1 methods by expanding grid points using interpft
+        fineGridFactor = parseFineGridFactor(fineGridFactor,method);
+        vft3 = interpft(v,size(v,1)*fineGridFactor);
+        vft3 = [vft3(end-2:end,:); vft3(:,:); vft3(1:4,:)];
+
         % Map indices from [0 1) to [4 size(v,1)*fineGridFactor+4)
         xq = xq.*(size(v,1)*fineGridFactor);
         xq = xq+4;
+        if(legacy || iscolumn(xq))
+            vq = interp1(vft3,xq,method);
+        else
+            % break xq into columns and apply to each corresponding column in v 
+            vq = cellfun(@(ii,xq) interp1(vft3(:,ii),xq,method),num2cell(1:numel(xq)/size(xq,1)),num2cell(xq(:,:),1),'UniformOutput',false);
+            vq = [vq{:}];
+            vSz = size(v);
+            vq = reshape(vq,[size(xq,1) vSz(2:end)]);
+        end
     end
-    vq = interp1(vft3,xq,method);
+    
+    if(legacy)
+        vq = reshape(vq,outSz);
+    end
 end
 
-function [x,v,xq,method,fineGridFactor] = parseinputs(varargin)
+function [x,v,xq,method,fineGridFactor,legacy] = parseinputs(varargin)
     % Optional arguments
-    x = [];
-    method = [];
+    method = 'pchip';
     fineGridFactor = [];
+    x = [];
+    legacy = false;
+    if(islogical(varargin{end}) && isscalar(varargin{end}))
+        legacy = varargin{end};
+        varargin = varargin(1:end-1);
+    end
     % Decide whether optional arguments are specified, based existence of x
-    switch(nargin)
-        case 2
-            % v xq
-
-            [v, xq]  = varargin{:};
-        case 3
-            if(ischar(varargin{3}))
-                % v xq method
-                [v, xq, method] = varargin{:};
-            else
-                % x v xq
-                [x, v, xq] = varargin{:};
-            end
+    switch(length(varargin))
+        case 6
+            [x, v,xq,method,fineGridFactor,legacy] = varargin{:};
+        case 5
+            % All args specified
+            % x v xq method fineGridFactor
+            [x, v,xq,method,fineGridFactor] = varargin{:};
         case 4
             if(ischar(varargin{4}))
                 % x v xq method
@@ -106,35 +155,66 @@ function [x,v,xq,method,fineGridFactor] = parseinputs(varargin)
                 % v xq method fineGridFactor
                 [v,xq,method,fineGridFactor] = varargin{:};
             end
-        case 5
-            % All args specified
-            % x v xq method fineGridFactor
-            [x, v,xq,method,fineGridFactor] = varargin{:};
+        case 3
+            if(ischar(varargin{3}))
+                % v xq method
+                [v, xq, method] = varargin{:};
+            else
+                % x v xq
+                [x, v, xq] = varargin{:};
+            end
+        case 2
+            % v xq
+
+            [v, xq]  = varargin{:};
         otherwise
             error('interpft1:nargin','Incorrect number of inputs');
     end
     % Row values are transposed automatically like interp1
     if(isrow(v))
-        v = v.';
+        v = v(:);
     end
     % Query points are transposed automatically like interp1
     if(isrow(xq))
-        xq = xq.';
+        if(iscolumn(v) || legacy)
+            xq = xq(:);
+        end
     end
     % Default x specifying periodic boundary f(x) == f(size(v,1)+x)
-    if(isempty(x))
-        x = [1 size(v,1)+1];
-    else
-        assert(numel(x) == 2,'interpft1:IncorrectX', ...
+    switch(numel(x))
+        case 2
+        case 0
+            x = [1 size(v,1)+1];
+        otherwise
+            error('interpft1:IncorrectX', ...
             'x must either be empty or have 2 elements.');
     end
-    % Use pchip which is reasonably fast and uses a modest amount of memory
-    if(isempty(method))
-        method = 'pchip';
+    if(~legacy)
+        % Not in legacy mode
+        xq_sz = size(xq);
+        v_sz = size(v);
+        % xq should either be a column such that all points are queried against all trig polynomials
+        %    outSz = [length(xq) v_sz(2:end)]
+        % OR be of the same size as v for every dimension except the first.
+        %    outSz = size(xq)
+        assert(iscolumn(xq) || all(xq_sz(2:end) == v_sz(2:end)), ...
+                'interpft1:InputDimensions', ...
+                'xq should either be a column or have similar dimensions as v if not in legacy mode');
+    % else
+        % Legacy mode
+        % outSz = size(xq) % if v is a vector
+        % outSz = [length(xq) v_sz(2:end)] % if xq is a vector, and v is not
+        % outSz = [xq_sz v_sz(2:end)] % if xq and v both are not vectors
     end
+end
+function fineGridFactor = parseFineGridFactor(fineGridFactor,method)
     % Courser methods should use a finer grid if none is specified
     if(isempty(fineGridFactor))
         switch(method)
+%             case 'horner'
+%                 fineGridFactor = NaN;
+%             case 'mmt'
+%                 fineGridFactor = NaN;
 %             case 'linear'
 %                 fineGridFactor = 10;
 %             case 'nearest'
@@ -196,4 +276,48 @@ function vq = matrixMultiplicationTransform(v,xq)
     % sum across waves weighted by Fourier coefficients
     % normalize by the the number of Fourier coefficients
     vq = sum(real(bsxfun(@times,waves,permute(v_h,dim_permute))),ndims_waves)/scale_factor;
+end
+function vq = horner_vec_real(v,xq)
+    % v represents the coefficients of the polynomial
+    %   D x N
+    %   D = degree of the polynomial - 1
+    %   N = number of polynomials
+    % xq represents the query points
+    %   Q x N
+    %   Q = number of query points per polynomial
+    %   N = number of polynomials
+    % vq will be a Q x N matrix of the value of each polynomial
+    %    evaluated at Q query points
+    s = size(v);
+    scale_factor = s(1);
+
+    % Calculate fft and nyquist frequency
+    v_h = fft(v);
+    nyquist = ceil((s(1)+1)/2);
+    
+    % If there is an even number of fourier coefficients, split the nyquist frequency
+    if(~rem(s(1),2))
+        % even number of coefficients
+        % split nyquist frequency
+        v_h(nyquist,:) = real(v_h(nyquist,:))/2;
+%        v_h = v_h([1:nyquist nyquist nyquist+1:end],:);
+%        v_h = reshape(v_h,[s(1)+1 s(2:end)]);
+    end
+    
+    % z is Q x N
+    z = exp(1i*pi*xq);
+    % vq starts as 1 x N
+    colon = {':'};
+    vq = v_h(nyquist,colon{ones(ndims(v_h)-1,1)});
+    for j = nyquist-1:-1:2
+        vq = bsxfun(@times,z,vq);
+        vq = bsxfun(@plus,v_h(j,:),vq);
+    end
+    % Last multiplication
+%     vq = bsxfun(@times,z,vq); % We only care about the real part
+    vq = bsxfun(@times,real(z),real(vq))-bsxfun(@times,imag(z),imag(vq));
+    % Add Constant Term and Scale
+    vq = bsxfun(@plus,v_h(1,:),vq*2);
+%     vq = real(vq); % We already selected the real part above
+    vq = vq./scale_factor;
 end
