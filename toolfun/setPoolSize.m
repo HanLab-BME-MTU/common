02d0a6a71ba385a1625a48d7da233f471e04da01
Hunter Elliott <Hunter_Elliott@hms.harvard.edu>
2016-08-11 09:06:00 -0400

    setPoolSize now supports empty input which uses default pool size

--- toolfun/setPoolSize.m
+++ toolfun/setPoolSize.m
@@ -1,12 +1,20 @@
 function setPoolSize(poolSize)
 %SETPOOLSIZE sets parallel pool (matlabpool) to specified size irrespective of current status
+%
+% setPoolSize
 % setPoolSize(poolSize)
 %
-% Input: poolSize - the parallel pool (matlabpool) size (number of workers)
+% Input: poolSize - the parallel pool (matlabpool) size (number of
+% workers). If not specified / empty then the default size is used, unless
+% a pool already exists in which case nothing is done.
 % 
 %
 % Hunter Elliott
 % 12/2015
+
+if nargin < 1
+    poolSize = [];
+end
 
 %Get current poolsize
 poolobj = gcp('nocreate'); %Just check the current size
@@ -16,7 +24,14 @@ else
     currSize = poolobj.NumWorkers;
 end
 
-if currSize ~= poolSize
+if isempty(poolSize)
+    %This appears to be the only way to use the default poolsize setting,
+    %and it throws an error if one already exists so we catch that.
+    try
+        parpool
+    catch
+    end
+elseif currSize ~= poolSize
     
     %Delete the current pool (Doesn't seem to be possible to just
     %add/remove...)
