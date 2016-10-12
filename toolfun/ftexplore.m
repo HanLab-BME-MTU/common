65f470ced69da1a7c504438b80616bc027637712
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2016-10-12 14:42:41 -0500

    ftexplore: Add a context menu to change display, as well as axes

--- toolfun/ftexplore.m
+++ toolfun/ftexplore.m
@@ -41,6 +41,17 @@ posCallback(getPosition(h_ellipse));
 
 freqFig = figure;
 hfreq = imshow(abs(fftshift(fft(Iz))),[]);
+info = @real;
+hax(1) = viscircles(hfreq.Parent,repmat([101 101],4,1),[10 20 40 80],'DrawBackgroundCircle',false,'EdgeColor',[1 1 1],'LineStyle',':','LineWidth',1);
+hax(2) = line(xlim-0.5,repmat(sum(xlim)/2,1,2),'Color',[1 1 1],'LineWidth',1,'LineStyle',':','Parent',hfreq.Parent);
+hax(3) = line(repmat(sum(ylim)/2,1,2),ylim-0.5,'Color',[1 1 1],'LineWidth',1,'LineStyle',':','Parent',hfreq.Parent);
+menu = uicontextmenu(freqFig);
+m_real = uimenu(menu,'Label','Real','Checked','on','Callback',@selectInfo);
+m_imag = uimenu(menu,'Label','Imag','Checked','off','Callback',@selectInfo);
+m_abs  = uimenu(menu,'Label','Magnitude','Checked','off','Callback',@selectInfo);
+m_angle = uimenu(menu,'Label','Phase','Checked','off','Callback',@selectInfo);
+m_axes = uimenu(menu,'Label','Axes','Checked','on','Callback',@toggleAxes,'Separator','on');
+hfreq.UIContextMenu = menu;
 colorbar
 cm = vertcat(bsxfun(@times,(1-(0:32)/32)',[0 1 1]),bsxfun(@times,(1:32)'/32,[1 1 0]));
 
@@ -65,13 +76,41 @@ end
         Is = circshift(Iz,-center+1);
         Is = Is.*gaussianWindow;
 %         Is = Is - mean(Is(:));
-        If = real(fftshift(fft2(Is)));
+        If = info(fftshift(fft2(Is)));
         cmax = max(abs(If(:)));
         figure(freqFig);
         set(hfreq,'CData',If);
 %         imshow(real(fftshift(fft2(Is))),[]);
         caxis(hfreq.Parent,cmax*[-1 1]);
         colormap(hfreq.Parent,cm);
+    end
+    function selectInfo(hObject,~)
+        set(menu.Children,'Checked','off');
+        switch(hObject.Label)
+            case 'Real'
+                info = @real;
+                set(m_real,'Checked','on');
+            case 'Imag'
+                info = @imag;
+                set(m_imag,'Checked','on');
+            case 'Magnitude'
+                info = @abs;
+                set(m_abs,'Checked','on');
+            case 'Phase'
+                info = @angle;
+                set(m_angle,'Checked','on');
+        end
+        showLocalizedFreq(getPosition(h_ellipse));
+    end
+    function toggleAxes(hObject,~)
+        switch(hax(1).Visible)
+            case 'on'
+                set(hax,'Visible','off');
+                m_axes.Checked = 'off';
+            case 'off'
+                set(hax,'Visible','on');
+                maxes.Checked = 'on';
+        end
     end
 
 end
