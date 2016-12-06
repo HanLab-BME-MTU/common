6a5858761df2004bb39c10ef13a305abbb52f2db
shan <sangyoon.han@utsouthwestern.edu>
2016-12-06 09:32:21 -0600

    ylim bug fixed

--- graphics/boxPlotCellArray.m
+++ graphics/boxPlotCellArray.m
@@ -1,4 +1,4 @@
-function []=boxPlotCellArray(cellArrayData,nameList,convertFactor,notchOn)
+function []=boxPlotCellArray(cellArrayData,nameList,convertFactor,notchOn,plotIndivPoint,forceShowP)
 % function []=boxPlotCellArray(FAarea) automatically converts cell array
 % format input to matrix input to use matlab function 'boxplot'
 % input: cellArrayData      cell array data
@@ -17,10 +17,20 @@ matrixData = NaN(lengthLongest,numConditions);
 for k=1:numConditions
     matrixData(1:length(cellArrayData{k}),k) = cellArrayData{k};
 end
+if nargin<6
+    forceShowP=false;
+end
+if nargin<5
+    forceShowP=false;
+    plotIndivPoint = true;
+end
 if nargin<4
+    forceShowP=false;
     notchOn=true;
+    plotIndivPoint = true;
 end
 if nargin<3
+    forceShowP=false;
     convertFactor = 1;
     notchOn=true;
 end
@@ -28,18 +38,29 @@ if nargin<2
     nameList=arrayfun(@(x) num2str(x),(1:numConditions),'UniformOutput',false);
     convertFactor = 1;
     notchOn=true;
+    forceShowP=false;
 end
 boxWidth=0.5;
-whiskerRatio=1;
+whiskerRatio=1.5;
 matrixData=matrixData*convertFactor;
 nameListNew = cellfun(@(x,y) [x '(N=' num2str(length(y)) ')'],nameList,cellArrayData,'UniformOutput', false);
+
+% individual data jitter plot
+numCategories = size(matrixData,2);
+% width=boxWidth/2;
+for ii=1:numCategories
+    xData = ii+0.1*boxWidth*(randn(size(matrixData,1),1));
+    scatter(xData,matrixData(:,ii),'filled','MarkerFaceColor',[.3 .3 .3],'MarkerEdgeColor','none','SizeData',2)
+    hold on
+end
+alpha(.5)
 
 if notchOn %min(sum(~isnan(matrixData),1))>20 || 
     boxplot(matrixData,'whisker',whiskerRatio,'notch','on',...
         'labels',nameListNew,'symbol','','widths',boxWidth,'jitter',1,'colors','k');%, 'labelorientation','inline');
 else % if the data is too small, don't use notch
     boxplot(matrixData,'whisker',whiskerRatio*0.5,'notch','off',...
-        'labels',nameListNew,'symbol','','widths',boxWidth,'jitter',1,'colors','k');%, 'labelorientation','inline');
+        'labels',nameListNew,'symbol','','widths',boxWidth,'jitter',0,'colors','k');%, 'labelorientation','inline');
 end
     
 set(findobj(gca,'LineStyle','--'),'LineStyle','-')
@@ -48,7 +69,7 @@ set(findobj(gca,'tag','Median'),'LineWidth',2)
 % set(gca,'XTickLabel',nameList)
 set(gca,'XTickLabelRotation',45)
 
-hold on
+% hold on
 % perform ranksum test for every single combination
 maxPoint = quantile(matrixData,[0.25 0.75]);
 maxPoint2 = maxPoint(2,:)+(maxPoint(2,:)-maxPoint(1,:))*whiskerRatio;
@@ -60,19 +81,19 @@ for k=1:(numConditions-1)
         if numel(cellArrayData{k})>1 && numel(cellArrayData{ii})>1
             if kstest(cellArrayData{k}) % this means the test rejects the null hypothesis
                 [p]=ranksum(cellArrayData{k},cellArrayData{ii});
-                if p<0.05
+                if p<0.05 || forceShowP
                     q=q+lineGap;
                     line([k ii], ones(1,2)*(maxPoint2+q),'Color','k')    
                     q=q+lineGap;
-                    text(floor((k+ii)/2), maxPoint2+q,['p=' num2str(p) ' (r)'])
+                    text(floor((k+ii)/2)+0.3, maxPoint2+q,['p=' num2str(p) ' (r)'])
                 end
             else
                 [~,p]=ttest2(cellArrayData{k},cellArrayData{ii});
-                if p<0.05
+                if p<0.05 || forceShowP
                     q=q+lineGap;
                     line([k ii], ones(1,2)*(maxPoint2+q),'Color','k')    
                     q=q+lineGap;
-                    text(floor((k+ii)/2), maxPoint2+q,['p=' num2str(p) ' (t)'])
+                    text(floor((k+ii)/2)+0.3, maxPoint2+q,['p=' num2str(p) ' (t)'])
                 end
             end
         end
@@ -82,8 +103,13 @@ q=q+lineGap*3;
 minPoint = quantile(matrixData,[0.25 0.75]);
 minPoint2 = minPoint(1,:)-(minPoint(2,:)-minPoint(1,:))*whiskerRatio;
 minPoint2 = min(minPoint2);
-ylim([minPoint2-lineGap*2 maxPoint2+q])
+if plotIndivPoint && ~forceShowP
+    maxPoint2 = quantile(matrixData(:),0.99);
+else
+    maxPoint2 = maxPoint2+q;
+end
+ylim([minPoint2-lineGap*2 maxPoint2])
 
-set(gca,'FontSize',6)
-set(findobj(gca,'Type','Text'),'FontSize',4)
+set(gca,'FontSize',7)
+set(findobj(gca,'Type','Text'),'FontSize',6)
 
