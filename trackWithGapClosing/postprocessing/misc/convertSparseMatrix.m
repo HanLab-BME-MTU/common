19c5315cacb6de995e2266ea7c5aed6336f74dd2
Mark Kittisopikul <mark.kittisopikul@utsouthwestern.edu>
2017-02-14 17:23:00 -0600

    convertSparseMatrix: Remove from common due to duplicate in applications

--- trackWithGapClosing/postprocessing/misc/convertSparseMatrix.m
+++
@@ -1,5 +0,0 @@
-for i=1:length(compTracks)
-    compTracks(i).tracksFeatIndxCG = full(compTracks(i).tracksFeatIndxCG);
-    compTracks(i).tracksCoordAmpCG = full(compTracks(i).tracksCoordAmpCG); 
-    compTracks(i).tracksCoordAmpCG(compTracks(i).tracksCoordAmpCG==0) = NaN;
-end
