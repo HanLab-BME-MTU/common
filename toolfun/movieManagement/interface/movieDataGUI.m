96cff8df1644be047b3830923d4680b5fc4d7681
Andrew R. Jamieson <andrew.jamieson@UTSouthwestern.edu>
2017-06-20 22:31:15 -0500

    minor changes to .fig for movieData details and added pixelSizeZ field

--- toolfun/movieManagement/interface/movieDataGUI.m
+++ toolfun/movieManagement/interface/movieDataGUI.m
@@ -22,7 +22,7 @@ function varargout = movieDataGUI(varargin)
 
 % Edit the above text to modify the response to help movieDataGUI
 
-% Last Modified by GUIDE v2.5 10-Jun-2017 18:25:16
+% Last Modified by GUIDE v2.5 20-Jun-2017 22:08:34
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -119,7 +119,7 @@ if ~isempty(userData.MD),
     set(handles.edit_notes, 'String', userData.MD.notes_)
     
     % GUI setting - parameters
-    propNames={'pixelSize_','timeInterval_','numAperture_','camBitdepth_'};
+    propNames={'pixelSize_','pixelSizeZ_','timeInterval_','numAperture_','camBitdepth_'};
     validProps = ~cellfun(@(x) isempty(userData.MD.(x)),propNames);
     
     propNames=propNames(validProps);
@@ -180,7 +180,7 @@ if isempty(outputDir) || ~exist(outputDir, 'dir')
 end
 
 % Concatenate numerical parameters as movie options
-propNames={'pixelSize_','timeInterval_','numAperture_','camBitdepth_'};
+propNames={'pixelSize_','pixelSizeZ_','timeInterval_','numAperture_','camBitdepth_'};
 propStrings = cell(numel(propNames), 1);
 for i = 1 : numel(propNames)
     propStrings{i} =get(handles.(['edit_' propNames{i}(1:end-1)]), 'String');
@@ -447,4 +447,27 @@ catch err
         msgbox('Cannot show metadata for a movie of this type',...
             'Metadata Unavailable');
     end
+end
+
+
+
+function edit_pixelSizeZ_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_pixelSizeZ (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_pixelSizeZ as text
+%        str2double(get(hObject,'String')) returns contents of edit_pixelSizeZ as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_pixelSizeZ_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_pixelSizeZ (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
 end
