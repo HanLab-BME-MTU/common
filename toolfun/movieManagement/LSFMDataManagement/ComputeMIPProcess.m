e0f4137c9755724f7768b6bed98993d26316809e
Andrew R. Jamieson <andrew.jamieson@UTSouthwestern.edu>
2017-08-03 14:55:16 -0500

    WIP MIP process

--- toolfun/movieManagement/LSFMDataManagement/ComputeMIPProcess.m
+++ toolfun/movieManagement/LSFMDataManagement/ComputeMIPProcess.m
@@ -1,6 +1,5 @@
 classdef  ComputeMIPProcess < ImageProcessingProcess
     % Concrete class for a computing Maximum Intensity Projections (MIP)
-    %
     % Andrew R. Jamieson Aug. 2017
     
     methods
@@ -17,8 +16,6 @@ classdef  ComputeMIPProcess < ImageProcessingProcess
                 ip.parse(owner,varargin{:});
                 outputDir = ip.Results.outputDir;
                 funParams = ip.Results.funParams;
-                
-                % Constructor for EfficientSubpixelRegistrationProcess
 
                 super_args{1} = owner;
                 super_args{2} = ComputeMIPProcess.getName;
@@ -29,6 +26,48 @@ classdef  ComputeMIPProcess < ImageProcessingProcess
                 super_args{4} = funParams;
             end
             obj = obj@ImageProcessingProcess(super_args{:});
+        end
+
+        function h = draw(obj, varargin)
+            % Function to draw process output
+            outputList = obj.getDrawableOutput();  
+                               
+            ip = inputParser;
+            ip.addRequired('obj',@(x) isa(x,'Process'));
+            ip.addRequired('iChan',@isnumeric);
+            ip.addOptional('iFrame',[],@isnumeric);
+            % ip.addParameter('iZ',[], @(x) ismember(x,1:obj.owner_.zSize_));
+            ip.addParameter('output', outputList(3).var,@(x) all(ismember(x,{outputList.var})));
+            ip.KeepUnmatched = true;
+            ip.parse(obj, varargin{:});
+
+            if strcmp('merged',ip.Results.output)
+                if numel(obj.owner_.channels_) > 1, cdim=3; else cdim=1; end
+                    data = zeros([obj.owner_.imSize_ cdim]);
+
+                iOutput = find(cellfun(@(y) isequal(ip.Results.output,y),{outputList.var}));
+
+                for iChan = 1:numel(obj.owner_.channels_)
+                    imData = obj.loadChannelOutput(iChan, ip.Results.iFrame);
+                    data(:,:,iChan) = outputList(iOutput).formatData(imData);
+                end                  
+
+                try
+                    assert(~isempty(obj.displayMethod_{iOutput,1}));
+                catch ME
+                    obj.displayMethod_{iOutput,1}=...
+                        outputList(iOutput).defaultDisplayMethod();
+                end
+
+                % Create graphic tag and delegate drawing to the display class
+                tag = ['process' num2str(obj.getIndex()) '_MergedOutput'];
+                h = obj.displayMethod_{3}.draw(data, tag, ip.Unmatched);
+
+            else
+                % Call superclass method
+                h = draw@ImageProcessingProcess(obj,varargin{1},varargin{2},...
+                        varargin{3:end});
+            end
         end
         
         function output = getDrawableOutput(obj, varargin)
@@ -58,7 +97,7 @@ classdef  ComputeMIPProcess < ImageProcessingProcess
             ip.addRequired('owner',@(x) isa(x,'MovieData'));
             ip.addOptional('outputDir',owner.outputDirectory_,@ischar);
             ip.parse(owner, varargin{:})
-            outputDir=ip.Results.outputDir;
+            outputDir = ip.Results.outputDir;
             
             % Set default parameters
             funParams.ChannelIndex = 1:numel(owner.channels_);
---
+++ toolfun/movieManagement/LSFMDataManagement/computeMovieMIP.m
@@ -0,0 +1,97 @@
+function computeMovieMIP(MD, varargin)
+%computeMovieMIP MovieData-Process framework wrapped printMIP function
+% Input Parameter arguments:
+%   Channel<numeric>: specifies which channel to MIP process
+%                     default (non provided) will combine all into montage image.
+%                                     
+    
+ip = inputParser;
+ip.addRequired('MD',@(MD) isa(MD,'MovieData'));
+% ip.addOptional('Channel', [], @(x) isnumeric(x) && ismember(x, 1:length(MD.channels_)))
+ip.parse(MD, varargin{:});
+
+selectedChannel = ip.Results.Channel;
+%progressText(0,'Print MIP');
+
+% turn a specific warning off
+warning('off', 'MATLAB:imagesci:tifftagsread:expectedAsciiDataFormat');
+
+if(MD.zSize_==1)
+    warning('This seems to be a 2D movie, No MIP produced.');
+    return;
+end
+
+if isempty(selectedChannel)
+    savePath = [MD.outputDirectory_ filesep 'MIP'];
+else
+    savePath = [MD.outputDirectory_ filesep 'MIP' filesep 'ch' num2str(selectedChannel)];
+end
+
+if ~isdir(savePath) || ~isdir([savePath filesep 'XY']) || ~isdir([savePath filesep 'ZY']) || ~isdir([savePath filesep 'ZX']) || ~isdir([savePath filesep 'Three'])
+    system(['mkdir -p "' savePath '"']);
+    system(['mkdir -p "' [savePath filesep 'XY'] '"' ]);
+    system(['mkdir -p "' [savePath filesep 'ZY'] '"']);
+    system(['mkdir -p "' [savePath filesep 'ZX'] '"']);
+    system(['mkdir -p "' [savePath filesep 'Three'] '"']);
+end
+
+ZXRatio = MD.pixelSizeZ_/MD.pixelSize_;
+minIntensityNorm = [];
+maxIntensityNorm = [];
+
+if isempty(selectedChannel)
+    for chIdx = 1:length(MD.channels_)
+        vol = MD.getChannel(chIdx).loadStack(1);
+        minIntensityNorm = [minIntensityNorm min(vol(:))];
+        maxIntensityNorm = [maxIntensityNorm max(vol(:))];
+    end
+else
+    vol = MD.getChannel(selectedChannel).loadStack(1);
+    minIntensityNorm = min(vol(:));
+    maxIntensityNorm = max(vol(:));
+end
+
+
+if isempty(selectedChannel)
+    nameCells = MD.getChannel(1).getImageFileNames;
+else
+    nameCells = MD.getChannel(selectedChannel).getImageFileNames;
+end
+
+parfor frameIdx = 1:MD.nFrames_
+
+    if isempty(selectedChannel)
+        maxXY=[]; maxZY=[]; maxZX=[]; three=[];
+        for chIdx = 1:length(MD.channels_)
+            vol = MD.getChannel(chIdx).loadStack(frameIdx);  
+            [cmaxXY, cmaxZY, cmaxZX, cthree] = computeMIPs(vol, ZXRatio, minIntensityNorm(chIdx), maxIntensityNorm(chIdx));
+            maxXY = [maxXY cmaxXY];
+            maxZY = [maxZY cmaxZY];
+            maxZX = [maxZX cmaxZX];
+            three = [three cthree];        
+        end
+    else
+        chIdx = selectedChannel;
+        vol = MD.getChannel(chIdx).loadStack(frameIdx);  
+        [maxXY, maxZY, maxZX, three] = computeMIPs(vol, ZXRatio, minIntensityNorm, maxIntensityNorm);
+    end
+    
+    % save the maximum intensity projections
+    imwrite(maxXY, [savePath filesep 'XY' filesep 'XY_' nameCells{frameIdx} ], 'Compression', 'none');
+    imwrite(maxZY, [savePath filesep 'ZY' filesep 'ZY_'  nameCells{frameIdx}], 'Compression', 'none');
+    imwrite(maxZX, [savePath filesep 'ZX' filesep 'ZX_'  nameCells{frameIdx}], 'Compression', 'none');
+    imwrite(three, [savePath filesep 'Three' filesep nameCells{frameIdx}], 'Compression', 'none');
+
+end
+
+threeVideo = VideoWriter([savePath filesep 'three.avi']);
+myVideo.FrameRate = 4;  % Default 30
+myVideo.Quality = 90;    % Default 75
+
+open(threeVideo);
+for frameIdx = 1:MD.nFrames_
+    % save the maximum intensity projections
+    three = imread([savePath filesep 'Three' filesep nameCells{frameIdx}]);
+    writeVideo(threeVideo, three)
+end
+close(threeVideo);
