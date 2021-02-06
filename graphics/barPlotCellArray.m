6e080766f5f8b5f614a2e55b2b43ba2de5ee2ba0
Sangyoon J Han <sjhan@mtu.edu>
2021-02-05 19:29:33 -0500

    debug

--- graphics/barPlotCellArray.m
+++ graphics/barPlotCellArray.m
@@ -17,7 +17,11 @@ end
 bar(1:numConditions, cellfun(@nanmean,cellArrayData)*convertFactor,'FaceColor',[1 1 1],'EdgeColor',[0 0 0],'LineWidth',1.5); hold on
 errorbar(1:numConditions,cellfun(@nanmean,cellArrayData)*convertFactor,cellfun(@(x) nanstd(x)/sqrt(length(x)),cellArrayData)*convertFactor,'Marker','none','LineStyle','none','Color','k','LineWidth',1.5); %SEM option 
 % errorbar(1:numConditions, cellfun(@nanmean,cellArrayData)*convertFactor,cellfun(@(x) nanstd(x),cellArrayData)*convertFactor,'Marker','none','LineStyle','none','Color','k','LineWidth',1.5); %SD option 
-nameListNew = cellfun(@(x,y) [x '(N=' num2str(length(y)) ')'],nameList,cellArrayData,'UniformOutput', false);
+try
+    nameListNew = cellfun(@(x,y) [x '(N=' num2str(length(y)) ')'],nameList,cellArrayData,'UniformOutput', false);
+catch
+    nameListNew = cellfun(@(x,y) [x '(N=' num2str(length(y)) ')'],nameList,cellArrayData','UniformOutput', false);
+end
 set(gca,'XTickLabel',nameListNew)
 set(gca,'XTickLabelRotation',45)
 
