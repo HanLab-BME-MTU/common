0837dc08b4e34167fb7eb7256de09de87a99bed4
Sangyoon Han <sjhan@mtu.edu>
2024-05-13 13:40:17 -0400

    Added comments.

--- imageProcessing/maskVectors.m
+++ imageProcessing/maskVectors.m
@@ -1,13 +1,26 @@
-function [insideIdx] = maskVectors(dispMatX,dispMatY,bwstackImg)
-Npoints = length(dispMatX);
+function [insideIdx] = maskVectors(X,Y,myMask)
+% function [insideIdx] = maskVectors(X,Y,myMask) finds indices in [X Y]
+% points which are inside myMask.
+% Example:
+% [xmat ymat]=meshgrid(1:10, 1:10);
+% myCenterPoint = [4,4];
+% myR=3;
+% myMask = sqrt((xmat-myCenterPoint(1)).^2+(ymat-myCenterPoint(2)).^2)<=myR;
+% xVec = xmat(:); yVec = ymat(:);
+% myIndexInside = maskVectors(xVec,yVec,myMask);
+% imshow(myMask)
+% hold on
+% plot(xVec(myIndexInside),yVec(myIndexInside),'ro')
+% Sangyoon Han
+Npoints = length(X);
 insideIdx = false(Npoints,1);
 
 for ii=1:Npoints
-    if round(dispMatY(ii)) <= size(bwstackImg,1) && ...
-            round(dispMatY(ii)) > 0 && ...
-            round(dispMatX(ii)) <= size(bwstackImg,2) && ...
-            round(dispMatX(ii)) > 0 && ...
-            bwstackImg(round(dispMatY(ii)),round(dispMatX(ii)))
+    if round(Y(ii)) <= size(myMask,1) && ...
+            round(Y(ii)) > 0 && ...
+            round(X(ii)) <= size(myMask,2) && ...
+            round(X(ii)) > 0 && ...
+            myMask(round(Y(ii)),round(X(ii)))
         insideIdx(ii) = true;
     end
 end
